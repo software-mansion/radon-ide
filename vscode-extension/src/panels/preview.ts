3ed302d9ec914170365acd20e786eac6e3d06d52
Krzysztof Magiera <krzys@swmansion.com>
2023-09-21 09:55:14 +0200

    Avoid hardcoded path to stream binary and look it up in Xcode build folder

--- vscode-extension/src/panels/preview.ts
+++ vscode-extension/src/panels/preview.ts
@@ -1,17 +1,40 @@
 import { ChildProcess } from "child_process";
 
+const path = require("path");
+const fs = require("fs");
+const fg = require("fast-glob");
 const child_process = require("child_process");
 const readline = require("readline");
+
+function findSimulatorStreamBinary() {
+  const derivedDataPath = path.join(process.env.HOME, "Library/Developer/Xcode/DerivedData");
+  const entries = fg.sync("SimulatorStreamServer*/**/Build/Products/Debug/SimulatorStreamServer", {
+    cwd: derivedDataPath,
+  });
+
+  let newestFile = null;
+  let newestMTime = 0;
+
+  entries.forEach((entry) => {
+    const absolutePath = path.join(derivedDataPath, entry);
+    const stats = fs.statSync(absolutePath);
+
+    if (stats.mtimeMs > newestMTime) {
+      newestMTime = stats.mtimeMs;
+      newestFile = absolutePath;
+    }
+  });
+
+  return newestFile;
+}
 
 export class Preview {
   private subprocess: ChildProcess;
 
   constructor(onReadyCallback: (previewURL: string) => void) {
-    console.log("Launching preview server");
-    this.subprocess = child_process.spawn(
-      "/Users/mdk/Library/Developer/Xcode/DerivedData/SimulatorStreamServer-fiwagsjzioxfbhfzhgdrjjsfsavj/Build/Products/Debug/SimulatorStreamServer",
-      ["android", "RNPreviews"]
-    );
+    console.log("Launching preview server", findSimulatorStreamBinary());
+
+    this.subprocess = child_process.spawn(findSimulatorStreamBinary(), ["android", "RNPreviews"]);
 
     const rl = readline.createInterface({
       input: this.subprocess.stdout,
