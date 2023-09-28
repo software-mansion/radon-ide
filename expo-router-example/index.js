e1282ff9f68b55aa9e85ac5f8a08d32a3e7e552c
Bartosz Sofinski <bartosz.sofinski@swmansion.com>
2023-09-28 14:28:38 +0200

    Disable shouldEmitW3CPointerEvents flag based on rn version

--- expo-router-example/index.js
+++ expo-router-example/index.js
@@ -1,2 +1,8 @@
 import 'expo-router/entry';
 import './preview';
+import RNVersion from 'react-native/Libraries/Core/ReactNativeVersion';
+import ReactNativeFeatureFlags from 'react-native/Libraries/ReactNative/ReactNativeFeatureFlags';
+
+if (RNVersion.version.major === 0 && RNVersion.version.minor <= 71) {
+  ReactNativeFeatureFlags.shouldEmitW3CPointerEvents = false;
+}
