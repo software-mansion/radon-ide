2e1bf4dc9d706b1c9589e409bb539af84df48a71
kacperkapusciak <kacper.kapusciak@swmansion.com>
2023-12-15 11:15:29 +0100

    Fix typo

--- vscode-extension/src/webview/index.jsx
+++ vscode-extension/src/webview/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import GlobalStateProvider from "./components/GlobalStateContext";
-import "./style/colors.css";
+import "./styles/colors.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
