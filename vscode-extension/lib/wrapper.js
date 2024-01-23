7a321993d178bb1972626043b79363376e926f7f
Mateusz Kowalski <wawel37@gmail.com>
2024-01-23 12:21:36 +0100

    Use react-devtools to update file -> navigation-route map more precisely (#42)
    This PR makes the associating fileName to application route more
    effective with the use of the react devtools `operations` event.
    
    We take the integer array retrieved from the event and parse it, so we
    are able to inspect the changed element ids and retrieve the source of
    the changed component
    ([here](https://github.com/facebook/react/blob/6c7b41da3de12be2d95c60181b3fe896f824f13a/packages/react-devtools-shared/src/devtools/store.js#L921)
    to read more about parsing the `operations` event payload).
    
    We only track the components that are outside of the `node_modules`
    directory.
    
    ---------
    
    Co-authored-by: Krzysztof Magiera <krzys@swmansion.com>

--- vscode-extension/lib/wrapper.js
+++ vscode-extension/lib/wrapper.js
@@ -6,10 +6,27 @@ export function registerNavigationPlugin(name, plugin) {
   navigationPlugins.push({ name, plugin });
 }
 
+/*
+ * We assume the Bridge protocol version is 2,
+ * as it's being used since React 18 (React-native 0.69). 
+ */
+const BRIDGE_PROTOCOL_VERSION = 2;
+
+const DevtoolsOperationType = {
+  TREE_OPERATION_ADD: 1,
+  TREE_OPERATION_REMOVE: 2,
+  TREE_OPERATION_REORDER_CHILDREN: 3,
+  TREE_OPERATION_UPDATE_TREE_BASE_DURATION: 4,
+  TREE_OPERATION_UPDATE_ERRORS_OR_WARNINGS: 5,
+  TREE_OPERATION_REMOVE_ROOT: 6,
+  TREE_OPERATION_SET_SUBTREE_MODE: 7,
+};
+
 let agent;
 let fileRouteMap = new Map();
-let activeEditorFile = undefined;
 let navigationHistory = new Map();
+
+let operationRequestId = 1;
 
 function isPreviewUrl(url) {
   return url.startsWith("preview://");
@@ -28,9 +45,6 @@ export function PreviewAppWrapper({ children, ...rest }) {
   const mainContainerRef = useRef();
 
   const handleNavigationChange = useCallback((navigationDescriptor) => {
-    if (activeEditorFile) {
-      fileRouteMap.set(activeEditorFile, navigationDescriptor);
-    }
     navigationHistory.set(navigationDescriptor.id, navigationDescriptor);
     agent &&
       agent._bridge.send("rnp_navigationChanged", {
@@ -47,14 +61,12 @@ export function PreviewAppWrapper({ children, ...rest }) {
 
   const handleActiveFileChange = useCallback(
     (filename, follow) => {
-      activeEditorFile = filename;
       if (follow) {
         const route = fileRouteMap[filename];
         if (route) {
           return route;
         }
       }
-      fileRouteMap[activeEditorFile] = getCurrentNavigationDescriptor();
     },
     [getCurrentNavigationDescriptor]
   );
@@ -94,6 +106,7 @@ export function PreviewAppWrapper({ children, ...rest }) {
         const url = payload.url;
         Linking.openURL(url);
       });
+
       agent._bridge.addListener("rnp_openNavigation", (payload) => {
         if (isPreviewUrl(payload.id)) {
           openPreview(payload.id);
@@ -145,7 +158,32 @@ export function PreviewAppWrapper({ children, ...rest }) {
     } else {
       hook.on("react-devtools", _attachToDevtools);
     }
+    attachHookListeners(hook);
+    
   }, []);
+
+  const attachHookListeners = (hook) => {
+    hook.sub('operations', (operations) => {
+      const {changedElements, rendererId} = getAllChangedElementIdsFromOperations(operations);
+      const rendererInterfaces = hook.rendererInterfaces.get(rendererId);
+      const currentNavigationDescriptior = getCurrentNavigationDescriptor();
+      changedElements.forEach((elementId) => {
+        try
+        {
+          const element = rendererInterfaces.inspectElement(operationRequestId, elementId, null, true);
+          if (element){
+            const fileName = element?.value?.source?.fileName;
+            if (fileName) {
+              fileRouteMap.set(fileName, currentNavigationDescriptior);
+            }
+          }
+         } catch (e) {
+          console.error("Error occured while trying to inspect element with renderer id:", rendererId, "requestId:", operationRequestId, "elementId:", elementId);
+         }
+      });
+      operationRequestId++;
+    });
+  };
 
   return (
     <View
@@ -177,4 +215,84 @@ export function PreviewAppWrapper({ children, ...rest }) {
       {children}
     </View>
   );
+}
+
+/*
+  * Before chaning anything in this function, please refer to:
+  * https://github.com/facebook/react/blob/6c7b41da3de12be2d95c60181b3fe896f824f13a/packages/react-devtools-shared/src/devtools/store.js#L921
+*/
+function getAllChangedElementIdsFromOperations(operations) {
+  const rendererID = operations[0];
+
+  const changedElementIds = [];
+
+  let i = 2;
+
+  const blockSize = operations[i];
+  i++;
+
+  const blockIndexEnd = i + blockSize;
+
+  while (i < blockIndexEnd) {
+    const nextLength = operations[i];
+    i += nextLength + 1;
+  }
+
+  while (i < operations.length) {
+    const operation = operations[i];
+    switch (operation) {
+      case DevtoolsOperationType.TREE_OPERATION_ADD: {
+        const id = operations[i + 1];
+        const type = operations[i + 2];
+
+        changedElementIds.push(id);
+
+        i += 3;
+        if (type === 11) {
+          i += 2;
+
+          if (BRIDGE_PROTOCOL_VERSION === null || BRIDGE_PROTOCOL_VERSION >= 2) {
+            i += 2;
+          }
+        } else {
+          i += 4;
+        }
+        break;
+      }
+      // We don't need to track the remove operation.
+      case DevtoolsOperationType.TREE_OPERATION_REMOVE: {
+        const removeLength = operations[i + 1];
+        i += 2 + removeLength;
+        break;
+      }
+      // We don't need to track the remove operation.
+      case DevtoolsOperationType.TREE_OPERATION_REMOVE_ROOT: {
+        i += 1;
+      }
+      case DevtoolsOperationType.TREE_OPERATION_REORDER_CHILDREN: {
+        const id = operations[i + 1];
+        changedElementIds.push(id);
+        i += 3 + operations[i + 2];
+        break;
+      }
+      case DevtoolsOperationType.TREE_OPERATION_SET_SUBTREE_MODE: {
+        const id = operations[i + 1];
+        changedElementIds.push(id);
+        i += 3;
+        break;
+      }
+      case DevtoolsOperationType.TREE_OPERATION_UPDATE_TREE_BASE_DURATION:
+        i += 3;
+        break;
+      case DevtoolsOperationType.TREE_OPERATION_UPDATE_ERRORS_OR_WARNINGS:
+        const id = operations[i + 1];
+        changedElementIds.push(id);
+        i += 4;
+        break;
+      default:
+        throw new Error("Unsupported bridge operation.");
+    }
+  }
+
+  return {changedElements: changedElementIds, rendererId: rendererID};
 }
