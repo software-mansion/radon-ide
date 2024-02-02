e477925a74fbddfa9c0d26266bd1c3e16725a6e2
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2024-02-02 10:20:52 +0100

    Unify webview code formatting (#56)

--- vscode-extension/src/webview/components/icons/PlayIcon.jsx
+++
@@ -1,7 +0,0 @@
-import "./Icon.css";
-
-function PlayIcon() {
-  return <span className="codicon codicon-debug-continue-small green-icon" />;
-}
-
-export default PlayIcon;
--- vscode-extension/src/webview/utilities/common.js
+++ vscode-extension/src/webview/utilities/common.js
@@ -1,24 +1,24 @@
 export function throttle(func, limit) {
-    let timeout;
-    let recentArgs;
+  let timeout;
+  let recentArgs;
 
-    return function (...args) {
-        const force = args[args.length - 1] === true; // Check if the last argument is true (force flag)
+  return function (...args) {
+    const force = args[args.length - 1] === true; // Check if the last argument is true (force flag)
 
-        if (force) {
+    if (force) {
+      timeout = null;
+      clearTimeout(timeout);
+      func(...args);
+      return;
+    }
+
+    if (!timeout) {
+      timeout = setTimeout(() => {
         timeout = null;
-        clearTimeout(timeout);
-        func(...args);
-        return;
-        }
-
-        if (!timeout) {
-        timeout = setTimeout(() => {
-            timeout = null;
-            func(...recentArgs);
-            recentArgs = null;
-        }, limit);
-        }
-        recentArgs = args;
-    };
+        func(...recentArgs);
+        recentArgs = null;
+      }, limit);
+    }
+    recentArgs = args;
+  };
 }
