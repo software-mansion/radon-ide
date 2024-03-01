e35f7d8853b5b23a8ed9a3f68757a113fb21174b
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2024-03-01 13:28:06 +0100

    Add `d.ts` to `react-native-ide` placeholder package (#113)
    When using `react-native-ide` preview feature in real life project I've
    stumbled upon typescript being not happy with react-native-ide package
    not having types included. I've fix this problem by adding a `d.ts` file
    with type annotations.
    
    ### Before
    
    Typescript not happy 😢 
    
    
    ![image](https://github.com/software-mansion-labs/react-native-sztudio/assets/39658211/8b4cce4d-3ced-4643-bb00-a2472970bf86)
    
    
    ### After 
    
    Typescript happy 😄 
    
    
    ![image](https://github.com/software-mansion-labs/react-native-sztudio/assets/39658211/24218c44-d358-4ae2-9f42-52ec395b5924)
    
    User can't make a mistake by passing a component in a wrong way
    
    
    ![image](https://github.com/software-mansion-labs/react-native-sztudio/assets/39658211/47bff720-1f4c-4aaa-bda1-21eb5f4d3285)
    
    
    ![image](https://github.com/software-mansion-labs/react-native-sztudio/assets/39658211/7500bd46-e069-408e-90a8-21ea76c51d6c)

---
+++ react-native-ide/index.d.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+// This function is a placeholder for the preview function.
+// It is provided as empty method so that packages that use
+// preview functionality can build and execute outside of the
+// context of VSCode's React Native extension.
+// When run inside VSCode, the extension will replace the
+// implementation of this method in order to support the preview
+// feature properly.
+export declare function preview(componentInstance: ReturnType<typeof React.createElement>): void;
