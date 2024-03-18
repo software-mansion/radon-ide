16f5fbee0d737a4badbc8cac6300f3a2530b54c5
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2024-03-18 15:55:29 +0100

    Add OG image to landing (#159)

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -37,7 +37,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/og-image.png",
     navbar: {
       logo: {
         alt: "IDE logo",
