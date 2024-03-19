171a5c431918521c33c169f739f621d832f8f472
Krzysztof Magiera <krzys@swmansion.com>
2024-03-19 12:12:13 +0100

    Update build script to pull in changes from submodule when needed

--- packages/vscode-extension/scripts/build-sim-server.sh
+++ packages/vscode-extension/scripts/build-sim-server.sh
@@ -9,6 +9,12 @@ configuration=${2:-Debug}
 # Get the directory where the script is located
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+# Locate sim-server package
+sim_server_package_dir="${script_dir}/../../simulator-server"
+
+# Make sure, the submodule is in the up-to-date state
+git -C $script_dir submodule update --init -- ${sim_server_package_dir}
+
 target_dir="${script_dir}/../${output_dir}"
 target_location="${target_dir}/sim-server"
 
@@ -16,9 +22,8 @@ target_location="${target_dir}/sim-server"
 mkdir -p "$target_dir"
 
 # execute the build from the simulator-server package
-build_script_location="$script_dir/../../simulator-server/build.sh"
-# the build product location is printed by the build script as the very last line, we capture this line to a variable to parse it:
-product_path=$("$build_script_location" "$configuration" | tail -n 1)
+# the build product location is printed by the build script as the very last line
+product_path=$("$sim_server_package_dir/build.sh" "$configuration" | tail -n 1)
 
 # Check if the build was successful
 if [[ $? -ne 0 ]]; then
