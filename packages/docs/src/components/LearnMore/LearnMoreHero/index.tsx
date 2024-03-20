893810b7603a6efb134adc2c992949a1159b7227
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2024-03-20 11:16:37 +0100

    Landing improvements (#11)

--- packages/docs/src/components/LearnMore/LearnMoreHero/index.tsx
+++ packages/docs/src/components/LearnMore/LearnMoreHero/index.tsx
@@ -1,11 +1,8 @@
-import React from 'react'
-import styles from './styles.module.css'
-import HomepageButton from '@site/src/components/HomepageButton'
-import {
-  ButtonStyling,
-  BorderStyling,
-} from '@site/src/components/HomepageButton'
-import Elipse from '@site/src/components/Elipse'
+import React from "react";
+import styles from "./styles.module.css";
+import HomepageButton from "@site/src/components/HomepageButton";
+import { ButtonStyling, BorderStyling } from "@site/src/components/HomepageButton";
+import Elipse from "@site/src/components/Elipse";
 
 const LearnMoreHero = () => {
   return (
@@ -15,19 +12,17 @@ const LearnMoreHero = () => {
         <Elipse size={300} className={styles.elipse} />
       </div>
       <div className={styles.learnMoreSectionHero}>
-        <div>
-          <p>Learn more about Software Mansion</p>
-        </div>
+        <p>Learn more about Software Mansion</p>
         <HomepageButton
-          target='_blank'
-          href='https://swmansion.com/'
+          target="_blank"
+          href="https://swmansion.com/"
           backgroundStyling={ButtonStyling.TO_NAVY}
           borderStyling={BorderStyling.NAVY}
-          title='See SWM page'
+          title="See our website"
         />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default LearnMoreHero
+export default LearnMoreHero;
