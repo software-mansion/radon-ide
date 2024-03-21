a46709d09efb85100d87d862d4e63370589aa062
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2024-03-21 12:46:57 +0100

    Make call-to-action a bit nicer on mobile (#16)

--- packages/docs/src/components/HomepageButton/index.tsx
+++ packages/docs/src/components/HomepageButton/index.tsx
@@ -1,31 +1,33 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React from "react";
+import styles from "./styles.module.css";
 
-import ArrowRight from '@site/static/img/arrow-right-hero.svg'
-import clsx from 'clsx'
+import ArrowRight from "@site/static/img/arrow-right-hero.svg";
+import clsx from "clsx";
 
 export const ButtonStyling = {
   TO_NAVY: styles.buttonTransparentStyling,
   TO_WHITE: styles.buttonWhiteStyling,
   TO_TRANSPARENT: styles.buttonGreenStyling,
-}
+};
 
 export const BorderStyling = {
   PURPLE: styles.buttonPurpleBorderStyling,
   NAVY: styles.buttonNavyBorderStyling,
-}
+};
 
 const HomepageButton: React.FC<{
-  title: string
-  href: string
-  target?: '_blank' | '_parent' | '_self' | '_top'
-  backgroundStyling?: string
-  borderStyling?: string
-  enlarged?: boolean
+  title: string;
+  subtitle?: string;
+  href: string;
+  target?: "_blank" | "_parent" | "_self" | "_top";
+  backgroundStyling?: string;
+  borderStyling?: string;
+  enlarged?: boolean;
 }> = ({
   title,
+  subtitle,
   href,
-  target = '_self',
+  target = "_self",
   backgroundStyling = ButtonStyling.TO_TRANSPARENT,
   borderStyling = BorderStyling.PURPLE,
 }) => {
@@ -35,17 +37,20 @@ const HomepageButton: React.FC<{
         className={clsx(
           styles.homepageButton,
           backgroundStyling,
-          borderStyling
-        )}
-      >
-        {title}
+          borderStyling,
+          subtitle && styles.homepageButtonWithSubtitle
+        )}>
+        <div className={clsx(subtitle && styles.homepageButtonSubtitleWrapper)}>
+          {title}
+          {subtitle && <span>{subtitle}</span>}
+        </div>
 
         <div className={styles.arrow}>
           <ArrowRight />
         </div>
       </div>
     </a>
-  )
-}
+  );
+};
 
-export default HomepageButton
+export default HomepageButton;
