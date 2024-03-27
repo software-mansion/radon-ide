59b8ec79d9a14e24003daf0cc455a32c8056551c
Fiiranek <jellybean@onet.eu>
2024-03-27 12:29:26 +0100

    Add functions for downloading and installing Expo Go

---
+++ packages/vscode-extension/src/builders/expoGo.ts
@@ -0,0 +1,45 @@
+import fs from "fs";
+import { createGunzip } from "zlib";
+import path from "path";
+import tar from "tar";
+import { downdloadFile, getAppCachesDir } from "../utilities/common";
+
+const IOS_EXPO_GO_DOWNLOAD_URL = "https://dpq5q02fu5f55.cloudfront.net/Exponent-2.30.10.tar.gz";
+const ANDROID_EXPO_GO_DOWNLOAD_URL = "https://d1ahtucjixef4r.cloudfront.net/Exponent-2.30.11.apk";
+
+export function shouldUseExpoGo(): boolean {
+  // TODO: Check for better solution to determine whether Expo Go should be used
+  const androidExists = fs.existsSync(path.join(getAppCachesDir(), "android"));
+  const iosExists = fs.existsSync(path.join(getAppCachesDir(), "ios"));
+  return !(androidExists && iosExists);
+}
+
+export function getIOSExpoGoAppPath(): string {
+  const iOSAppName = "Exponent-2.30.10.app";
+  return path.join(getAppCachesDir(), iOSAppName);
+}
+
+export async function downloadIOSExpoGo(): Promise<string> {
+  const archivePath = path.join(getAppCachesDir(), "Exponent-2.30.10.tar.gz");
+  const appPath = getIOSExpoGoAppPath();
+  if (!fs.existsSync(appPath)) {
+    fs.mkdirSync(appPath, { recursive: true });
+  }
+  await downdloadFile(IOS_EXPO_GO_DOWNLOAD_URL, archivePath);
+  const readStream = fs.createReadStream(archivePath);
+  const extractStream = tar.x({ cwd: appPath });
+  readStream.pipe(createGunzip()).pipe(extractStream);
+  fs.unlinkSync(archivePath);
+  return appPath;
+}
+
+export function getAndroidExpoGoApkPath(): string {
+  const androidApkName = "Exponent-2.30.11.apk";
+  return path.join(getAppCachesDir(), androidApkName);
+}
+
+export async function downloadAndroidExpoGo(): Promise<string> {
+  const apkPath = getAndroidExpoGoApkPath();
+  await downdloadFile(ANDROID_EXPO_GO_DOWNLOAD_URL, apkPath);
+  return apkPath;
+}
