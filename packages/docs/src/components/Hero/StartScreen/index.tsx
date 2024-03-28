6802e3be77ec038fbe43825195321e3411bb4898
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2024-03-28 14:27:03 +0100

    Fix landing for 4k monitors (#50)
    ## Before
    
    
    ![image](https://github.com/software-mansion/react-native-ide/assets/39658211/9bf4ba8a-2708-474c-87a8-ee647f3ff1ac)
    
    
    ## After 
    
    
    ![image](https://github.com/software-mansion/react-native-ide/assets/39658211/5cc3fcdf-0320-41b2-bd19-7c95e7c05b16)

--- packages/docs/src/components/Hero/StartScreen/index.tsx
+++ packages/docs/src/components/Hero/StartScreen/index.tsx
@@ -3,53 +3,48 @@ import styles from "./styles.module.css";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 import HomepageButton from "@site/src/components/HomepageButton";
 import InfoIcon from "@site/static/img/info-circle.svg";
-import { useWindowSize } from "@docusaurus/theme-common";
 
 const StartScreen = () => {
-  const windowSize = useWindowSize();
-
   return (
-    <>
-      <section className={styles.hero}>
-        <div className={styles.heroImageContainer}>
-          <div className={styles.heroImageWrapper}>
-            <img className={styles.heroImage} src={useBaseUrl("/img/hero.png")} draggable={false} />
-          </div>
+    <section className={styles.hero}>
+      <div className={styles.heroImageContainer}>
+        <div className={styles.heroImageWrapper}>
+          <img className={styles.heroImage} src={useBaseUrl("/img/hero.png")} draggable={false} />
         </div>
-        <div className={styles.heading}>
-          <div>
-            <h1 className={styles.headingLabel}>
-              <span>React Native</span>
-              <span>IDE</span>
-            </h1>
-            <div className={styles.poweredBy}>
-              <p>by</p>
-              <a href="https://swmansion.com" target="_blank">
-                <img
-                  src={useBaseUrl("/img/swm-logo.svg")}
-                  alt="Software Mansion"
-                  className={styles.swmLogo}
-                />
-              </a>
-            </div>
-            <h2 className={styles.subheadingLabel}>
-              A better developer experience for React Native developers.
-            </h2>
+      </div>
+      <div className={styles.heading}>
+        <div>
+          <h1 className={styles.headingLabel}>
+            <span>React Native</span>
+            <span>IDE</span>
+          </h1>
+          <div className={styles.poweredBy}>
+            <p>by</p>
+            <a href="https://swmansion.com" target="_blank">
+              <img
+                src={useBaseUrl("/img/swm-logo.svg")}
+                alt="Software Mansion"
+                className={styles.swmLogo}
+              />
+            </a>
           </div>
-          <div className={styles.buttonContainer}>
-            <HomepageButton
-              target="_blank"
-              href="https://forms.gle/URs4bsbkcvNJagzeA"
-              title="Request Beta Access"
-            />
-          </div>
-          <div className={styles.headingDisclaimer}>
-            <InfoIcon className={styles.headingDisclaimerIcon} />
-            We are currently testing the IDE with a limited number of users.
-          </div>
+          <h2 className={styles.subheadingLabel}>
+            A better developer experience for React Native developers.
+          </h2>
         </div>
-      </section>
-    </>
+        <div className={styles.buttonContainer}>
+          <HomepageButton
+            target="_blank"
+            href="https://forms.gle/URs4bsbkcvNJagzeA"
+            title="Request Beta Access"
+          />
+        </div>
+        <div className={styles.headingDisclaimer}>
+          <InfoIcon className={styles.headingDisclaimerIcon} />
+          We are currently testing the IDE with a limited number of users.
+        </div>
+      </div>
+    </section>
   );
 };
 
