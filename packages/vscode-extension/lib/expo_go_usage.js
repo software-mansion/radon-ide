3ee1bf9205f5be2c86dae79c1a28e2a5e253fb91
Krzysztof Magiera <krzys@swmansion.com>
2024-04-02 22:30:35 +0200

    Add better logic for recognizing expo-go projects

--- packages/vscode-extension/lib/expo_go_usage.js
+++
@@ -1,9 +0,0 @@
-const { requireFromAppDir, appRoot } = require("./metro_helpers");
-const { resolveOptionsAsync } = requireFromAppDir("@expo/cli/build/src/start/resolveOptions");
-
-async function main() {
-  const { devClient } = await resolveOptionsAsync(appRoot, {});
-  console.log(!devClient);
-}
-
-main();
--- packages/vscode-extension/src/builders/expoGo.ts
+++ packages/vscode-extension/src/builders/expoGo.ts
@@ -1,19 +1,52 @@
 import path from "path";
 import { extensionContext, getAppRootFolder } from "../utilities/extensionContext";
 import http from "http";
+import fs from "fs";
 import { exec } from "../utilities/subprocess";
 import { Platform } from "../common/DeviceManager";
 import { CancelToken } from "./BuildManager";
 
 type ExpoDeeplinkChoice = "expo-go" | "expo-dev-client";
 
+function fileExists(filePath: string, ...additionalPaths: string[]) {
+  return fs.existsSync(path.join(filePath, ...additionalPaths));
+}
+
 export async function isExpoGoProject(): Promise<boolean> {
-  const libPath = path.join(extensionContext.extensionPath, "lib");
-  const { stdout } = await exec(`node`, [path.join(libPath, "expo_go_usage.js")], {
-    cwd: getAppRootFolder(),
-  });
-  const useExpoGo = stdout === "true";
-  return useExpoGo;
+  // There is no straightforward way to tell apart different react native project
+  // setups. i.e. expo-go, expo-dev-client, bare react native, etc.
+  // Here, we are using a heuristic to determine if the project is expo-go based
+  // on the following factors:
+  // 1) The project has app.json or app.config.js
+  // 2) The project doesn't have an android or ios folder
+  // 3) The expo_go_project_tester.js script runs successfully – the script uses expo-cli
+  // internals to resolve project config and tells expo-go and dev-client apart.
+  const appRoot = getAppRootFolder();
+
+  if (!fileExists(appRoot, "app.json") && !fileExists(appRoot, "app.config.js")) {
+    // app.json or app.config.js is required for expo-go projects
+    return false;
+  }
+
+  if (fileExists(appRoot, "android") || fileExists(appRoot, "ios")) {
+    // expo-go projects don't have android or ios folders
+    return false;
+  }
+
+  const expoGoProjectTesterScript = path.join(
+    extensionContext.extensionPath,
+    "lib",
+    "expo_go_project_tester.js"
+  );
+  try {
+    const result = await exec(`node`, [expoGoProjectTesterScript], {
+      cwd: getAppRootFolder(),
+      allowNonZeroExit: true,
+    });
+    return result.exitCode === 0;
+  } catch (e) {
+    return false;
+  }
 }
 
 export function fetchExpoLaunchDeeplink(
