0a64528df97d3a36005ab8be4323463c6514a287
Patrycja Kalińska <cysiak0412@gmail.com>
2024-04-11 14:08:57 +0200

    Add mobile sidebar

--- packages/docs/src/theme/Navbar/Layout/index.js
+++ packages/docs/src/theme/Navbar/Layout/index.js
@@ -1,24 +1,23 @@
-import React from 'react'
-import clsx from 'clsx'
-import { useThemeConfig } from '@docusaurus/theme-common'
-import {
-  useHideableNavbar,
-  useNavbarMobileSidebar,
-} from '@docusaurus/theme-common/internal'
-import { translate } from '@docusaurus/Translate'
-import NavbarMobileSidebar from '@theme/Navbar/MobileSidebar'
-import HeroElipse from '@site/src/components/Hero/HeroElipse'
-import styles from './styles.module.css'
-import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment'
+import React from "react";
+import clsx from "clsx";
+import { useThemeConfig } from "@docusaurus/theme-common";
+import { useHideableNavbar, useNavbarMobileSidebar } from "@docusaurus/theme-common/internal";
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
+import { translate } from "@docusaurus/Translate";
+import NavbarMobileSidebar from "@theme/Navbar/MobileSidebar";
+
+import HeroElipse from "@site/src/components/Hero/HeroElipse";
+import usePageType from "@site/src/hooks/usePageType";
+import styles from "./styles.module.css";
 
 function NavbarBackdrop(props) {
   return (
     <div
-      role='presentation'
+      role="presentation"
       {...props}
-      className={clsx('navbar-sidebar__backdrop', props.className)}
+      className={clsx("navbar-sidebar__backdrop", props.className)}
     />
-  )
+  );
 }
 
 const LandingBackground = () => {
@@ -26,44 +25,44 @@ const LandingBackground = () => {
     <div className={styles.heroBackground}>
       {ExecutionEnvironment.canUseViewport && <HeroElipse />}
     </div>
-  )
-}
+  );
+};
 
 export default function NavbarLayout({ children }) {
   const {
     navbar: { hideOnScroll, style },
-  } = useThemeConfig()
-  const mobileSidebar = useNavbarMobileSidebar()
-  const { navbarRef, isNavbarVisible } = useHideableNavbar(hideOnScroll)
+  } = useThemeConfig();
+  const mobileSidebar = useNavbarMobileSidebar();
+  const { navbarRef, isNavbarVisible } = useHideableNavbar(hideOnScroll);
+  const { isLanding } = usePageType();
+
   return (
     <div>
+      {/* version without the elipse */}
+      {/* {isLanding && <LandingBackground />} */}
       <LandingBackground />
       <nav
         ref={navbarRef}
         aria-label={translate({
-          id: 'theme.NavBar.navAriaLabel',
-          message: 'Main',
-          description: 'The ARIA label for the main navigation',
+          id: "theme.NavBar.navAriaLabel",
+          message: "Main",
+          description: "The ARIA label for the main navigation",
         })}
         className={clsx(
-          'navbar',
-          'navbar--fixed-top',
+          "navbar",
+          "navbar--fixed-top",
           styles.navbarLanding,
-          hideOnScroll && [
-            styles.navbarHideable,
-            !isNavbarVisible && styles.navbarHidden,
-          ],
+          hideOnScroll && [styles.navbarHideable, !isNavbarVisible && styles.navbarHidden],
           {
-            'navbar--dark': style === 'dark',
-            'navbar--primary': style === 'primary',
-            'navbar-sidebar--show': mobileSidebar.shown,
+            "navbar--dark": style === "dark",
+            "navbar--primary": style === "primary",
+            "navbar-sidebar--show": mobileSidebar.shown,
           }
-        )}
-      >
+        )}>
         {children}
         <NavbarBackdrop onClick={mobileSidebar.toggle} />
         <NavbarMobileSidebar />
       </nav>
     </div>
-  )
+  );
 }
--- packages/docs/src/theme/Navbar/MobileSidebar/Header/index.js
+++ packages/docs/src/theme/Navbar/MobileSidebar/Header/index.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import {useNavbarMobileSidebar} from '@docusaurus/theme-common/internal';
-import {translate} from '@docusaurus/Translate';
-import NavbarColorModeToggle from '@theme/Navbar/ColorModeToggle';
-import IconClose from '@theme/Icon/Close';
-import NavbarLogo from '@theme/Navbar/Logo';
+import React from "react";
+import { useNavbarMobileSidebar } from "@docusaurus/theme-common/internal";
+import { translate } from "@docusaurus/Translate";
+import IconClose from "@theme/Icon/Close";
+import NavbarLogo from "@theme/Navbar/Logo";
 function CloseButton() {
   const mobileSidebar = useNavbarMobileSidebar();
   return (
     <button
       type="button"
       aria-label={translate({
-        id: 'theme.docs.sidebar.closeSidebarButtonAriaLabel',
-        message: 'Close navigation bar',
-        description: 'The ARIA label for close button of mobile sidebar',
+        id: "theme.docs.sidebar.closeSidebarButtonAriaLabel",
+        message: "Close navigation bar",
+        description: "The ARIA label for close button of mobile sidebar",
       })}
       className="clean-btn navbar-sidebar__close"
       onClick={() => mobileSidebar.toggle()}>
@@ -24,7 +23,6 @@ export default function NavbarMobileSidebarHeader() {
   return (
     <div className="navbar-sidebar__brand">
       <NavbarLogo />
-      <NavbarColorModeToggle className="margin-right--md" />
       <CloseButton />
     </div>
   );
