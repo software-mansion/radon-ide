295181d3ba157d8d29f196763e1b6c6ab5098cf6
alduzy <alduzy@gmail.com>
2024-04-22 12:39:29 +0200

    added auto num script to be used in troubleshooting

---
+++ packages/docs/src/remark/auto-num.js
@@ -0,0 +1,20 @@
+const visit = require("unist-util-visit");
+
+const plugin = (options) => {
+  const transformer = async (ast) => {
+    let number = 1;
+    visit(ast, "heading", (node) => {
+      if (node.children.length > 0) {
+        node.children.forEach((child) => {
+          if (child.value.includes("-auto-num-")) {
+            child.value = child.value.replace("-auto-num-", `${number}.`);
+            number++;
+          }
+        });
+      }
+    });
+  };
+  return transformer;
+};
+
+module.exports = plugin;
