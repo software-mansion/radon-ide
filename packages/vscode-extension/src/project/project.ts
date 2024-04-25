4b195118e4ca8b42e4472e1b1783dd344441fa00
Jakub Gonet <jakub.gonet@swmansion.com>
2024-04-25 12:13:21 +0200

    Check for platform when comparing fingerprints

--- packages/vscode-extension/src/project/project.ts
+++ packages/vscode-extension/src/project/project.ts
@@ -436,7 +436,7 @@ export class Project implements Disposable, MetroDelegate, ProjectInterface {
 
   private checkIfNativeChanged = throttle(async () => {
     if (!this.nativeFilesChangedSinceLastBuild) {
-      if (await didFingerprintChange()) {
+      if (await didFingerprintChange(this.projectState.selectedDevice!.platform)) {
         this.nativeFilesChangedSinceLastBuild = true;
         this.eventEmitter.emit("needsNativeRebuild");
       }
