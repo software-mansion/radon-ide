480a7280ac5d37c75c7132b86c50680b6874bff2
Krzysztof Magiera <krzys@swmansion.com>
2024-04-30 00:02:57 +0200

    Implement jsx transform workaround that doesn't affect order of plugins

--- packages/vscode-extension/lib/babel_transformer.js
+++ packages/vscode-extension/lib/babel_transformer.js
@@ -1,25 +1,6 @@
 const ORIGINAL_TRANSFORMER_PATH = process.env.REACT_NATIVE_IDE_ORIG_BABEL_TRANSFORMER_PATH;
 
-// React Native in some configurations may pull several different version of JSX transoform plugins:
-// plugin-transform-react-jsx-self, plugin-transform-react-jsx-source, plugin-transform-react-jsx and
-// plugin-transform-react-jsx-development. We want to use plugin-transform-react-jsx-development, however, this package
-// may not always be installed as it's been only added recently to RN presents. But since it only imports
-// plugin-transform-react-jsx/lib/development we can rely on loading that instead.
-// Unfortunately, when one of the other plugins is installed while we add plugin-transform-react-jsx/lib/development,
-// we start getting build errors.
-// Below, we implemented a workaround that disables all other plugins from being loaded using require. It returns an empty
-// babel plugin and allows for the only transformer that we want to be included.
-const { overrideModuleFromAppDir, requireFromAppDir } = require("./metro_helpers");
-
-function disablePlugin(moduleNameToOverride) {
-  overrideModuleFromAppDir(moduleNameToOverride, function () {
-    return { visitor: {} };
-  });
-}
-disablePlugin("@babel/plugin-transform-react-jsx-self");
-disablePlugin("@babel/plugin-transform-react-jsx-source");
-disablePlugin("@babel/plugin-transform-react-jsx");
-disablePlugin("@babel/plugin-transform-react-jsx-development");
+const { requireFromAppDir } = require("./metro_helpers");
 
 function transformWrapper({ filename, src, plugins, ...rest }) {
   const { transform } = require(ORIGINAL_TRANSFORMER_PATH);
@@ -29,19 +10,40 @@ function transformWrapper({ filename, src, plugins, ...rest }) {
     // expo-router v2 and v3 integration
     const { version } = requireFromAppDir("expo-router/package.json");
     if (version.startsWith("2.")) {
-      src = `${src};require("__RNIDE_lib__/expo_router_v2_plugin.js");`;
+      // src = `${src};require("__RNIDE_lib__/expo_router_v2_plugin.js");`;
     } else if (version.startsWith("3.")) {
-      src = `${src};require("__RNIDE_lib__/expo_router_plugin.js");`;
+      // src = `${src};require("__RNIDE_lib__/expo_router_plugin.js");`;
     }
   } else if (filename.endsWith("node_modules/react-native-ide/index.js")) {
     src = `${src};preview = require("__RNIDE_lib__/preview.js").preview;`;
   }
 
   const newPlugins = [
-    require(require.resolve("@babel/plugin-transform-react-jsx/lib/development", {
-      paths: [ORIGINAL_TRANSFORMER_PATH],
-    })),
-    ...(plugins || []),
+    {
+      name: "disable-non-dev-jsx-transformer-exit",
+      pre(state) {
+        // In some configurations, React Native may pull several different version of JSX transoform plugins:
+        // plugin-transform-react-jsx-self, plugin-transform-react-jsx-source, plugin-transform-react-jsx and
+        // plugin-transform-react-jsx-development. For line and columnt numbers to be added to components, we
+        // need the development version of the plugin to produce the JSXElement node output. Apparently,
+        // in the default configuration, non-dev version is typically added and runs before the dev version
+        // resulting in the JSXElement node being processed by the non-dev version and the latter, dev version
+        // not seeing the already transformed node. Below, we implement a workaround that deletes JSXElement
+        // visitor from jsx transform plugin, as long as development version of the plugin is also present.
+        // This way we let the development version handle transforming JSXElement nodes.
+        const hasJsxDevTransformer = state.opts.plugins.some(
+          (p) => p.key === "transform-react-jsx/development"
+        );
+        if (hasJsxDevTransformer) {
+          state.opts.plugins.forEach((plugin) => {
+            if (plugin.key === "transform-react-jsx") {
+              delete plugin.visitor.JSXElement;
+            }
+          });
+        }
+      },
+    },
+    ...plugins,
   ];
 
   return transform({ filename, src, plugins: newPlugins, ...rest });
