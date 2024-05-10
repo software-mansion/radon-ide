2ddb84b7eeb5b50a3dcaeb215c59696a04d00966
Krzysztof Magiera <krzys@swmansion.com>
2024-05-10 23:47:51 +0200

    Gracefully handle attempts of launching the ide launch configuration

--- packages/vscode-extension/src/extension.ts
+++ packages/vscode-extension/src/extension.ts
@@ -9,7 +9,9 @@ import {
   ExtensionMode,
   ConfigurationChangeEvent,
   DebugConfigurationProviderTriggerKind,
+  DebugAdapterExecutable,
 } from "vscode";
+import vscode from "vscode";
 import { TabPanel } from "./panels/Tabpanel";
 import { PreviewCodeLensProvider } from "./providers/PreviewCodeLensProvider";
 import { DebugConfigProvider } from "./providers/DebugConfigProvider";
@@ -27,7 +29,6 @@ import fs from "fs";
 import { SidePanelViewProvider } from "./panels/SidepanelViewProvider";
 import { PanelLocation } from "./common/WorkspaceConfig";
 import { getLaunchConfiguration } from "./utilities/launchConfiguration";
-import { getTelemetryReporter } from "./utilities/telemetry";
 
 const BIN_MODIFICATION_DATE_KEY = "bin_modification_date";
 const OPEN_PANEL_ON_ACTIVATION = "open_panel_on_activation";
@@ -109,6 +110,17 @@ export async function activate(context: ExtensionContext) {
     commands.registerCommand("RNIDE.diagnose", diagnoseWorkspaceStructure)
   );
 
+  // Debug adapter used by custom launch configuration, we register it in case someone tries to run the IDE configuration
+  // The current workflow is that people shouldn't run it, but since it is listed under launch options it might happen
+  // When it does happen, we open the IDE panel and restart the app.
+  context.subscriptions.push(
+    debug.registerDebugAdapterDescriptorFactory(
+      "react-native-ide",
+      new LaunchConfigDebugAdapterDescriptorFactory()
+    )
+  );
+
+  // Debug adapter used for debugging React Native apps
   context.subscriptions.push(
     debug.registerDebugConfigurationProvider(
       "com.swmansion.react-native-ide",
@@ -143,6 +155,17 @@ export async function activate(context: ExtensionContext) {
   );
 
   await configureAppRootFolder();
+}
+
+class LaunchConfigDebugAdapterDescriptorFactory implements vscode.DebugAdapterDescriptorFactory {
+  createDebugAdapterDescriptor(
+    session: vscode.DebugSession
+  ): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
+    commands.executeCommand("RNIDE.openPanel");
+    // we can't return undefined or throw here because then VSCode displays an ugly error dialog
+    // so we return a dummy adapter that calls echo command and exists immediately
+    return new DebugAdapterExecutable("echo", ["noop"]);
+  }
 }
 
 async function findSingleFileInWorkspace(fileGlobPattern: string, excludePattern: string | null) {
