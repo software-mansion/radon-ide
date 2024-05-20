184f78d409225b41b08f9ee6cb669d942bf1ab02
Patrycja Kalińska <59940332+patrycjakalinska@users.noreply.github.com>
2024-05-20 09:45:31 +0200

    Change navbar to a reusable component (#268)
    In order to use navbar and footer theme components from
    `@swmansion/t-rex-ui`, we need to rewrite current navbar to be more
    simplified.
    
    To achieve that we moved landing background `<HeroElipse />` from Navbar
    to separate component.
    
    <img width="1558" alt="image"
    src="https://github.com/software-mansion/react-native-ide/assets/59940332/b5c16925-7cb5-4614-b1e4-e1abbac5c76d">

--- packages/docs/src/pages/index.tsx
+++ packages/docs/src/pages/index.tsx
@@ -5,7 +5,8 @@ import Hero from "@site/src/components/Hero/StartScreen";
 import LearnMoreHero from "@site/src/components/LearnMore/LearnMoreHero";
 import LearnMoreFooter from "@site/src/components/LearnMore/LearnMoreFooter";
 import Installation from "@site/src/components/Sections/Installation";
-import FooterBackground from "../components/FooterBackground";
+import LandingBackground from "@site/src/components/Hero/LandingBackground";
+import FooterBackground from "@site/src/components/FooterBackground";
 import Overview from "@site/src/components/Sections/Overview";
 import FAQ from "@site/src/components/Sections/FAQ";
 
@@ -14,6 +15,7 @@ import styles from "./index.module.css";
 export default function Home(): JSX.Element {
   return (
     <Layout description="A better developer experience for React Native developers.">
+      <LandingBackground />
       <div className={styles.preventfulContainer}>
         <div className={styles.container}>
           <Hero />
