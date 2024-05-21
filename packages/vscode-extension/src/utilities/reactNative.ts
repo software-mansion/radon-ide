464ffa3103d351c4b3b54536508e2b8aa17e4b18
filip131311 <159789821+filip131311@users.noreply.github.com>
2024-05-22 00:15:27 +0200

    Fix duplicate dependencies during gradle build.  (#269)
    Description: 
    some 3rd party libraries, could cause duplication of classes during
    transformation we performed in configureReactNativeOverrides.
    
    This is a solution for RN versions 74 and higher that relays on the fact
    that we don't need to perform configureReactNativeOverrides for those
    versions, because the solution were marged upstream.
    see: https://github.com/facebook/react-native/pull/42617
    
    How to reproduce a bug? 
    
    - run `npx react-native init your-project`
    - run `npm i react-native-biometrics`
    - try to run react-native-ide on android 
    
    note that react-native-ide will believe that it is not a native change
    so it you will have to manually clean rebuild every time you add or
    remove react-native-biometrics.
    
    
    This is the reported error in build logs: 
    <img width="421" alt="Screenshot 2024-05-13 at 02 38 41"
    src="https://github.com/software-mansion/react-native-ide/assets/159789821/23007fdf-141a-4dbd-a596-7a1c269f5a03">
    
    ---------
    
    Co-authored-by: filip131311 <filip.kaminski@swmansiom.com>

---
+++ packages/vscode-extension/src/utilities/reactNative.ts
@@ -0,0 +1,11 @@
+import { getAppRootFolder } from "./extensionContext";
+import path from "path";
+
+export async function getReactNativeVersion() {
+  const workspacePath = getAppRootFolder();
+  const reactNativeRoot = path.dirname(require.resolve("react-native", { paths: [workspacePath] }));
+  const packageJsonPath = path.join(reactNativeRoot, "package.json");
+  const packageJson = require(packageJsonPath);
+
+  return packageJson.version ?? "0.74.0";
+}
