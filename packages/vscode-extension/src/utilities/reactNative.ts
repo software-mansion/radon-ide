cc1e9d3dd7a8adfbb144f88bb708c2fae54e8871
filip131311 <filip.kaminski@swmansiom.com>
2024-05-21 12:23:25 +0200

    change the suorce of information about RN version

--- packages/vscode-extension/src/utilities/reactNative.ts
+++ packages/vscode-extension/src/utilities/reactNative.ts
@@ -1,20 +1,11 @@
-import semver from "semver";
-import { Logger } from "../Logger";
 import { getAppRootFolder } from "./extensionContext";
-import fs from "fs";
 import path from "path";
 
 export async function getReactNativeVersion() {
   const workspacePath = getAppRootFolder();
-  const packageJsonPath = path.join(workspacePath, "package.json");
+  const reactNativeRoot = path.dirname(require.resolve("react-native", { paths: [workspacePath] }));
+  const packageJsonPath = path.join(reactNativeRoot, "package.json");
+  const packageJson = require(packageJsonPath);
 
-  try {
-    const jsonString = await fs.promises.readFile(packageJsonPath, "utf8");
-    const packageJson = JSON.parse(jsonString);
-    return semver.valid(semver.coerce(packageJson!.dependencies["react-native"])) ?? "0.74.0";
-  } catch (err) {
-    Logger.debug("Error:", err);
-  }
-
-  return "0.74.0";
+  return packageJson.version ?? "0.74.0";
 }
