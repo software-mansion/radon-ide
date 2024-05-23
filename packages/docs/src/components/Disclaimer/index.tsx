f7bbb55f763105effedb0a959c693e69b5c2629e
Patrycja Kalińska <cysiak0412@gmail.com>
2024-05-23 08:51:44 +0200

    Remove comments

--- packages/docs/src/components/Disclaimer/index.tsx
+++ packages/docs/src/components/Disclaimer/index.tsx
@@ -4,7 +4,6 @@ import styles from "./styles.module.css";
 const Disclaimer = () => {
   return (
     <div className={styles.disclaimerContainer}>
-      {/* <h2 className={styles.disclaimerHeading}>Note</h2> */}
       <p>
         <strong>Note: </strong>React Native IDE is not a ready product (yet). Currently it's in beta
         stage and only supports development on macOS in VSCode and Cursor. We are hoping that
