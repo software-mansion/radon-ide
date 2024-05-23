1403a8c9c8bb948088b95370f0ac823c907c2f41
Patrycja Kalińska <59940332+patrycjakalinska@users.noreply.github.com>
2024-05-23 20:30:40 +0200

    Add disclaimer section (#279)
    Add disclaimer section that informs user that our product it's not final
    and is currently in beta, and can only be run on macOS.
    
    <img width="559" alt="image"
    src="https://github.com/software-mansion/react-native-ide/assets/59940332/20d54c70-35af-4174-a764-7fc01f004701">

---
+++ packages/docs/src/components/Disclaimer/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+const Disclaimer = () => {
+  return (
+    <div className={styles.disclaimerContainer}>
+      <p>
+        <strong>Note: </strong>React Native IDE is not a ready product (yet). Currently it's in beta
+        stage and only supports development on macOS in VSCode and Cursor. We are hoping that
+        together with the community we will be able to get there soon.
+      </p>
+    </div>
+  );
+};
+
+export default Disclaimer;
