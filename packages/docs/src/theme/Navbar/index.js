d43dad3bcde840c219d4f30e4bf20ba9d1fdacfc
Kacper Kapuściak <kapusciak.k@gmail.com>
2024-05-25 14:02:36 +0200

    docs: enable anonymous analytics

--- packages/docs/src/theme/Navbar/index.js
+++ packages/docs/src/theme/Navbar/index.js
@@ -1,17 +1,21 @@
 import useBaseUrl from "@docusaurus/useBaseUrl";
 import React from "react";
 import { Navbar } from "@swmansion/t-rex-ui";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function NavbarWrapper(props) {
   const heroImages = {
     logo: useBaseUrl("/img/logo.svg"),
   };
   return (
-    <Navbar
-      isAlgoliaActive={false}
-      isThemeSwitcherShown={false}
-      heroImages={heroImages}
-      {...props}
-    />
+    <>
+      <Navbar
+        isAlgoliaActive={false}
+        isThemeSwitcherShown={false}
+        heroImages={heroImages}
+        {...props}
+      />
+      <Analytics />
+    </>
   );
 }
