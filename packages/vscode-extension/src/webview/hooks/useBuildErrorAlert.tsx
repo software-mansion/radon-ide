a531f58d83b83397fdcc2658a7f06587b0f28302
Krzysztof Magiera <krzys@swmansion.com>
2024-06-06 15:10:23 +0200

    Don't perform clean rebuild on bundle errors (#352)
    Bundle errors shouldn't require full rebuild to get fix as they are due
    to JS-only code changes. We shouldn't trigger clean builds from the
    bundle error dialog.

--- packages/vscode-extension/src/webview/hooks/useBuildErrorAlert.tsx
+++ packages/vscode-extension/src/webview/hooks/useBuildErrorAlert.tsx
@@ -18,7 +18,7 @@ function Actions() {
       <IconButton
         type="secondary"
         onClick={() => {
-          project.restart(true);
+          project.restart(false);
         }}
         tooltip={{ label: "Reload IDE", side: "bottom" }}>
         <span className="codicon codicon-refresh" />
