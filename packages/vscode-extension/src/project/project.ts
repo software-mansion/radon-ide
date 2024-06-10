software-mansion/radon-ide

commit 71ec697babc6c1a78f7eb75327531a5d69bad65c

Add static geolocation (#317)
This PR adds an ability to set static device location. 

added functionality:
- setting location
- enable/disable location services
- coordinates validation



https://github.com/software-mansion/react-native-ide/assets/159789821/a22754ba-a832-4fab-a13a-23545da14b79



https://github.com/software-mansion/react-native-ide/assets/159789821/cf25e213-2af1-41d9-9ddd-6baef5c3444d

Fixes: #330

---------

Co-authored-by: filip131311 <filip.kaminski@swmansiom.com>
Co-authored-by: Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>

packages/vscode-extension/src/project/project.ts
@@ -65,6 +65,11 @@ export class Project implements Disposable, MetroDelegate, ProjectInterface {
   ) ?? {
     appearance: "dark",
     contentSize: "normal",
+    location: {
+      latitude: 50.048653,
+      longitude: 19.965474,
+      isDisabled: false,
+    },
   };
 
   constructor(private readonly deviceManager: DeviceManager) {

packages/vscode-extension/src/common/Project.ts
@@ -3,6 +3,11 @@ import { DeviceInfo } from "./DeviceManager";
 export type DeviceSettings = {
   appearance: "light" | "dark";
   contentSize: "xsmall" | "small" | "normal" | "large" | "xlarge" | "xxlarge" | "xxxlarge";
+  location: {
+    latitude: number;
+    longitude: number;
+    isDisabled: boolean;
+  };
 };
 
 export type ProjectState = {

packages/vscode-extension/src/devices/AndroidEmulatorDevice.ts
@@ -64,6 +64,40 @@ export class AndroidEmulatorDevice extends DeviceBase {
       "&&",
       `cmd uimode night ${settings.appearance === "light" ? "no" : "yes"}`,
     ]);
+    // location_mode: LOCATION_MODE_OFF: 0 LOCATION_MODE_HIGH_ACCURACY: 3 LOCATION_MODE_BATTERY_SAVING: 2 LOCATION_MODE_SENSORS_ONLY: 1
+    if (settings.location.isDisabled) {
+      await exec(ADB_PATH, [
+        "-s",
+        this.serial!,
+        "shell",
+        "settings",
+        "put",
+        "secure",
+        "location_mode",
+        "0",
+      ]);
+    } else {
+      await exec(ADB_PATH, [
+        "-s",
+        this.serial!,
+        "shell",
+        "settings",
+        "put",
+        "secure",
+        "location_mode",
+        "3",
+      ]);
+      // note that geo fix command takes arguments: $longitude , $latitude so the order is reversed compared to most conventions
+      await exec(ADB_PATH, [
+        "-s",
+        this.serial!,
+        "emu",
+        "geo",
+        "fix",
+        settings.location.longitude.toString(),
+        settings.location.latitude.toString(),
+      ]);
+    }
   }
 
   async bootDevice() {

packages/vscode-extension/src/devices/IosSimulatorDevice.ts
@@ -91,6 +91,26 @@ export class IosSimulatorDevice extends DeviceBase {
       "content_size",
       convertToSimctlSize(settings.contentSize),
     ]);
+    if (settings.location.isDisabled) {
+      await exec("xcrun", [
+        "simctl",
+        "--set",
+        deviceSetLocation,
+        "location",
+        this.deviceUDID,
+        "clear",
+      ]);
+    } else {
+      await exec("xcrun", [
+        "simctl",
+        "--set",
+        deviceSetLocation,
+        "location",
+        this.deviceUDID,
+        "set",
+        `${settings.location.latitude.toString()},${settings.location.longitude.toString()}`,
+      ]);
+    }
   }
 
   async configureMetroPort(bundleID: string, metroPort: number) {

packages/vscode-extension/src/webview/components/DeviceSettingsDropdown.tsx
@@ -11,6 +11,11 @@ import "./DeviceSettingsDropdown.css";
 import Label from "./shared/Label";
 import { useProject } from "../providers/ProjectProvider";
 import { DeviceSettings } from "../../common/Project";
+import DoctorIcon from "./icons/DoctorIcon";
+import { DeviceLocationView } from "../views/DeviceLocationView";
+import { JSX } from "react/jsx-runtime";
+import DiagnosticView from "../views/DiagnosticView";
+import { useModal } from "../providers/ModalProvider";
 
 const contentSizes = [
   "xsmall",
@@ -29,6 +34,7 @@ interface DeviceSettingsDropdownProps {
 
 function DeviceSettingsDropdown({ children, disabled }: DeviceSettingsDropdownProps) {
   const { project, deviceSettings } = useProject();
+  const { openModal } = useModal();
 
   return (
     <DropdownMenu.Root>
@@ -97,6 +103,15 @@ function DeviceSettingsDropdown({ children, disabled }: DeviceSettingsDropdownPr
 
             <DropdownMenu.Arrow className="dropdown-menu-arrow" />
           </form>
+          <Label>Device Location</Label>
+          <DropdownMenu.Item
+            className="dropdown-menu-item"
+            onSelect={() => {
+              openModal("Location", <DeviceLocationView />);
+            }}>
+            <span className="codicon codicon-location" />
+            Set Device Location
+          </DropdownMenu.Item>
         </DropdownMenu.Content>
       </DropdownMenu.Portal>
     </DropdownMenu.Root>

packages/vscode-extension/src/webview/components/shared/Button.tsx
@@ -4,7 +4,7 @@ import Tooltip from "./Tooltip";
 
 interface ButtonProps {
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
-  type?: "primary" | "secondary" | "ternary";
+  type?: "primary" | "secondary" | "ternary" | "submit";
   disabled?: boolean;
   counter?: number;
   active?: boolean;

packages/vscode-extension/src/webview/components/shared/Tooltip.tsx
@@ -4,9 +4,9 @@ import classnames from "classnames";
 
 interface TooltipProps {
   children: React.ReactNode;
-  label: string;
+  label: React.ReactNode;
   side?: "top" | "right" | "bottom" | "left";
-  type?: "primary" | "secondary" | "ternary";
+  type?: "primary" | "secondary" | "ternary" | "submit";
   instant?: boolean;
 }
 

packages/vscode-extension/src/webview/providers/ProjectProvider.tsx
@@ -17,7 +17,15 @@ const ProjectContext = createContext<ProjectContextProps>({
     selectedDevice: undefined,
     previewZoom: undefined,
   },
-  deviceSettings: { appearance: "dark", contentSize: "normal" },
+  deviceSettings: {
+    appearance: "dark",
+    contentSize: "normal",
+    location: {
+      latitude: 50.048653,
+      longitude: 19.965474,
+      isDisabled: false,
+    },
+  },
   project,
 });
 
@@ -31,6 +39,11 @@ export default function ProjectProvider({ children }: PropsWithChildren) {
   const [deviceSettings, setDeviceSettings] = useState<DeviceSettings>({
     appearance: "dark",
     contentSize: "normal",
+    location: {
+      latitude: 50.048653,
+      longitude: 19.965474,
+      isDisabled: false,
+    },
   });
 
   useEffect(() => {

commit 08da25cd262ab697ecc5abda74312f54a85318b3

Fix errors caused by #317 (#367)
In #317 new fields have been added to device settings. However, since we
persist device settings, the changes are not compatible with the old
data that might've been stored there. To prevent this we are adding a
versioning of device settings key (will increment version with future
changes).

This PR also changes the defaults to use disabled location, especially
that we need to have some coordinates specified, it'd be weird to have
everyone using IDE pointing to the same geo location.

packages/vscode-extension/src/project/project.ts
@@ -24,6 +24,7 @@ import {
   ProjectInterface,
   ProjectState,
   StartupMessage,
+  ZoomLevelType,
 } from "../common/Project";
 import { EventEmitter } from "stream";
 import { openFileAtPosition } from "../utilities/openFileAtPosition";
@@ -32,9 +33,8 @@ import stripAnsi from "strip-ansi";
 import { minimatch } from "minimatch";
 import { IosSimulatorDevice } from "../devices/IosSimulatorDevice";
 import { AndroidEmulatorDevice } from "../devices/AndroidEmulatorDevice";
-import { ZoomLevelType } from "../webview/components/ZoomControls";
 
-const DEVICE_SETTINGS_KEY = "device_settings";
+const DEVICE_SETTINGS_KEY = "device_settings_v15";
 const LAST_SELECTED_DEVICE_KEY = "last_selected_device";
 const PREVIEW_ZOOM_KEY = "preview_zoom";
 
@@ -68,7 +68,7 @@ export class Project implements Disposable, MetroDelegate, ProjectInterface {
     location: {
       latitude: 50.048653,
       longitude: 19.965474,
-      isDisabled: false,
+      isDisabled: true,
     },
   };
 

packages/vscode-extension/src/webview/components/ZoomControls.tsx
@@ -6,7 +6,6 @@ import { ZoomLevelType } from "../../common/Project";
 import { DeviceProperties } from "../utilities/consts";
 
 const ZOOM_STEP = 0.05;
-const DEFAULT_ZOOM_LEVEL = 1;
 const ZOOM_SELECT_NUMERIC_VALUES = [0.5, 0.6, 0.7, 0.8, 0.9, 1];
 export const DEVICE_DEFAULT_SCALE = 1 / 3;
 