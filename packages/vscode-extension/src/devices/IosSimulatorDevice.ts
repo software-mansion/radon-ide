cc3af0912864bb51b792e6325cc86fc6b6df4dc2
filip131311 <filip.kaminski@swmansiom.com>
2024-06-10 13:52:05 +0200

    refactor

--- packages/vscode-extension/src/devices/IosSimulatorDevice.ts
+++ packages/vscode-extension/src/devices/IosSimulatorDevice.ts
@@ -91,16 +91,26 @@ export class IosSimulatorDevice extends DeviceBase {
       "content_size",
       convertToSimctlSize(settings.contentSize),
     ]);
-    await exec("xcrun", [
-      "simctl",
-      "--set",
-      deviceSetLocation,
-      "location",
-      this.deviceUDID,
-      settings.location.isDisabled
-        ? "clear"
-        : `set ${settings.location.latitude.toString()},${settings.location.longitude.toString()}`,
-    ]);
+    if (settings.location.isDisabled) {
+      await exec("xcrun", [
+        "simctl",
+        "--set",
+        deviceSetLocation,
+        "location",
+        this.deviceUDID,
+        "clear",
+      ]);
+    } else {
+      await exec("xcrun", [
+        "simctl",
+        "--set",
+        deviceSetLocation,
+        "location",
+        this.deviceUDID,
+        "set",
+        `${settings.location.latitude.toString()},${settings.location.longitude.toString()}`,
+      ]);
+    }
   }
 
   async configureMetroPort(bundleID: string, metroPort: number) {
