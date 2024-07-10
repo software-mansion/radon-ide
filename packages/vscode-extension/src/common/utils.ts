f307c25f065e5b1779dd6a07e1d114e67427a50d
filip131311 <filip.kaminski@swmansiom.com>
2024-07-10 18:25:10 +0200

    a utility class callasble from webview

--- packages/vscode-extension/src/common/utils.ts
+++ packages/vscode-extension/src/common/utils.ts
@@ -1,29 +1,20 @@
-export function throttle<T extends (...args: any[]) => any>(
-  func: T,
-  limitMs: number
-): (...args: [...Parameters<T>, force?: boolean]) => ReturnType<T> {
-  let timeout: NodeJS.Timeout | null = null;
-  let recentArgs: any;
+export interface UtilsEventMap {
+  // log: { type: string };
+  // projectStateChanged: ProjectState;
+  // deviceSettingsChanged: DeviceSettings;
+  // navigationChanged: { displayName: string; id: string };
+  // needsNativeRebuild: void;
+}
 
-  return function (...args: any) {
-    const force = args[args.length - 1] === true; // Check if the last argument is true (force flag)
+export interface UtilsEventListener<T> {
+  (event: T): void;
+}
 
-    if (force) {
-      if (timeout != null) {
-        clearTimeout(timeout);
-      }
-      timeout = null;
-      func(...args);
-      return;
-    }
+export interface UtilsInterface {
+  getCommandsCurrentKeyBinding(commandName: string): Promise<string | undefined>;
 
-    if (!timeout) {
-      timeout = setTimeout(() => {
-        timeout = null;
-        func(...recentArgs);
-        recentArgs = null;
-      }, limitMs);
-    }
-    recentArgs = args;
-  } as T;
+  reportIssue(): Promise<void>;
+
+  openFileAt(filePath: string, line0Based: number, column0Based: number): Promise<void>;
+  movePanelToNewWindow(): void;
 }
