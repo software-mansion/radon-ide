software-mansion/radon-ide

commit 1aa8eede3fdf223e640a940600a34c8ef97ca21e

Avoid passing PWD entirely to subprocesses when cwd option is set (#432)
This fixes an issue when VSCode started from app launcher would populate
PWD environment constant which interfers with some tooling.

We've already attempted to fix that in #369 but the fix really only
worked when PWD was set as options.

Aparently execa library that we use for subprocess management by default
inheritcs process.env (which is what we want). But in that case env.PWD
would be provided even despite the fact it wasn't included in env from
the options. This got broken along with #378 where we stopped manually
passing process.env when launching.

As a consequence, we will now always override PWD with "0" which will
result in third-party checks to fail on that variable.

Test plan:
Launch react-conf-app repo with VSCode started from launcher

---------

Co-authored-by: Jakub Gonet <jakub.gonet@swmansion.com>

packages/vscode-extension/src/utilities/subprocess.ts
@@ -4,15 +4,19 @@ import readline from "readline";
 
 export type ChildProcess = ExecaChildProcess<string>;
 
-function updatePWDEnvWhenCwdIsSet(options?: execa.Options) {
-  // Some processes rely on PWD environment variable that may be copied from
-  // the parent process. However, when cwd option is set, we never should use
-  // PWD of the parent process as it may point to a different directory. In case
-  // both cwd and PWD environment variable are set, we should update PWD to the
-  // same location as requested by the cwd option.
-  if (options && options.cwd && options.env && options.env.PWD) {
-    options.env.PWD = options.cwd;
+function overridePWD<T extends execa.Options>(options?: T) {
+  // Some processes rely on PWD environment variable to tell the current working
+  // directory in which cases PWD takes precedence over process.cwd.
+  // By default, execa would copy all process env to the subprocess (which is desired)
+  // including PWD that may point to a different location that the selected cwd (indicated
+  // by options.cwd). Specifically, when VSCode is launched from the launcher (as opposed to
+  // being launched from command line using 'code' command), the PWD is set to "/".
+  // This method overrides PWD to the current cwd option when it's set for the subprocess call
+  // therefore removing the risk of the subprocess using the wrong working directory.
+  if (options?.cwd) {
+    return { ...options, env: { ...options.env, PWD: options.cwd } };
   }
+  return undefined;
 }
 
 /**
@@ -44,39 +48,26 @@ export function lineReader(childProcess: ExecaChildProcess<string>, includeStder
 }
 
 export function exec(
-  ...args: [string, string[]?, (execa.Options & { allowNonZeroExit?: boolean })?]
+  name: string,
+  args?: string[],
+  options?: execa.Options & { allowNonZeroExit?: boolean }
 ) {
-  if (args.length > 1) {
-    updatePWDEnvWhenCwdIsSet(args[2]);
-  }
-  const subprocess = execa(...args);
-  const allowNonZeroExit = args[2]?.allowNonZeroExit;
+  const subprocess = execa(name, args, overridePWD(options));
+  const allowNonZeroExit = options?.allowNonZeroExit;
   async function printErrorsOnExit() {
     try {
       const result = await subprocess;
       if (result.stderr) {
-        Logger.debug(
-          "Subprocess",
-          args[0],
-          args[1]?.join(" "),
-          "produced error output:",
-          result.stderr
-        );
+        Logger.debug("Subprocess", name, args?.join(" "), "produced error output:", result.stderr);
       }
     } catch (e) {
       // @ts-ignore idk how to deal with error objects in ts
       const { exitCode, signal } = e;
       if (exitCode === undefined && signal !== undefined) {
-        Logger.info("Subprocess", args[0], "was terminated with", signal);
+        Logger.info("Subprocess", name, "was terminated with", signal);
       } else {
         if (!allowNonZeroExit || !exitCode) {
-          Logger.error(
-            "Subprocess",
-            args[0],
-            args[1]?.join(" "),
-            "execution resulted in an error:",
-            e
-          );
+          Logger.error("Subprocess", name, args?.join(" "), "execution resulted in an error:", e);
         }
       }
     }
@@ -85,36 +76,24 @@ export function exec(
   return subprocess;
 }
 
-export function execSync(...args: [string, string[]?, execa.SyncOptions?]) {
-  if (args.length > 1) {
-    updatePWDEnvWhenCwdIsSet(args[2]);
-  }
-  const result = execa.sync(...args);
+export function execSync(name: string, args?: string[], options?: execa.SyncOptions) {
+  const result = execa.sync(name, args, overridePWD(options));
   if (result.stderr) {
-    Logger.debug(
-      "Subprocess",
-      args[0],
-      args[1]?.join(" "),
-      "produced error output:",
-      result.stderr
-    );
+    Logger.debug("Subprocess", name, args?.join(" "), "produced error output:", result.stderr);
   }
   return result;
 }
 
-export function command(...args: [string, execa.Options?]) {
-  if (args.length > 1) {
-    updatePWDEnvWhenCwdIsSet(args[1]);
-  }
-  const subprocess = execa.command(...args);
+export function command(commandWithArgs: string, options?: execa.Options) {
+  const subprocess = execa.command(commandWithArgs, overridePWD(options));
   async function printErrorsOnExit() {
     try {
       const result = await subprocess;
       if (result.stderr) {
-        Logger.debug("Command", args[0], "produced error output:", result.stderr);
+        Logger.debug("Command", commandWithArgs, "produced error output:", result.stderr);
       }
     } catch (e) {
-      Logger.error("Command", args[0], "execution resulted in an error:", e);
+      Logger.error("Command", commandWithArgs, "execution resulted in an error:", e);
     }
   }
   printErrorsOnExit(); // don't want to await here not to block the outer method

commit 98d0ca0d7f6d7211752c5c5455847eca8fb5ab0a

Correctly pass options to exec-like functions (#438)
This PR fixes a bug introduced in #432.

The bug caused "options" argument to be removed from subprocesses that
did not specify the cwd.

Co-authored-by: filip131311 <filip.kaminski@swmansiom.com>

packages/vscode-extension/src/utilities/subprocess.ts
@@ -16,7 +16,7 @@ function overridePWD<T extends execa.Options>(options?: T) {
   if (options?.cwd) {
     return { ...options, env: { ...options.env, PWD: options.cwd } };
   }
-  return undefined;
+  return options;
 }
 
 /**