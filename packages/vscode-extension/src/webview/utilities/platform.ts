7b9fc7852b820fd16d8039946c9f73643ff9cfea
Paweł Małecki <92953623+p-malecki@users.noreply.github.com>
2024-07-23 16:14:39 +0200

    Remove "user-agent-data-types" package and replace with type any

--- packages/vscode-extension/src/webview/utilities/platform.ts
+++ packages/vscode-extension/src/webview/utilities/platform.ts
@@ -1,6 +1,6 @@
 export class Platform {
   static OS: "macos" | "windows" | "unknown" = (() => {
-    const platform = navigator.userAgentData?.platform ?? navigator.platform ?? "unknown";
+    const platform = (navigator as any).userAgentData?.platform ?? navigator.platform ?? "unknown";
     if (/mac/.test(platform)) return "macos";
     if (/win/.test(platform)) return "windows";
     return "macos";
