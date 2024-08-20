d5ba14deffc702ce76ad15a23d3e7725ace35b50
Paweł Małecki <92953623+p-malecki@users.noreply.github.com>
2024-08-20 13:42:26 +0200

    Remove test app (rn-simple-app)

--- test-apps/rn-simple-app/StickerSmash/components/MyButton.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "react-native";
-
-export default function MyButton() {
-  return (
-    <div>
-      <Button title="FRYTKI" onPress={() => alert("Button clicked!")} />
-    </div>
-  );
-}
