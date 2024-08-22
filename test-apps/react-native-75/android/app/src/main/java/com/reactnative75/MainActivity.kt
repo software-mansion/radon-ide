aa034a162aba044ecaeec13722732764b0172d49
filip131311 <filip.kaminski@swmansiom.com>
2024-08-22 16:19:14 +0200

    init

--- test-apps/react-native-75/android/app/src/main/java/com/reactnative75/MainActivity.kt
+++ test-apps/react-native-75/android/app/src/main/java/com/reactnative75/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "ReactNative75"
+  override fun getMainComponentName(): String = "reactNative75"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
