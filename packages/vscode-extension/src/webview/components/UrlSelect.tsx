def9ac11a18ce775a5eb87c86f64812a3bf0b53d
Paweł Małecki <92953623+p-malecki@users.noreply.github.com>
2024-09-03 13:15:37 +0200

    Add recent URLs list to SelectUrl

--- packages/vscode-extension/src/webview/components/UrlSelect.tsx
+++ packages/vscode-extension/src/webview/components/UrlSelect.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren } from "react";
+import React, { PropsWithChildren, useState, useEffect } from "react";
 import * as Select from "@radix-ui/react-select";
 import "./UrlSelect.css";
 
@@ -15,17 +15,30 @@ const SelectItem = React.forwardRef<HTMLDivElement, PropsWithChildren<Select.Sel
 interface UrlSelectProps {
   value: string;
   onValueChange: (newValue: string) => void;
+  recentItems: { id: string; name: string }[];
   items: { id: string; name: string }[];
   disabled?: boolean;
 }
 
-function UrlSelect({ onValueChange, items, value, disabled }: UrlSelectProps) {
+function UrlSelect({ onValueChange, recentItems, items, value, disabled }: UrlSelectProps) {
+  // We use two lists for URL selection: one with recently used URLs and another
+  // with all available URLs. Since recentItems is a subset of items, each recentItems's
+  // value is prefixed to differentiate their origins when presented in the Select
+  // component. This prefix is stripped off when the selected value is passed back
+  // through onValueChange.
   const maxLineLenght = 37; // maximum length for an item name without truncation
   const longestURl = Math.max(...items.map((item) => item.name.length));
   const urlWidth = Math.min(Math.max(longestURl * 7, 180), 280);
 
-// Reformats text to max 37 characters per line, breaking at last dash if possible.
-  function splitLines(text: string): string {
+  const handleValueChange = (newValue: string) => {
+    const dashIndex = newValue.indexOf("#");
+    const strippedValue = dashIndex === -1 ? newValue : newValue.substring(dashIndex + 1);
+    console.log("FRYTKI strippedValue", strippedValue);
+    onValueChange(strippedValue);
+  };
+
+  function splitLines(text: string) {
+    // Reformats text to max 37 characters per line, breaking at last dash if possible.
     const findBreakPoint = (chunk: string, startIndex: number): number => {
       const lastDash = chunk.lastIndexOf("-");
       if (lastDash !== -1) {
@@ -50,9 +63,9 @@ function UrlSelect({ onValueChange, items, value, disabled }: UrlSelectProps) {
   }
 
   return (
-    <Select.Root onValueChange={onValueChange} value={value} disabled={disabled}>
+    <Select.Root onValueChange={handleValueChange} value={value} disabled={disabled}>
       <Select.Trigger className="url-select-trigger" style={{ width: urlWidth }}>
-        <Select.Value placeholder="/" aria-label={value} />
+        <Select.Value placeholder="/" aria-label={value}></Select.Value>
       </Select.Trigger>
       <Select.Portal>
         <Select.Content
@@ -60,14 +73,29 @@ function UrlSelect({ onValueChange, items, value, disabled }: UrlSelectProps) {
           position="popper"
           style={{ width: urlWidth }}>
           <Select.Viewport className="url-select-viewport">
-            {items.map(
-              (item) =>
-                item.name && (
-                  <SelectItem value={item.id} key={item.id}>
-                    {item.name.length > maxLineLenght ? splitLines(item.name) : item.name}
-                  </SelectItem>
-                )
-            )}
+            <Select.Group>
+              <Select.Label className="url-select-label">Recently used:</Select.Label>
+              {recentItems.map(
+                (item) =>
+                  item.name && (
+                    <SelectItem value={`recent#${item.id}`} key={item.id}>
+                      {item.name.length > maxLineLenght ? splitLines(item.name) : item.name}
+                    </SelectItem>
+                  )
+              )}
+            </Select.Group>
+            <Select.Separator className="url-select-separator" />
+            <Select.Group>
+              <Select.Label className="url-select-label">All paths:</Select.Label>
+              {items.map(
+                (item) =>
+                  item.name && (
+                    <SelectItem value={item.id} key={item.id}>
+                      {item.name.length > maxLineLenght ? splitLines(item.name) : item.name}
+                    </SelectItem>
+                  )
+              )}
+            </Select.Group>
           </Select.Viewport>
         </Select.Content>
       </Select.Portal>
