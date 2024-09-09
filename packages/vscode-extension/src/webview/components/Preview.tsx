3ff69344a2e180453b4c252d895c55653069afc5
Paweł Małecki <92953623+p-malecki@users.noreply.github.com>
2024-09-09 16:15:38 +0200

    Change multitouch trigger btn on windows

--- packages/vscode-extension/src/webview/components/Preview.tsx
+++ packages/vscode-extension/src/webview/components/Preview.tsx
@@ -19,7 +19,7 @@ import { Resizable } from "re-resizable";
 import { useResizableProps } from "../hooks/useResizableProps";
 import ZoomControls from "./ZoomControls";
 import { throttle } from "../../utilities/throttle";
-import { useUtils } from "../providers/UtilsProvider";
+import { Platform, useUtils } from "../providers/UtilsProvider";
 import { useWorkspaceConfig } from "../providers/WorkspaceConfigProvider";
 
 declare module "react" {
@@ -408,7 +408,12 @@ function Preview({ isInspecting, setIsInspecting, zoomLevel, onZoomChanged }: Pr
         e.preventDefault();
         const isKeydown = e.type === "keydown";
 
-        if (e.code === "AltLeft" || e.code === "AltRight") {
+        const isMultitouchKeyPressed = Platform.select({
+          macos: e.code === "AltLeft" || e.code === "AltRight",
+          windows: e.code === "ControlLeft" || e.code === "ControlRight",
+        });
+
+        if (isMultitouchKeyPressed) {
           isKeydown && setAnchorPoint({ x: 0.5, y: 0.5 });
           setIsMultiTouching(isKeydown);
         }
