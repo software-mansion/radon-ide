9e93bdbb08910d52e97b98d1642555305a965d8c
Jakub Gonet <jakub.gonet@swmansion.com>
2024-09-30 22:59:57 +0200

    Move helper functions to the bottom

--- packages/vscode-extension/src/utilities/launchConfiguration.ts
+++ packages/vscode-extension/src/utilities/launchConfiguration.ts
@@ -1,6 +1,20 @@
 import { workspace } from "vscode";
 import { LaunchConfigurationOptions } from "../common/LaunchConfig";
 
+export function getLaunchConfiguration() {
+  const ideConfig: LaunchConfigurationOptions = workspace
+    .getConfiguration("launch")
+    ?.configurations?.find(isIdeConfig);
+
+  if (!ideConfig) {
+    return {};
+  }
+
+  assertValidConfig(ideConfig);
+
+  return ideConfig;
+}
+
 function isIdeConfig(config: any) {
   return config.type === "react-native-ide" || config.type === "radon-ide"; // we keep previous type name for compatibility with old configuration files
 }
@@ -24,18 +38,4 @@ function assertValidConfig({ buildScript, eas }: LaunchConfigurationOptions) {
       );
     }
   }
-}
-
-export function getLaunchConfiguration() {
-  const ideConfig: LaunchConfigurationOptions = workspace
-    .getConfiguration("launch")
-    ?.configurations?.find(isIdeConfig);
-
-  if (!ideConfig) {
-    return {};
-  }
-
-  assertValidConfig(ideConfig);
-
-  return ideConfig;
 }
