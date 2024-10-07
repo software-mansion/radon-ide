45698e65de98fcd210d9987f4f517838ac03306d
Katzper Michno <katzper.michno@gmail.com>
2024-10-07 10:16:48 +0200

    display box with dimensions next to inspected element

---
+++ packages/vscode-extension/src/webview/components/DimensionsBox.tsx
@@ -0,0 +1,123 @@
+import { InspectData } from "../../common/Project";
+import { DeviceProperties } from "../utilities/consts";
+import "./DimensionsBox.css";
+
+const BOX_HEIGHT_FRACTION = 0.033;
+const FONT_SIZE_FRACTION = 0.8;
+const BORDER_RADIUS_FRACTION = 0.15;
+const HORIZONTAL_PADDING_FRACTION = 0.15;
+const ARROW_SIZE_FRACTION = 0.4;
+
+const VERTICAL_POSITION_THRESHOLD = 0.3;
+const HORIZONTAL_POSITION_THRESHOLD = 0.5;
+
+const VERTICAL_ARROW_MARGIN = 0.015;
+const HORIZONTAL_ARROW_MARGIN = 0.03;
+
+type DimensionsBoxProps = {
+  device?: DeviceProperties;
+  frame: InspectData["frame"];
+  wrapperDivRef: React.RefObject<HTMLDivElement>;
+};
+
+type DimensionsBoxPosition = 'above' | 'below' | 'left' | 'right' | 'inside';
+
+function DimensionsBox({ device, frame, wrapperDivRef } : DimensionsBoxProps) {
+  if (!device) {
+    return;
+  }
+
+  const width = parseFloat((frame.width * device.screenWidth).toFixed(2));
+  const height = parseFloat((frame.height * device.screenHeight).toFixed(2));
+
+  const previewDiv = wrapperDivRef.current?.childNodes?.[0] as unknown;
+
+  if (!previewDiv || typeof previewDiv !== 'object' 
+    || !('clientHeight' in previewDiv) 
+    || typeof previewDiv.clientHeight !== 'number'
+  ) {
+    return;
+  }
+
+  const { clientHeight: previewHeight } = previewDiv;
+
+  const boxHeight = previewHeight * BOX_HEIGHT_FRACTION;
+  const fontSize = boxHeight * FONT_SIZE_FRACTION;
+  const borderRadius = boxHeight * BORDER_RADIUS_FRACTION;
+  const horizontalPadding = boxHeight * HORIZONTAL_PADDING_FRACTION;
+  const arrowSize = boxHeight * ARROW_SIZE_FRACTION;
+
+  const boxPosition: DimensionsBoxPosition = (() => {
+    if (frame.y >= VERTICAL_POSITION_THRESHOLD) {return 'above';}
+    else if (frame.y + frame.height <= 1 - VERTICAL_POSITION_THRESHOLD) {return 'below';}
+    else if (frame.x + frame.width <= HORIZONTAL_POSITION_THRESHOLD) {return 'right';}
+    else if (frame.x >= 1 - HORIZONTAL_POSITION_THRESHOLD) {return 'left';}
+    return 'inside';
+  })();
+
+  const positionalProps = (() => {
+    switch (boxPosition) {
+      case 'above': 
+        return {
+          top: `${(frame.y - VERTICAL_ARROW_MARGIN) * 100}%`,
+          left: `${(frame.x + frame.width/2) * 100}%`,
+          transform: `translate(-50%, -100%)`
+        };
+      case 'below':
+        return {
+          top: `${(frame.y + frame.height + VERTICAL_ARROW_MARGIN) * 100}%`,
+          left: `${(frame.x + frame.width/2) * 100}%`,
+          transform: `translate(-50%, 0%)`
+        };
+      case 'right':
+        return {
+          top: `${(frame.y + frame.height/2) * 100}%`,
+          left: `${(frame.x + frame.width + HORIZONTAL_ARROW_MARGIN) * 100}%`,
+          transform: `translate(0%, -50%)`
+        };
+      case 'left':
+        return {
+          top: `${(frame.y + frame.height/2) * 100}%`,
+          left: `${(frame.x - HORIZONTAL_ARROW_MARGIN) * 100}%`,
+          transform: `translate(-100%, -50%)`
+        };
+      default: 
+        return {
+          top: `${(frame.y + frame.height/2) * 100}%`,
+          left: `${(frame.x + frame.width/2) * 100}%`,
+          transform: `translate(-50%, -50%)`
+        }; 
+    }
+  })();
+
+  return (
+    <>
+      { boxPosition !== 'inside' && (
+        <div 
+          className="arrow"
+          style={{
+            width: `${arrowSize}px`,
+            height: `${arrowSize}px`,
+            left: positionalProps.left,
+            top: positionalProps.top,
+          }}/>
+        )
+      }
+      <div 
+        className="dimensions-box" 
+        style={{
+          height: `${boxHeight}px`,
+          fontSize: `${fontSize}px`,
+          borderRadius: `${borderRadius}px`,
+          paddingLeft: `${horizontalPadding}px`,
+          paddingRight: `${horizontalPadding}px`,
+          ...positionalProps
+        }}
+      >
+        { width } × { height }
+      </div> 
+    </>
+  );
+};
+
+export default DimensionsBox;
--- packages/vscode-extension/src/webview/components/Preview.tsx
+++ packages/vscode-extension/src/webview/components/Preview.tsx
@@ -1,6 +1,7 @@
 import { useState, useRef, useEffect, MouseEvent, forwardRef, RefObject, ReactNode } from "react";
 import clamp from "lodash/clamp";
 import { VSCodeProgressRing } from "@vscode/webview-ui-toolkit/react";
+import { Resizable } from "re-resizable";
 import { keyboardEventToHID } from "../utilities/keyMapping";
 import "./Preview.css";
 import { useProject } from "../providers/ProjectProvider";
@@ -15,12 +16,12 @@ import Debugger from "./Debugger";
 import { useNativeRebuildAlert } from "../hooks/useNativeRebuildAlert";
 import { InspectData, InspectDataStackItem, ZoomLevelType } from "../../common/Project";
 import { InspectDataMenu } from "./InspectDataMenu";
-import { Resizable } from "re-resizable";
 import { useResizableProps } from "../hooks/useResizableProps";
 import ZoomControls from "./ZoomControls";
 import { throttle } from "../../utilities/throttle";
 import { Platform, useUtils } from "../providers/UtilsProvider";
 import { useWorkspaceConfig } from "../providers/WorkspaceConfigProvider";
+import DimensionsBox from "./DimensionsBox";
 
 declare module "react" {
   interface CSSProperties {
@@ -169,22 +170,6 @@ function ButtonGroupLeft({ children }: ButtonGroupLeftProps) {
     </div>
   );
 }
-
-type InspectDataContainerProps = {
-  device: DeviceProperties;
-  frame: InspectData["frame"] ;
-};
-
-const InspectDataContainer = ({ device, frame } : InspectDataContainerProps) => {
-  const width = parseFloat((frame.width * device.screenWidth).toFixed(2));
-  const height = parseFloat((frame.height * device.screenHeight).toFixed(2));
-
-  return (
-    <div className="inspect-data-container">
-      { width } × { height } 
-    </div> 
-  );
-};
 
 type InspectStackData = {
   requestLocation: { x: number; y: number };
@@ -531,150 +516,150 @@ function Preview({
 
   return (
     <>
-      <div className="preview-wrapper">
-        {inspectFrame && device && (
-          <InspectDataContainer device={device} frame={inspectFrame} />
-        )}
-        <div
-          className="phone-wrapper"
-          style={cssPropertiesForDevice(device!, isFrameDisabled)}
-          tabIndex={0} // allows keyboard events to be captured
-          ref={wrapperDivRef}>
-          {showDevicePreview && (
-            <Resizable {...resizableProps}>
-              <div className="phone-content">
-                <div className="touch-area" {...touchHandlers}>
-                  <MjpegImg
-                    src={previewURL}
-                    ref={previewRef}
+      <div
+        className="phone-wrapper"
+        style={cssPropertiesForDevice(device!, isFrameDisabled)}
+        tabIndex={0} // allows keyboard events to be captured
+        ref={wrapperDivRef}>
+        {showDevicePreview && (
+          <Resizable {...resizableProps}>
+            <div className="phone-content">
+              <div className="touch-area" {...touchHandlers}>
+                <MjpegImg
+                  src={previewURL}
+                  ref={previewRef}
+                  style={{
+                    cursor: isInspecting ? "crosshair" : "default",
+                  }}
+                  className="phone-screen"
+                />
+
+                {isMultiTouching && (
+                  <div
                     style={{
-                      cursor: isInspecting ? "crosshair" : "default",
-                    }}
-                    className="phone-screen"
-                  />
+                      "--x": `${touchPoint.x * 100}%`,
+                      "--y": `${touchPoint.y * 100}%`,
+                      "--size": `${normalTouchIndicatorSize}px`,
+                    }}>
+                    <TouchPointIndicator isPressing={isPressing} />
+                  </div>
+                )}
+                {isMultiTouching && (
+                  <div
+                    style={{
+                      "--x": `${anchorPoint.x * 100}%`,
+                      "--y": `${anchorPoint.y * 100}%`,
+                      "--size": `${smallTouchIndicatorSize}px`,
+                    }}>
+                    <TouchPointIndicator isPressing={false} />
+                  </div>
+                )}
+                {isMultiTouching && (
+                  <div
+                    style={{
+                      "--x": `${mirroredTouchPosition.x * 100}%`,
+                      "--y": `${mirroredTouchPosition.y * 100}%`,
+                      "--size": `${normalTouchIndicatorSize}px`,
+                    }}>
+                    <TouchPointIndicator isPressing={isPressing} />
+                  </div>
+                )}
 
-                  {isMultiTouching && (
+                {inspectFrame && (
+                  <div className="phone-screen phone-inspect-overlay">
                     <div
+                      className="inspect-area"
                       style={{
-                        "--x": `${touchPoint.x * 100}%`,
-                        "--y": `${touchPoint.y * 100}%`,
-                        "--size": `${normalTouchIndicatorSize}px`,
+                        left: `${inspectFrame.x * 100}%`,
+                        top: `${inspectFrame.y * 100}%`,
+                        width: `${inspectFrame.width * 100}%`,
+                        height: `${inspectFrame.height * 100}%`,
+                      }}
+                    />
+                    <DimensionsBox
+                      device={device}
+                      frame={inspectFrame} 
+                      wrapperDivRef={wrapperDivRef}
+                    />
+                  </div>
+                )}
+                {projectStatus === "refreshing" && (
+                  <div className="phone-screen phone-refreshing-overlay">
+                    <VSCodeProgressRing />
+                    <div>Refreshing...</div>
+                  </div>
+                )}
+                {debugPaused && (
+                  <div className="phone-screen phone-debug-overlay">
+                    <Debugger />
+                  </div>
+                )}
+                {debugException && (
+                  <div className="phone-screen phone-debug-overlay phone-exception-overlay">
+                    <button className="uncaught-button" onClick={() => project.resumeDebugger()}>
+                      Uncaught exception&nbsp;
+                      <span className="codicon codicon-debug-continue" />
+                    </button>
+                  </div>
+                )}
+                {/* TODO: Add different label in case of bundle/incremental bundle error */}
+                {hasBundleError && (
+                  <div className="phone-screen phone-debug-overlay phone-exception-overlay">
+                    <button
+                      className="uncaught-button"
+                      onClick={() => {
+                        project.restart(false);
                       }}>
-                      <TouchPointIndicator isPressing={isPressing} />
-                    </div>
-                  )}
-                  {isMultiTouching && (
-                    <div
-                      style={{
-                        "--x": `${anchorPoint.x * 100}%`,
-                        "--y": `${anchorPoint.y * 100}%`,
-                        "--size": `${smallTouchIndicatorSize}px`,
-                      }}>
-                      <TouchPointIndicator isPressing={false} />
-                    </div>
-                  )}
-                  {isMultiTouching && (
-                    <div
-                      style={{
-                        "--x": `${mirroredTouchPosition.x * 100}%`,
-                        "--y": `${mirroredTouchPosition.y * 100}%`,
-                        "--size": `${normalTouchIndicatorSize}px`,
-                      }}>
-                      <TouchPointIndicator isPressing={isPressing} />
-                    </div>
-                  )}
-
-                  {inspectFrame && (
-                    <div className="phone-screen phone-inspect-overlay">
-                      <div
-                        className="inspect-area"
-                        style={{
-                          left: `${inspectFrame.x * 100}%`,
-                          top: `${inspectFrame.y * 100}%`,
-                          width: `${inspectFrame.width * 100}%`,
-                          height: `${inspectFrame.height * 100}%`,
-                        }}
-                      />
-                    </div>
-                  )}
-                  {projectStatus === "refreshing" && (
-                    <div className="phone-screen phone-refreshing-overlay">
-                      <VSCodeProgressRing />
-                      <div>Refreshing...</div>
-                    </div>
-                  )}
-                  {debugPaused && (
-                    <div className="phone-screen phone-debug-overlay">
-                      <Debugger />
-                    </div>
-                  )}
-                  {debugException && (
-                    <div className="phone-screen phone-debug-overlay phone-exception-overlay">
-                      <button className="uncaught-button" onClick={() => project.resumeDebugger()}>
-                        Uncaught exception&nbsp;
-                        <span className="codicon codicon-debug-continue" />
-                      </button>
-                    </div>
-                  )}
-                  {/* TODO: Add different label in case of bundle/incremental bundle error */}
-                  {hasBundleError && (
-                    <div className="phone-screen phone-debug-overlay phone-exception-overlay">
-                      <button
-                        className="uncaught-button"
-                        onClick={() => {
-                          project.restart(false);
-                        }}>
-                        Bundle error&nbsp;
-                        <span className="codicon codicon-refresh" />
-                      </button>
-                    </div>
-                  )}
-                  {hasIncrementalBundleError && (
-                    <div className="phone-screen phone-debug-overlay phone-exception-overlay">
-                      <button className="uncaught-button" onClick={() => project.restart(false)}>
-                        Bundle error&nbsp;
-                        <span className="codicon codicon-refresh" />
-                      </button>
-                    </div>
-                  )}
-                </div>
-                <DeviceFrame device={device} isFrameDisabled={isFrameDisabled} />
-                {inspectStackData && (
-                  <InspectDataMenu
-                    inspectLocation={inspectStackData.requestLocation}
-                    inspectStack={inspectStackData.stack}
-                    onSelected={onInspectorItemSelected}
-                    onHover={(item) => {
-                      if (item.frame) {
-                        setInspectFrame(item.frame);
-                      }
-                    }}
-                    onCancel={() => resetInspector()}
-                  />
+                      Bundle error&nbsp;
+                      <span className="codicon codicon-refresh" />
+                    </button>
+                  </div>
+                )}
+                {hasIncrementalBundleError && (
+                  <div className="phone-screen phone-debug-overlay phone-exception-overlay">
+                    <button className="uncaught-button" onClick={() => project.restart(false)}>
+                      Bundle error&nbsp;
+                      <span className="codicon codicon-refresh" />
+                    </button>
+                  </div>
                 )}
               </div>
-            </Resizable>
-          )}
-          {!showDevicePreview && !hasBuildError && (
-            <Resizable {...resizableProps}>
-              <div className="phone-content">
-                <div className="phone-sized phone-content-loading-background" />
-                <div className="phone-sized phone-content-loading ">
-                  <PreviewLoader onRequestShowPreview={() => setShowPreviewRequested(true)} />
-                </div>
-                <DeviceFrame device={device} isFrameDisabled={isFrameDisabled} />
+              <DeviceFrame device={device} isFrameDisabled={isFrameDisabled} />
+              {inspectStackData && (
+                <InspectDataMenu
+                  inspectLocation={inspectStackData.requestLocation}
+                  inspectStack={inspectStackData.stack}
+                  onSelected={onInspectorItemSelected}
+                  onHover={(item) => {
+                    if (item.frame) {
+                      setInspectFrame(item.frame);
+                    }
+                  }}
+                  onCancel={() => resetInspector()}
+                />
+              )}
+            </div>
+          </Resizable>
+        )}
+        {!showDevicePreview && !hasBuildError && (
+          <Resizable {...resizableProps}>
+            <div className="phone-content">
+              <div className="phone-sized phone-content-loading-background" />
+              <div className="phone-sized phone-content-loading ">
+                <PreviewLoader onRequestShowPreview={() => setShowPreviewRequested(true)} />
               </div>
-            </Resizable>
-          )}
-          {hasBuildError && (
-            <Resizable {...resizableProps}>
-              <div className="phone-content">
-                <div className="phone-sized extension-error-screen" />
-                <DeviceFrame device={device} isFrameDisabled={isFrameDisabled} />
-              </div>
-            </Resizable>
-          )}
-        </div>
+              <DeviceFrame device={device} isFrameDisabled={isFrameDisabled} />
+            </div>
+          </Resizable>
+        )}
+        {hasBuildError && (
+          <Resizable {...resizableProps}>
+            <div className="phone-content">
+              <div className="phone-sized extension-error-screen" />
+              <DeviceFrame device={device} isFrameDisabled={isFrameDisabled} />
+            </div>
+          </Resizable>
+        )}
       </div>
       <ButtonGroupLeft>
         <ZoomControls
