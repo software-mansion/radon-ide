ea259491b40681d16a047aa792b715f0519786aa
Paweł Małecki <92953623+p-malecki@users.noreply.github.com>
2024-10-18 19:27:24 +0200

    Rename classes in css

--- packages/vscode-extension/src/webview/components/InspectDataMenu.tsx
+++ packages/vscode-extension/src/webview/components/InspectDataMenu.tsx
@@ -50,8 +50,8 @@ export function InspectDataMenu({
         }}>
         <DropdownMenu.Trigger />
         <DropdownMenu.Portal>
-          <DropdownMenu.Content className="context-menu-content">
-            <DropdownMenu.Label className="context-menu-label">
+          <DropdownMenu.Content className="dropdown-menu-content">
+            <DropdownMenu.Label className="dropdown-menu-label">
               {displayDimensionsText}
             </DropdownMenu.Label>
             {filteredData.map((item, index) => {
@@ -59,7 +59,7 @@ export function InspectDataMenu({
               const fileName = item.source.fileName.split("/").pop();
               return (
                 <DropdownMenu.Item
-                  className="context-menu-item"
+                  className="dropdown-menu-item"
                   key={index}
                   onSelect={() => onSelected(item)}
                   onMouseEnter={() => onHover(item)}>
