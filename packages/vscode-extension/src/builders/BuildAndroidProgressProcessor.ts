c57d1851aa49de0538b7d99aad2ae8465c6a489b
filip131311 <filip.kaminski@swmansion.com>
2024-10-18 10:57:00 +0200

    coments

--- packages/vscode-extension/src/builders/BuildAndroidProgressProcessor.ts
+++ packages/vscode-extension/src/builders/BuildAndroidProgressProcessor.ts
@@ -10,6 +10,8 @@ export class BuildAndroidProgressProcessor implements BuildProgressProcessor {
     if (!this.tasksToComplete) {
       return;
     }
+    // the 0.999 max value is here to prevent situations in which users see 100% indiction,
+    // but the build process is still takeing a couple more seconds.
     this.progressListener(Math.min(0.999, this.completedTasks / this.tasksToComplete));
   }
 
