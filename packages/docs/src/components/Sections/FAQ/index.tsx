2b84594b41eaad83503243132e3c6cd535777887
kacperkapusciak <kacper.kapusciak@swmansion.com>
2024-10-24 11:06:30 +0200

    Update legal & faq

--- packages/docs/src/components/Sections/FAQ/index.tsx
+++ packages/docs/src/components/Sections/FAQ/index.tsx
@@ -26,7 +26,7 @@ const faqs = [
   },
   {
     topic: "When are you planning to close the public Beta?",
-    answer: "We're aiming to close the beta in October 2024.",
+    answer: "We're aiming to close the beta in November 2024.",
   },
   {
     topic: "Why did you make the source code public?",
