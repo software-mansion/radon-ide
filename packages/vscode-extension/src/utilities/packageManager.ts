b2667c1c2344246f92fb674c30a5ed8104539af7
filip131311 <filip.kaminski@swmansion.com>
2024-11-16 21:19:11 +0000

    worrning pop up

--- packages/vscode-extension/src/utilities/packageManager.ts
+++ packages/vscode-extension/src/utilities/packageManager.ts
@@ -4,6 +4,7 @@ import { command } from "./subprocess";
 import { getAppRootFolder } from "./extensionContext";
 import { isWorkspaceRoot } from "./common";
 import { Logger } from "../Logger";
+import { window } from "vscode";
 
 export type PackageManagerInfo = {
   name: "npm" | "pnpm" | "yarn" | "bun";
@@ -61,7 +62,11 @@ export async function resolvePackageManager(): Promise<PackageManagerInfo | unde
 
     if (packageManagerCandidates.length > 1) {
       Logger.warn(
-        "Your workspace contains multiple package manager lock files, it might cause wrong manager to be used by the Radon IDE."
+        "Your workspace contains multiple package manager lock files, it might cause wrong manager to be used"
+      );
+      window.showWarningMessage(
+        "Your workspace contains multiple package manager lock files, it might cause wrong manager to be used.",
+        "dismiss"
       );
     }
 
