aee8ad62d6c32a7766b33fc9cdc4a8df3e48dd3a
kacperkapusciak <kacper.kapusciak@swmansion.com>
2024-11-20 09:22:34 +0100

    Add feature names on landing

--- packages/docs/src/components/Sections/Overview/OverviewItem/index.tsx
+++ packages/docs/src/components/Sections/Overview/OverviewItem/index.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import styles from "./styles.module.css";
 
 interface Props {
+  label: string;
   title: string;
   body: string;
   mediaSrc?: string;
 }
 
-const OverviewItem = ({ title, body, mediaSrc }: Props) => {
+const OverviewItem = ({ label, title, body, mediaSrc }: Props) => {
   return (
     <>
       <section className={styles.description}>
+        <p className={styles.itemLabel}>{label}</p>
         <h2 className={styles.itemTitle}>{title}</h2>
         <p className={styles.itemBody}>{body}</p>
       </section>
