software-mansion/radon-ide

commit 11206ca075c52baafc911aaac0e4b0af00bf430c

Fix inspect stack not returning all ancestors (#693)
## Changes

Fixes #692 

This PR is also necessary for #666 

This PR aims to fix the issue with inspector stack that not all
ancestors of inspected elements are discovered. It turns out that the
hierarchy returned by `getInspectorData()` is already missing these
ancestors, so we have to traverse the component tree ourselves.

The first node is the `closestInstance` in object returned by
`getInspectorDataViewAtPoint`. To access parent node of a node, we look
at `node.return`. This way we can simply traverse the whole path from
first node up to the root. Along the way, we collect all necessary
information and create a stack that is returned at the end. We use
`getInspectorDataForInstance(node)` renderer function to obtain
inspector data for given fiber node.

This version is a lot more accurate, from my testing it returns all the
most used visible components (like `<View>`, `<Text>`, `<Button>`),
which is already an improvement compared to the current solution.

## Demos

The following are demos how the inspect stack looks like before and
after changes from this PR. The component hierarchy of the app consists
of several nested components, which have border for visualization.

#### Before


https://github.com/user-attachments/assets/39501ea3-9c95-46bb-9a06-9b1db9c6123f

#### After


https://github.com/user-attachments/assets/414f43f8-62a6-429f-a25e-6ab374dfcab9

### How Has This Been Tested: 

**Test 1:**
- Open shared app 
- Expand step
- Inspect (right-click) text in step
- Make sure all components are in the trace (for example ScrollView)

<img width="425" alt="step"
src="https://github.com/user-attachments/assets/301fca24-dca9-4c76-a3f5-a869dec0b241">


**Test 2:**

- Open shared app 
- Add after last `Step`:
```
<View>
  <View>
    <View>
      <Text>Nested texts</Text>
    </View>
  </View>
</View>
```
- Inspect (right click) `Nested texts`

packages/vscode-extension/lib/wrapper.js
@@ -4,13 +4,16 @@ const { useContext, useState, useEffect, useRef, useCallback } = require("react"
 const {
   LogBox,
   AppRegistry,
+  Dimensions,
   RootTagContext,
   View,
-  Dimensions,
   Linking,
   findNodeHandle,
 } = require("react-native");
 const { storybookPreview } = require("./storybook_helper");
+
+// https://github.com/facebook/react/blob/c3570b158d087eb4e3ee5748c4bd9360045c8a26/packages/react-reconciler/src/ReactWorkTags.js#L62
+const OffscreenComponentReactTag = 22;
 
 const navigationPlugins = [];
 export function registerNavigationPlugin(name, plugin) {
@@ -67,6 +70,113 @@ function useAgentListener(agent, eventName, listener, deps = []) {
       };
     }
   }, [agent, ...deps]);
+}
+
+function getRendererConfig() {
+  const renderers = Array.from(window.__REACT_DEVTOOLS_GLOBAL_HOOK__?.renderers?.values());
+  if (!renderers) {
+    return undefined;
+  }
+  for (const renderer of renderers) {
+    if (renderer.rendererConfig?.getInspectorDataForInstance) {
+      return renderer.rendererConfig;
+    }
+  }
+  return undefined;
+}
+
+/**
+ * Return an array of component data representing a stack of components by traversing
+ * the component hierarchy up from the startNode.
+ * Each stack entry carries the component name, source location and measure function.
+ */
+function extractComponentStack(startNode) {
+  const rendererConfig = getRendererConfig();
+
+  if (!rendererConfig) {
+    console.warn("Unable to find functional renderer config.");
+    return [];
+  }
+
+  const componentStack = [];
+  let node = startNode;
+
+  // Optimization: we break after reaching fiber node corresponding to OffscreenComponent
+  while (node && node.tag !== OffscreenComponentReactTag) {
+    const data = rendererConfig.getInspectorDataForInstance(node);
+
+    const item = data.hierarchy[data.hierarchy.length - 1];
+    const inspectorData = item.getInspectorData(findNodeHandle);
+    if (inspectorData.source) {
+      componentStack.push({
+        measure: inspectorData.measure,
+        name: item.name,
+        source: inspectorData.source,
+      });
+    }
+
+    node = node.return;
+  }
+  return componentStack;
+}
+
+function getInspectorDataForCoordinates(mainContainerRef, x, y, requestStack, callback) {
+  const { width: screenWidth, height: screenHeight } = Dimensions.get("screen");
+
+  RNInternals.getInspectorDataForViewAtPoint(
+    mainContainerRef.current,
+    x * screenWidth,
+    y * screenHeight,
+    (viewData) => {
+      const frame = viewData.frame;
+      const scaledFrame = {
+        x: frame.left / screenWidth,
+        y: frame.top / screenHeight,
+        width: frame.width / screenWidth,
+        height: frame.height / screenHeight,
+      };
+
+      if (!requestStack) {
+        callback({ frame: scaledFrame });
+        return;
+      }
+
+      const inspectorDataStack = extractComponentStack(viewData.closestInstance);
+      Promise.all(
+        inspectorDataStack.map(
+          (inspectorData) =>
+            new Promise((res, rej) => {
+              try {
+                inspectorData.measure((_x, _y, viewWidth, viewHeight, pageX, pageY) => {
+                  const source = inspectorData.source;
+                  res({
+                    componentName: inspectorData.name,
+                    source: {
+                      fileName: source.fileName,
+                      line0Based: source.lineNumber - 1,
+                      column0Based: source.columnNumber - 1,
+                    },
+                    frame: {
+                      x: pageX / screenWidth,
+                      y: pageY / screenHeight,
+                      width: viewWidth / screenWidth,
+                      height: viewHeight / screenHeight,
+                    },
+                  });
+                });
+              } catch (e) {
+                rej(e);
+              }
+            })
+        )
+      ).then((componentDataStack) => {
+        callback({
+          frame: scaledFrame,
+          stack: componentDataStack,
+        });
+      });
+    }
+  );
 }
 
 export function AppWrapper({ children, initialProps, fabric }) {
@@ -180,68 +290,14 @@ export function AppWrapper({ children, initialProps, fabric }) {
     devtoolsAgent,
     "RNIDE_inspect",
     (payload) => {
-      const getInspectorDataForViewAtPoint = RNInternals.getInspectorDataForViewAtPoint;
-      const { width, height } = Dimensions.get("screen");
+      const { id, x, y, requestStack } = payload;
 
-      getInspectorDataForViewAtPoint(
-        mainContainerRef.current,
-        payload.x * width,
-        payload.y * height,
-        (viewData) => {
-          const frame = viewData.frame;
-          const scaledFrame = {
-            x: frame.left / width,
-            y: frame.top / height,
-            width: frame.width / width,
-            height: frame.height / height,
-          };
-          let stackPromise = Promise.resolve(undefined);
-          if (payload.requestStack) {
-            stackPromise = Promise.all(
-              viewData.hierarchy.reverse().map((item) => {
-                const inspectorData = item.getInspectorData((arg) => findNodeHandle(arg));
-                const framePromise = new Promise((resolve, reject) => {
-                  try {
-                    inspectorData.measure((_x, _y, viewWidth, viewHeight, pageX, pageY) => {
-                      resolve({
-                        x: pageX / width,
-                        y: pageY / height,
-                        width: viewWidth / width,
-                        height: viewHeight / height,
-                      });
-                    });
-                  } catch (e) {
-                    reject(e);
-                  }
-                });
-
-                return framePromise
-                  .catch(() => undefined)
-                  .then((frame) => {
-                    return inspectorData.source
-                      ? {
-                          componentName: item.name,
-                          source: {
-                            fileName: inspectorData.source.fileName,
-                            line0Based: inspectorData.source.lineNumber - 1,
-                            column0Based: inspectorData.source.columnNumber - 1,
-                          },
-                          frame,
-                        }
-                      : undefined;
-                  });
-              })
-            ).then((stack) => stack?.filter(Boolean));
-          }
-          stackPromise.then((stack) => {
-            devtoolsAgent._bridge.send("RNIDE_inspectData", {
-              id: payload.id,
-              frame: scaledFrame,
-              stack: stack,
-            });
-          });
-        }
-      );
+      getInspectorDataForCoordinates(mainContainerRef, x, y, requestStack, (inspectorData) => {
+        devtoolsAgent._bridge.send("RNIDE_inspectData", {
+          id,
+          ...inspectorData,
+        });
+      });
     },
     [mainContainerRef]
   );

commit 3b3b1a213e68493d807d4de00cb4b5ce918d90aa

Fix inspector on RN 73 after #693 (#765)
This PR fixes a regression in element inspector on RN 73 introduced in
#693

Because of the new approach of traversing component hierarchy added
there, we broke compatibility with older versions of react native where
renderer didn't have `getInspectorDataForInstance` method.

The fix is to restore the old behavior as a fallback and rely on
component hierarchy returned by `getInspectorDataForViewAtPoint` when
this renderer method isn't available.

### How Has This Been Tested: 
1. Open inspector hierarchy on expo-go app
2. Make sure inspector works on RN 75 and RN 76

---------

Co-authored-by: Maciej Stosio <maciekstosio@users.noreply.github.com>

packages/vscode-extension/lib/wrapper.js
@@ -89,34 +89,42 @@ function getRendererConfig() {
  * Return an array of component data representing a stack of components by traversing
  * the component hierarchy up from the startNode.
  * Each stack entry carries the component name, source location and measure function.
+ * We try to use React's renderer getInspectorDataForInstance to get the details about
+ * each particular component instance. However, with older versions of React Native where
+ * this method is not available, we fallback to using hierarchy provided by getInspectorDataForViewAtPoint
  */
-function extractComponentStack(startNode) {
+function extractComponentStack(startNode, viewDataHierarchy) {
   const rendererConfig = getRendererConfig();
 
-  if (!rendererConfig) {
-    console.warn("Unable to find functional renderer config.");
-    return [];
+  let stackItems = [];
+  if (rendererConfig) {
+    // when we find renderer config with getInspectorDataForInstance we use fiber node
+    // "return" property to traverse the component hierarchy
+    let node = startNode;
+
+    // Optimization: we break after reaching fiber node corresponding to OffscreenComponent
+    while (node && node.tag !== OffscreenComponentReactTag) {
+      const data = rendererConfig.getInspectorDataForInstance(node);
+      const item = data.hierarchy[data.hierarchy.length - 1];
+      stackItems.push(item);
+      node = node.return;
+    }
+  } else if (viewDataHierarchy && viewDataHierarchy.length > 0) {
+    // fallback to using viewDataHierarchy
+    stackItems = viewDataHierarchy.reverse();
   }
 
   const componentStack = [];
-  let node = startNode;
-
-  // Optimization: we break after reaching fiber node corresponding to OffscreenComponent
-  while (node && node.tag !== OffscreenComponentReactTag) {
-    const data = rendererConfig.getInspectorDataForInstance(node);
-
-    const item = data.hierarchy[data.hierarchy.length - 1];
+  stackItems.forEach((item) => {
     const inspectorData = item.getInspectorData(findNodeHandle);
     if (inspectorData.source) {
       componentStack.push({
-        measure: inspectorData.measure,
         name: item.name,
         source: inspectorData.source,
+        measure: inspectorData.measure,
       });
     }
-
-    node = node.return;
-  }
+  });
   return componentStack;
 }
 
@@ -141,7 +149,10 @@ function getInspectorDataForCoordinates(mainContainerRef, x, y, requestStack, ca
         return;
       }
 
-      const inspectorDataStack = extractComponentStack(viewData.closestInstance);
+      const inspectorDataStack = extractComponentStack(
+        viewData.closestInstance,
+        viewData.hierarchy
+      );
       Promise.all(
         inspectorDataStack.map(
           (inspectorData) =>