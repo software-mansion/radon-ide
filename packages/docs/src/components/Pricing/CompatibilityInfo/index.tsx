9a0a461870686cdc3b9fbb434837495f3044d559
kacperkapusciak <kacper.kapusciak@swmansion.com>
2024-11-30 12:18:16 +0100

    Implement paddle checkout

--- packages/docs/src/components/Pricing/CompatibilityInfo/index.tsx
+++ packages/docs/src/components/Pricing/CompatibilityInfo/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./styles.module.css";
-import HomepageButton from "../../HomepageButton";
+import HomepageButton, { BorderStyling, ButtonStyling } from "../../HomepageButton";
 
 export function CompatibilityInfo() {
   return (
@@ -9,7 +9,12 @@ export function CompatibilityInfo() {
         Before making a purchase make sure to consult the compatibility page to see whether your
         project is officially supported by Radon IDE.
       </p>
-      <HomepageButton href="/docs/getting-started/compatibility" title="See&nbsp;compatibility" />
+      <HomepageButton
+        href="/docs/getting-started/compatibility"
+        title="See&nbsp;compatibility"
+        backgroundStyling={ButtonStyling.TO_WHITE}
+        borderStyling={BorderStyling.NAVY}
+      />
     </div>
   );
 }
--- packages/docs/src/components/Pricing/PricingPlansList/index.tsx
+++ packages/docs/src/components/Pricing/PricingPlansList/index.tsx
@@ -1,37 +1,69 @@
 import React, { useState } from "react";
 import styles from "./styles.module.css";
+import usePaddle from "@site/src/hooks/usePaddle";
 import pricing from "../PricingCard/pricing.module.css";
 import Button from "../../Button";
 import PricingCard from "../PricingCard";
-import useBaseUrl from "@docusaurus/useBaseUrl";
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const INDIVIDUAL_MONTHLY_PRICE_ID = isProduction
+  ? "pri_01hx944ht3wnpvgktatj6v5k4b"
+  : "pri_01j0tjqzqhv6vezhf14pwtxfm0";
+const INDIVIDUAL_YEARLY_PRICE_ID = isProduction
+  ? "pri_01hzf02s579nwrwb756enh8r7g"
+  : "pri_01jb1ajv7btj3cbnshrdq4ncjf";
+const BUSINESS_MONTHLY_PRICE_ID = isProduction
+  ? "pri_01jdyc0j8wkfqx3a7nbf6tsaxy"
+  : "pri_01jdyap7jcydxvewmek2r0e35q";
+const BUSINESS_YEARLY_PRICE_ID = isProduction
+  ? "pri_01jdyc1z1nh3pgp01ya4h8g075"
+  : "pri_01jdyaqnwf3w4pm6hsgwehm1by";
 
 const PricingPlansList = () => {
+  const paddle = usePaddle();
   const [isMonthly, setIsMonthly] = useState(true);
 
-  const individualMonthly = (
+  const individual = isMonthly ? (
     <>
       $19 <span className={pricing.plan__currency}>USD</span>
       <p className={pricing.plan__price_second_line}> per seat/month excl. VAT </p>
     </>
-  );
-  const individualAnnually = (
+  ) : (
     <>
       $190 <span className={pricing.plan__currency}>USD</span>
       <p className={pricing.plan__price_second_line}> per seat/year excl. VAT </p>
     </>
   );
-  const businessMonthly = (
+  const business = isMonthly ? (
     <>
       $29 <span className={pricing.plan__currency}>USD</span>
       <p className={pricing.plan__price_second_line}> per seat/month excl. VAT </p>
     </>
-  );
-  const businessAnnually = (
+  ) : (
     <>
       $290 <span className={pricing.plan__currency}>USD</span>
       <p className={pricing.plan__price_second_line}> per seat/year excl. VAT </p>
     </>
   );
+
+  const openIndividualCheckout = () => {
+    paddle?.Checkout.open({
+      items: [
+        {
+          priceId: isMonthly ? INDIVIDUAL_MONTHLY_PRICE_ID : INDIVIDUAL_YEARLY_PRICE_ID,
+          quantity: 1,
+        },
+      ],
+    });
+  };
+  const openBusinessCheckout = () => {
+    paddle?.Checkout.open({
+      items: [
+        { priceId: isMonthly ? BUSINESS_MONTHLY_PRICE_ID : BUSINESS_YEARLY_PRICE_ID, quantity: 1 },
+      ],
+    });
+  };
   return (
     <>
       <div className={styles.plan_pay_annually}>
@@ -53,27 +85,21 @@ const PricingPlansList = () => {
       <ul className={styles.list}>
         <PricingCard>
           <h2 className={pricing.plan__name}>Radon IDE Individual</h2>
-          <h3 className={pricing.plan__price}>
-            {isMonthly ? individualMonthly : individualAnnually}
-          </h3>
+          <h3 className={pricing.plan__price}>{individual}</h3>
           <p className={pricing.plan__tagline}>
             For individual developers and freelancers craving more enjoyable coding sessions.
           </p>
           <div className={pricing.plan__spacer} />
-          <Button href="/" disabled>
-            Available soon
-          </Button>
+          <Button onClick={openIndividualCheckout}>Buy Individual</Button>
         </PricingCard>
         <PricingCard>
           <h2 className={pricing.plan__name}>Radon IDE Business</h2>
-          <h3 className={pricing.plan__price}>{isMonthly ? businessMonthly : businessAnnually}</h3>
+          <h3 className={pricing.plan__price}>{business}</h3>
           <p className={pricing.plan__tagline}>
             For companies seeking to drastically improve their developer experience.
           </p>
           <div className={pricing.plan__spacer} />
-          <Button href="https://swmansion.com/contact/projects" disabled>
-            Available soon
-          </Button>
+          <Button onClick={openBusinessCheckout}>Buy Business</Button>
         </PricingCard>
       </ul>
     </>
