software-mansion/radon-ide

commit 8dab1d01b22682b184aa268881525b33f7726dc6

Rerender preview if its function changes (#726)
This PR fixes an issue when changes made to `preview` component directly
wouldn't trigger UI updates.

The reason why this wasn't happening is due to the limitation of
fast-refresh, which, when happens, replaces the implementation of the
previous component. As a consequence, the root Preview component doesn't
see the previewed one got updated.

In this PR we're adding `renderTrigger` callback that the root Preview
component uses to register for updates in the preview map. When
registering the preview, we check if a one already exists, and trigger
update for it.

Caveat: note that with this approach the root component that is used by
the preview won't persist state. I don't see how can we workaround this,
but it doesn't seem too limiting at this point.

Fixes #725

### How Has This Been Tested: 
1. Run expo-router example, use preview from UglyButton.tsx
2. Change color prop passed to UglyButton directly – before this change
the component wouldn't update.

packages/vscode-extension/lib/preview.js
@@ -1,4 +1,5 @@
-const { AppRegistry, SafeAreaView } = require("react-native");
+const { useEffect, useState } = require("react");
+const { AppRegistry, SafeAreaView, View } = require("react-native");
 
 export const PREVIEW_APP_KEY = "RNIDE_preview";
 
@@ -9,6 +10,18 @@ export function Preview({ previewKey }) {
   if (!previewData || !previewData.component) {
     return null;
   }
+
+  // only needed to force re-render when new preview is registered
+  const [_, setDummyState] = useState(0);
+  useEffect(() => {
+    previewData.renderTrigger = () => {
+      setDummyState((s) => s + 1);
+    };
+    return () => {
+      previewData.renderTrigger = null;
+    };
+  }, [previewData]);
+
   return (
     <SafeAreaView style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       {previewData.component}
@@ -38,10 +51,18 @@ export function preview(component) {
   }
 
   const key = `preview:/${component._source.fileName}:${component._source.lineNumber}`;
+
+  const lastPreview = global.__RNIDE_previews.get(key);
+
   global.__RNIDE_previews.set(key, {
     component,
     name: getComponentName(component),
   });
+
+  // send update request to the last preview instance if it existed
+  if (lastPreview && lastPreview.renderTrigger) {
+    lastPreview.renderTrigger();
+  }
 }
 
 AppRegistry.registerComponent(PREVIEW_APP_KEY, () => Preview);

commit 990e515f16b1eea5a2b20c788531fe91469c8336

Fix fast refresh for previews after #726 (#805)
This PR fixes fast refresh of previews after a regression introduced in
#726

The issue that started occuring in #726 was that previews after getting
fast refresh would sometimes fail to render. Here's an example of
problematic component:
https://github.com/software-mansion-labs/radon-ide-demo/blob/main/src/components/Message.tsx#L21
– when you update emoji list in that component, the render would throw
exception when attempting to access styles.

The reason for the above exception was that in #726 we started
triggering state updates. However, the state update would be triggered
at the point when preview method is called which is before the whole
file is executed. As a consequence, we'd still try to render the old
version of component that would trigger for hoisted variables – in this
instance it'd be because of styles that are defined after component is
defined.

The fix is to allow the fast refresh to finish processing updates. The
only thing that's necessary is to trigger state update in the next cycle
of JS loop. In this PR we're adding `setTimeout(fun, 0)` to allow for
the state update to run in the following loop cycle as opposed to
immediately when preview method is called.

Note that it is safe not to read lastPreview in the method that is move
to timeout. This is ok, because the last preview may only update as a
result of the Preview container being rendered. So if the Preview
component gets rendered, there's no reason for us to also trigger that
re-render.

### How Has This Been Tested: 
1. Open
https://github.com/software-mansion-labs/radon-ide-demo/blob/main/src/components/Message.tsx#L21
and modify list of emojis – before it'd throw exception upon saving, now
the preview updates properly
2. Test the scenario from #726 to make sure the updates attributes are
still processed properly after this change: one way is to add prop to

packages/vscode-extension/lib/preview.js
@@ -61,7 +61,7 @@ export function preview(component) {
 
   // send update request to the last preview instance if it existed
   if (lastPreview && lastPreview.renderTrigger) {
-    lastPreview.renderTrigger();
+    setTimeout(lastPreview.renderTrigger, 0);
   }
 }
 