41ec7d5b74080cbf01e5ab1fab7e6f9fcdd2eb4f
Krzysztof Magiera <krzys@swmansion.com>
2024-12-02 14:40:43 +0100

    Update docusaurus.config.js

--- packages/docs/docusaurus.config.js
+++ packages/docs/docusaurus.config.js
@@ -61,7 +61,7 @@ const config = {
             position: "right",
           },
           {
-            to: "/docs/category/getting-started",
+            to: "docs/category/getting-started",
             activeBasePath: "docs",
             label: "Docs",
             position: "right",
