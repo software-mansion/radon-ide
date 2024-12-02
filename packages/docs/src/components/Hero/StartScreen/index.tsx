9fc132cb8be31219bd3086c860abd1b01bae29ea
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2024-12-02 11:18:31 +0100

    Update landing (#744)
    Updated copy on landing and added eye-catchy animations
    
    
    https://github.com/user-attachments/assets/8f7926dd-f085-4a67-8d82-09b20f20a79d

--- packages/docs/src/components/Hero/StartScreen/index.tsx
+++ packages/docs/src/components/Hero/StartScreen/index.tsx
@@ -3,47 +3,76 @@ import styles from "./styles.module.css";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 import HomepageButton from "@site/src/components/HomepageButton";
 import InfoIcon from "@site/static/img/info-circle.svg";
+import { motion } from "motion/react";
 
 const StartScreen = () => {
   return (
     <section className={styles.hero}>
-      <div className={styles.heroImageContainer}>
+      <motion.div
+        className={styles.heroImageContainer}
+        initial={{ x: 16 }}
+        animate={{ x: 0 }}
+        transition={{ duration: 0.5 }}>
         <div className={styles.heroImageWrapper}>
           <img className={styles.heroImage} src={useBaseUrl("/img/hero.png")} draggable={false} />
         </div>
-      </div>
+      </motion.div>
       <div className={styles.heading}>
-        <div>
-          <h1 className={styles.headingLabel}>
-            <span>Radon</span>
-            <span>IDE</span>
-            <span className={styles.betaChip}>BETA</span>
-          </h1>
-          <div className={styles.poweredBy}>
-            <p>by</p>
-            <a href="https://swmansion.com" target="_blank">
-              <img
-                src={useBaseUrl("/img/swm-logo.svg")}
-                alt="Software Mansion"
-                className={styles.swmLogo}
-              />
-            </a>
-          </div>
-          <h2 className={styles.subheadingLabel}>
-            A better developer experience for React Native developers.
-          </h2>
-        </div>
-        <div className={styles.buttonContainer}>
-          <HomepageButton
-            target="_blank"
-            href="https://marketplace.visualstudio.com/items?itemName=swmansion.react-native-ide"
-            title="Download from VSCode marketplace"
+        <motion.div
+          className={styles.poweredBy}
+          initial={{ opacity: 0, y: 8 }}
+          animate={{ opacity: 1, y: 0 }}>
+          <img src={useBaseUrl("/img/logo.svg")} alt="Radon IDE logo" className={styles.logo} />
+          <p>by</p>
+          <a href="https://swmansion.com" target="_blank" className={styles.swmLogoWrapper}>
+            <img
+              src={useBaseUrl("/img/swm-logo.svg")}
+              alt="Software Mansion"
+              className={styles.swmLogo}
+            />
+          </a>
+        </motion.div>
+        <motion.h1
+          className={styles.headingLabel}
+          initial={{ opacity: 0, y: 8 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.1 }}>
+          An <span>IDE</span> for&nbsp;React Native
+          <motion.div
+            initial={{ x: 0, opacity: 0 }}
+            animate={{ opacity: [0, 1, 1, 1, 0], x: 500 }}
+            transition={{ delay: 0.8, duration: 0.8 }}
+            className={styles.headingSwoosh}
           />
+        </motion.h1>
+        <motion.h2
+          className={styles.subheadingLabel}
+          initial={{ opacity: 0, y: 8 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.2 }}>
+          Radon IDE is VSCode extension that turns your editor into a fully fledged IDE for React
+          Native and Expo.
+        </motion.h2>
+        <div className={styles.buttonContainer}>
+          <motion.div
+            initial={{ opacity: 0, y: 8 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.3 }}>
+            <HomepageButton
+              target="_blank"
+              href="https://marketplace.visualstudio.com/items?itemName=swmansion.react-native-ide"
+              title="Download from VSCode marketplace"
+            />
+          </motion.div>
         </div>
-        <div className={styles.headingDisclaimer}>
+        <motion.div
+          className={styles.headingDisclaimer}
+          initial={{ opacity: 0, y: 8 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.4 }}>
           <InfoIcon className={styles.headingDisclaimerIcon} />
           Works with VSCode 1.86+ and Cursor 0.32 on macOS and Windows
-        </div>
+        </motion.div>
       </div>
     </section>
   );
--- packages/docs/src/components/Sections/Overview/index.tsx
+++ packages/docs/src/components/Sections/Overview/index.tsx
@@ -5,41 +5,49 @@ import OverviewItem from "@site/src/components/Sections/Overview/OverviewItem";
 
 const items = [
   {
+    label: "An IDE",
     title: "Integrated experience",
     body: "See the preview of your application right where you need it the most – close to your codebase. \n\nRadon IDE runs IOS Simulator and Android emulator directly in your Visual Studio Code and Cursor project.",
     mediaSrc: "video/1_sztudio_integrated.mp4",
   },
   {
+    label: "Element Inspector",
     title: "Click to inspect",
     body: "Using the built-in inspector you can jump directly from preview to a file where your component is defined. It can't really get simpler than that.",
     mediaSrc: "video/2_sztudio_inspect.mp4",
   },
   {
+    label: "Debugger",
     title: "Use breakpoints right in VSCode",
     body: "Without any additional setup the extension allows to add a breakpoints in Visual Studio Code to debug your React Native application. It. Just. Works.\n\nIDE also automatically stops at runtime exceptions showing you the exact line of code where they happened.",
     mediaSrc: "video/3_sztudio_debugger.mp4",
   },
   {
+    label: "Router integration",
     title: "Navigation made easier",
     body: "The Radon IDE integrates tightly with your deep-linked application allowing you to jump around the navigation structure (supports both React Navigation and Expo Router projects).",
     mediaSrc: "video/4_sztudio_url_bar.mp4",
   },
   {
+    label: "Logs",
     title: "Search through the logs easily",
     body: "Radon IDE uses the built-in VSCode console allowing you to filter through the logs.\n\nThe links displayed in the console are automatically linking back to your source code.",
     mediaSrc: "video/5_sztudio_logs_panel.mp4",
   },
   {
+    label: "Previews",
     title: "Develop components in isolation",
     body: "Radon IDE comes with a package allowing to preview components in full isolation.\n\nDevelop your components individually without distractions.",
     mediaSrc: "video/6_sztudio_preview.mp4",
   },
   {
+    label: "Device settings",
     title: "Adjust device settings on the fly",
     body: "You can adjust text size and light/dark mode right from the Radon IDE.\n\nFocus just on your app without switching between windows.",
     mediaSrc: "video/7_sztudio_device_settings.mp4",
   },
   {
+    label: "Screen recording",
     title: "Instant replays",
     body: "Missed a bug? At any time you can re-watch what just happened on the device.\n\nNo need to manually start the recording ever again.",
     mediaSrc: "video/ide_screen_recording.mp4",
@@ -58,7 +66,12 @@ const Overview = () => {
         <div className={styles.overviewItemsContainer}>
           {items.map((item, idx) => (
             <div key={idx} className={styles.item}>
-              <OverviewItem title={item.title} body={item.body} mediaSrc={item.mediaSrc} />
+              <OverviewItem
+                label={item.label}
+                title={item.title}
+                body={item.body}
+                mediaSrc={item.mediaSrc}
+              />
             </div>
           ))}
         </div>
