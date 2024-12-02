befa3a4ff319593d4a7f3f8e1be52856f680d9a9
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2024-12-02 10:58:43 +0100

    Update pricing page (#799)
    https://github.com/user-attachments/assets/aeb95f4f-b95b-4d9c-9057-ef3867cacea3

--- packages/docs/src/components/Pricing/index.tsx
+++ packages/docs/src/components/Pricing/index.tsx
@@ -3,44 +3,27 @@ import styles from "./styles.module.css";
 import PricingPlansList from "./PricingPlansList";
 import FAQ from "../Sections/FAQ";
 import Motivation from "../Motivation";
-import SupporterPlan from "./SupporterPlan";
 import { CompatibilityInfo } from "./CompatibilityInfo";
 
 const Pricing = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.headingLabel}>Pricing</h1>
-      <p>
-        Radon IDE is currently in Beta. Using{" "}
-        <span className={styles.highlight}>
-          the Beta version doesn't require purchasing any license
-        </span>{" "}
-        and it's free for everyone. We expect the Beta period to <b>end in November 2024</b> after
-        which we will require an Individual or Team license to use the tool. However, if you'd like
-        to support the development and having more input into the development process (request
-        specific features or prioritize fixes for specific issues), below we offer a Supporter's
-        license:
-      </p>
-      <div className={styles.wrapper}>
-        <SupporterPlan />
-      </div>
-      <p>
-        *The discount for Individual or Team license is valid for the same duration as your
-        Supporter's license. For every month your Supporter's license is active, you receive the 50%
-        discount on the Individual or Team license.
-      </p>
-      <p className={styles.disclaimer}>
-        Disclaimer: The following pricing plans will be introduced after the Beta period ends. They
-        are for informational purposes and may change in the future.
-      </p>
+      <h1 className={styles.headingLabel}>Get the best developer experience in React Native.</h1>
+      <h3 className={styles.subheadlingLabel}>
+        Choose a plan that works for your needs. Pay monthly for flexibility or yearly for the best
+        price.
+      </h3>
       <div className={styles.wrapper}>
         <PricingPlansList />
       </div>
-      <p>
-        If you want to help the development of Radon IDE, affect the prioritization of features and
-        get better support, you can buy our Supporter's License.
-      </p>
       <CompatibilityInfo />
+      <p>
+        Windows users can use beta version of Radon IDE for Windows which is available under{" "}
+        <a href="https://ide.swmansion.com/legal" target="_blank" className={styles.highlight}>
+          free beta license
+        </a>
+        .
+      </p>
       <Motivation />
       <FAQ />
     </div>
