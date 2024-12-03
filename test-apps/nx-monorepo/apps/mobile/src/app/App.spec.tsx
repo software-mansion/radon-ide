415f88b77f58abba14b256f4a33ddd3859a7f0e3
Krzysztof Magiera <krzys@swmansion.com>
2024-12-03 11:40:15 +0100

    Move test-apps to separate repo: software-mansion-labs/radon-ide-test-apps (#815)
    This PR moves test-apps folder to a separate repo.
    
    There are no clear benefits of having it in this repo any longer,
    especially that the list of test-apps grew over time which just causes
    git to be sloer when working with Radon IDE repo.
    
    I moved the folder keeping the changes history to new repo under
    https://github.com/software-mansion-labs/radon-ide-test-apps
    
    PRs that require new test apps, should first land in that repo and then
    the app can be referenced in the PR here.
    
    This change will also require some adjustments in development docs, but
    I'll submit it as separate PR because that change would got lost under
    so many files being deleted here.

--- test-apps/nx-monorepo/apps/mobile/src/app/App.spec.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react-native';
-
-import App from './App';
-
-test('renders correctly', () => {
-  const { getByTestId } = render(<App />);
-  expect(getByTestId('heading')).toHaveTextContent('Welcome');
-});
