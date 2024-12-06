ef707be59cf57ba95f794c7d97de2f64690a83e8
kacperkapusciak <kacper.kapusciak@swmansion.com>
2024-12-06 15:01:00 +0100

    view demo button

---
+++ packages/docs/src/components/CloseIcon/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+export default function CloseIcon(props) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width={28} height={28} fill="none" {...props}>
+      <path
+        stroke="#001A72"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={1.5}
+        d="m5.833 5.833 16.333 16.334m-16.333 0L22.166 5.833"
+      />
+    </svg>
+  );
+}
