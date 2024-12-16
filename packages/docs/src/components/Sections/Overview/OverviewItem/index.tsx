7d0607f0881a589fe83ac63253dc184236e52e6d
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2024-12-16 15:57:58 +0100

    Improve landing performance (#858)
    This PR:
    - makes sure to only use one H1 tag on the page
    - shrinks the hero image from `371 KB` to `77.8 KB` and sets the `width`
    and `height` attributes to prevent layout shift
    - compresses videos used on landing by `50%`
    - lazy loads videos using intersection observer – videos will only be
    loaded when in view
    - during video loading a lightweight placeholder is shown to prevent
    layout shifts
    - this is important because we used to load 10MB of video on page load
    even for users which never scrolled down the page
      
     See before:
    <img width="387" alt="image"
    src="https://github.com/user-attachments/assets/34fad20d-6cdd-452f-9dbf-78a3e354867f"
    />

--- packages/docs/src/components/Sections/Overview/OverviewItem/index.tsx
+++ packages/docs/src/components/Sections/Overview/OverviewItem/index.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import styles from "./styles.module.css";
 import { track } from "@vercel/analytics";
 import LinkButton from "@site/src/components/LinkButton";
+import { InView } from "react-intersection-observer";
 
 interface Props {
   label: string;
   title: string;
   body: string;
   mediaSrc?: string;
+  placeholderSrc?: string;
 }
 
-const OverviewItem = ({ label, title, body, mediaSrc }: Props) => {
+const OverviewItem = ({ label, title, body, mediaSrc, placeholderSrc }: Props) => {
   const handleButtonClick = () => {
     track("Overview CTA", { section: label });
   };
@@ -18,7 +20,7 @@ const OverviewItem = ({ label, title, body, mediaSrc }: Props) => {
     <>
       <section className={styles.description}>
         <p className={styles.itemLabel}>{label}</p>
-        <h2 className={styles.itemTitle}>{title}</h2>
+        <h3 className={styles.itemTitle}>{title}</h3>
         <p className={styles.itemBody}>{body}</p>
         <LinkButton
           title="Get started for free"
@@ -26,11 +28,19 @@ const OverviewItem = ({ label, title, body, mediaSrc }: Props) => {
           onClick={handleButtonClick}
         />
       </section>
-      <div className={styles.media}>
-        <video autoPlay loop muted playsInline>
-          <source src={mediaSrc} type="video/mp4" />
-        </video>
-      </div>
+      <InView triggerOnce>
+        {({ inView, ref }) => (
+          <div className={styles.media} ref={ref}>
+            {inView ? (
+              <video autoPlay loop muted playsInline poster={placeholderSrc}>
+                <source src={mediaSrc} type="video/mp4" />
+              </video>
+            ) : (
+              <img src={placeholderSrc} alt={title} fetchPriority="high" />
+            )}
+          </div>
+        )}
+      </InView>
     </>
   );
 };
