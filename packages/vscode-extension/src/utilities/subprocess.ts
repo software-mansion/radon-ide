7de7a78c318da82da5965b63449c8379ed6119fd
Maciej Stosio <maciekstosio@users.noreply.github.com>
2025-01-14 11:19:46 +0100

    Fix lint

--- packages/vscode-extension/src/utilities/subprocess.ts
+++ packages/vscode-extension/src/utilities/subprocess.ts
@@ -18,7 +18,11 @@ async function getPathEnv(appRoot: string) {
 
   // Fish, bash, and zsh all support -i and -c flags.
   const shellPath = process.env.SHELL ?? "/bin/zsh";
-  const { stdout: path } = await execa(shellPath, ["-i", "-c", `cd "${appRoot}" && echo "${PRE_PATH_TOKEN}$PATH${PRE_PATH_TOKEN}"`]);
+  const { stdout: path } = await execa(shellPath, [
+    "-i",
+    "-c",
+    `cd "${appRoot}" && echo "${PRE_PATH_TOKEN}$PATH${PRE_PATH_TOKEN}"`,
+  ]);
   return path.split(PRE_PATH_TOKEN)[1].trim();
 }
 
