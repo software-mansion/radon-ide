f1f35bff3f8815490c1fd1f2aaafd7edd0b194d4
Maciej Stosio <maciekstosio@users.noreply.github.com>
2025-02-07 13:29:32 +0100

    Revert previous changes

--- packages/vscode-extension/src/webview/providers/WorkspaceConfigProvider.tsx
+++ packages/vscode-extension/src/webview/providers/WorkspaceConfigProvider.tsx
@@ -24,7 +24,6 @@ const WorkspaceConfigContext = createContext<WorkspaceConfigContextType>({
 });
 
 export default function WorkspaceConfigProvider({ children }: PropsWithChildren) {
-  const [initialized, setInitialized] = useState(false);
   const [config, setConfig] = useState<WorkspaceConfigProps>({
     panelLocation: "tab",
     showDeviceFrame: true,
@@ -34,7 +33,6 @@ export default function WorkspaceConfigProvider({ children }: PropsWithChildren)
   useEffect(() => {
     function watchConfigChange(e: WorkspaceConfigProps) {
       document.body.setAttribute("data-use-code-theme", `${e.themeType === "vscode"}`);
-      setInitialized(true);
       setConfig(e);
     }
 
@@ -62,10 +60,6 @@ export default function WorkspaceConfigProvider({ children }: PropsWithChildren)
   const contextValue = useMemo(() => {
     return { ...config, update };
   }, [config, update]);
-
-  if (!initialized) {
-    return null;
-  }
 
   return (
     <WorkspaceConfigContext.Provider value={contextValue}>
