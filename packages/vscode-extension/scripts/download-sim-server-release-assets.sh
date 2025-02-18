33b4e2c89e9c82c2426e1257228ed9b2622a417f
Jakub Balinski <31112335+balins@users.noreply.github.com>
2025-02-18 14:58:11 +0100

    adjust to CR

--- packages/vscode-extension/scripts/download-sim-server-release-assets.sh
+++ packages/vscode-extension/scripts/download-sim-server-release-assets.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This script downloads pre-built signed release versions of the simulator-server
-# binaries and places them in the build location for packaging.
+# along with third party licenses and places them in the build location for packaging.
 
 set -e
 
@@ -11,30 +11,24 @@ cd "$(dirname "$0")/.."
 # take output directory from first argument or default to out - relative to the vscode-extension package location
 output_dir="${1:-out}"
 
-# second argument is the tag of the simulator-server release to download, defaults to latest
-sim_server_tag="${2:-latest}"
+# second argument is the tag of the simulator-server release to download, set to latest if not provided
+if [ -z "$2" ]; then
+    sim_server_tag=$(gh release list --json name,isLatest --jq '.[] | select(.isLatest)|.name' -R software-mansion-labs/simulator-server-releases)
+    echo "No tag provided, using latest release: $sim_server_tag"
+else
+    sim_server_tag="$2"
+fi
 
 # Create the target directory if it doesn't exist
 mkdir -p "$output_dir"
 
 echo "Downloading simulator-server assets for tag $sim_server_tag"
 
-# List release assets using GitHub API
-release_info=$(curl -s "https://api.github.com/repos/software-mansion-labs/simulator-server-releases/releases/$sim_server_tag")
-
-# Download simulator-server assets and place them in the output directory
-echo "$release_info" |
-    grep "browser_download_url.*simulator-server" |
-    cut -d '"' -f 4 |
-    while read -r url; do
-        filename=$(basename "$url")
-        echo "Downloading $filename..."
-        curl -L "$url" -o "$output_dir/$filename"
-    done
-
-# Make binaries executable
+# Download simulator-server binaries using gh CLI and place them in the output directory with correct file mode
+gh release download "$sim_server_tag" --clobber -R software-mansion-labs/simulator-server-releases -p "simulator-server*" -D "$output_dir"
 chmod +x "$output_dir"/simulator-server*
 
-mv "$output_dir"/THIRDPARTY.json "$output_dir"/simulator-server-NOTICES.json
+# Download Third Party Notices file for simulator-server
+gh release download "$sim_server_tag" --clobber -R software-mansion-labs/simulator-server-releases -p "THIRDPARTY.json" -O "$output_dir/simulator-server-NOTICES.json"
 
 echo "Successfully downloaded simulator-server assets for tag $sim_server_tag"
