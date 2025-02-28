software-mansion/radon-ide

commit 6c175d391fae8d01184d1fd7c0d325641cd9bca9

Fix Path Set Up Process (#975)
This PR fixes a problem with extension not being able to refresh node
version after it was changed by nvm. Even when the extension was
deactivated and activated again. This would in turn lead to user
frustration when the node version they ware using was in initially
incorrect and applied user solution would not take effect.

The issue was triggered when the following conditions were met: 
- process vscode was either not restarted completely, or was opened from
terminal that would hold the old configuration
- the `$HOME/.nvm/versions/node/{NODE_VERSION}/bin` path was the first
one in vscodes path

Because we would open a new shell with vscode processes path passed as
an env variable it wouldn't start completely fresh and nvm scripts
located in `.zshrc` files responsible for aliasing seem to have an
optimization to not change the version path if it was already the newest
one, causing as receiving the old path to node still living in vscode
process.

The solution is to strip the env of a new shell process and allow it to
boot fresh.

### How Has This Been Tested: 

- run `react-native-78` and set a brakepoint after path was recived, run
it in following configurations:
    - run with a node 21 
    - reload after switching the node version in seprate terminal 
    - run from scratch

packages/vscode-extension/src/utilities/subprocess.ts
@@ -18,11 +18,19 @@ async function getPathEnv(appRoot: string) {
   const RNIDE_PATH_DELIMITER = "{RNIDE_PATH_DELIMITER}";
 
   const shellPath = process.env.SHELL ?? "/bin/zsh";
-  const { stdout } = await execa(shellPath, [
-    "-i",
-    "-c",
-    `cd "${appRoot}" && echo "${RNIDE_PATH_DELIMITER}$PATH${RNIDE_PATH_DELIMITER}"`,
-  ]);
+
+  // Our goal is to determine the PATH variable that would be set when running commands from the application root.
+  // To simulate this accurately, we need to avoid inheriting the vscode process's environment variables.
+  // Therefore, we explicitly set the environment variable object to empty and disable automatic extension,
+  // which would otherwise fill missing variables with those from the currently running process.
+  const { stdout } = await execa(
+    shellPath,
+    ["-i", "-c", `cd "${appRoot}" && echo "${RNIDE_PATH_DELIMITER}$PATH${RNIDE_PATH_DELIMITER}"`],
+    {
+      extendEnv: false,
+      env: {},
+    }
+  );
   const path = stdout.split(RNIDE_PATH_DELIMITER)[1].trim();
   Logger.debug("Obtained PATH environment variable:", path);
   return path;

commit 12ed4f289e0cc40d444fdcf79e29ff87d960c2b8

Add a fallback path to the end of the path variable (#995)
This PR fixes the issue introduced by #975, that was causing for the
path variable to be incomplete.
The problem could be surfaced when trying to use `sim-server
fingerprint` functionality as it relies on the PATH variable.

The solution is to in addition to the new path finding method, append
the old path at the end of the new one to fill in any missing paths.

### How Has This Been Tested: 

- Run `react-native-78` test app
- add license

packages/vscode-extension/src/utilities/subprocess.ts
@@ -31,7 +31,14 @@ async function getPathEnv(appRoot: string) {
       env: {},
     }
   );
-  const path = stdout.split(RNIDE_PATH_DELIMITER)[1].trim();
+
+  // The approach above is not always loading the whole path as it ignores some setup scripts,
+  // like .zprofile for example, to mitigate the effects of that problem we append the PATH
+  // of the current running process to the end of the ne PATH variable.
+  const vsCodeProcessPath = process.env.PATH;
+
+  const path = `${stdout.split(RNIDE_PATH_DELIMITER)[1].trim()}:${vsCodeProcessPath}`;
+
   Logger.debug("Obtained PATH environment variable:", path);
   return path;
 }