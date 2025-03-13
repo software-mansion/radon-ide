software-mansion/radon-ide

commit ed0e9d28f802cac8a9d7b628b62cb9e6f721dba4

Pass app name to gradle (#1004)
This PR fixes an issue with some setups that contain complicated native
structures.
Some apps contain native android code that is failing gradle builds, but
is not application dependency, to avoid that we try to determine app
name and add it as a prefix to build task name.

### How Has This Been Tested: 

- run clean build in `react-native-78` test app

packages/vscode-extension/src/builders/buildAndroid.ts
@@ -2,6 +2,7 @@ import fs from "fs";
 import path from "path";
 import semver from "semver";
 import { OutputChannel } from "vscode";
+import loadConfig from "@react-native-community/cli-config";
 import { getNativeABI } from "../utilities/common";
 import { ANDROID_HOME, findJavaHome } from "../utilities/android";
 import { Logger } from "../Logger";
@@ -65,12 +66,17 @@ export async function getAndroidBuildPaths(
   return { apkPath, packageName };
 }
 
-function makeBuildTaskName(productFlavor: string, buildType: string) {
-  // task name is in the format of assemble<ProductFlavor><BuildType> where productFlavor and buildType
+function makeBuildTaskName(productFlavor: string, buildType: string, appName?: string) {
+  // task name is in the format of :<appName>:assemble<ProductFlavor><BuildType> where productFlavor and buildType
   // are the names of the productFlavor and buildType that each start with a capital letter.
   // By default, Android Gradle Plugin always creates staging and release buildTypes and does not define any productFlavor.
-  const flavor = productFlavor.charAt(0).toUpperCase() + productFlavor.slice(1);
-  return "assemble" + flavor + buildType.charAt(0).toUpperCase() + buildType.slice(1);
+  // and appName is the name of the application as seen by the gradle build system, omitting appName leads to
+  // whole android project being build.
+  const flavorUppercase =
+    productFlavor && productFlavor.charAt(0).toUpperCase() + productFlavor.slice(1);
+  const buildTypeUppercase = buildType && buildType.charAt(0).toUpperCase() + buildType.slice(1);
+  const prefix = appName ? `:${appName}:` : "";
+  return `${prefix}assemble${flavorUppercase}${buildTypeUppercase}`;
 }
 
 export async function buildAndroid(
@@ -142,6 +148,10 @@ export async function buildAndroid(
   }
 
   const androidSourceDir = getAndroidSourceDir(appRoot);
+  const androidAppName = loadConfig({
+    projectRoot: appRoot,
+    selectedPlatform: "android",
+  }).platforms.android.projectConfig(appRoot)?.appName;
   const productFlavor = android?.productFlavor || "";
   const buildType = android?.buildType || "debug";
   const gradleArgs = [
@@ -149,7 +159,7 @@ export async function buildAndroid(
     "lint",
     `-PreactNativeArchitectures=${getNativeABI()}`,
     ...(forceCleanBuild ? ["clean"] : []),
-    makeBuildTaskName(productFlavor, buildType),
+    makeBuildTaskName(productFlavor, buildType, androidAppName),
     "--init-script", // buildProgressEvaluation init script is used log build task count for build progress logging
     path.join(
       extensionContext.extensionPath,

commit ba266814bd16fc691bcb390e1cb346516d4778d1

Fix android app name detection (#1030)
This PR fixes a regression introduces in #1004. It seems that in some
configuration loadConfig is returning an object but gives no details for
`platform.android` that caused Radon to crash. As we already expect that
config might not exist the fix is to read `projectConfig` conditionally.

### How Has This Been Tested: 

The problem surfaced itself for me in `expo-52-prebuild-with.-plugins`
test app. It build correctly after this fix

packages/vscode-extension/src/builders/buildAndroid.ts
@@ -151,7 +151,7 @@ export async function buildAndroid(
   const androidAppName = loadConfig({
     projectRoot: appRoot,
     selectedPlatform: "android",
-  }).platforms.android.projectConfig(appRoot)?.appName;
+  }).platforms.android?.projectConfig(appRoot)?.appName;
   const productFlavor = android?.productFlavor || "";
   const buildType = android?.buildType || "debug";
   const gradleArgs = [