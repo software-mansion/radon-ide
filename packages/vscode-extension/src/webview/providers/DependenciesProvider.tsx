software-mansion/radon-ide

commit 46995a182c07a0a317ccd7a66092584d48d21c1e

Refactor dependency manager proxy (#586)
- replaced vscode event emit calls with listeners and proxies
abstracting them
- cleaned up the API of both dependency manager and provider on frontend
- changed incorrect approach to detecting pods change (it should be
improved in the future though)
- allowed to package manager be not installed and not crashing the IDE
when installing node modules

## Testing

Ran expo-51-dev-client, tested autoinstallation, ran expo-go, tested
missing `npm` command.

packages/vscode-extension/src/webview/providers/DependenciesProvider.tsx
@@ -6,243 +6,90 @@ import {
   useEffect,
   useState,
 } from "react";
-import { vscode } from "../utilities/vscode";
+import { makeProxy } from "../utilities/rpc";
+import {
+  DependencyManagerInterface,
+  DependencyStatus,
+  InstallationStatus,
+  MinSupportedVersion,
+} from "../../common/DependencyManager";
+import { Platform } from "../providers/UtilsProvider";
 
-export enum InstallationStatus {
-  NotInstalled,
-  InProgress,
-  Installed,
-  Optional,
-}
+const dependencyManager = makeProxy<DependencyManagerInterface>("DependencyManager");
 
-export interface DependencyState {
-  installed: InstallationStatus;
-  info: string;
-  error?: string;
-  isOptional?: boolean;
-}
+const dependencies = [
+  "nodejs",
+  "androidEmulator",
+  "xcode",
+  "cocoaPods",
+  "nodeModules",
+  "pods",
+  "reactNative",
+  "expo",
+  "expoRouter",
+  "storybook",
+] as const;
 
-interface DependencyMessageData {
-  command: string;
-  data: {
-    installed: boolean;
-    info: string;
-    error?: string;
-    isOptional?: boolean;
-  };
-}
+type Dependency = typeof dependencies[number];
 
-// when editing, update hasError function
-type Dependencies = {
-  Nodejs?: DependencyState;
-  AndroidEmulator?: DependencyState;
-  Xcode?: DependencyState;
-  CocoaPods?: DependencyState;
-  NodeModules?: DependencyState;
-  ReactNative?: DependencyState;
-  Expo?: DependencyState;
-  Pods?: DependencyState;
-  ExpoRouter?: DependencyState;
-  Storybook?: DependencyState;
-};
-
-const defaultDependencies: Dependencies = {
-  Nodejs: undefined,
-  AndroidEmulator: undefined,
-  Xcode: undefined,
-  CocoaPods: undefined,
-  NodeModules: undefined,
-  ReactNative: undefined,
-  Expo: undefined,
-  Pods: undefined,
-  ExpoRouter: undefined,
-  Storybook: undefined,
-};
-
-const prerequisites = Object.keys(defaultDependencies);
-
-function runDiagnostics() {
-  prerequisites.forEach((prerequisite) => {
-    vscode.postMessage({
-      command: `check${prerequisite}Installed`,
-    });
-  });
-}
-
-function hasError(dependencies: Dependencies, domain: "ios" | "android" | "common"): boolean {
-  function errored(state: DependencyState | undefined) {
-    if (state === undefined) {
-      return false;
-    }
-    return state.error !== undefined;
-  }
-
-  const required = {
-    ios: ["Xcode", "CocoaPods", "Pods"],
-    android: ["AndroidEmulator"],
-    common: ["Nodejs", "NodeModules"],
-  }[domain];
-
-  return entries(dependencies)
-    .filter(([dependency, _state]) => required.includes(dependency))
-    .some(([_dependency, state]) => errored(state));
-}
-
-function adaptDependencyData(data: DependencyMessageData["data"]): DependencyState {
-  const installationStatus = data.installed
-    ? InstallationStatus.Installed
-    : InstallationStatus.NotInstalled;
-
-  return {
-    ...data,
-    installed: installationStatus,
-    error: data.isOptional ? undefined : data.error,
-    isOptional: data.isOptional ?? false,
-  };
-}
-
-function entries<K extends string, T>(object: Partial<Record<K, T>>) {
-  return Object.entries(object) as [K, T][];
-}
+type ErrorType = "ios" | "simulator" | "emulator" | "android" | "common";
+type Errors = Partial<Record<ErrorType, { message: string }>>;
 
 interface DependenciesContextProps {
-  dependencies: Dependencies;
-  isReady: boolean;
-  isCommonError: boolean;
-  isAndroidError: boolean;
-  isIosError: boolean;
-  androidEmulatorError: string | undefined;
-  iosSimulatorError: string | undefined;
-  runDiagnostics: () => void;
-  isExpoRouterInstalled: boolean;
-  isStorybookInstalled: boolean;
+  dependencies: Record<Dependency, DependencyStatus | undefined>;
+  errors: Errors | undefined;
+  runDiagnostics: () => Promise<void>;
 }
 
 const DependenciesContext = createContext<DependenciesContextProps>({
-  dependencies: defaultDependencies,
-  isReady: false,
-  isCommonError: false,
-  isAndroidError: false,
-  isIosError: false,
-  androidEmulatorError: undefined,
-  iosSimulatorError: undefined,
-  runDiagnostics,
-  isExpoRouterInstalled: false,
-  isStorybookInstalled: false,
+  dependencies: objectFromKeys<Dependency, DependencyStatus | undefined>(dependencies, undefined),
+  errors: undefined,
+  runDiagnostics: () => {
+    throw new Error("Provider not initialized");
+  },
 });
 
 export default function DependenciesProvider({ children }: PropsWithChildren) {
-  const [dependencies, setDependencies] = useState<Dependencies>({});
-
-  // `isReady` is true when all dependencies were checked
-  const isReady = !Object.values<DependencyState | undefined>(dependencies).includes(undefined);
-  const isExpoRouterInstalled = false;
-  const isStorybookInstalled = false;
-
-  const isCommonError = hasError(dependencies, "common");
-  const isIosError = hasError(dependencies, "ios");
-  const isAndroidError = hasError(dependencies, "android");
-
-  const androidEmulatorError = dependencies.AndroidEmulator?.error;
-  const iosSimulatorError = dependencies.Xcode?.error;
-
-  const rerunDiagnostics = useCallback(() => {
-    // reset `.installed` and .error, leave other data as is
-    setDependencies((prevState) => {
-      const newState: Dependencies = {};
-      for (const [dependency, data] of entries(prevState)) {
-        newState[dependency] = {
-          ...data,
-          installed: InstallationStatus.NotInstalled,
-          error: undefined,
-        };
-      }
-      return newState;
-    });
-
-    runDiagnostics();
-  }, []);
-
-  const updateDependency = useCallback(
-    (name: keyof Dependencies, newState: Partial<DependencyState>) => {
-      if (newState.isOptional && newState.installed === InstallationStatus.NotInstalled) {
-        newState.installed = InstallationStatus.Optional;
-      }
-
-      setDependencies((prev) => ({
-        ...prev,
-        [name]: { ...prev[name], ...newState },
-      }));
-    },
-    [setDependencies]
+  const [dependencyState, updateDependencies] = useObjectState<Dependency, DependencyStatus>(
+    dependencies
   );
 
+  const runDiagnostics = useCallback(async () => {
+    const checkableDependencies = dependencies.filter(availableOnPlatform);
+    const diagnostics = await dependencyManager.getStatus(checkableDependencies);
+
+    updateDependencies(diagnostics);
+  }, []);
+
   useEffect(() => {
-    const listener = (event: MessageEvent<DependencyMessageData>) => {
-      const { command, data: rawData } = event.data;
-      if (!rawData) {
-        return;
-      }
-      const data = adaptDependencyData(rawData);
-      switch (command) {
-        case "isNodejsInstalled":
-          updateDependency("Nodejs", data);
-          break;
-        case "isAndroidEmulatorInstalled":
-          updateDependency("AndroidEmulator", data);
-          break;
-        case "isXcodeInstalled":
-          updateDependency("Xcode", data);
-          break;
-        case "isCocoaPodsInstalled":
-          updateDependency("CocoaPods", data);
-          break;
-        case "isNodeModulesInstalled":
-          updateDependency("NodeModules", data);
-          break;
-        case "installingNodeModules":
-          updateDependency("NodeModules", { installed: InstallationStatus.InProgress });
-          break;
-        case "isReactNativeInstalled":
-          updateDependency("ReactNative", data);
-          break;
-        case "isExpoInstalled":
-          updateDependency("Expo", data);
-          break;
-        case "isPodsInstalled":
-          updateDependency("Pods", data);
-          break;
-        case "installingPods":
-          updateDependency("Pods", { error: undefined, installed: InstallationStatus.InProgress });
-          break;
-        case "isExpoRouterInstalled":
-          updateDependency("ExpoRouter", data);
-          break;
-        case "isStorybookInstalled":
-          updateDependency("Storybook", data);
-          break;
-      }
-    };
+    function handleUpdatedDependency(
+      dependency: Dependency,
+      installationStatus: InstallationStatus
+    ) {
+      updateDependencies((prev) => {
+        const previousState = prev[dependency];
+        if (!previousState) {
+          throw new Error(`Dependency ${dependency} updated before it was initialized`);
+        }
+
+        return { [dependency]: { ...previousState, status: installationStatus } };
+      });
+    }
 
     runDiagnostics();
-    window.addEventListener("message", listener);
 
-    return () => window.removeEventListener("message", listener);
+    dependencyManager.addListener(handleUpdatedDependency);
+    return () => {
+      dependencyManager.removeListener(handleUpdatedDependency);
+    };
   }, []);
 
   return (
     <DependenciesContext.Provider
       value={{
-        dependencies,
-        isReady,
-        isCommonError,
-        isAndroidError,
-        isIosError,
-        androidEmulatorError,
-        iosSimulatorError,
-        runDiagnostics: rerunDiagnostics,
-        isExpoRouterInstalled,
-        isStorybookInstalled,
+        dependencies: dependencyState,
+        runDiagnostics,
+        errors: getErrors(dependencyState),
       }}>
       {children}
     </DependenciesContext.Provider>
@@ -257,4 +104,133 @@ export function useDependencies() {
   }
 
   return context;
+}
+
+function objectFromKeys<K extends string, V>(keys: readonly K[], defaultValue: V): Record<K, V> {
+  return Object.fromEntries(keys.map((key) => [key, defaultValue])) as Record<K, V>;
+}
+
+function useObjectState<K extends string, V>(keys: readonly K[]) {
+  const [state, setState] = useState(() => objectFromKeys<K, V | undefined>(keys, undefined));
+
+  function update(
+    objectOrUpdater:
+      | Partial<Record<K, V>>
+      | ((prev: Record<K, V | undefined>) => Partial<Record<K, V>>)
+  ): void {
+    const isUpdater = typeof objectOrUpdater === "function";
+    if (isUpdater) {
+      const updater = objectOrUpdater;
+      setState((prev) => ({ ...prev, ...updater(prev) }));
+    } else {
+      const object = objectOrUpdater;
+      setState((prev) => ({ ...prev, ...object }));
+    }
+  }
+
+  return [state, update] as const;
+}
+
+function getErrors(statuses: Record<Dependency, DependencyStatus | undefined>) {
+  const errors: Errors = {};
+  let hasErrors = false;
+  function setFirstError(dependency: Dependency, errorName: ErrorType) {
+    hasErrors = true;
+    if (!errors[errorName]) {
+      const message = dependencyDescription(dependency).error;
+      errors.simulator = { message };
+    }
+  }
+
+  Object.entries(statuses)
+    .filter(([_dependency, info]) => {
+      const notInstalled = info !== undefined && !info.isOptional && info.status === "notInstalled";
+      return notInstalled;
+    })
+    .forEach(([dependency, _info]) => {
+      switch (dependency) {
+        case "xcode":
+          setFirstError(dependency, "emulator");
+        /* fallthrough */
+        case "cocoaPods":
+        case "pods":
+          setFirstError(dependency, "ios");
+          break;
+        case "androidEmulator":
+          setFirstError(dependency, "android");
+          setFirstError(dependency, "emulator");
+          break;
+        case "nodejs":
+        case "nodeModules":
+          setFirstError(dependency, "common");
+          break;
+        default:
+          break;
+      }
+    });
+  return hasErrors ? errors : undefined;
+}
+
+function availableOnPlatform(dependency: Dependency) {
+  const macosOnly = ["xcode", "cocoaPods", "pods"].includes(dependency);
+
+  if (macosOnly) {
+    return Platform.OS === "macos";
+  }
+  return true;
+}
+
+export function dependencyDescription(dependency: Dependency) {
+  switch (dependency) {
+    case "nodejs":
+      return {
+        info: "Used for running scripts and getting dependencies.",
+        error: "Node.js was not found. Make sure to [install Node.js](https://nodejs.org/en).",
+      };
+    case "androidEmulator":
+      return {
+        info: "Used for running Android apps.",
+        error:
+          "Android Emulator was not found. Make sure to [install Android Emulator](https://developer.android.com/studio/run/managing-avds).",
+      };
+    case "xcode":
+      return {
+        info: "Used for building and running iOS apps.",
+        error:
+          "Xcode was not found. If you are using alternative Xcode version you can find out more in troubleshooting section of our documentation. Otherwise, [Install Xcode from the Mac App Store](https://apps.apple.com/us/app/xcode/id497799835?mt=12) and have Xcode Command Line Tools enabled.",
+      };
+    case "cocoaPods":
+      return {
+        info: "Used for installing iOS dependencies.",
+        error:
+          "CocoaPods was not found. Make sure to [install CocoaPods](https://guides.cocoapods.org/using/getting-started.html).",
+      };
+    case "nodeModules":
+      return {
+        info: "Whether node modules are installed",
+        error: "Node modules are not installed.",
+      };
+    case "pods":
+      return { info: "Whether iOS dependencies are installed.", error: "Pods are not installed." };
+    case "reactNative":
+      return {
+        info: "Whether supported version of React Native is installed.",
+        error: `React Native is not installed or it is older than supported version ${MinSupportedVersion.reactNative}.`,
+      };
+    case "expo":
+      return {
+        info: "Whether supported version of Expo SDK is installed.",
+        error: `Expo is not installed or it is older than supported version ${MinSupportedVersion.expo}.`,
+      };
+    case "expoRouter":
+      return {
+        info: "Whether supported version of Expo Router is installed.",
+        error: `expo-router is not installed.`,
+      };
+    case "storybook":
+      return {
+        info: "Whether Storybook is installed.",
+        error: "Storybook is not installed.",
+      };
+  }
 }

packages/vscode-extension/src/builders/BuildManager.ts
@@ -79,10 +79,10 @@ export class BuildManager {
           this.buildOutputChannel,
           progressListener,
           () => {
-            return this.dependencyManager.checkPodsInstalled();
+            return this.dependencyManager.isInstalled("pods");
           },
-          (appRootFolder: string, cleanBuild: boolean, token: CancelToken) => {
-            return this.dependencyManager.installPods(appRootFolder, cleanBuild, token);
+          () => {
+            return this.dependencyManager.installPods({ forceCleanBuild, cancelToken });
           }
         );
       }

packages/vscode-extension/src/common/DependencyManager.ts
@@ -1,0 +1,48 @@
+import type { CancelToken } from "../builders/cancelToken";
+
+export const MinSupportedVersion = {
+  reactNative: "0.71.0",
+  expo: "49.0.0",
+  storybook: "5.2.0",
+} as const;
+
+export type Dependency =
+  | "androidEmulator"
+  | "xcode"
+  | "cocoaPods"
+  | "nodejs"
+  | "nodeModules"
+  | "pods"
+  | "reactNative"
+  | "expo"
+  | "expoRouter"
+  | "storybook";
+
+export type InstallationStatus = "installed" | "notInstalled" | "installing";
+
+export type DependencyStatus = {
+  status: InstallationStatus;
+  isOptional: boolean;
+};
+
+export type DependenciesStatus = Record<Dependency, DependencyStatus>;
+
+export type InstallPodsOptions = {
+  forceCleanBuild: boolean;
+  cancelToken: CancelToken;
+};
+
+export type DependencyListener = (
+  dependency: Dependency,
+  installationStatus: InstallationStatus
+) => void;
+
+export interface DependencyManagerInterface {
+  getStatus(dependencies: Dependency[]): Promise<Record<Dependency, DependencyStatus>>;
+  isInstalled(dependency: Dependency): Promise<boolean>;
+  installNodeModules(): Promise<boolean>;
+  installPods(options: InstallPodsOptions): Promise<void>;
+
+  addListener(listener: DependencyListener): Promise<void>;
+  removeListener(listener: DependencyListener): Promise<void>;
+}

packages/vscode-extension/src/panels/WebviewController.ts
@@ -43,8 +43,7 @@ export class WebviewController implements Disposable {
     // Set an event listener to listen for messages passed from the webview context
     this.setWebviewMessageListener(webview);
 
-    // Set the manager to listen and change the persisting storage for the extension.
-    this.dependencyManager = new DependencyManager(webview);
+    this.dependencyManager = new DependencyManager();
 
     this.deviceManager = new DeviceManager();
     this.project = new Project(this.deviceManager, this.dependencyManager);
@@ -63,6 +62,7 @@ export class WebviewController implements Disposable {
 
     this.callableObjects = new Map([
       ["DeviceManager", this.deviceManager as object],
+      ["DependencyManager", this.dependencyManager as object],
       ["Project", this.project as object],
       ["WorkspaceConfig", this.workspaceConfig as object],
       ["LaunchConfig", this.launchConfig as object],

packages/vscode-extension/src/project/project.ts
@@ -340,7 +340,7 @@ export class Project
     }
 
     Logger.debug("Installing Node Modules");
-    const installNodeModules = this.installNodeModules();
+    const waitForNodeModules = this.installNodeModules();
 
     Logger.debug(`Launching devtools`);
     const waitForDevtools = this.devtools.start();
@@ -351,13 +351,13 @@ export class Project
       throttle((stageProgress: number) => {
         this.reportStageProgress(stageProgress, StartupMessage.WaitingForAppToLoad);
       }, 100),
-      [installNodeModules]
+      [waitForNodeModules]
     );
 
     Logger.debug("Checking expo router");
-    this.expoRouterInstalled = await this.dependencyManager.checkExpoRouterInstalled();
+    this.expoRouterInstalled = await this.dependencyManager.isInstalled("expoRouter");
     Logger.debug("Checking storybook");
-    this.storybookInstalled = await this.dependencyManager.checkStorybookInstalled();
+    this.storybookInstalled = await this.dependencyManager.isInstalled("storybook");
   }
   //#endregion
 
@@ -494,12 +494,16 @@ export class Project
   }
 
   private async installNodeModules(): Promise<void> {
-    const nodeModulesStatus = await this.dependencyManager.checkNodeModulesInstalled();
-
-    if (!nodeModulesStatus.installed) {
-      await this.dependencyManager.installNodeModules(nodeModulesStatus.packageManager);
+    let installationSuccess = false;
+    const installed = await this.dependencyManager.isInstalled("nodeModules");
+    if (!installed) {
+      installationSuccess = await this.dependencyManager.installNodeModules();
     }
-    Logger.debug("Node Modules installed");
+    if (!installationSuccess) {
+      Logger.error("Node Modules installation failed");
+    } else {
+      Logger.debug("Node Modules installed");
+    }
   }
 
   //#region Select device

packages/vscode-extension/src/utilities/packageManager.ts
@@ -3,6 +3,7 @@ import { promises as fs } from "fs";
 import { command } from "./subprocess";
 import { getAppRootFolder } from "./extensionContext";
 import { isWorkspaceRoot } from "./common";
+import { Logger } from "../Logger";
 
 export type PackageManagerInfo = {
   name: "npm" | "pnpm" | "yarn" | "bun";
@@ -18,7 +19,7 @@ async function pathExists(p: string) {
   }
 }
 
-export async function resolvePackageManager(): Promise<PackageManagerInfo> {
+export async function resolvePackageManager(): Promise<PackageManagerInfo | undefined> {
   function findWorkspace(appRoot: string) {
     let currentDir = appRoot;
     let parentDir = path.resolve(currentDir, "..");
@@ -68,15 +69,14 @@ export async function resolvePackageManager(): Promise<PackageManagerInfo> {
 
   const name = await findPackageManager(workspacePath ?? appRootPath);
 
-  return { name, workspacePath };
-}
-
-export function isPackageManagerAvailable(manager: PackageManagerInfo): boolean {
   try {
-    command(`${manager.name} --version`);
-    return true;
-  } catch {}
-  return false;
+    await command(`${name} --version`);
+  } catch (e) {
+    Logger.error(`Required package manager: ${name} is not installed`);
+    return undefined;
+  }
+
+  return { name, workspacePath };
 }
 
 async function isNpmModulesInstalled(workspacePath: string): Promise<boolean> {

packages/vscode-extension/src/webview/hooks/useDiagnosticAlert.tsx
@@ -40,11 +40,11 @@ const diagnosticAlert = {
   actions: <Actions />,
 };
 export function useDiagnosticAlert(platform?: DevicePlatform) {
-  const { isCommonError, isAndroidError, isIosError } = useDependencies();
-  let open = isCommonError;
-  if (platform === DevicePlatform.Android && isAndroidError) {
+  const { errors } = useDependencies();
+  let open = Boolean(errors?.common);
+  if (platform === DevicePlatform.Android && errors?.android) {
     open = true;
-  } else if (platform === DevicePlatform.IOS && isIosError) {
+  } else if (platform === DevicePlatform.IOS && errors?.ios) {
     open = true;
   }
   useToggleableAlert(open, diagnosticAlert);

commit 9498151754bcb4c4df87ff58a160b7442e995bdf

Fix home screen when xcode is not installed (#1056)
This PR fixes a mistake introduced in #586, causing missing xcode
installation to be registered as emulator and not simulator error.

### How Has This Been Tested: 

- Uninstall Xcode form your machine 
- run `react-native-79` test app and remove all devices 
- click `add iPhone` and `add android` and check if it works

packages/vscode-extension/src/webview/providers/DependenciesProvider.tsx
@@ -118,7 +118,7 @@ function getErrors(statuses: DependencyRecord) {
     .forEach(([dependency, _info]) => {
       switch (dependency) {
         case "xcode":
-          setFirstError(dependency, "emulator");
+          setFirstError(dependency, "simulator");
         /* fallthrough */
         case "cocoaPods":
         case "pods":