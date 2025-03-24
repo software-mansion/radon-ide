c90940291b44536bc91991ee588db91040798685
kacperkapusciak <kacper.kapusciak@swmansion.com>
2025-03-24 16:29:40 +0100

    Improve bottom CTA

--- packages/docs/src/components/LearnMore/LearnMoreFooter/index.tsx
+++ packages/docs/src/components/LearnMore/LearnMoreFooter/index.tsx
@@ -1,27 +1,62 @@
-import React from "react";
+import React, { useEffect } from "react";
+import HomepageButton, { ButtonStyling, BorderStyling } from "@site/src/components/HomepageButton";
 import styles from "./styles.module.css";
-import HomepageButton from "@site/src/components/HomepageButton";
 import { track } from "@vercel/analytics";
+
+const RADON_IDE_MARKETPLACE_URL =
+  "https://marketplace.visualstudio.com/items?itemName=swmansion.react-native-ide";
+const DEFAULT_INSTALLS_NO = "20,000+";
 
 const LearnMoreFooter = () => {
   const handleBottomCTAClick = () => {
     track("Bottom CTA");
   };
 
+  const [isLoaded, setIsLoaded] = React.useState(false);
+  const [installs, setInstalls] = React.useState(DEFAULT_INSTALLS_NO);
+
+  useEffect(() => {
+    async function getInstalls() {
+      try {
+        const response = await fetch(RADON_IDE_MARKETPLACE_URL);
+        if (!response.ok) {
+          throw new Error("Failed to fetch marketplace page");
+        }
+
+        const htmlString = await response.text();
+        const regex =
+          /<span class="installs-text" [^>]*>\s*\d{1,3}(?:,\d{3})*\s*installs\s*<\/span>/g;
+        const match = htmlString.match(regex);
+        const installsWithLabel = match[0].replace(/<\/?[^>]+(>|$)/g, "").trim();
+        const installsWithoutLabel = installsWithLabel.replace(/installs/i, "").trim();
+        setInstalls(installsWithoutLabel);
+      } catch (err) {
+        console.error(err);
+        setInstalls(DEFAULT_INSTALLS_NO);
+      } finally {
+        setIsLoaded(true);
+      }
+    }
+    getInstalls();
+  }, []);
+
   return (
-    <section>
-      <div className={styles.learnMoreSectionFooter}>
-        <div>
-          <p>Hundreds of engineers are building better apps with Radon IDE right now.</p>
-        </div>
-        <HomepageButton
-          target="_blank"
-          href="https://marketplace.visualstudio.com/items?itemName=swmansion.react-native-ide"
-          title="Try Radon IDE free"
-          onClick={handleBottomCTAClick}
-        />
+    <div className={`${styles.learnMoreSectionFooter} ${isLoaded ? styles.show : ""}`}>
+      <div className={styles.ellipse} />
+      <div className={styles.ellipse} />
+      <div className={styles.contentContainer}>
+        <h2>
+          Join {installs} engineers using Radon IDE for faster, more efficient app development.
+        </h2>
       </div>
-    </section>
+      <HomepageButton
+        href="/pricing"
+        backgroundStyling={ButtonStyling.TO_WHITE}
+        borderStyling={BorderStyling.NAVY}
+        title="Try Radon IDE for Free"
+        onClick={handleBottomCTAClick}
+      />
+    </div>
   );
 };
 
