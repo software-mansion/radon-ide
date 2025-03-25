80fc99d3b65913c83c191c8f674b9742dac12108
Krzysztof Magiera <krzys@swmansion.com>
2025-03-25 14:37:28 +0100

    Stashing moar work

--- packages/vscode-extension/src/connect/Connector.ts
+++ packages/vscode-extension/src/connect/Connector.ts
@@ -1,15 +1,24 @@
-import { MarkdownString, StatusBarAlignment, StatusBarItem, window, workspace } from "vscode";
+import {
+  commands,
+  Disposable,
+  MarkdownString,
+  StatusBarAlignment,
+  StatusBarItem,
+  window,
+  workspace,
+} from "vscode";
 import { DebugSession } from "../debugging/DebugSession";
 import { Metro } from "../project/metro";
 import { sleep } from "../utilities/retry";
 import { extensionContext } from "../utilities/extensionContext";
+import { disposeAll } from "../utilities/disposables";
 
 const PORT_SCAN_INTERVAL_MS = 4000;
 const DEFAULT_PORTS = [8081, 8082, 8083];
 
 const RADON_CONNECT_ENABLED_KEY = "radon_connect_enabled";
 
-export class Connector {
+export class Connector implements Disposable {
   private static instance: Connector | null = null;
 
   private statusBarItem: StatusBarItem;
@@ -17,6 +26,7 @@ export class Connector {
   private metro: Metro | null = null;
 
   private portsStatus: Record<number, string> = {};
+  private disposables: Disposable[] = [];
 
   private constructor() {
     this.statusBarItem = window.createStatusBarItem(
@@ -24,10 +34,32 @@ export class Connector {
       Number.MIN_SAFE_INTEGER
     );
     this.statusBarItem.command = "RNIDE.openPanel";
+    this.disposables.push(this.statusBarItem);
+
+    this.disposables.push(
+      commands.registerCommand("RNIDE.disableAutoConnect", () => {
+        extensionContext.workspaceState.update(RADON_CONNECT_ENABLED_KEY, false);
+        this.updateStatusBarItem();
+      })
+    );
+  }
+
+  public dispose() {
+    this.disconnect();
+    disposeAll(this.disposables);
+    Connector.instance = null;
+  }
+
+  private disconnect() {
+    this.metro = null;
+    this.debugSession?.dispose();
+    this.debugSession = null;
   }
 
   private scanPortsPeriodically() {
-    const enabled = extensionContext.workspaceState.get(RADON_CONNECT_ENABLED_KEY, true);
+    const enabled =
+      this === Connector.instance &&
+      extensionContext.workspaceState.get(RADON_CONNECT_ENABLED_KEY, true);
     if (!enabled || this.debugSession) {
       return;
     }
@@ -96,6 +128,7 @@ export class Connector {
   public start() {
     this.updateStatusBarItem();
     this.statusBarItem.show();
+    commands.executeCommand("setContext", "RNIDE.showsStatusBarItem", true);
     this.scanPortsPeriodically();
   }
 
@@ -113,11 +146,18 @@ export class Connector {
     markdownText.supportThemeIcons = true;
     markdownText.isTrusted = true;
 
+    const enabled = extensionContext.workspaceState.get(RADON_CONNECT_ENABLED_KEY, true);
+
     if (this.debugSession && this.metro) {
       this.statusBarItem.text = "Radon IDE $(debug)";
       markdownText.appendMarkdown("Connected on port " + this.metro.port);
       markdownText.appendMarkdown("\n\n");
       markdownText.appendMarkdown("[Open debug console](command:workbench.panel.repl.view.focus)");
+    } else if (!enabled) {
+      this.statusBarItem.text = "Radon IDE $(open-preview)";
+      markdownText.appendMarkdown(
+        "Auto-connect is disabled [enable](command:RNIDE.enableAutoConnect)"
+      );
     } else {
       this.statusBarItem.text = "Radon IDE $(debug-disconnect)";
       markdownText.appendMarkdown(
@@ -138,6 +178,96 @@ export class Connector {
       Connector.instance = new Connector();
     }
     return Connector.instance;
+  }
+}
+
+type ScannerDelegate = {
+  onPortStatusUpdated: () => void;
+  onDeviceCandidateFound: (port: number, projectRoot: string, websocketAddress: string) => void;
+};
+
+class Scanner implements Disposable {
+  public portsStatus: Record<number, string> = {};
+  private disposed = false;
+  private delegate: ScannerDelegate | null = null;
+
+  public constructor(delegate: ScannerDelegate) {
+    this.delegate = delegate;
+  }
+
+  public dispose() {
+    this.delegate = null;
+    this.disposed = true;
+  }
+
+  public start() {
+    this.scanPortsPeriodically();
+  }
+
+  private scanPortsPeriodically() {
+    if (this.disposed) {
+      return;
+    }
+    Promise.all(DEFAULT_PORTS.map(this.scanPort.bind(this)))
+      .then(() => sleep(PORT_SCAN_INTERVAL_MS))
+      .then(this.scanPortsPeriodically.bind(this))
+      .then(this.delegate?.onPortStatusUpdated);
+  }
+
+  private async tryConnectMetroAndJSDebugger(port: number, projectRoot: string) {
+    const metro = new Metro(port, [projectRoot]);
+    const websocketAddress = await metro.getDebuggerURL();
+    if (!websocketAddress) {
+      this.portsStatus[port] = "no connected device";
+      return false;
+    }
+    if (!metro.isUsingNewDebugger) {
+      this.portsStatus[port] = "using old debugger";
+      return false;
+    }
+
+    const debugSession = new DebugSession({
+      onDebugSessionTerminated: () => {
+        this.metro = null;
+        this.debugSession = null;
+        this.updateStatusBarItem();
+      },
+    });
+    const success = await debugSession.startJSDebugSession(metro, {
+      websocketAddress,
+      displayDebuggerOverlay: true,
+    });
+    if (success) {
+      this.metro = metro;
+      this.debugSession = debugSession;
+    } else {
+      this.portsStatus[port] = "unable to connect";
+      debugSession.dispose();
+    }
+  }
+
+  private async scanPort(port: number) {
+    try {
+      const response = await fetch(`http://localhost:${port}/status`);
+      if (response.ok) {
+        // we expect metro to include a response header X-React-Native-Project-Root
+        // that points to the project root folder
+        const projectRoot = response.headers.get("X-React-Native-Project-Root");
+        if (projectRoot && isInWorkspace(projectRoot)) {
+          await this.tryConnectMetroAndJSDebugger(port, projectRoot);
+        } else if (projectRoot) {
+          this.portsStatus[port] = "running for a different workspace";
+        } else {
+          this.portsStatus[port] = "not recognized as metro process";
+        }
+      } else {
+        this.portsStatus[port] = "not running";
+      }
+    } catch (error) {
+      this.portsStatus[port] = "not running";
+    } finally {
+      this.updateStatusBarItem();
+    }
   }
 }
 
