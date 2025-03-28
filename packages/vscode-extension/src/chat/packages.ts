8144ccd8da7a4d0551a81dba3df654d1fdfb2f04
Ignacy Łątka <latkaignacy@gmail.com>
2025-03-28 13:59:37 +0100

    [Radon AI] Add support for monorepos with multiple `node_module` directories (#1067)
    ### This PR resolves a
    [discussion](https://github.com/software-mansion/radon-ide/pull/1000#discussion_r2014231136)
    in the main Radon AI PR
    
    Changes in this PR includes all node_modules directories into both the
    search for node_modules, the hash calculation, and the context creation.
    
    Example of the new prompt generated inside the `react-native-reanimated`
    monorepo:
    
    <details>
    <summary>
    Collapsed output
    </summary>
    
    ```
    User has the following packages installed in the project:
    - expo-application@5.1.1
    - expo-keep-awake@12.0.1
    - react-native-screens@4.5.0
    - babel-preset-expo@9.3.2
    - react-native-gesture-handler@2.22.0
    - react-native-svg@15.11.1
    - react-native-pager-view@6.6.1
    - react-native-drawer-layout@4.1.1
    - metro-react-native-babel-preset@0.73.9
    - react-native-builder-bob@0.33.1
    - expo-constants@14.3.0
    - react-native-web@0.19.11
    - expo-modules-core@1.2.7
    - eslint-plugin-react-native@4.1.0
    - eslint-plugin-react-native-globals@0.1.2
    - expo-font@11.1.1
    - react-native-worklets@0.0.1
    - expo@48.0.21
    - babel-plugin-react-native-web@0.18.12
    - react-native-safe-area-context@5.1.0
    - react-native-monorepo-tools@1.2.1
    - react-native@0.77.0
    - expo-asset@8.9.2
    - react-native-reanimated@4.0.0-beta.2
    - expo-modules-autolinking@1.2.0
    - expo-file-system@15.3.0
    
    "apps/macos-example":
    - eslint-plugin-react-native@4.1.0
    - eslint-plugin-react-native-globals@0.1.2
    - react-native@0.75.4
    - react-native-macos@0.75.16
    - react-native-reanimated@4.0.0-beta.2
    
    "apps/tvos-example":
    - react-native-reanimated@4.0.0-beta.2
    - react-native-tvos@0.77.0-0
    
    "apps/web-example":
    - babel-preset-expo@12.0.0
    - react-native-web@0.19.13
    - expo-font@13.0.1
    - expo-modules-core@2.0.1
    - expo-constants@17.0.2
    - expo@52.0.4
    - babel-plugin-react-native-web@0.19.13
    - expo-asset@11.0.1
    - expo-keep-awake@14.0.1
    - expo-modules-autolinking@2.0.1
    - expo-file-system@18.0.2
    
    "packages/docs-reanimated":
    - babel-preset-expo@9.2.2
    - react-native-gesture-handler@2.18.0
    - react-native-svg@15.4.0
    - deprecated-react-native-prop-types@3.0.1
    - babel-preset-react-native@4.0.1
    - metro-react-native-babel-preset@0.73.8
    - react-native-web@0.18.12
    - metro-react-native-babel-transformer@0.73.8
    - babel-plugin-react-native-web@0.18.12
    - react-native-gradle-plugin@0.71.16
    - react-native@0.71.4
    - css-to-react-native@3.2.0
    - react-native-reanimated@3.15.0-nightly-20240722-a15e0edec
    - react-native-codegen@0.71.5
    
    "packages/react-native-worklets":
    - react-native@0.77.0
    
    ```
    
    </details>

--- packages/vscode-extension/src/chat/packages.ts
+++ packages/vscode-extension/src/chat/packages.ts
@@ -6,76 +6,111 @@ import { calculateMD5 } from "../utilities/common";
 import { CHAT_LOG } from ".";
 
 interface Package {
+  path: string;
   name: string;
   version: string;
 }
 
 const appRootFolder = findAppRootFolder() ?? "";
 const packages: Package[] = [];
-let packageJsonHash: string;
+let packageJsonHashes: string[] = [];
 
-async function getPackageJsonHash(): Promise<string> {
-  const rootPackageJson = await vscode.workspace.findFiles(
+async function getPackageJsonHashes(): Promise<string[]> {
+  const packageJsonFiles = await vscode.workspace.findFiles(
     new RelativePattern(appRootFolder, "package.json"),
-    null,
-    1
+    null
   );
 
-  const hash = await calculateMD5(rootPackageJson[0].fsPath);
-  return hash.digest("hex");
+  if (!packageJsonFiles.length) {
+    Logger.error("No package.json found in the workspace");
+    return [];
+  }
+
+  const hashes = packageJsonFiles.map((packageJson) =>
+    calculateMD5(packageJson.fsPath).then((hash) => hash.digest("hex"))
+  );
+
+  const resolvedHashes = await Promise.all(hashes);
+
+  return resolvedHashes;
+}
+
+export function compareUnorderedArrays(a: unknown[], b: unknown[]): boolean {
+  if (a.length !== b.length) {
+    return false;
+  }
+  const sortedA = [...a].sort();
+  const sortedB = [...b].sort();
+  return sortedA.every((value, i) => value === sortedB[i]);
 }
 
 async function getReactNativePackages(): Promise<Package[]> {
   // if we have already scanned the packages, we can skip the scan if package.json has not changed
-  if (packageJsonHash) {
+  if (packageJsonHashes.length !== 0) {
     // check if hash is the same
-    const newPackageJsonHash = await getPackageJsonHash();
-    if (newPackageJsonHash === packageJsonHash) {
+    const newPackageJsonHashes = await getPackageJsonHashes();
+    if (compareUnorderedArrays(newPackageJsonHashes, packageJsonHashes)) {
       return packages;
     }
+
     Logger.info(CHAT_LOG, "Found changes in package.json, rescanning packages");
     // update hash
-    packageJsonHash = newPackageJsonHash;
+    packageJsonHashes = newPackageJsonHashes;
     // empty packages array
     packages.length = 0;
   } else {
     // generate hash for the first time
-    packageJsonHash = await getPackageJsonHash();
+    packageJsonHashes = await getPackageJsonHashes();
   }
 
   Logger.info(CHAT_LOG, "Scanning node_modules for React Native packages");
   // we scan node_modules here because different package managers use different lock files
   // and bun uses binary format - bun.lockb - which is not the easiest to parse
   const packageFiles = await vscode.workspace.findFiles(
-    new RelativePattern(
-      appRootFolder,
-      "**/node_modules/**/*{expo,react-native,react-navigation}*/package.json"
-    ),
-    new RelativePattern(appRootFolder, "**/node_modules/**/*{export,exponent}*")
+    "**/node_modules/**/*{expo,react-native,react-navigation}*/package.json",
+    "**/node_modules/**/*{export,exponent}*"
   );
 
   for await (const pkg of packageFiles) {
     try {
       const packageJsonDoc = await vscode.workspace.openTextDocument(pkg);
       const { name, version } = JSON.parse(packageJsonDoc.getText());
-      packages.push({ name, version });
+      const relativePath = vscode.workspace.asRelativePath(pkg.path);
+
+      // last 3 segments will always be: node_modules/<package-name>/package.json
+      // we are only interested in the part before these 3 segments
+      const trimmedPath = relativePath.split("/").slice(0, -3).join("/");
+
+      if (trimmedPath.includes("node_modules")) {
+        // ignore packages that are nested inside other node_modules packages
+        continue;
+      }
+
+      packages.push({ path: trimmedPath, name, version });
     } catch (err) {
       Logger.error(`Error reading package.json: ${err}`);
       return [];
     }
   }
 
+  packages.sort((a, b) => a.path.localeCompare(b.path));
   return packages;
 }
 
 export async function getReactNativePackagesPrompt(): Promise<string> {
-  let prompt = "User has the following packages installed in the project :\n";
+  let prompt = "User has the following packages installed in the project:\n";
   const rnPackages = await getReactNativePackages();
 
   if (rnPackages.length === 0) {
     return "";
   }
+  let prevPath = "";
   rnPackages.forEach((pkg) => {
+    if (pkg.path !== prevPath) {
+      prompt += `\n"${pkg.path}":\n`;
+      prevPath = pkg.path;
+    }
+
     prompt += `- ${pkg.name}@${pkg.version}\n`;
   });
 
