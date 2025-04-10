100c7e7b8e8361646bbdc1c866589b4cd601c5c5
kacperkapusciak <kacper.kapusciak@swmansion.com>
2025-04-10 16:29:42 +0200

    Format TIming nicer

--- packages/vscode-extension/src/network/components/TimingTab.tsx
+++ packages/vscode-extension/src/network/components/TimingTab.tsx
@@ -13,18 +13,27 @@ const TimingTab = ({ networkLog }: TimingTabProps) => {
   const responseLoadingPercent = ((totalTime - ttfb) / totalTime) * 100;
 
   return (
-    <div className="timing-container">
+    <>
       <div className="timing-bar">
         <div className="bar request-sent-bar" style={{ width: `${ttfbPercent}%` }} />
         <div className="bar response-receive-bar" style={{ width: `${responseLoadingPercent}%` }} />
       </div>
 
-      <div className="timing-section">
-        <p>Waiting (TTFB): {ttfb} ms</p>
-        <p>Downloading response: {totalTime - ttfb} ms</p>
-        <p>Total: {totalTime} ms</p>
-      </div>
-    </div>
+      <table className="timing-section">
+        <tr>
+          <td>Waiting (TTFB):</td>
+          <td>{ttfb} ms</td>
+        </tr>
+        <tr>
+          <td>Downloading response:</td>
+          <td>{totalTime - ttfb} ms</td>{" "}
+        </tr>
+        <tr>
+          <td>Total:</td>
+          <td> {totalTime} ms</td>
+        </tr>
+      </table>
+    </>
   );
 };
 
