ac8927499b4de3a86a122be4aca58c15ef569e29
kacperkapusciak <kacper.kapusciak@swmansion.com>
2025-04-15 15:22:06 +0200

    Add learn more footer to pricing page

--- packages/docs/src/pages/pricing.tsx
+++ packages/docs/src/pages/pricing.tsx
@@ -3,6 +3,7 @@ import Layout from "@theme/Layout";
 
 import styles from "./pricing.module.css";
 import PricingScreen from "../components/Pricing";
+import LearnMoreFooter from "../components/LearnMore/LearnMoreFooter";
 
 export default function Pricing(): JSX.Element {
   return (
@@ -10,6 +11,7 @@ export default function Pricing(): JSX.Element {
       <div className={styles.preventfulContainer}>
         <div className={styles.container}>
           <PricingScreen />
+          <LearnMoreFooter />
         </div>
       </div>
     </Layout>
