0a563052faad3ee4c468e922ff059776ee5f71c6
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2025-04-18 15:54:57 +0200

    docs: add changelog page (#1105)
    This PR adds new `/docs/changelog` page to documentation with highlight
    of most important changes added in new versions of Radon IDE.
    
    <img width="1087" alt="image"
    src="https://github.com/user-attachments/assets/971bd449-4bbc-4ced-97a7-b77b307e0716"
    />
    
    ## How to test
    
    Open the Vercel preview or build documentation locally
    
    ```
    cd packages/docs
    yarn
    yarn start
    ```
    
    ---------
    
    Co-authored-by: Juliusz Wajgelt <49338439+jwajgelt@users.noreply.github.com>

---
+++ packages/docs/src/components/ChainIcon/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function LinkIcon({ width, height }) {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M15 16H17C19.2091 16 21 14.2091 21 12C21 9.79086 19.2091 8 17 8H15M8 12H16M9 8H7C4.79086 8 3 9.79086 3 12C3 14.2091 4.79086 16 7 16H9"
+        stroke="#001A72"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
