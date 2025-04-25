73da23233da77900bf23511595dcfe3a9018d688
Juliusz Wajgelt <juliusz.wajgelt@swmansion.com>
2025-04-25 09:19:34 +0200

    fixes issues causing long startup of js-debug

--- packages/vscode-extension/src/debugging/ProxyDebugAdapter.ts
+++ packages/vscode-extension/src/debugging/ProxyDebugAdapter.ts
@@ -160,7 +160,10 @@ export class ProxyDebugAdapter extends DebugSession {
           continueOnAttach: true,
           sourceMapPathOverrides: args.sourceMapPathOverrides,
           resolveSourceMapLocations: ["**", "!**/node_modules/!(expo)/**"],
-          skipFiles: this.session.configuration.skipFiles,
+          // NOTE: setting skipFiles increases startup time _significantly_, so we omit them until we figure out
+          // how to work around this problem
+          skipFiles: [],
+          outFiles: [],
         },
         {
           suppressDebugStatusbar: true,
