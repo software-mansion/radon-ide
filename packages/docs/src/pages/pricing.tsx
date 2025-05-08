615bdb58f9dd31661c50072c235bd6a30da081f0
kacperkapusciak <kacper.kapusciak@swmansion.com>
2025-05-08 13:07:15 +0200

    docs: update pricing page title, description and h1

--- packages/docs/src/pages/pricing.tsx
+++ packages/docs/src/pages/pricing.tsx
@@ -7,7 +7,9 @@ import LearnMoreFooter from "../components/LearnMore/LearnMoreFooter";
 
 export default function Pricing(): JSX.Element {
   return (
-    <Layout description="A better developer experience for React Native developers.">
+    <Layout
+      title="Pricing & Plans – Radon IDE: VSCode Extension for React Native"
+      description="Radon IDE is the best IDE for React Native. It comes with built-in tools for debugging, network inspection, and more. Try it for free or upgrade for more.">
       <div className={styles.preventfulContainer}>
         <div className={styles.container}>
           <PricingScreen />
