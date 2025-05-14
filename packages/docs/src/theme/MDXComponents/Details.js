1d7a2fa3cbabbe7d2d76f17f954ee0e9fb50d7c8
Patrycja Kalinska <cysiak0412@gmail.com>
2025-05-14 12:38:53 +0200

    smol fix of prop

--- packages/docs/src/theme/MDXComponents/Details.js
+++ packages/docs/src/theme/MDXComponents/Details.js
@@ -4,9 +4,7 @@ const MDXDetails = (props) => {
   const items = React.Children.toArray(props.children);
   // Split summary item from the rest to pass it as a separate prop to the
   // Details theme component
-  const summary = items.find(
-    (item) => React.isValidElement(item) && item.props?.mdxType === "summary"
-  );
+  const summary = items.find((item) => React.isValidElement(item) && item.type === "summary");
 
   const children = <>{items.filter((item) => item !== summary)}</>;
   return (
