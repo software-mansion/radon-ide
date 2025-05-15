eb69c6b2e6411dcd79d9b94d694a8f0fc322b27b
Krzysztof Magiera <krzys@swmansion.com>
2025-05-15 23:28:20 +0200

    Fix eslint config

--- packages/vscode-extension/eslint.config.mjs
+++ packages/vscode-extension/eslint.config.mjs
@@ -1,57 +1,23 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import _import from "eslint-plugin-import";
-import { fixupPluginRules } from "@eslint/compat";
-import tsParser from "@typescript-eslint/parser";
+import eslint from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
 
-export default defineConfig([
-  globalIgnores(["webview-ui/**/*"]),
+export default [
+  eslint.configs.recommended,
   {
-    plugins: {
-      "@typescript-eslint": typescriptEslint,
-      "import": fixupPluginRules(_import),
-    },
-
+    files: ["**/*.{ts,tsx,js,jsx}"],
     languageOptions: {
-      parser: tsParser,
-      ecmaVersion: 6,
-      sourceType: "module",
-    },
-
-    settings: {
-      "import/parsers": {
-        "@typescript-eslint/parser": [".ts", ".tsx"],
-      },
-
-      "import/resolver": {
-        typescript: {
-          alwaysTryTypes: true,
-        },
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
       },
     },
-
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
     rules: {
-      "curly": "warn",
-      "eqeqeq": "warn",
-      "no-throw-literal": "warn",
-      "semi": "off",
-      "@typescript-eslint/naming-convention": "off",
-      "@typescript-eslint/no-shadow": "error",
-
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        {
-          varsIgnorePattern: "^_",
-          args: "none",
-        },
-      ],
-
-      "import/order": [
-        "warn",
-        {
-          groups: ["builtin", "external"],
-        },
-      ],
+      ...tseslint.configs.recommended.rules,
     },
   },
-]);
+];
