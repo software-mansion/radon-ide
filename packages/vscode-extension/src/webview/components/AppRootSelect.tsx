5f0efe1e4fa921cdca4c1e45285f488199f6dbc5
Juliusz Wajgelt <juliusz.wajgelt@swmansion.com>
2025-05-20 14:38:13 +0200

    factor out common Select code

--- packages/vscode-extension/src/webview/components/AppRootSelect.tsx
+++ packages/vscode-extension/src/webview/components/AppRootSelect.tsx
@@ -1,61 +1,11 @@
-import React, { PropsWithChildren } from "react";
 import * as Select from "@radix-ui/react-select";
 import "./DeviceSelect.css";
 import "./AppRootSelect.css";
 import "./shared/Dropdown.css";
-import Tooltip from "./shared/Tooltip";
 import { useLaunchConfig } from "../providers/LaunchConfigProvider";
 import { useProject } from "../providers/ProjectProvider";
 import { ApplicationRoot } from "../../common/LaunchConfig";
-
-interface RichSelectItemProps extends Select.SelectItemProps {
-  icon: React.ReactNode;
-  title: string;
-  subtitle?: string;
-  isSelected?: boolean;
-}
-
-const RichSelectItem = React.forwardRef<HTMLDivElement, PropsWithChildren<RichSelectItemProps>>(
-  ({ children, icon, title, subtitle, isSelected, ...props }, forwardedRef) => {
-    function renderSubtitle() {
-      if (!subtitle) {
-        return null;
-      }
-
-      const subtitleComponent = <div className="device-select-rich-item-subtitle">{subtitle}</div>;
-      const isLongText = subtitle.length > 20;
-
-      if (isLongText) {
-        <Tooltip label={subtitle} side="right" instant>
-          {subtitleComponent}
-        </Tooltip>;
-      }
-      return subtitleComponent;
-    }
-
-    return (
-      <Select.Item className="device-select-rich-item" {...props} ref={forwardedRef}>
-        <div
-          className={
-            isSelected ? "device-select-rich-item-icon-selected" : "device-select-rich-item-icon"
-          }>
-          {icon}
-        </div>
-        <div>
-          {isSelected ? (
-            <div className="device-select-rich-item-title">
-              <b>{title}</b>
-            </div>
-          ) : (
-            <div className="device-select-rich-item-title">{title}</div>
-          )}
-
-          {renderSubtitle()}
-        </div>
-      </Select.Item>
-    );
-  }
-);
+import RichSelectItem from "./shared/RichSelectItem";
 
 function renderAppRoots(
   applicationRoots: ApplicationRoot[],
