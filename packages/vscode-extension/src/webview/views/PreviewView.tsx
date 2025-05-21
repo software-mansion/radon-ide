7109953b96160968f6d9e1f7dd3e6420770d4ac2
Krzysztof Magiera <krzys@swmansion.com>
2025-05-21 13:05:39 +0200

    Make app and device seletor look prettier (#1164)
    This PR updates the style of app and device switcher to look a bit
    better in my opinion.
    
    Before the buttons would only be rounded on one side with a gap in
    between. This looked a bit weird as you don't typically expect buttons
    to only be rounded on one side and not the other. This might've
    resembled the zoom switcher but that one doesn't have gaps.
    
    Also, the highlighted version blends too much with the background, and
    the "opened" menu has a flat bottom without rounded corners which also
    look a bit off.
    
    What we're changing here:
    1) Both buttons are now in a single container with background and
    rounded corners
    2) In between the buttons we placed a chevron icon
    3) We're changing the highlight effect to be fully enclosed within the
    group background
    4) The menu now is rounded from all the sides + we reduce the font size
    such that it matches the size in the trigger
    5) Handle shrinking when the panel is narrow
    
    Before:
    <img width="230" alt="image"
    src="https://github.com/user-attachments/assets/7a71a4dc-fa77-46e8-a7c1-508c1b7e2634"
    />
    
    Before highlighted:
    <img width="278" alt="image"
    src="https://github.com/user-attachments/assets/1e4259ab-8ec6-4129-97c7-dfb2a4834024"
    />
    
    Before opened:
    <img width="369" alt="image"
    src="https://github.com/user-attachments/assets/af311e14-9525-4b9a-ba85-f74965d08c5c"
    />
    
    
    After:
    <img width="327" alt="image"
    src="https://github.com/user-attachments/assets/459f9925-60f6-4b6f-93cf-a0ef793bb93a"
    />
    
    After highlighted:
    <img width="290" alt="image"
    src="https://github.com/user-attachments/assets/579b7fe0-dd32-4058-9ecb-31d78d9167b4"
    />
    
    After opened: 
    <img width="395" alt="image"
    src="https://github.com/user-attachments/assets/58be9427-9573-478f-8055-f0a3867a1a19"
    />
    
    After on narrow panel:
    <img width="332" alt="image"
    src="https://github.com/user-attachments/assets/bf0c5f80-517d-4ba8-8bc7-8412190289d5"
    />
    
    
    ### How Has This Been Tested: 
    1. Open IDE and play with the select buttons to check if everything is
    in order

--- packages/vscode-extension/src/webview/views/PreviewView.tsx
+++ packages/vscode-extension/src/webview/views/PreviewView.tsx
@@ -312,8 +312,11 @@ function PreviewView() {
 
         <span className="group-separator" />
 
-        <AppRootSelect />
-        <DeviceSelect />
+        <div className="app-device-group">
+          <AppRootSelect />
+          <span className="codicon codicon-chevron-right" />
+          <DeviceSelect />
+        </div>
 
         <div className="spacer" />
         {Platform.OS === "macos" && !hasActiveLicense && <ActivateLicenseButton />}
