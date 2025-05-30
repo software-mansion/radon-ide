4414dcd89a78884cf8c1ac231dbfb409e4d2c603
Ignacy Łątka <latkaignacy@gmail.com>
2025-05-30 15:53:45 +0200

    add working tool calling API logic

--- packages/vscode-extension/src/mcp/server.ts
+++ packages/vscode-extension/src/mcp/server.ts
@@ -46,44 +46,75 @@ interface ToolsInfo {
   tools: ToolSchema[];
 }
 
-const DEBUG_BACKEND_URL = "http://localhost:8000/api/";
-const BACKEND_URL = DEBUG_BACKEND_URL; // "https://radon-ai-backend.swmansion.com/api/";
-const TOOL_INFO_URL = BACKEND_URL + "get_tool_schema/";
-const TOOL_CALL_URL = BACKEND_URL + "tool_calls/";
+interface ToolResult {
+  tool_results: {
+    content: string;
+    tool_call_id: string;
+  }[];
+}
+
+const BACKEND_URL = "http://localhost:8000/"; // "https://radon-ai-backend.swmansion.com/api/";
 
 async function callTool(toolName: string, args: unknown): ToolResponse {
-  const url = TOOL_CALL_URL + toolName;
+  // this function is similar to `chat:invokeToolCall()`, could merge them in the future
   try {
-    return await fetch(url, {
+    const url = new URL("/api/tool_calls/", BACKEND_URL);
+    const response = await fetch(url, {
       method: "POST",
-      body: JSON.stringify(args),
-      headers: { Authorization: `Bearer ${getLicenseToken()}` },
-    }).then((res) => res.json());
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${getLicenseToken()}`,
+      },
+      body: JSON.stringify({
+        tool_calls: [
+          {
+            name: toolName,
+            id: "3241", // temporarily necessary placeholder
+            args,
+          },
+        ],
+      }),
+    });
+
+    if (!response.ok) {
+      // network error is not accurate, as the tool call might've been malformed by the agent
+      return "Failed tool call.";
+    }
+
+    const results: ToolResult = await response.json();
+
+    if (results.tool_results.length === 0) {
+      return "Tool response empty.";
+    }
+
+    const toolResults: string = results.tool_results[0].content;
+
+    return toolResults;
   } catch {
     return "Failed tool call.";
   }
 }
 
-function typeToZodType(schemaType: string): z.ZodType {
-  switch (schemaType) {
-    case "string":
-      // with current tools, it's always "z.string()"
-      return z.string();
-    default:
-      return z.unknown();
-  }
-}
-
 function buildZodSchema(toolSchema: ToolSchema): z.ZodType<unknown, z.ZodTypeDef, unknown> {
   const props = Object.values(toolSchema.inputSchema.properties);
-  const entries = props.map((v) => [v.title, typeToZodType(v.type)]);
+  const entries = props.map((v) => [v.title, z.string()]);
   const obj = z.object(Object.fromEntries(entries));
   return obj;
 }
 
 async function getToolSchema(): Promise<ToolsInfo> {
   try {
-    return (await fetch(TOOL_INFO_URL)).json();
+    const url = new URL("/api/get_tool_schema/", BACKEND_URL);
+    const resp = await fetch(url);
+
+    if (!resp.ok) {
+      Logger.error("Network error while fetching tool schema.");
+      return {
+        tools: [],
+      };
+    }
+
+    return resp.json();
   } catch {
     Logger.error("Failed fetching tool schema.");
     return {
@@ -121,6 +152,7 @@ async function screenshotToolDefinition(): ToolResponse {
     isError: false,
   };
 }
+
 async function startMcpServer() {
   const server = new LiteMCP("RadonAiServer", "1.0.0");
 
@@ -139,7 +171,8 @@ async function startMcpServer() {
       description: tool.description,
       parameters: zodSchema,
       execute: async (args): ToolResponse => {
-        return await callTool(tool.name, args);
+        const toolResponse = await callTool(tool.name, args);
+        return toolResponse;
       },
     });
   }
