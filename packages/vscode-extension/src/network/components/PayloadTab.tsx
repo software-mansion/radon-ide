0332faaf2a1b55a171493582bcef3f7928ab8dbc
Juliusz Wajgelt <49338439+jwajgelt@users.noreply.github.com>
2025-06-06 14:45:09 +0200

    fix Network Inspector crash caused by invalid request URLs (#1221)
    Currently, if a request with a malformed URL is made, the Network
    Inspector will crash when you try to open the request details.
    This PR safeguards against that.
    
    Fixes #1215
    
    ### How Has This Been Tested: 
    - open the network inspector
    - make a request to an invalid URL
    - open the details of the request
    - don't crash :)

--- packages/vscode-extension/src/network/components/PayloadTab.tsx
+++ packages/vscode-extension/src/network/components/PayloadTab.tsx
@@ -5,12 +5,16 @@ interface PayloadTabProps {
 }
 
 const getParams = (url: string): Record<string, string> => {
-  const urlObj = new URL(url);
-  const params: Record<string, string> = {};
-  urlObj.searchParams.forEach((value, key) => {
-    params[key] = value;
-  });
-  return params;
+  try {
+    const urlObj = new URL(url);
+    const params: Record<string, string> = {};
+    urlObj.searchParams.forEach((value, key) => {
+      params[key] = value;
+    });
+    return params;
+  } catch {
+    return {};
+  }
 };
 
 const PayloadTab = ({ networkLog }: PayloadTabProps) => {
