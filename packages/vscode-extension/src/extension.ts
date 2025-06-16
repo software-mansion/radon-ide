software-mansion/radon-ide

commit 4db4d4c3e645295b622bdfb2f241ed45a9a0bf3e

add keyboard shortcuts for quickly switching running devices (#1247)
Adds vscode commands and keyboard shortcuts for quickly switching
between running devices.
- "Cmd + Shift + ]" switches to the next running device
- "Cmd + Shift + [" switches to the previous running device

### Known issues:
- holding the shortcut causes the devices to be switched rapidly, extra
debug consoles appear (#1254)

### How Has This Been Tested: 
- open a project in radon
- launch multiple devices at the same time
- check the keyboard shortcuts work

packages/vscode-extension/src/project/DeviceSessionsManager.ts
@@ -1,4 +1,5 @@
-import { Disposable, window } from "vscode";
+import { commands, Disposable, window } from "vscode";
+import _ from "lodash";
 import { DeviceInfo, DevicePlatform } from "../common/DeviceManager";
 import { DeviceAlreadyUsedError, DeviceManager } from "../devices/DeviceManager";
 import { Logger } from "../Logger";
@@ -16,6 +17,7 @@ import { disposeAll } from "../utilities/disposables";
 import { DeviceId, DeviceSessionsManagerState } from "../common/Project";
 
 const LAST_SELECTED_DEVICE_KEY = "last_selected_device";
+const SWITCH_DEVICE_THROTTLE_MS = 300;
 
 export type DeviceSessionsManagerDelegate = {
   onInitialized(): void;
@@ -27,6 +29,7 @@ export class DeviceSessionsManager implements Disposable, DeviceSessionsManagerI
   private activeSessionId: DeviceId | undefined;
   private findingDevice: boolean = false;
   private previousDevices: DeviceInfo[] = [];
+  private disposables: Disposable[] = [];
 
   constructor(
     private readonly applicationContext: ApplicationContext,
@@ -36,6 +39,14 @@ export class DeviceSessionsManager implements Disposable, DeviceSessionsManagerI
     this.findInitialDeviceAndStartSession();
     this.deviceManager.addListener("deviceRemoved", this.removeDeviceListener);
     this.deviceManager.addListener("devicesChanged", this.devicesChangedListener);
+    this.disposables.push(
+      commands.registerCommand("RNIDE.nextRunningDevice", () => this.selectNextNthRunningSession(1))
+    );
+    this.disposables.push(
+      commands.registerCommand("RNIDE.previousRunningDevice", () =>
+        this.selectNextNthRunningSession(-1)
+      )
+    );
   }
 
   public get selectedDeviceSession(): DeviceSession | undefined {
@@ -181,7 +192,7 @@ export class DeviceSessionsManager implements Disposable, DeviceSessionsManagerI
     }
   };
 
-  private updateSelectedSession(session: DeviceSession | undefined) {
+  private async updateSelectedSession(session: DeviceSession | undefined) {
     const previousSession = this.selectedDeviceSession;
     this.activeSessionId = session?.getState().deviceInfo.id;
     if (previousSession === session) {
@@ -191,8 +202,8 @@ export class DeviceSessionsManager implements Disposable, DeviceSessionsManagerI
       this.deviceSessionManagerDelegate.onDeviceSessionsManagerStateChange(this.state);
       return;
     }
-    previousSession?.deactivate();
-    session.activate();
+    await previousSession?.deactivate();
+    await session.activate();
     extensionContext.workspaceState.update(LAST_SELECTED_DEVICE_KEY, this.activeSessionId);
     this.deviceSessionManagerDelegate.onDeviceSessionsManagerStateChange(this.state);
   }
@@ -226,7 +237,17 @@ export class DeviceSessionsManager implements Disposable, DeviceSessionsManagerI
     return undefined;
   }
 
+  private selectNextNthRunningSession = _.throttle((offset: number) => {
+    const runningSessions = this.deviceSessions.keys().toArray();
+    const currentSessionIndex =
+      this.activeSessionId !== undefined ? runningSessions.indexOf(this.activeSessionId) : -offset;
+    const nextSessionIndex =
+      (currentSessionIndex + offset + runningSessions.length) % runningSessions.length;
+    this.updateSelectedSession(this.deviceSessions.get(runningSessions[nextSessionIndex]));
+  }, SWITCH_DEVICE_THROTTLE_MS);
+
   dispose() {
+    disposeAll(this.disposables);
     disposeAll(this.deviceSessions.values().toArray());
     this.deviceManager.removeListener("deviceRemoved", this.removeDeviceListener);
     this.deviceManager.removeListener("devicesChanged", this.devicesChangedListener);

packages/vscode-extension/src/debugging/ProxyDebugAdapter.ts
@@ -223,21 +223,21 @@ export class ProxyDebugAdapter extends DebugSession {
     args: DebugProtocol.DisconnectArguments,
     request?: DebugProtocol.Request
   ) {
-    this.terminate();
+    await this.terminate();
     // since the application resumes once the debugger is disconnected, we need to send a continued event
     // to the frontend to update the UI
     this.sendEvent(new Event("RNIDE_continued"));
     this.sendResponse(response);
   }
 
-  private terminate() {
+  private async terminate() {
     if (this.terminated) {
       return;
     }
     this.terminated = true;
-    this.cdpProxy.stopServer();
+    await this.cdpProxy.stopServer();
     disposeAll(this.disposables);
-    vscode.commands.executeCommand("workbench.action.debug.stop", undefined, {
+    await vscode.commands.executeCommand("workbench.action.debug.stop", undefined, {
       sessionId: this.session.id,
     });
   }

commit b690281f6e73c4fb1e7bfb89e9d549b1fc9ea1cc

fix command registration causing the IDE to not reopen correctly (#1255)
Fixes a bug introduced in #1247 caused by registering vscode commands
multiple times (in the ctor of `DeviceSessionsManager`, which is not a
singleton) which broke the AppRoot switcher and broke the IDE tab when
closing and reopening

### How Has This Been Tested: 
- just switch an app
- or reopen the radon tab

packages/vscode-extension/src/extension.ts
@@ -216,6 +216,16 @@ export async function activate(context: ExtensionContext) {
   );
   context.subscriptions.push(commands.registerCommand("RNIDE.openChat", openChat));
 
+  context.subscriptions.push(
+    commands.registerCommand("RNIDE.nextRunningDevice", () =>
+      IDE.getInstanceIfExists()?.project.deviceSessionsManager.selectNextNthRunningSession(1)
+    )
+  );
+  context.subscriptions.push(
+    commands.registerCommand("RNIDE.previousRunningDevice", () =>
+      IDE.getInstanceIfExists()?.project.deviceSessionsManager.selectNextNthRunningSession(-1)
+    )
+  );
   // Debug adapter used by custom launch configuration, we register it in case someone tries to run the IDE configuration
   // The current workflow is that people shouldn't run it, but since it is listed under launch options it might happen
   // When it does happen, we open the IDE panel and restart the app.

packages/vscode-extension/src/project/ApplicationContext.ts
@@ -29,8 +29,11 @@ export class ApplicationContext implements Disposable {
 
     await this.dependencyManager.runAllDependencyChecks();
 
+    disposeAll(this.disposables);
+
     this.launchConfig = new LaunchConfigController(newAppRoot);
     this.buildCache = new BuildCache(newAppRoot);
+    this.disposables.push(this.launchConfig, this.dependencyManager);
   }
 
   public dispose() {

packages/vscode-extension/src/project/DeviceSessionsManager.ts
@@ -1,4 +1,4 @@
-import { commands, Disposable, window } from "vscode";
+import { Disposable, window } from "vscode";
 import _ from "lodash";
 import { DeviceInfo, DevicePlatform } from "../common/DeviceManager";
 import { DeviceAlreadyUsedError, DeviceManager } from "../devices/DeviceManager";
@@ -32,7 +32,6 @@ export class DeviceSessionsManager implements Disposable, DeviceSessionsManagerI
   private activeSessionId: DeviceId | undefined;
   private findingDevice: boolean = false;
   private previousDevices: DeviceInfo[] = [];
-  private disposables: Disposable[] = [];
 
   constructor(
     private readonly applicationContext: ApplicationContext,
@@ -42,14 +41,6 @@ export class DeviceSessionsManager implements Disposable, DeviceSessionsManagerI
     this.findInitialDeviceAndStartSession();
     this.deviceManager.addListener("deviceRemoved", this.removeDeviceListener);
     this.deviceManager.addListener("devicesChanged", this.devicesChangedListener);
-    this.disposables.push(
-      commands.registerCommand("RNIDE.nextRunningDevice", () => this.selectNextNthRunningSession(1))
-    );
-    this.disposables.push(
-      commands.registerCommand("RNIDE.previousRunningDevice", () =>
-        this.selectNextNthRunningSession(-1)
-      )
-    );
   }
 
   public get selectedDeviceSession(): DeviceSession | undefined {
@@ -271,7 +262,7 @@ export class DeviceSessionsManager implements Disposable, DeviceSessionsManagerI
     return undefined;
   }
 
-  private selectNextNthRunningSession = _.throttle((offset: number) => {
+  public selectNextNthRunningSession = _.throttle((offset: number) => {
     const runningSessions = this.deviceSessions.keys().toArray();
     const currentSessionIndex =
       this.activeSessionId !== undefined ? runningSessions.indexOf(this.activeSessionId) : -offset;
@@ -281,7 +272,6 @@ export class DeviceSessionsManager implements Disposable, DeviceSessionsManagerI
   }, SWITCH_DEVICE_THROTTLE_MS);
 
   dispose() {
-    disposeAll(this.disposables);
     disposeAll(this.deviceSessions.values().toArray());
     this.deviceManager.removeListener("deviceRemoved", this.removeDeviceListener);
     this.deviceManager.removeListener("devicesChanged", this.devicesChangedListener);