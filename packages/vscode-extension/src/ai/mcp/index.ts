29619a23b4bb1e7372f9883e2e7b31e4ac385c31
Krzysztof Magiera <krzys@swmansion.com>
2025-06-18 13:42:00 +0200

    Use vscode API for registerting MCP when available

--- packages/vscode-extension/src/ai/mcp/index.ts
+++ packages/vscode-extension/src/ai/mcp/index.ts
@@ -5,6 +5,9 @@ import { insertRadonEntry, newMcpConfig } from "./configCreator";
 import { readMcpConfig, writeMcpConfig } from "./fsReadWrite";
 import { startLocalMcpServer } from "./server";
 import { MCP_LOG } from "./utils";
+import { lm, McpHttpServerDefinition, Uri, EventEmitter } from "vscode";
+import "../../../vscode.mcpConfigurationProvider.d.ts";
+import { extensionContext } from "../../utilities/extensionContext";
 
 async function updateMcpConfig(port: number) {
   const mcpConfig = (await readMcpConfig()) || newMcpConfig();
@@ -12,7 +15,41 @@ async function updateMcpConfig(port: number) {
   await writeMcpConfig(updatedConfig);
 }
 
-async function loadRadonAi() {
+export function loadRadonAIOnVscode1_100() {
+  const serverStartPromise = startLocalMcpServer();
+  const didChangeEmitter = new EventEmitter<void>();
+
+  // version suffix is incremented whenever we get auth token update notification
+  // this way we request vscode to reload the tool on regular basis but also immediately
+  // after the user inputs the license token
+  let versionSuffix = 0;
+  extensionContext.subscriptions.push(
+    watchLicenseTokenChange(() => {
+      versionSuffix += 1;
+      didChangeEmitter.fire();
+    })
+  );
+
+  extensionContext.subscriptions.push(
+    lm.registerMcpServerDefinitionProvider("RadonAIMCPProvider", {
+      onDidChangeServerDefinitions: didChangeEmitter.event,
+      provideMcpServerDefinitions: async () => {
+        const port = await serverStartPromise;
+        return [
+          new McpHttpServerDefinition(
+            "RadonAI",
+            Uri.parse(`http://127.0.0.1:${port}/mcp`),
+            {},
+            extensionContext.extension.packageJSON.version + `.${versionSuffix}`
+          ),
+        ];
+      },
+    })
+  );
+  return true;
+}
+
+async function loadRadonAI() {
   try {
     // Server has to be online before the config is written
     const mcpPort = await startLocalMcpServer();
@@ -29,8 +66,16 @@ async function loadRadonAi() {
 }
 
 export default function registerRadonAi() {
-  watchLicenseTokenChange(() => {
-    // starts regardless of token validity - offline tools don't require a valid token
-    loadRadonAi();
-  });
+  // @ts-ignore lm.registerMcpServerDefinitionProvider API is only availble in VSCode 1.100+, we do runtime check here as it shouldn't be used in Cursor or Windsurf
+  if (lm.registerMcpServerDefinitionProvider) {
+    // We need to register MCP definition before extension's activate resolves.
+    // As this is a sync call, we don't need to await for the server to actually load
+    // and we can simply return here to not delay the activation flow.
+    // We will need the server to start before
+    return loadRadonAIOnVscode1_100();
+  } else {
+    watchLicenseTokenChange(() => {
+      loadRadonAI();
+    });
+  }
 }
