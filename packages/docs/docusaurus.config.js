3f9b2d1d206911beaaaab28ad7d85f5acec69185
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2025-07-02 08:05:21 +0200

    docs: add Google Tag Manager (#1289)

--- packages/docs/docusaurus.config.js
+++ packages/docs/docusaurus.config.js
@@ -39,6 +39,9 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+        googleTagManager: {
+          containerId: "GTM-532228ST",
+        },
       }),
     ],
   ],
