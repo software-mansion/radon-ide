6baadef41219a9b4ec0d7e9c5c2de66e7c13d350
pFornagiel <pawel.fornagiel2@gmail.com>
2025-07-04 10:50:26 +0200

    feat: [ZOOM NOT WORKING] implement device rotation handling using pixel resizing and update CSS custom properties for device sizing

--- packages/vscode-extension/src/webview/Preview/Device/Device.tsx
+++ packages/vscode-extension/src/webview/Preview/Device/Device.tsx
@@ -7,6 +7,13 @@ import { useProject } from "../../providers/ProjectProvider";
 import { DeviceProperties, DevicePropertiesFrame } from "../../utilities/deviceContants";
 import { DeviceRotationType } from "../../../common/Project";
 
+const rotationAngle = {
+  Portrait: "0deg",
+  LandscapeLeft: "-90deg",
+  LandscapeRight: "90deg",
+  PortraitUpsideDown: "180deg",
+}
+
 interface DeviceProps {
   device: DeviceProperties;
   resizableProps: ResizableProps;
@@ -17,27 +24,47 @@ function cssPropertiesForDevice(
   device: DeviceProperties,
   frame: DevicePropertiesFrame,
   rotation: DeviceRotationType,
-  phoneElement: HTMLDivElement | null
+  phoneElement: HTMLDivElement | null,
+  resizableProps: ResizableProps
 ) {
-  let rotate = "0deg";
-  let scale = 1;
+  const aspectRatio = frame.width / frame.height;
+  const minHeight = 350;
+  const minWidth = minHeight * aspectRatio;
+  const rotate = rotationAngle[rotation] || "0deg";
+  const isHorizontal = rotation === "LandscapeLeft" || rotation === "LandscapeRight"
+  
+  
+  let newHeight = "100%";
+  let newWidth = "auto";
+  
+  if (isHorizontal) {
+    const parentWidth =
+    phoneElement?.parentElement?.parentElement?.clientWidth || window.innerWidth;
+    const parentHeight =
+    phoneElement?.parentElement?.parentElement?.clientHeight || window.innerHeight;
+    // const resizedHeight = parseFloat(resizableProps.size?.height?.toString()) || parentHeight
 
-  if (rotation === "LandscapeLeft" || rotation === "LandscapeRight") {
-    rotate = rotation === "LandscapeLeft" ? "-90deg" : "90deg";
-    if (phoneElement) {
-      scale = (Math.min(1, window.innerWidth / frame.height) * frame.height) / frame.width;
-    }
+    const adjustedHeight =
+      parentWidth * aspectRatio < parentHeight
+        ? parentWidth
+        : Math.max(parentHeight / aspectRatio, minHeight);
+
+    newHeight = `${adjustedHeight}px`;
+    newWidth = `${adjustedHeight * aspectRatio}px`;
   }
 
   return {
+    "--phone-content-min-height": `${minHeight}px`,
+    "--phone-content-min-width": `${minWidth}px`,
+    "--phone-content-width": newWidth,
+    "--phone-content-height": newHeight,
     "--phone-screen-height": `${(device.screenHeight / frame.height) * 100}%`,
     "--phone-screen-width": `${(device.screenWidth / frame.width) * 100}%`,
-    "--phone-aspect-ratio": `${frame.width / frame.height}`,
+    "--phone-aspect-ratio": `${aspectRatio}`,
     "--phone-top": `${(frame.offsetY / frame.height) * 100}%`,
     "--phone-left": `${(frame.offsetX / frame.width) * 100}%`,
     "--phone-mask-image": `url(${device.maskImage})`,
     "rotate": rotate,
-    "transform": `scale(${scale})`,
   };
 }
 
@@ -48,29 +75,34 @@ export default function Device({ device, resizableProps, children }: DeviceProps
   const rotation =
     selectedDeviceSession?.status === "running" ? selectedDeviceSession.rotation : "Portrait";
 
-  const handleResize = () => {
-    if (phoneContentRef.current) {
-      const style = cssPropertiesForDevice(device, frame, rotation, phoneContentRef.current);
-      phoneContentRef.current.style.setProperty("transform", style["transform"] as string);
-    }
-  };
-
   useEffect(() => {
-    if (rotation === "LandscapeLeft" || rotation === "LandscapeRight") {
-      window.addEventListener("resize", handleResize);
-    }
+    const handleResize = () => {
+      if (phoneContentRef.current) {
+        const style = cssPropertiesForDevice(device, frame, rotation, phoneContentRef.current, resizableProps);
+        phoneContentRef.current?.style.setProperty(
+          "--phone-content-width",
+          style["--phone-content-width"]
+        );
+        phoneContentRef.current?.style.setProperty(
+          "--phone-content-height",
+          style["--phone-content-height"]
+        );
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
 
     return () => {
       window.removeEventListener("resize", handleResize);
     };
-  }, [rotation, device, frame]);
+  }, [rotation, device, frame]); // React to changes in rotation, device, or frame
 
   return (
     <Resizable {...resizableProps}>
       <div
         ref={phoneContentRef}
         className="phone-content"
-        style={cssPropertiesForDevice(device, frame, rotation, phoneContentRef.current)}>
+        style={cssPropertiesForDevice(device, frame, rotation, phoneContentRef.current, resizableProps)}>
         <DeviceFrame frame={frame} />
         <img src={device.screenImage} className="phone-screen-background" />
         {children}
