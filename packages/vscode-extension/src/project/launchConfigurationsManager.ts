d2e80bb79c42ce3ce4b1cfa1460f0a503d4461bd
Juliusz Wajgelt <juliusz.wajgelt@swmansion.com>
2025-07-04 09:45:54 +0200

    add warning about multiple approots

--- packages/vscode-extension/src/project/launchConfigurationsManager.ts
+++ packages/vscode-extension/src/project/launchConfigurationsManager.ts
@@ -1,19 +1,34 @@
 import path from "path";
 import { ConfigurationChangeEvent, Disposable, EventEmitter, workspace } from "vscode";
+import vscode from "vscode";
 import { LaunchConfiguration, LaunchConfigurationOptions } from "../common/LaunchConfig";
 import { Logger } from "../Logger";
 import { findAppRootCandidates } from "../utilities/extensionContext";
 import { getLaunchConfigurations } from "../utilities/launchConfiguration";
 
+function findDefaultAppRoot(showWarning = false) {
+  const appRoots = findAppRootCandidates();
+  const defaultAppRoot = appRoots.length > 0 ? appRoots[0] : undefined;
+  if (appRoots.length > 0 && showWarning) {
+    vscode.window
+      .showWarningMessage(
+        "Multiple application roots found in workspace, but no 'appRoot' specified in launch configuration. Using the first found application root: " +
+          defaultAppRoot,
+        "Add Launch Configuration"
+      )
+      .then((selection) => {
+        if (selection === "Add Launch Configuration") {
+          vscode.commands.executeCommand("debug.addConfiguration");
+        }
+      });
+  }
+  return defaultAppRoot;
+}
+
 export function launchConfigurationFromOptions(
   options: LaunchConfigurationOptions
 ): LaunchConfiguration {
-  const appRoots = findAppRootCandidates();
-  const defaultAppRoot = appRoots.length > 0 ? appRoots[0] : undefined;
-  if (appRoots.length > 0) {
-    // TODO: warning?
-  }
-  return launchConfigFromOptionsWithDefaultAppRoot(options, defaultAppRoot);
+  return launchConfigFromOptionsWithDefaultAppRoot(options, findDefaultAppRoot());
 }
 
 function launchConfigFromOptionsWithDefaultAppRoot(
@@ -21,8 +36,11 @@ function launchConfigFromOptionsWithDefaultAppRoot(
   defaultAppRoot: string | undefined
 ): LaunchConfiguration {
   if ((options.appRoot ?? defaultAppRoot) === undefined) {
+    const maybeName =
+      options.name === undefined ? "" : ` for launch configuration "${options.name}"`;
     throw new Error(
-      "No app root found. Please specify 'appRoot' in your launch configuration or ensure your workspace contains a valid React Native or Expo project."
+      `No app root found${maybeName}.` +
+        ` Please specify 'appRoot' in your launch configuration or ensure your workspace contains a valid React Native or Expo project.`
     );
   }
   const appRoot = (options.appRoot ?? defaultAppRoot) as string;
@@ -40,11 +58,7 @@ function launchConfigFromOptionsWithDefaultAppRoot(
 
 function createLaunchConfigs() {
   const launchConfigOptions = getLaunchConfigurations();
-  const appRoots = findAppRootCandidates();
-  const defaultAppRoot = appRoots.length > 0 ? appRoots[0] : undefined;
-  if (appRoots.length > 0) {
-    // TODO: warning?
-  }
+  const defaultAppRoot = findDefaultAppRoot();
   const launchConfigurations = launchConfigOptions.flatMap<LaunchConfiguration>((config) => {
     try {
       return [launchConfigFromOptionsWithDefaultAppRoot(config, defaultAppRoot)];
@@ -86,7 +100,7 @@ export class LaunchConfigurationsManager implements Disposable {
     if (this._launchConfigurations.length > 0) {
       return this._launchConfigurations[0];
     }
-    return launchConfigurationFromOptions({});
+    return launchConfigFromOptionsWithDefaultAppRoot({}, findDefaultAppRoot(true));
   }
 
   dispose() {
