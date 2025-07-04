365524b6dde19ba62c30800392e1281587182f7e
Ignacy Łątka <latkaignacy@gmail.com>
2025-07-04 17:14:03 +0200

    Fix local MCP server not being reloaded on license token change (#1296)
    Local MCP was intended to be restarted on every change of the license
    token.
    This behaviour ensures that the paid MCP tools are loaded as soon as a
    valid license is available.
    
    With direct-injection mode (vscode `1.101+`), this behaviour was bugged,
    and the server never restarted
    due to the server-starting promise being only executed once before being
    cached.
    
    This PR: 
    - Fixes said bug
    - Adds a missing `Disposable` (`watchLicenseTokenChange`) to the
    `extensionContext.subscriptions` array
    - Prevents AI registration from blocking IDE initialisation
    - Applies a more uniform naming scheme in the `ai/mcp/index.ts`.
    
    ### How Has This Been Tested: 
    
    - On each token reload, a new `port` is being used, and the server now
    sends appropriate web requests to the backend.

--- packages/vscode-extension/src/ai/mcp/index.ts
+++ packages/vscode-extension/src/ai/mcp/index.ts
@@ -1,3 +1,4 @@
+import { lm, McpHttpServerDefinition, Uri, EventEmitter, version } from "vscode";
 import { Logger } from "../../Logger";
 import { watchLicenseTokenChange } from "../../utilities/license";
 import { getTelemetryReporter } from "../../utilities/telemetry";
@@ -5,7 +6,6 @@ import { insertRadonEntry, newMcpConfig } from "./configCreator";
 import { readMcpConfig, writeMcpConfig } from "./fsReadWrite";
 import { startLocalMcpServer } from "./server";
 import { MCP_LOG } from "./utils";
-import { lm, McpHttpServerDefinition, Uri, EventEmitter, version } from "vscode";
 import "../../../vscode.mcpConfigurationProvider.d.ts";
 import { extensionContext } from "../../utilities/extensionContext";
 
@@ -15,8 +15,7 @@ async function updateMcpConfig(port: number) {
   await writeMcpConfig(updatedConfig);
 }
 
-export function loadRadonAIOnVscode1_101() {
-  const serverStartPromise = startLocalMcpServer();
+function directLoadRadonAI() {
   const didChangeEmitter = new EventEmitter<void>();
 
   // version suffix is incremented whenever we get auth token update notification
@@ -34,7 +33,7 @@ export function loadRadonAIOnVscode1_101() {
     lm.registerMcpServerDefinitionProvider("RadonAIMCPProvider", {
       onDidChangeServerDefinitions: didChangeEmitter.event,
       provideMcpServerDefinitions: async () => {
-        const port = await serverStartPromise;
+        const port = await startLocalMcpServer();
         return [
           new McpHttpServerDefinition(
             "RadonAI",
@@ -46,10 +45,9 @@ export function loadRadonAIOnVscode1_101() {
       },
     })
   );
-  return true;
 }
 
-async function loadRadonAI() {
+async function fsLoadRadonAI() {
   try {
     // Server has to be online before the config is written
     const mcpPort = await startLocalMcpServer();
@@ -65,20 +63,22 @@ async function loadRadonAI() {
   }
 }
 
-export default function registerRadonAi() {
-  if (
-    // @ts-ignore lm.registerMcpServerDefinitionProvider API is only availble in VSCode 1.101+, we do runtime check here as it shouldn't be used in Cursor or Windsurf
+function isDirectLoadingAvailable() {
+  return (
+    // @ts-ignore lm.registerMcpServerDefinitionProvider API is only available in VSCode 1.101+. Cursor and Windsurf both use <1.101 VSCode versions
     lm.registerMcpServerDefinitionProvider &&
     version.localeCompare("1.101.0", undefined, { numeric: true }) >= 0
-  ) {
-    // We need to register MCP definition before extension's activate resolves.
-    // As this is a sync call, we don't need to await for the server to actually load
-    // and we can simply return here to not delay the activation flow.
-    // We will need the server to start before
-    return loadRadonAIOnVscode1_101();
+  );
+}
+
+export default function registerRadonAi() {
+  if (isDirectLoadingAvailable()) {
+    directLoadRadonAI();
   } else {
-    watchLicenseTokenChange(() => {
-      loadRadonAI();
-    });
+    extensionContext.subscriptions.push(
+      watchLicenseTokenChange(() => {
+        fsLoadRadonAI();
+      })
+    );
   }
 }
