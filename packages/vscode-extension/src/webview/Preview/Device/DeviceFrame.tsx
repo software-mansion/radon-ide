bb459f512748893d523e4fe465862ef757ba8c32
pFornagiel <pawel.fornagiel2@gmail.com>
2025-07-09 11:53:10 +0200

    fix: fix the scrollbar appearing by default on Portait mode when frame is off by removing unnecessary workaround from previous code

--- packages/vscode-extension/src/webview/Preview/Device/DeviceFrame.tsx
+++ packages/vscode-extension/src/webview/Preview/Device/DeviceFrame.tsx
@@ -9,18 +9,11 @@ function DeviceFrame({ frame }: DeviceFrameProps) {
     return null;
   }
 
-  if (frame.type === "mask") {
-    return (
-      <div style={{ "--phone-frame-image": `url(${frame.image})` }} className="phone-bezel">
-        {/* ZoomControl works in away that we change the height, and leave width auto which works for images,
-            that have it's intrinsic width and height, but I couldn't achieve the same effect with the div,
-            thus we add the image and use opacity 0 to hide it. */}
-        <img src={frame.image} className="phone-bezel-image" />
-      </div>
-    );
-  }
-
-  return <img src={frame.image} className="phone-frame" />;
+  return frame.type === "mask" ? (
+    <div style={{ "--phone-frame-image": `url(${frame.image})` } as React.CSSProperties} className="phone-bezel"></div>
+  ) : (
+    <img src={frame.image} className="phone-frame" />
+  );
 }
 
 export default DeviceFrame;
