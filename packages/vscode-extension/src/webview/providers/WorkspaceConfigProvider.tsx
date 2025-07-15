software-mansion/radon-ide

commit 88840dd4f3a7e0695107519e0e6a896834198d10

Delete theme type setting (#1324)
This PR deletes all the code that was supporting the theme type
selection.

Historically, we had our own theme for the panel UI for light and dark
editor themes. A while ago, we added a way to match the editor theme
which in most of the cases look much better and consistent. Now, with us
adapting vscode-elements component library, we don't want to fight the
vscode matching elements any longer such that they look ok on the theme
not many people use.

This PR phases out the support for the setting while only keeping it in
in the setting scheme with the deprecation notice in case anyone still
has it set.

There was one variable `--swm-select-item-disabled-background` that was
present in the built-in themes but missing in the vscode ones. I
investigated this variable and found out it was used by the select
dropdown for disabled options. I consulted the existing vscode
components, which instead of changing the background for disabled items,
change the foreground only. Finally, I ended up updating the stylesheet
to no longer use that variable, and instead use a proper disabled
foreground color for disable select items.

### How Has This Been Tested: 
1) Open Radon IDE and see it look ok with the editor theme
2) Set the theme setting to "built-in" and make sure it falls back to
the editor theme.

---------

Co-authored-by: Juliusz Wajgelt <49338439+jwajgelt@users.noreply.github.com>

packages/vscode-extension/src/webview/providers/WorkspaceConfigProvider.tsx
@@ -3,7 +3,6 @@ import {
   useContext,
   createContext,
   useState,
-  useEffect,
   useCallback,
   useMemo,
 } from "react";
@@ -19,7 +18,6 @@ type WorkspaceConfigContextType = WorkspaceConfigProps & {
 const INITIAL_WORKSPACE_CONFIG: WorkspaceConfigProps = {
   panelLocation: "tab",
   showDeviceFrame: true,
-  themeType: "vscode",
   stopPreviousDevices: false,
 };
 
@@ -30,20 +28,6 @@ const WorkspaceConfigContext = createContext<WorkspaceConfigContextType>({
 
 export default function WorkspaceConfigProvider({ children }: PropsWithChildren) {
   const [config, setConfig] = useState<WorkspaceConfigProps>(INITIAL_WORKSPACE_CONFIG);
-
-  useEffect(() => {
-    function watchConfigChange(e: WorkspaceConfigProps) {
-      document.body.setAttribute("data-use-code-theme", `${e.themeType === "vscode"}`);
-      setConfig(e);
-    }
-
-    workspaceConfig.getConfig().then(watchConfigChange);
-    workspaceConfig.addListener("configChange", watchConfigChange);
-
-    return () => {
-      workspaceConfig.removeListener("configChange", watchConfigChange);
-    };
-  }, []);
 
   const update = useCallback(
     <K extends keyof WorkspaceConfigProps>(

packages/vscode-extension/src/common/WorkspaceConfig.ts
@@ -1,10 +1,8 @@
 export type PanelLocation = "tab" | "side-panel";
-export type ThemeType = "vscode" | "built-in";
 
 export type WorkspaceConfigProps = {
   panelLocation: PanelLocation;
   showDeviceFrame: boolean;
-  themeType: ThemeType;
   stopPreviousDevices: boolean;
 };
 

packages/vscode-extension/src/panels/WorkspaceConfigController.ts
@@ -6,7 +6,6 @@ import {
   WorkspaceConfigProps,
   WorkspaceConfigEventMap,
   WorkspaceConfigEventListener,
-  ThemeType,
 } from "../common/WorkspaceConfig";
 import { getTelemetryReporter } from "../utilities/telemetry";
 
@@ -20,7 +19,6 @@ export class WorkspaceConfigController implements Disposable, WorkspaceConfig {
     this.config = {
       panelLocation: configuration.get<PanelLocation>("panelLocation")!,
       showDeviceFrame: configuration.get<boolean>("showDeviceFrame")!,
-      themeType: configuration.get<ThemeType>("themeType")!,
       stopPreviousDevices: configuration.get<boolean>("stopPreviousDevices")!,
     };
 
@@ -33,7 +31,6 @@ export class WorkspaceConfigController implements Disposable, WorkspaceConfig {
       const newConfig = {
         panelLocation: config.get<PanelLocation>("panelLocation")!,
         showDeviceFrame: config.get<boolean>("showDeviceFrame")!,
-        themeType: config.get<ThemeType>("themeType")!,
         stopPreviousDevices: config.get<boolean>("stopPreviousDevices")!,
       };
 

packages/vscode-extension/src/panels/webviewContentGenerator.ts
@@ -1,4 +1,4 @@
-import { ExtensionContext, Webview, Uri, workspace, ExtensionMode } from "vscode";
+import { ExtensionContext, Webview, Uri, ExtensionMode } from "vscode";
 import { getUri } from "../utilities/getUri";
 import { getNonce } from "../utilities/getNonce";
 import { Platform } from "../utilities/platform";
@@ -13,8 +13,6 @@ export function generateWebviewContent(
   wsEndpoint?: string,
   allowUnsafeEval?: boolean
 ) {
-  const config = workspace.getConfiguration("RadonIDE");
-  const useCodeTheme = config.get("themeType") === "vscode";
   const IS_DEV = context.extensionMode === ExtensionMode.Development;
 
   // The JS file from the React build output
@@ -74,7 +72,7 @@ export function generateWebviewContent(
         `
         }
       </head>
-      <body data-use-code-theme="${useCodeTheme}">
+      <body>
         <div id="root"></div>
         <script nonce="${nonce}">window.RNIDE_hostOS = "${Platform.OS}";window.RNIDE_isDev = ${IS_DEV};</script>
         <script type="module" nonce="${nonce}" src="${scriptUri}"></script>

packages/vscode-extension/src/webview/components/SettingsDropdown.tsx
@@ -22,7 +22,7 @@ interface SettingsDropdownProps {
 }
 
 function SettingsDropdown({ project, isDeviceRunning, children, disabled }: SettingsDropdownProps) {
-  const { panelLocation, themeType, update } = useWorkspaceConfig();
+  const { panelLocation } = useWorkspaceConfig();
   const { openModal } = useModal();
   const { movePanelTo, reportIssue } = useUtils();
   const { telemetryEnabled } = useTelemetry();
@@ -116,34 +116,6 @@ function SettingsDropdown({ project, isDeviceRunning, children, disabled }: Sett
               <div className="dropdown-menu-item-content">Report Issue</div>
             </span>
           </DropdownMenu.Item>
-          <DropdownMenu.Sub>
-            <DropdownMenu.SubTrigger className="dropdown-menu-item">
-              <span className="codicon codicon-layout" />
-              Theme
-              <span className="codicon codicon-chevron-right right-slot" />
-            </DropdownMenu.SubTrigger>
-            <DropdownMenu.Portal>
-              <DropdownMenu.SubContent
-                className="dropdown-menu-subcontent"
-                sideOffset={2}
-                alignOffset={-5}>
-                <DropdownMenu.Item
-                  className="dropdown-menu-item"
-                  onSelect={() => update("themeType", "vscode")}>
-                  Match editor theme
-                  {themeType === "vscode" && <span className="codicon codicon-check right-slot" />}
-                </DropdownMenu.Item>
-                <DropdownMenu.Item
-                  className="dropdown-menu-item"
-                  onSelect={() => update("themeType", "built-in")}>
-                  Use Radon IDE theme
-                  {themeType === "built-in" && (
-                    <span className="codicon codicon-check right-slot" />
-                  )}
-                </DropdownMenu.Item>
-              </DropdownMenu.SubContent>
-            </DropdownMenu.Portal>
-          </DropdownMenu.Sub>
           {telemetryEnabled && <SendFeedbackItem />}
           <div className="dropdown-menu-item device-settings-version-text">
             Radon IDE version: {extensionVersion}

packages/vscode-extension/src/webview/components/shared/Select.css
@@ -62,8 +62,7 @@ button {
 }
 
 .select-item[data-disabled] {
-  background-color: var(--swm-select-item-disabled-background);
-  color: var(--swm-secondary-text);
+  color: var(--swm-select-disabled);
   pointer-events: none;
 }
 

packages/vscode-extension/src/webview/styles/theme.css
@@ -139,367 +139,7 @@
   --swm-button-size: 30px;
 }
 
-/* Light theme */
-body[data-vscode-theme-kind="vscode-light"],
-body[data-vscode-theme-kind="vscode-high-contrast-light"] {
-  --swm-active-item: var(--blue-dark-100);
-  --swm-preview-background: var(--white);
-
-  --swm-default-text: var(--navy-light-100);
-  --swm-marked-text: var(--yellow-dark-120);
-  --swm-highlighted-text: var(--navy-light-40);
-  --swm-error-color: var(--red-light-100);
-  --swm-secondary-text: var(--navy-light-60);
-  --swm-disabled-text: var(--navy-light-20);
-
-  --swm-dropdown-item-highlighted: var(--navy-light-10);
-
-  --swm-popover-background: var(--off-white);
-
-  --swm-separator: var(--navy-light-40);
-
-  /* Shadows */
-  --swm-focus-outline: 0 0 0 2px var(--navy-light-20);
-  --swm-backdrop-shadow:
-    0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1),
-    1px -4px 6px -3px rgb(0 0 0 / 0.1);
-  /* Device select */
-  --swm-device-select-background: var(--navy-light-10);
-  --swm-device-select-trigger-text: var(--navy-light-100);
-  --swm-device-select-trigger-hover: var(--navy-light-10);
-  --swm-device-select-trigger-disabled: var(--white);
-  --swm-device-select-separator: var(--navy-light-40);
-  --swm-device-select-highlighted: var(--off-white);
-  --swm-device-select-disabled-text: var(--navy-light-20);
-  --swm-device-select-stop-badge-background: var(--red-light-100);
-  --swm-device-select-stop-badge-foreground: var(--navy-light-10);
-  --swm-device-select-label: var(--navy-light-60);
-  --swm-device-select-rich-item-text: var(--navy-light-100);
-  --swm-device-select-rich-item-subtitle: var(--navy-light-40);
-  --swm-device-select-rich-item-icon: var(--navy-light-20);
-  --swm-device-select-rich-item-icon-selected: var(--navy-light-40);
-
-  /* Slider */
-  --swm-slider: var(--navy-light-60);
-
-  /* Radio group */
-  --swm-radio-group-item: var(--navy-light-60);
-
-  /* Startup message */
-  --swm-startup-message: var(--off-white);
-  --swm-phone-content-loading-background: rgba(0, 0, 0, 0.75);
-
-  /* Button */
-  --swm-button: var(--swm-default-text);
-  --swm-button-background: var(--navy-light-20);
-  --swm-button-disabled: var(--swm-disabled-text);
-  --swm-button-hover: var(--navy-light-10);
-
-  --swm-button-active: var(--navy-light-100);
-  --swm-button-active-background: var(--navy-light-20);
-
-  --swm-button-selected: var(--white);
-  --swm-button-selected-background: var(--navy-light-60);
-  --swm-button-selected-hover: var(--off-white);
-  --swm-button-selected-hover-background: var(--navy-light-40);
-
-  --swm-button-secondary: var(--navy-light-80);
-  --swm-button-secondary-background: var(--navy-light-10);
-  --swm-button-secondary-hover: var(--navy-light-20);
-
-  --swm-button-ternary: var(--off-white);
-  --swm-button-ternary-background: var(--navy-light-60);
-  --swm-button-ternary-hover: var(--navy-light-40);
-
-  --swm-button-counter: var(--off-white);
-  --swm-button-counter-background: var(--navy-light-60);
-  --swm-button-counter-border: var(--navy-light-60);
-
-  --swm-button-recording-on-background: var(--navy-light-60);
-  --swm-button-replay-hover: var(--navy-light-transparent);
-
-  /* Tooltip */
-  --swm-tooltip-primary: var(--swm-default-text);
-  --swm-tooltip-primary-background: var(--swm-popover-background);
-  --swm-tooltip-secondary: var(--swm-default-text);
-  --swm-tooltip-secondary-background: var(--navy-light-10);
-  --swm-tooltip-ternary: var(--navy-dark-100);
-  --swm-tooltip-ternary-background: var(--off-white);
-
-  /* ScrollArea */
-  --swm-scroll-area-background: var(--white);
-  --swm-scroll-area-shadow: 0 0 0 1px var(--navy-light-10);
-  --swm-scrollbar-background: var(--navy-light-10);
-  --swm-scrollbar-thumb: var(--navy-light-60);
-  --swm-scrollbar-hover: var(--navy-light-20);
-
-  /* Input */
-  --swm-input-background: var(--white);
-  --swm-input-shadow: 0 0 0 1px var(--navy-light-10);
-
-  /* Select */
-  --swm-select-background: var(--white);
-  --swm-select-hover-background: var(--off-white);
-  --swm-select-shadow: 0 0 0 1px var(--navy-light-10);
-  --swm-select-disabled: var(--navy-light-20);
-  --swm-select-disabled-background: var(--navy-light-10);
-  --swm-select-content-background: var(--navy-light-10);
-  --swm-select-item-disabled-background: var(--navy-light-40);
-  --swm-select-item-highlighted-background: var(--off-white);
-  --swm-focus-outline-color: var(--navy-dark-10);
-  --swm-select-border: var(--navy-light-40);
-
-  /* Input */
-  --swm-input-background: var(--white);
-  --swm-input-border: var(--navy-light-40);
-
-  /* Switch */
-  --swm-switch-root-background: var(--navy-light-10);
-  --swm-switch-root-shadow: 0 0 0 1px var(--navy-light-10);
-  --swm-switch-root-disabled: var(--navy-dark-20);
-  --swm-switch-root-background-checked: var(--navy-light-40);
-
-  /* Manage devices */
-  --swm-device-title: var(--swm-default-text);
-  --swm-device-subtitle: var(--navy-light-60);
-  --swm-device-icon: var(--swm-default-text);
-  --swm-device-icon-background: var(--navy-light-20);
-  --swm-device-icon-background-selected: var(--green-light-80);
-  --swm-device-delete-icon: var(--red-light-100);
-  --swm-device-warning-icon: var(--yellow-light-80);
-
-  /* Devices not found */
-  --swm-devices-not-found-icon: var(--white);
-  --swm-devices-not-found-icon-background: var(--navy-light-20);
-
-  /* URL select */
-  --swm-url-select: var(--navy-light-100);
-  --swm-url-select-background: var(--navy-light-10);
-  --swm-url-select-hover-background: var(--navy-light-10);
-  --swm-url-select-disabled-background: var(--off-white);
-  --swm-url-select-highlighted-background: var(--white);
-  --swm-url-select-label: var(--navy-light-60);
-  --swm-url-select-special-text: var(--navy-light-60);
-  --swm-url-select-separator: var(--navy-light-40);
-
-  /* Debugger */
-  --swm-debugger-label: var(--off-white);
-  --swm-debugger-button-group: var(--white);
-  --swm-debugger-button-group-background: var(--navy-light-20);
-  --swm-debugger-button-group-border: 1px solid var(--navy-light-40);
-
-  /* Zoom controls */
-  --swm-zoom-controls-background: var(--navy-light-20);
-  --swm-zoom-controls-button-background-hover: var(--navy-light-10);
-  --swm-zoom-select-trigger-text: var(--navy-light-100);
-  --swm-zoom-select-trigger-hover: var(--navy-light-10);
-  --swm-zoom-select-trigger-disabled: var(--white);
-  --swm-zoom-select-highlighted: var(--off-white);
-  --swm-zoom-select-disabled-text: var(--navy-light-20);
-  --swm-zoom-select-separator: var(--navy-light-40);
-
-  /* Replay */
-  --swm-replay-overlay-background: linear-gradient(
-    0deg,
-    rgba(232, 232, 232, 0.9),
-    rgba(185, 185, 185, 0.9)
-  );
-
-  --swm-replay-len-select: var(--swm-default-text);
-  --swm-replay-len-select-background: var(--grey-light-60);
-  --swm-replay-len-select-hover-background: var(--navy-light-transparent);
-  --swm-replay-len-select-highlighted-background: var(--grey-light-80);
-
-  /* Open Deep Link */
-  --deep-link-history-border: var(--navy-light-10);
-  --deep-link-history-selected-item-background: var(--navy-light-10);
-
-  --swm-radio-group-indicator: var(--navy-light-10);
-}
-
-/* Dark theme */
-body[data-vscode-theme-kind="vscode-dark"],
-body[data-vscode-theme-kind="vscode-high-contrast"] {
-  --swm-active-item: var(--blue-dark-100);
-  --swm-preview-background: var(--background-dark-100);
-
-  --swm-default-text: var(--off-white);
-  --swm-marked-text: var(--yellow-dark-100);
-  --swm-highlighted-text: black;
-  --swm-error-color: var(--red-dark-100);
-  --swm-secondary-text: var(--background-dark-10);
-  --swm-disabled-text: var(--background-dark-40);
-
-  --swm-dropdown-item-highlighted: var(--background-dark-70);
-
-  --swm-popover-background: var(--background-dark-80);
-
-  --swm-separator: var(--background-dark-40);
-
-  /* Shadows */
-  --swm-focus-outline: 0 0 0 2px var(--background-dark-50);
-  --swm-backdrop-shadow:
-    0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1),
-    1px -4px 6px -3px rgb(0 0 0 / 0.1);
-  /* Device select */
-  --swm-device-select-background: var(--background-dark-80);
-  --swm-device-select-trigger-text: var(--off-white);
-  --swm-device-select-trigger-hover: var(--background-dark-70);
-  --swm-device-select-trigger-disabled: var(--background-dark-90);
-  --swm-device-select-separator: var(--background-dark-50);
-  --swm-device-select-highlighted: var(--background-dark-60);
-  --swm-device-select-disabled-text: var(--background-dark-60);
-  --swm-device-select-stop-badge-background: var(--red-dark-100);
-  --swm-device-select-stop-badge-foreground: var(--off-white);
-  --swm-device-select-label: var(--background-dark-30);
-  --swm-device-select-rich-item-text: var(--off-white);
-  --swm-device-select-rich-item-subtitle: var(--background-dark-10);
-  --swm-device-select-rich-item-icon: var(--background-dark-70);
-  --swm-device-select-rich-item-icon-selected: var(--background-dark-50);
-
-  /* Slider */
-  --swm-slider: var(--white);
-
-  /* Radio group */
-  --swm-radio-group-item: var(--off-white);
-
-  /* Startup message */
-  --swm-startup-message: var(--off-white);
-  --swm-phone-content-loading-background: rgba(255, 255, 255, 0.15);
-
-  /* Button */
-  --swm-button: var(--swm-default-text);
-  --swm-button-background: var(--background-dark-20);
-  --swm-button-size: 30px;
-  --swm-button-disabled: var(--swm-disabled-text);
-  --swm-button-hover: var(--background-dark-80);
-
-  --swm-button-active: var(--white);
-  --swm-button-active-background: var(--background-dark-70);
-
-  --swm-button-selected: var(--white);
-  --swm-button-selected-background: var(--background-dark-70);
-  --swm-button-selected-hover: var(--off-white);
-  --swm-button-selected-hover-background: var(--background-dark-50);
-
-  --swm-button-secondary: var(--swm-default-text);
-  --swm-button-secondary-background: var(--background-dark-100);
-  --swm-button-secondary-hover: var(--background-dark-50);
-
-  --swm-button-ternary: var(--off-white);
-  --swm-button-ternary-background: var(--background-dark-30);
-  --swm-button-ternary-hover: var(--background-dark-10);
-
-  --swm-button-counter: var(--off-white);
-  --swm-button-counter-background: var(--background-dark-40);
-  --swm-button-counter-border: var(--background-dark-40);
-
-  --swm-button-recording-on-background: var(--background-dark-40);
-  --swm-button-replay-hover: var(--background-dark-160);
-
-  /* Tooltip */
-  --swm-tooltip-primary: var(--swm-default-text);
-  --swm-tooltip-primary-background: var(--swm-popover-background);
-  --swm-tooltip-secondary: var(--swm-default-text);
-  --swm-tooltip-secondary-background: var(--background-dark-120);
-  --swm-tooltip-ternary: var(--navy-dark-100);
-  --swm-tooltip-ternary-background: var(--off-white);
-
-  /* ScrollArea */
-  --swm-scroll-area-background: var(--background-dark-100);
-  --swm-scroll-area-shadow: 0 1px 10px var(--background-dark-100);
-  --swm-scrollbar-background: var(--navy-dark-10);
-  --swm-scrollbar-thumb: var(--navy-dark-60);
-  --swm-scrollbar-hover: var(--navy-dark-20);
-
-  /* Input */
-  --swm-input-background: var(--background-dark-100);
-  --swm-input-shadow: 0 1px 10px var(--background-dark-100);
-
-  /* Select */
-  --swm-select-background: var(--background-dark-100);
-  --swm-select-hover-background: var(--background-dark-80);
-  --swm-select-shadow: 0 1px 10px var(--background-dark-100);
-  --swm-select-disabled: var(--navy-dark-60);
-  --swm-select-disabled-background: var(--background-dark-120);
-  --swm-select-content-background: var(--background-dark-100);
-  --swm-select-item-disabled-background: var(--background-dark-100);
-  --swm-select-item-highlighted-background: var(--background-dark-60);
-
-  --swm-focus-outline-color: var(--navy-dark-10);
-
-  /* Input */
-  --swm-input-background: var(--background-dark-100);
-  --swm-select-border: var(--navy-dark-40);
-  --swm-input-border: var(--navy-dark-40);
-
-  /* Switch */
-  --swm-switch-root-background: var(--background-dark-100);
-  --swm-switch-root-shadow: 0 1px 10px var(--background-dark-100);
-  --swm-switch-root-disabled: var(--background-dark-60);
-  --swm-switch-root-background-checked: var(--background-dark-20);
-
-  /* Manage devices */
-  --swm-device-title: var(--swm-default-text);
-  --swm-device-subtitle: var(--background-dark-10);
-  --swm-device-icon: var(--swm-default-text);
-  --swm-device-icon-background: var(--background-dark-70);
-  --swm-device-icon-background-selected: var(--green-dark-120);
-  --swm-device-delete-icon: var(--red-dark-80);
-  --swm-device-warning-icon: var(--yellow-dark-100);
-
-  /* Devices not found */
-  --swm-devices-not-found-icon: var(--background-dark-100);
-  --swm-devices-not-found-icon-background: var(--background-dark-80);
-
-  /* URL select */
-  --swm-url-select: var(--off-white);
-  --swm-url-select-background: var(--background-dark-80);
-  --swm-url-select-hover-background: var(--background-dark-70);
-  --swm-url-select-disabled-background: var(--background-dark-90);
-  --swm-url-select-highlighted-background: var(--background-dark-60);
-  --swm-url-select-label: var(--background-dark-30);
-  --swm-url-select-special-text: var(--navy-dark-60);
-  --swm-url-select-separator: var(--background-dark-60);
-
-  /* Debugger */
-  --swm-debugger-label: var(--off-white);
-  --swm-debugger-button-group: var(--white);
-  --swm-debugger-button-group-background: var(--background-dark-100);
-  --swm-debugger-button-group-border: 1px solid var(--background-dark-40);
-
-  /* Zoom controls */
-  --swm-zoom-controls-background: var(--background-dark-70);
-  --swm-zoom-controls-button-background-hover: var(--background-dark-80);
-  --swm-zoom-select-trigger-text: var(--off-white);
-  --swm-zoom-select-trigger-hover: var(--background-dark-80);
-  --swm-zoom-select-trigger-disabled: var(--background-dark-90);
-  --swm-zoom-select-highlighted: var(--background-dark-60);
-  --swm-zoom-select-disabled-text: var(--background-dark-60);
-  --swm-zoom-select-separator: var(--background-dark-50);
-
-  /* Replay */
-  --swm-replay-overlay-background: linear-gradient(
-    0deg,
-    rgba(0, 0, 0, 1),
-    rgba(0, 0, 0, 0.5),
-    rgba(0, 0, 0, 0.5),
-    rgba(0, 0, 0, 0.5),
-    rgba(0, 0, 0, 0.5)
-  );
-  --swm-replay-len-select: var(--off-white);
-  --swm-replay-len-select-background: var(--background-dark-160);
-  --swm-replay-len-select-hover-background: var(--background-dark-120);
-  --swm-replay-len-select-highlighted-background: var(--background-dark-80);
-
-  /* Open Deep Link */
-  --deep-link-history-border: var(--background-dark-100);
-  --deep-link-history-selected-item-background: var(--background-dark-100);
-
-  --swm-radio-group-indicator: var(--background-dark-10);
-}
-
-body[data-use-code-theme="true"] {
+body {
   /* Open Deep Link */
   --deep-link-history-query-match-color: var(--swm-button-background);
 

commit 62fe66d389ec0db7dab6eb6ce0e50cef443829a0

Fix workspace configuration fetching (#1328)
This PR fixes a regression introduced in #1324, where we removed support
for legacy IDE themes and by accident we removed the listeners for the
whole workspace configuration, not just the themes.

### How Has This Been Tested: 

- run test app and change "show device frame" setting 
- restart extension

packages/vscode-extension/src/webview/providers/WorkspaceConfigProvider.tsx
@@ -5,6 +5,7 @@ import {
   useState,
   useCallback,
   useMemo,
+  useEffect,
 } from "react";
 import { makeProxy } from "../utilities/rpc";
 import { WorkspaceConfig, WorkspaceConfigProps } from "../../common/WorkspaceConfig";
@@ -29,12 +30,21 @@ const WorkspaceConfigContext = createContext<WorkspaceConfigContextType>({
 export default function WorkspaceConfigProvider({ children }: PropsWithChildren) {
   const [config, setConfig] = useState<WorkspaceConfigProps>(INITIAL_WORKSPACE_CONFIG);
 
+  useEffect(() => {
+    function watchConfigChange(e: WorkspaceConfigProps) {
+      setConfig(e);
+    }
+
+    workspaceConfig.getConfig().then(watchConfigChange);
+    workspaceConfig.addListener("configChange", watchConfigChange);
+
+    return () => {
+      workspaceConfig.removeListener("configChange", watchConfigChange);
+    };
+  }, []);
+
   const update = useCallback(
-    <K extends keyof WorkspaceConfigProps>(
-      key: K,
-      value: WorkspaceConfigProps[K],
-      configurationTarget?: boolean
-    ) => {
+    <K extends keyof WorkspaceConfigProps>(key: K, value: WorkspaceConfigProps[K]) => {
       const newState = { ...config, [key]: value };
       setConfig(newState);
       workspaceConfig.update(key, value);