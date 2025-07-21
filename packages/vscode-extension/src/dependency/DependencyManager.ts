0b3149b463f0b765f7370672cb3ea5a06c596914
Juliusz Wajgelt <49338439+jwajgelt@users.noreply.github.com>
2025-07-21 11:29:33 +0200

    fix: cocoapods diagnostics not working when `bundle` is used (#1351)
    - fixes cocoapods diagnostics not working when the `bundle` utility is
    used in the project
    - fixes cocoapods diagnostics not using the envs set in the launch
    config
    - moves code calling the pods command out of `DependencyManager` to a
    helper `pods` module
    
    ### How Has This Been Tested: 
    - run test-apps/react-native-80 app
    - check diagnostics are correct
    - remove pods and check that installing pods automatically still works

--- packages/vscode-extension/src/dependency/DependencyManager.ts
+++ packages/vscode-extension/src/dependency/DependencyManager.ts
@@ -5,7 +5,7 @@ import { Disposable, OutputChannel } from "vscode";
 import semver, { SemVer } from "semver";
 import { Logger } from "../Logger";
 import { EMULATOR_BINARY } from "../devices/AndroidEmulatorDevice";
-import { command, exec, lineReader } from "../utilities/subprocess";
+import { command, exec } from "../utilities/subprocess";
 import { getIosSourceDir } from "../builders/buildIOS";
 import { isExpoGoProject } from "../builders/expoGo";
 import {
@@ -31,20 +31,17 @@ import { isEasCliInstalled } from "../builders/easCommand";
 import { getMinimumSupportedNodeVersion } from "../utilities/getMinimumSupportedNodeVersion";
 import { LaunchConfiguration } from "../common/LaunchConfig";
 import { BuildConfig, BuildType } from "../common/BuildConfig";
+import { Pods } from "../utilities/pods";
 
 export class DependencyManager implements Disposable, DependencyManagerInterface {
   // React Native prepares build scripts based on node_modules, we need to reinstall pods if they change
   private eventEmitter = new EventEmitter();
   private packageManagerInternal: PackageManagerInfo | undefined;
+  private pods: Pods;
 
-  private podsInstallationProcess:
-    | {
-        podsPromise: Promise<void>;
-        cancelToken: CancelToken;
-      }
-    | undefined;
-
-  constructor(private launchConfiguration: LaunchConfiguration) {}
+  constructor(private launchConfiguration: LaunchConfiguration) {
+    this.pods = new Pods(launchConfiguration.absoluteAppRoot, launchConfiguration.env);
+  }
 
   setLaunchConfiguration(newLaunchConfiguration: LaunchConfiguration) {
     const newRoot = newLaunchConfiguration.absoluteAppRoot;
@@ -53,10 +50,14 @@ export class DependencyManager implements Disposable, DependencyManagerInterface
       this.packageManagerInternal = undefined;
     }
     this.launchConfiguration = newLaunchConfiguration;
+    const oldPods = this.pods;
+    this.pods = new Pods(newRoot, newLaunchConfiguration.env);
+    oldPods.dispose(); // dispose the old pods instance to clean up resources
   }
 
   public dispose() {
     this.eventEmitter.removeAllListeners();
+    this.pods.dispose();
   }
 
   public async addListener(listener: DependencyListener) {
@@ -231,56 +232,20 @@ export class DependencyManager implements Disposable, DependencyManagerInterface
     getTelemetryReporter().sendTelemetryEvent("build:install-pods", {
       platform: DevicePlatform.IOS,
     });
-    if (this.podsInstallationProcess) {
-      this.podsInstallationProcess.cancelToken.cancel();
-    }
-
-    // we create a new cancel token to avoid cancelling the calling process when the pod installation is cancelled
-    const cancelPodInstallToken = new CancelToken();
-    cancelToken.onCancel(() => cancelPodInstallToken.cancel());
-    const { promise, resolve } = Promise.withResolvers<void>();
-    this.podsInstallationProcess = {
-      podsPromise: promise,
-      cancelToken: cancelPodInstallToken,
-    };
 
     try {
-      const appRoot = this.launchConfiguration.absoluteAppRoot;
-      const iosDirPath = getIosSourceDir(appRoot);
-
-      if (!iosDirPath) {
-        this.emitEvent("pods", { status: "notInstalled", isOptional: false });
-        throw new Error("ios directory was not found inside the workspace.");
-      }
-
-      try {
-        outputChannel.clear();
-        const shouldUseBundle = await this.shouldUseBundleCommand();
-        const process = command(
-          shouldUseBundle ? "bundle install && bundle exec pod install" : "pod install",
-          {
-            shell: shouldUseBundle, // when using bundle, we need shell to run multiple commands
-            cwd: iosDirPath,
-            env: { ...this.launchConfiguration.env, LANG: "en_US.UTF-8" },
-          }
-        );
-        lineReader(process).onLineRead((line) => outputChannel.appendLine(line));
-        await cancelPodInstallToken.adapt(process);
-      } catch (e) {
-        Logger.error("Pods not installed", e);
-        getTelemetryReporter().sendTelemetryEvent("build:pod-install-failed", {
-          platform: DevicePlatform.IOS,
-        });
-        this.emitEvent("pods", { status: "notInstalled", isOptional: false });
-        return;
-      }
-
-      this.emitEvent("pods", { status: "installed", isOptional: false });
-      Logger.debug("Project pods installed");
-    } finally {
-      this.podsInstallationProcess = undefined;
-      resolve();
+      await this.pods.installPods(outputChannel, cancelToken);
+    } catch (e) {
+      Logger.error("Pods not installed", e);
+      getTelemetryReporter().sendTelemetryEvent("build:pod-install-failed", {
+        platform: DevicePlatform.IOS,
+      });
+      this.emitEvent("pods", { status: "notInstalled", isOptional: false });
+      return;
     }
+
+    this.emitEvent("pods", { status: "installed", isOptional: false });
+    Logger.debug("Project pods installed");
   }
 
   private async installPodsIfNeeded(
@@ -336,22 +301,8 @@ export class DependencyManager implements Disposable, DependencyManagerInterface
     });
   }
 
-  private async shouldUseBundleCommand() {
-    const appRoot = this.launchConfiguration.absoluteAppRoot;
-    const gemfile = path.join(appRoot, "Gemfile");
-    try {
-      await fs.promises.access(gemfile);
-      return true;
-    } catch (e) {
-      return false;
-    }
-  }
-
   private async checkPodsCommandStatus() {
-    const shouldUseBundle = await this.shouldUseBundleCommand();
-    const installed = await testCommand(
-      shouldUseBundle ? "bundle exec pod --version" : "pod --version"
-    );
+    const installed = await this.pods.isPodsCommandInstalled();
     this.emitEvent("cocoaPods", {
       status: installed ? "installed" : "notInstalled",
       isOptional: !(await projectRequiresNativeBuild(this.launchConfiguration)),
@@ -395,44 +346,13 @@ export class DependencyManager implements Disposable, DependencyManagerInterface
   }
 
   public async checkPodsInstallationStatus() {
-    if (this.podsInstallationProcess) {
-      await this.podsInstallationProcess.podsPromise;
-    }
-    const appRoot = this.launchConfiguration.absoluteAppRoot;
-    const requiresNativeBuild = await projectRequiresNativeBuild(this.launchConfiguration);
-    if (!requiresNativeBuild) {
-      this.emitEvent("pods", { status: "notInstalled", isOptional: true });
-      return true;
-    }
-
-    const iosDirPath = getIosSourceDir(appRoot);
-
-    const podfileLockExists = fs.existsSync(path.join(iosDirPath, "Podfile.lock"));
-    const podsDirExists = fs.existsSync(path.join(iosDirPath, "Pods"));
-
-    const podsInstallationIsPresent = podfileLockExists && podsDirExists;
-
-    if (!podsInstallationIsPresent) {
-      this.emitEvent("pods", { status: "notInstalled", isOptional: false });
-      return false;
-    }
-
-    // finally, we perform check between Podfile.lock and Pods/Manifest.lock
-    // this is what xcode does in Check Pods build phase and is used to determine
-    // if pods are up to date
-
-    // run diff command:
-    const { failed } = await command("diff Podfile.lock Pods/Manifest.lock", {
-      cwd: iosDirPath,
-      reject: false,
-      quietErrorsOnExit: true,
-    });
+    const installed = await this.pods.arePodsInstalled();
 
     this.emitEvent("pods", {
-      status: failed ? "notInstalled" : "installed",
+      status: installed ? "installed" : "notInstalled",
       isOptional: false,
     });
-    return !failed;
+    return installed;
   }
 
   public async checkEasCliInstallationStatus() {
