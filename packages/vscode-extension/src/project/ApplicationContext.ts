5cefc01101b5d103fc5c7a11b271b1c17569cce9
Juliusz Wajgelt <49338439+jwajgelt@users.noreply.github.com>
2025-07-23 07:41:30 +0200

    feat: load environment variables from project`s dotenv files (#1361)
    Loads the environment variables for the application root using
    `@expo/env` and appends it to the system and configured environment that
    is passed to the other parts of the extension.
    Also changes the build cache key to use a hash of the environment,
    instead of the full stringified env.
    
    Closes #1350 
    
    ### How Has This Been Tested: 
    - check that adding a `.env` file in the app root folder results in the
    variables being added to the `ApplicationContext.launchConfig` object
    - check that `.env` contents don't override values set in the system or
    custom launch config
    - check that switching app roots doesn't carry over the variables loaded
    from the previously chosen approot

--- packages/vscode-extension/src/project/ApplicationContext.ts
+++ packages/vscode-extension/src/project/ApplicationContext.ts
@@ -1,11 +1,14 @@
 import path from "path";
 import { Disposable, workspace } from "vscode";
+import { loadProjectEnv } from "@expo/env";
+import _ from "lodash";
 import { BuildCache } from "../builders/BuildCache";
 import { DependencyManager } from "../dependency/DependencyManager";
 import { disposeAll } from "../utilities/disposables";
 import { BuildManagerImpl, BuildManager } from "../builders/BuildManager";
 import { BatchingBuildManager } from "../builders/BatchingBuildManager";
 import { LaunchConfiguration, LaunchOptions } from "../common/LaunchConfig";
+import { Logger } from "../Logger";
 
 /**
  * Represents a launch configuration that has been resolved with additional properties.
@@ -18,12 +21,38 @@ export type ResolvedLaunchConfig = LaunchOptions & {
   env: Record<string, string>;
 };
 
+function resolveEnvironment(
+  appRoot: string,
+  configuredEnv: Record<string, string>
+): Record<string, string> {
+  const systemEnv = process.env as NodeJS.ProcessEnv;
+  const mergedEnv = { ...systemEnv, ...configuredEnv };
+  // load the dotenv files for the project into `mergedEnv`
+  const loadEnvResult = loadProjectEnv(appRoot, { force: true, systemEnv: mergedEnv });
+
+  if (loadEnvResult.result === "loaded") {
+    Logger.info(
+      `Project in "${appRoot}" loaded environment variables from .env files:`,
+      loadEnvResult.files
+    );
+  }
+
+  // filter out any `undefined` values from the environment variables
+  const env = _.pickBy(mergedEnv, _.isString);
+  return env;
+}
+
 function resolveLaunchConfig(configuration: LaunchConfiguration): ResolvedLaunchConfig {
   const appRoot = configuration.appRoot;
   const absoluteAppRoot = path.resolve(workspace.workspaceFolders![0].uri.fsPath, appRoot);
+
+  const configuredEnv = configuration.env || {};
+
   return {
-    env: {},
     ...configuration,
+    get env() {
+      return resolveEnvironment(absoluteAppRoot, configuredEnv);
+    },
     absoluteAppRoot,
     preview: {
       waitForAppLaunch: configuration.preview?.waitForAppLaunch ?? true,
