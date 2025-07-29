a9dcac05a3cbd90cfa7033d73d6959a9339657e9
Filip131311 <Filip.kaminski@swmansion.com>
2025-07-29 16:18:33 +0200

    code review

--- packages/vscode-extension/src/project/telemetryManager.ts
+++ packages/vscode-extension/src/project/telemetryManager.ts
@@ -1,19 +1,26 @@
 import { Disposable, env } from "vscode";
 import { StateManager } from "./StateManager";
 import { TelemetryState } from "../common/State";
+import { disposeAll } from "../utilities/disposables";
 
 export class TelemetryManager implements Disposable {
+  private disposables: Disposable[] = [];
+
   constructor(private readonly stateManager: StateManager<TelemetryState>) {
     const isTelemetryEnabled = env.isTelemetryEnabled;
 
     this.stateManager.setState({ enabled: isTelemetryEnabled });
 
-    env.onDidChangeTelemetryEnabled((telemetryEnabled) => {
-      this.stateManager.setState({ enabled: telemetryEnabled });
-    });
+    this.disposables.push(
+      env.onDidChangeTelemetryEnabled((telemetryEnabled) => {
+        this.stateManager.setState({ enabled: telemetryEnabled });
+      })
+    );
+
+    this.disposables.push(this.stateManager);
   }
 
   dispose(): void {
-    this.stateManager.dispose();
+    disposeAll(this.disposables);
   }
 }
