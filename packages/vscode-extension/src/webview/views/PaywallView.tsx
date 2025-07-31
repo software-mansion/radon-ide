a9ddd43736af8d0216d2c664ba3ae0f33770b38f
kacperkapusciak <kacper.kapusciak@swmansion.com>
2025-07-31 12:39:06 +0200

    draft paywall styles

--- packages/vscode-extension/src/webview/views/PaywallView.tsx
+++ packages/vscode-extension/src/webview/views/PaywallView.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./PaywallView.css";
 import Button from "../components/shared/Button";
+import RadonBackgroundImage from "../components/RadonBackgroundImage";
 
 type SubscriptionPlan = "monthly" | "yearly";
 
@@ -14,28 +15,29 @@ function PaywallView() {
 
   return (
     <div className="paywall-view">
+      <RadonBackgroundImage color="#222" className="paywall-background-image" />
       <div className="paywall-container">
         <h1 className="paywall-title">Unlock Radon IDE Pro</h1>
 
         <div className="subscription-options">
           <div
-            className={`subscription-option ${selectedPlan === "monthly" ? "selected" : ""}`}
-            onClick={() => setSelectedPlan("monthly")}>
-            <div className="plan-header">
-              <span className="plan-name">Monthly</span>
-              <span className="plan-price">$99.99/month</span>
-            </div>
-            <div className="plan-description">Billed monthly</div>
-          </div>
-
-          <div
             className={`subscription-option ${selectedPlan === "yearly" ? "selected" : ""}`}
             onClick={() => setSelectedPlan("yearly")}>
             <div className="plan-header">
-              <span className="plan-name">Yearly</span>
-              <span className="plan-price">$999.99/year</span>
+              <span className="plan-name">Yearly Plan</span>
+              <span className="plan-price">$390 / year</span>
             </div>
             <div className="plan-description">Save 17% • Billed annually</div>
+          </div>
+
+          <div
+            className={`subscription-option ${selectedPlan === "monthly" ? "selected" : ""}`}
+            onClick={() => setSelectedPlan("monthly")}>
+            <div className="plan-header">
+              <span className="plan-name">Monthly Plan</span>
+              <span className="plan-price">$39 / month</span>
+            </div>
+            <div className="plan-description">Billed monthly</div>
           </div>
         </div>
 
