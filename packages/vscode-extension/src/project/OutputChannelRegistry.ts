27093236945b6335b877991054389835ceb7f6ab
Ignacy Łątka <latkaignacy@gmail.com>
2025-07-31 15:59:29 +0200

    add readable output channel

--- packages/vscode-extension/src/project/OutputChannelRegistry.ts
+++ packages/vscode-extension/src/project/OutputChannelRegistry.ts
@@ -2,18 +2,41 @@ import { Disposable, LogOutputChannel, window } from "vscode";
 import { Output } from "../common/OutputChannel";
 import { Logger } from "../Logger";
 
+interface ReadableLogOutputChannel extends LogOutputChannel {
+  readAll: () => string[];
+}
+
+function createReadableOutputChannel(channel: string): ReadableLogOutputChannel {
+  const blindOutput = window.createOutputChannel(channel, { log: true });
+
+  const logRegistry: string[] = [];
+
+  return {
+    ...blindOutput,
+    readAll: () => logRegistry,
+    append: (value: string) => {
+      logRegistry.push(value);
+      blindOutput.append(value);
+    },
+    appendLine: (value: string) => {
+      logRegistry.push(value);
+      blindOutput.appendLine(value);
+    },
+  };
+}
+
 export class OutputChannelRegistry implements Disposable {
   private channelByName = new Map<Output, LogOutputChannel>([
     [Output.Ide, Logger.rawOutputChannel],
   ]);
 
-  getOrCreateOutputChannel(channel: Output) {
+  getOrCreateOutputChannel(channel: Output): LogOutputChannel {
     const logOutput = this.channelByName.get(channel);
     if (logOutput) {
       return logOutput;
     }
 
-    const newOutputChannel = window.createOutputChannel(channel, { log: true });
+    const newOutputChannel = createReadableOutputChannel(channel);
     this.channelByName.set(channel, newOutputChannel);
     return newOutputChannel;
   }
