c68d19cfaed7da5931987ebe64e7fc552ba5d5d5
Filip131311 <Filip.kaminski@swmansion.com>
2025-08-07 13:27:50 +0200

    init

--- packages/vscode-extension/src/utilities/getMinimumSupportedNodeVersion.ts
+++ packages/vscode-extension/src/utilities/getMinimumSupportedNodeVersion.ts
@@ -1,5 +1,8 @@
 import fs from "fs";
 import { Logger } from "../Logger";
+import semver from "semver";
+
+const DEFAULT_MINIMUM_NODE_VERSION = ">=18";
 
 /* 
 This function return a minimum supported node version for given appRoot assuming node_modules are installed,
@@ -8,8 +11,10 @@ of react native, so it should be always used after node_modules installation.
 */
 export function getMinimumSupportedNodeVersion(appRoot: string): string {
   const appPackageJson = require(`${appRoot}/package.json`);
+  let applicationMinimumNodeVersion;
+  let reactNativeMinimumNodeVersion;
   if (appPackageJson.engines && appPackageJson.engines.node) {
-    return appPackageJson.engines.node;
+    applicationMinimumNodeVersion = appPackageJson.engines.node;
   }
 
   if (
@@ -18,15 +23,33 @@ export function getMinimumSupportedNodeVersion(appRoot: string): string {
   ) {
     const reactNativePackageJson = require(`${appRoot}/node_modules/react-native/package.json`);
     if (reactNativePackageJson.engines && reactNativePackageJson.engines.node) {
-      return reactNativePackageJson.engines.node;
+      reactNativeMinimumNodeVersion = reactNativePackageJson.engines.node;
     }
   }
 
-  // this is minimum supported version at the time this code was written, and therefore a default for this function,
-  // but this code should never be reached as react-native contains this information in its package json and reaching,
-  // this code suggests that the function was incorrectly called before installing node_modules.
-  Logger.warn(
-    "[Minimum Supported node version] Utility function called before node modules were installed, the returned value might, be deprecated."
-  );
-  return ">=18";
+  const ranges = [
+    applicationMinimumNodeVersion,
+    reactNativeMinimumNodeVersion,
+    DEFAULT_MINIMUM_NODE_VERSION,
+  ].filter(Boolean);
+
+  if (ranges.length === 1) {
+    // this is minimum supported version at the time this code was written, and therefore a default for this function,
+    // but this code should never be reached as react-native contains this information in its package json and reaching,
+    // this code suggests that the function was incorrectly called before installing node_modules.
+    Logger.warn(
+      "[Minimum Supported node version] Utility function called before node modules were installed, the returned value might, be deprecated."
+    );
+    return DEFAULT_MINIMUM_NODE_VERSION;
+  }
+
+  return ranges.reduce((mostRestrictive, current) => {
+    const currentMinVersion = semver.minVersion(current);
+    const restrictiveMinVersion = semver.minVersion(mostRestrictive);
+
+    if (currentMinVersion && restrictiveMinVersion) {
+      return semver.gt(currentMinVersion, restrictiveMinVersion) ? current : mostRestrictive;
+    }
+    return current || mostRestrictive;
+  });
 }
