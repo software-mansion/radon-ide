software-mansion/radon-ide

commit f5e6aec42ce3f301864e528725f06c53404cc859

refactor: devices state managment (#1404)
This PR is another one in the series that move us to the new state
management system and single unified backend API.

In this PR we: 
- remove `DevicesProvider` 
  -  move all the state it was responsible for to the new `store$` 
- remove `DeviceSessionsManagerInterface` and move its methods to the
`ProjectInterface`
- remove `DevicesManagerInterface` and move its methods to the
`ProjectInterface`
- Adjust the way updates are merged to the `store$` to allow for
removing elements from stored arrays
- Generally clean up the `project` class to make its growth sustainable
we:
  - devide it in clear sections and mark them using `#region` comments 
  - remove helper functions into utilis 
- clean up the `state.ts` file to make its growth sustainable
- remove `DeviceManage`, `DeviceSessionsManager` and `WorkspaceConfig`
callable objects from the webview controller callable object list


### How Has This Been Tested: 

- run `react-native-81` test app 
- add a device 
- rename a device 
- remove a device
- change devices theme (to test something still in old state system)
- change rotation 

### How Has This Change Been Documented:

This is internal change

packages/vscode-extension/src/project/StateManager.ts
@@ -1,8 +1,8 @@
 import _ from "lodash";
 import { Disposable, EventEmitter } from "vscode";
 import { disposeAll } from "../utilities/disposables";
-import { mergeAndCalculateChanges } from "../utilities/mergeAndCalculateChanges";
 import { RecursivePartial } from "../common/State";
+import { mergeAndCalculateChanges } from "../common/Merge";
 
 export abstract class StateManager<T extends object> implements Disposable {
   static create<T extends object>(initialState: T): StateManager<T> {

packages/vscode-extension/src/webview/providers/storeProvider.tsx
@@ -3,6 +3,7 @@ import { synced, SyncedSetParams, SyncedSubscribeParams } from "@legendapp/state
 import { vscode } from "../utilities/vscode";
 import { createContext, PropsWithChildren, useContext } from "react";
 import { initialState, State } from "../../common/State";
+import { merge } from "../../common/Merge";
 
 let instanceToken = Math.floor(Math.random() * 1000000);
 let globalCallCounter = 1;
@@ -69,10 +70,11 @@ const setState = async (params: SyncedSetParams<State>) => {
 };
 
 const subscribeToState = (params: SyncedSubscribeParams<State>) => {
-  const { update } = params;
+  const { update, value$ } = params;
   const listener = (event: any) => {
     if (event.data.command === "RNIDE_state_updated") {
-      update({ value: event.data.state, mode: "merge" });
+      const newState = merge(value$.get(), event.data.state);
+      update({ value: newState, mode: "set" });
     }
   };
 

packages/vscode-extension/src/builders/BatchingBuildManager.test.ts
@@ -3,9 +3,9 @@ import Sinon from "sinon";
 import { describe, beforeEach, it } from "mocha";
 
 import { BatchingBuildManager } from "./BatchingBuildManager";
-import { DevicePlatform } from "../common/DeviceManager";
 import { BuildConfig, BuildType } from "../common/BuildConfig";
 import { CancelToken } from "../utilities/cancelToken";
+import { DevicePlatform } from "../common/State";
 
 describe("BatchingBuildManager", () => {
   let buildAppMock = Sinon.stub();

packages/vscode-extension/src/builders/BuildCache.ts
@@ -3,12 +3,12 @@ import assert from "assert";
 import crypto from "crypto";
 import { Logger } from "../Logger";
 import { extensionContext } from "../utilities/extensionContext";
-import { DevicePlatform } from "../common/DeviceManager";
 import { IOSBuildResult } from "./buildIOS";
 import { AndroidBuildResult } from "./buildAndroid";
 import { calculateMD5 } from "../utilities/common";
 import { BuildResult } from "./BuildManager";
 import { FingerprintOptions, FingerprintProvider } from "../project/FingerprintProvider";
+import { DevicePlatform } from "../common/State";
 
 const ANDROID_BUILD_CACHE_KEY = "android_build_cache";
 const IOS_BUILD_CACHE_KEY = "ios_build_cache";

packages/vscode-extension/src/builders/BuildManager.test.ts
@@ -3,11 +3,11 @@ import Sinon from "sinon";
 import { describe, afterEach, beforeEach, it } from "mocha";
 
 import { BuildType } from "../common/BuildConfig";
-import { DevicePlatform } from "../common/DeviceManager";
 import { CustomBuild, EasConfig } from "../common/LaunchConfig";
 import { createBuildConfig, inferBuildType } from "./BuildManager";
 import * as ExpoGo from "./expoGo";
 import { ResolvedLaunchConfig } from "../project/ApplicationContext";
+import { DevicePlatform } from "../common/State";
 
 const APP_ROOT = "appRoot";
 const APP_ROOT_ABSOLUTE = "/appRoot";

packages/vscode-extension/src/builders/BuildManager.ts
@@ -4,13 +4,13 @@ import { OutputChannel } from "vscode";
 import { BuildCache } from "./BuildCache";
 import { AndroidBuildResult, buildAndroid } from "./buildAndroid";
 import { IOSBuildResult, buildIos } from "./buildIOS";
-import { DevicePlatform } from "../common/DeviceManager";
 import { CancelError, CancelToken } from "../utilities/cancelToken";
 import { getTelemetryReporter } from "../utilities/telemetry";
 import { Logger } from "../Logger";
 import { BuildConfig, BuildType } from "../common/BuildConfig";
 import { isExpoGoProject } from "./expoGo";
 import { ResolvedLaunchConfig } from "../project/ApplicationContext";
+import { DevicePlatform } from "../common/State";
 
 export type BuildResult = IOSBuildResult | AndroidBuildResult;
 

packages/vscode-extension/src/builders/buildAndroid.ts
@@ -11,12 +11,12 @@ import { CancelToken } from "../utilities/cancelToken";
 import { extensionContext } from "../utilities/extensionContext";
 import { BuildAndroidProgressProcessor } from "./BuildAndroidProgressProcessor";
 import { EXPO_GO_PACKAGE_NAME, downloadExpoGo } from "./expoGo";
-import { DevicePlatform } from "../common/DeviceManager";
 import { getReactNativeVersion } from "../utilities/reactNative";
 import { runExternalBuild } from "./customBuild";
 import { fetchEasBuild, performLocalEasBuild } from "./eas";
 import { getTelemetryReporter } from "../utilities/telemetry";
 import { AndroidBuildConfig, AndroidLocalBuildConfig, BuildType } from "../common/BuildConfig";
+import { DevicePlatform } from "../common/State";
 
 export type AndroidBuildResult = {
   platform: DevicePlatform.Android;

packages/vscode-extension/src/builders/buildIOS.ts
@@ -4,7 +4,6 @@ import { exec, lineReader } from "../utilities/subprocess";
 import { Logger } from "../Logger";
 import { CancelToken } from "../utilities/cancelToken";
 import { BuildIOSProgressProcessor } from "./BuildIOSProgressProcessor";
-import { DevicePlatform } from "../common/DeviceManager";
 import { EXPO_GO_BUNDLE_ID, downloadExpoGo } from "./expoGo";
 import { findXcodeProject, findXcodeScheme, IOSProjectInfo } from "../utilities/xcode";
 import { runExternalBuild } from "./customBuild";
@@ -12,6 +11,7 @@ import { fetchEasBuild, performLocalEasBuild } from "./eas";
 import { getXcodebuildArch } from "../utilities/common";
 import { getTelemetryReporter } from "../utilities/telemetry";
 import { BuildType, IOSBuildConfig, IOSLocalBuildConfig } from "../common/BuildConfig";
+import { DevicePlatform } from "../common/State";
 
 export type IOSBuildResult = {
   platform: DevicePlatform.IOS;

commit 3925eb165cf1ec869154081a4186177bc7873c7d

fix: state updates lost when frequently updated (#1418)
- Fixes a bug introduced in #1404 which caused some updates to be lost
when the extension sent frequent updates to the webview[^1].
- Skips calling `onSetState` when nothing changed in `StateManager`.
- Adds a random `useMemo` in `AppRootSelect` because why not.

[^1]: The bug was caused by changing the legend-state update mode to
`"set"` and setting a merge of the current state in the store with the
updates received in the listener which synchronizes state changes in the
Extension with the Webview's state.
Because the `update` function is called asynchronously, if the listener
is called multiple times before the update can be applied, we merge the
changes with the stale state stored in the Legend State observable and
set that, losing the intermediate updates.
The fix passes a callback to the `update` function which receives the
value set at the time the assignment happens, not at the time the
changes were received.
The `update` function is typed to expect the actual values, but the
implementation _does_ accept the callback and handles it correctly.

### How Has This Been Tested: 
- set a breakpoint in `ide.ts` on `this.stateManager.setState({
applicationRoots });`
- open a workspace in Radon
- after stopping at the breakpoint and waiting a while, resume the
debugger
- the application roots should set up correctly

packages/vscode-extension/src/project/StateManager.ts
@@ -62,8 +62,10 @@ class RootStateManager<T extends object> extends StateManager<T> {
 
   setState(partialState: RecursivePartial<T>): void {
     const [newState, changes] = mergeAndCalculateChanges(this.state, partialState);
-    this.state = newState;
-    this.onSetStateEmitter.fire(changes);
+    if (this.state !== newState) {
+      this.state = newState;
+      this.onSetStateEmitter.fire(changes);
+    }
   }
 
   getState(): T {

packages/vscode-extension/src/webview/components/AppRootSelect.tsx
@@ -2,7 +2,7 @@ import * as Select from "@radix-ui/react-select";
 import "./AppRootSelect.css";
 import "./shared/Dropdown.css";
 import _ from "lodash";
-import React, { PropsWithChildren, useEffect } from "react";
+import React, { PropsWithChildren, useEffect, useMemo } from "react";
 import { use$ } from "@legendapp/state/react";
 import { useProject } from "../providers/ProjectProvider";
 import { LaunchConfiguration, LaunchConfigurationKind } from "../../common/LaunchConfig";
@@ -159,14 +159,18 @@ function AppRootSelect() {
     );
   }
 
-  const detectedConfigurations = applicationRoots.map(({ path, displayName, name }) => {
-    return {
-      appRoot: path,
-      name: displayName || name,
-      kind: LaunchConfigurationKind.Detected,
-      env: {},
-    };
-  });
+  const detectedConfigurations = useMemo(
+    () =>
+      applicationRoots.map(({ path, displayName, name }) => {
+        return {
+          appRoot: path,
+          name: displayName || name,
+          kind: LaunchConfigurationKind.Detected,
+          env: {},
+        };
+      }),
+    [applicationRoots]
+  );
 
   const handleAppRootChange = async (value: string) => {
     if (value === "manage") {

packages/vscode-extension/src/webview/providers/storeProvider.tsx
@@ -2,7 +2,7 @@ import { Change, Observable, observable } from "@legendapp/state";
 import { synced, SyncedSetParams, SyncedSubscribeParams } from "@legendapp/state/sync";
 import { vscode } from "../utilities/vscode";
 import { createContext, PropsWithChildren, useContext } from "react";
-import { initialState, State } from "../../common/State";
+import { initialState, RecursivePartial, State } from "../../common/State";
 import { merge } from "../../common/Merge";
 
 let instanceToken = Math.floor(Math.random() * 1000000);
@@ -70,11 +70,17 @@ const setState = async (params: SyncedSetParams<State>) => {
 };
 
 const subscribeToState = (params: SyncedSubscribeParams<State>) => {
-  const { update, value$ } = params;
+  const { update } = params;
   const listener = (event: any) => {
     if (event.data.command === "RNIDE_state_updated") {
-      const newState = merge(value$.get(), event.data.state);
-      update({ value: newState, mode: "set" });
+      const changes = event.data.state as RecursivePartial<State>;
+      update({
+        //@ts-ignore Legend State is mistyping the param, but it works. _Trust me_.
+        value: (prev) => {
+          return merge(prev, changes);
+        },
+        mode: "set",
+      });
     }
   };
 