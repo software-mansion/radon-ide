04eda819f5f261da4c0b489bcd4f2e1dc36469a3
pFornagiel <pawel.fornagiel2@gmail.com>
2025-08-11 13:42:14 +0200

    Revert "feat: temporarily disable PortraitUpsideDown rotation (#1416)"
    This reverts commit 303a19520c454386a891f62a7c7c22071b7dfb0d.

--- packages/vscode-extension/src/extension.ts
+++ packages/vscode-extension/src/extension.ts
@@ -45,6 +45,7 @@ const ROTATIONS: DeviceRotation[] = [
   DeviceRotation.LandscapeLeft,
   DeviceRotation.Portrait,
   DeviceRotation.LandscapeRight,
+  DeviceRotation.PortraitUpsideDown,
 ] as const;
 
 function handleUncaughtErrors(context: ExtensionContext) {
