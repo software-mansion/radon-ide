26ff579b3e85d0ab061f0fe56ee6d3a6dc8b5a1b
Ignacy Łątka <latkaignacy@gmail.com>
2025-08-12 18:21:48 +0200

    expand CircularBuffer tests

--- packages/vscode-extension/src/project/index.test.ts
+++ packages/vscode-extension/src/project/index.test.ts
@@ -6,8 +6,13 @@ describe("CircularBuffer", () => {
   const bufferCapacity = 5;
   const amountOfTestLoops = 3;
 
-  it("should return the correct output", async () => {
-    const buffer = new CircularBuffer<number>(bufferCapacity);
+  const fillBuffer = (buffer: CircularBuffer<number>) => {
+    for (let i = 0; i < bufferCapacity * amountOfTestLoops; i++) {
+      buffer.write(i);
+    }
+  };
+
+  const fillAndTestBuffer = (buffer: CircularBuffer<number>) => {
     let bufferSize = 0;
 
     for (let i = 0; i < bufferCapacity * amountOfTestLoops; i++) {
@@ -23,19 +28,19 @@ describe("CircularBuffer", () => {
         assert.strictEqual(expectedValue, actualValue);
       }
     }
+  };
+
+  it("should return the correct output", async () => {
+    const buffer = new CircularBuffer<number>(bufferCapacity);
+    fillAndTestBuffer(buffer);
   });
 
-  it("should clear properly", async () => {
+  it("should return the correct output after being cleared", async () => {
     const buffer = new CircularBuffer<number>(bufferCapacity);
-
-    for (let i = 0; i < bufferCapacity * amountOfTestLoops; i++) {
-      buffer.write(i);
-    }
-
+    fillBuffer(buffer);
     buffer.clear();
-
     const contents = buffer.readAll();
-
     assert.strictEqual(contents.length, 0);
+    fillAndTestBuffer(buffer);
   });
 });
