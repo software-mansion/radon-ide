ae746e2f604818535e8b7a67b1457ed8ebca3051
Ignacy Łątka <latkaignacy@gmail.com>
2025-08-12 15:18:58 +0200

    rename tailIndex var

--- packages/vscode-extension/src/project/CircularBuffer.ts
+++ packages/vscode-extension/src/project/CircularBuffer.ts
@@ -2,31 +2,31 @@ export class CircularBuffer<T> {
   private buffer: Array<T>;
   private size: number;
   private capacity: number;
-  private headIndex: number;
+  private tailIndex: number;
 
   constructor(capacity: number) {
     this.size = 0;
-    this.headIndex = 0;
+    this.tailIndex = 0;
     this.capacity = capacity;
     this.buffer = Array<T>(this.capacity);
   }
 
   public write(value: T) {
-    this.buffer[this.headIndex] = value;
-    this.headIndex = (this.headIndex + 1) % this.capacity;
+    this.buffer[this.tailIndex] = value;
+    this.tailIndex = (this.tailIndex + 1) % this.capacity;
     this.size = Math.min(this.size + 1, this.capacity);
   }
 
   public clear() {
-    this.headIndex = 0;
+    this.tailIndex = 0;
     this.buffer.length = 0;
     this.size = 0;
   }
 
   public readAll(): T[] {
     return [
-      ...this.buffer.slice(this.headIndex, this.size),
-      ...this.buffer.slice(0, this.headIndex),
+      ...this.buffer.slice(this.tailIndex, this.size),
+      ...this.buffer.slice(0, this.tailIndex),
     ];
   }
 }
