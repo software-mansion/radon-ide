2df6f88b933388fc046ae469f048ccabfaa65211
spaleka <agata_spalek@interia.eu>
2025-08-13 09:10:59 +0200

    PR suggestions applied

---
+++ packages/docs/src/components/Footer/FooterNavigation/index.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+interface FooterProps {
+  title: string;
+  links: { label: string; to: string }[];
+}
+
+const footerLinks: FooterProps[] = [
+  {
+    title: "Product",
+    links: [
+      { label: "Features", to: "/features" },
+      { label: "Enterprise", to: "/enterprise" },
+      { label: "Pricing", to: "/pricing" },
+      {
+        label: "Download",
+        to: "https://marketplace.visualstudio.com/items?itemName=swmansion.react-native-ide",
+      },
+    ],
+  },
+  {
+    title: "Support",
+    links: [
+      { label: "Customer Portal", to: "/customer-portal" },
+      { label: "Docs", to: "/docs/category/getting-started" },
+      { label: "Changelog", to: "/changelog" },
+      { label: "Contact", to: "/contact" },
+    ],
+  },
+  {
+    title: "Legal",
+    links: [
+      { label: "Privacy", to: "/privacy" },
+      { label: "Legal", to: "/legal" },
+    ],
+  },
+];
+
+export default function FooterNavigation() {
+  return (
+    <>
+      {footerLinks.map((section, index) => (
+        <div key={index} className={styles.footerSection}>
+          <p className={styles.sectionTitle}>{section.title}</p>
+          <ul className={styles.sectionLinks}>
+            {section.links.map((link, idx) => (
+              <li key={idx}>
+                <a href={link.to} className={styles.footerLink}>
+                  {link.label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}{" "}
+    </>
+  );
+}
---
+++ packages/docs/src/components/XIcon/index.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+export default function XIcon(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      {...props}>
+      <path
+        d="M13.4888 10.7748L19.3171 4H17.9362L12.8753 9.88246L8.83341 4H4.17188L10.2839 12.8954L4.17188 20H5.5528L10.8968 13.7877L15.1651 20H19.8273L13.4888 10.7748ZM11.5971 12.9735L10.978 12.088L6.05064 5.04H8.17187L12.1485 10.728L12.7676 11.6135L17.9368 19.0074H15.8156L11.5977 12.9742L11.5971 12.9735Z"
+        fill="#FAFAFA"
+      />
+    </svg>
+  );
+}
