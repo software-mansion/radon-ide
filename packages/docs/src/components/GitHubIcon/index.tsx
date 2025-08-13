9c684d21f779ed885aec0704ca3dd47a8e8a60cf
spaleka <agata_spalek@interia.eu>
2025-08-13 09:22:12 +0200

    GitHubIcon improve

--- packages/docs/src/components/GitHubIcon/index.tsx
+++ packages/docs/src/components/GitHubIcon/index.tsx
@@ -1,7 +1,13 @@
 import * as React from "react";
-export default function GitHubIcon() {
+export default function GitHubIcon(props) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      {...props}>
       <g clip-path="url(#clip0_1233_602)">
         <path
           fill-rule="evenodd"
