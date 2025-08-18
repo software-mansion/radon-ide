d25c030a4fa43a6a6cc0c51d7871786530c927cd
kacperkapusciak <kacper.kapusciak@swmansion.com>
2025-08-18 13:48:34 +0200

    simplify markup, fix styles

--- packages/vscode-extension/src/webview/components/SendFilesOverlay.tsx
+++ packages/vscode-extension/src/webview/components/SendFilesOverlay.tsx
@@ -46,12 +46,10 @@ export function SendFilesOverlay() {
       className={`phone-screen send-files-overlay ${isVisible ? "visible" : "hidden"}`}>
       <div className="send-files-overlay-container">
         <div className="send-files-overlay-content">
-          <div className="send-files-content-inner">
-            <div className="send-files-icon">
-              <span className="codicon codicon-keyboard-tab"></span>
-            </div>
-            <p>Drop files here</p>
+          <div className="send-files-icon">
+            <span className="codicon codicon-keyboard-tab"></span>
           </div>
+          <p>Drop files here</p>
         </div>
       </div>
     </div>
