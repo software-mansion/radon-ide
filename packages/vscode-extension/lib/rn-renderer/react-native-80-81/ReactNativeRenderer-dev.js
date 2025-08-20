d3e72e14fe4f1630f69e12ef88702d84294a6a53
Filip131311 <Filip.kaminski@swmansion.com>
2025-08-20 12:07:12 +0200

    fix paper renderer (nobady used it so we didn't know it's broken <3)

--- packages/vscode-extension/lib/rn-renderer/react-native-80-81/ReactNativeRenderer-dev.js
+++ packages/vscode-extension/lib/rn-renderer/react-native-80-81/ReactNativeRenderer-dev.js
@@ -16870,11 +16870,11 @@ __DEV__ &&
       shouldSuspendImpl = newShouldSuspendImpl;
     };
     var isomorphicReactPackageVersion = React.version;
-    if ("19.1.0-canary-7fa3d1db-20250613" !== isomorphicReactPackageVersion)
+    if ("19.1.0" !== isomorphicReactPackageVersion)
       throw Error(
         'Incompatible React versions: The "react" and "react-native-renderer" packages must have the exact same version. Instead got:\n  - react:                  ' +
           (isomorphicReactPackageVersion +
-            "\n  - react-native-renderer:  19.1.0-canary-7fa3d1db-20250613\nLearn more: https://react.dev/warnings/version-mismatch")
+            "\n  - react-native-renderer:  19.1.0\nLearn more: https://react.dev/warnings/version-mismatch")
       );
     if (
       "function" !==
@@ -16900,10 +16900,10 @@ __DEV__ &&
     (function () {
       var internals = {
         bundleType: 1,
-        version: "19.1.0-canary-7fa3d1db-20250613",
+        version: "19.1.0",
         rendererPackageName: "react-native-renderer",
         currentDispatcherRef: ReactSharedInternals,
-        reconcilerVersion: "19.1.0-canary-7fa3d1db-20250613"
+        reconcilerVersion: "19.1.0"
       };
       null !== extraDevToolsConfig &&
         (internals.rendererConfig = extraDevToolsConfig);
