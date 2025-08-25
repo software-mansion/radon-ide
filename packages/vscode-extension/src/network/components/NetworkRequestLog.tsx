f2cf538657be4ff0fb6ca3be174df5c5c98335ee
Paweł Fornagiel <85541429+pFornagiel@users.noreply.github.com>
2025-08-25 14:05:14 +0200

    feat: network inspector - table virtualization (#1478)
    ### Description
    
    This PR virtualises the NetworkRequestLog component table, using
    [TenStack Virtualizer](https://tanstack.com/virtual/latest), to handle
    large number of logs seemlesly.
    
    ### How Has This Been Tested: 
    
    Features were tested in local development environment of extension as
    follows:
    - open radon-ide/packages/vscode-extension
    - run the extension locally using Run and Debug menu -> Run Extension
    - open an application supporting Radon IDE in editor - example of app
    supporting rotation: radon-ide-test-apps/react-native-80
    - run Radon IDE extension panel, launch app on selected device, open
    network inspector
    - **fetch large number of logs (1000), see how component behaves, look
    for visual issues**
    
    ### How Has This Change Been Documented:
    
    Not applicable.

--- packages/vscode-extension/src/network/components/NetworkRequestLog.tsx
+++ packages/vscode-extension/src/network/components/NetworkRequestLog.tsx
@@ -1,7 +1,9 @@
 import classNames from "classnames";
-import { useMemo, useLayoutEffect, useRef, useState } from "react";
+import { useRef, useState, useEffect, useLayoutEffect, useMemo } from "react";
+import { useVirtualizer } from "@tanstack/react-virtual";
 import { capitalize } from "lodash";
 import type { VscodeTable as VscodeTableElement } from "@vscode-elements/elements/dist/vscode-table/vscode-table.js";
+import type { VscodeTableRow as VscodeTableRowElement } from "@vscode-elements/elements/dist/vscode-table-row/vscode-table-row";
 import {
   VscodeTable,
   VscodeTableBody,
@@ -30,11 +32,45 @@ interface NetworkRequestLogProps {
   parentHeight: number | undefined;
 }
 
+type logDetails = {
+  title: NetworkLogColumn;
+  getClass?: (log: NetworkLog) => string;
+};
+
 const SCROLL_TO_TOP_TIMEOUT = 200;
 const DEFAULT_SORT_STATE: SortState = {
   column: null,
   direction: null,
 };
+
+const LOG_DETAILS_CONFIG: logDetails[] = [
+  { title: NetworkLogColumn.Name },
+  {
+    title: NetworkLogColumn.Status,
+    getClass: (log: NetworkLog) => getStatusClass(log.response?.status) + " status",
+  },
+  { title: NetworkLogColumn.Method },
+  { title: NetworkLogColumn.Type },
+  { title: NetworkLogColumn.Size },
+  { title: NetworkLogColumn.Time },
+];
+
+function getStatusClass(status: number | string | undefined) {
+  if (!status) {
+    return "";
+  }
+
+  const statusNum = Number(status);
+  if (statusNum >= 200 && statusNum < 400) {
+    return "status-success";
+  }
+
+  if (statusNum >= 400) {
+    return "status-error";
+  }
+
+  return "";
+}
 
 /**
  * Navigates through the shadow DOM hierarchy to find the scrollable container within a VSCode table element.
@@ -49,7 +85,7 @@ const DEFAULT_SORT_STATE: SortState = {
  */
 function getScrollableTableContainer(table: VscodeTableElement): HTMLDivElement | null | undefined {
   //@ts-ignore - ignore accessing the private property - needed to avoid ugly selectors
-  return table._scrollableElement?._scrollableContainer;
+  return table?._scrollableElement?._scrollableContainer;
 }
 
 const NetworkRequestLog = ({
@@ -60,13 +96,102 @@ const NetworkRequestLog = ({
 }: NetworkRequestLogProps) => {
   const tableRef = useRef<VscodeTableElement>(null);
   const [sortState, setSortState] = useState<SortState>(DEFAULT_SORT_STATE);
-  const [cellWidths, setCellWidths] = useState<number[]>([]);
   const { addColumnFilterToInputField } = useNetworkFilter();
 
   // Sort the network logs based on current sort state
   const sortedNetworkLogs = useMemo(() => {
     return sortNetworkLogs(networkLogs, sortState.column, sortState.direction);
   }, [networkLogs, sortState]);
+
+  const handleHeaderClick = (column: NetworkLogColumn) => {
+    setSortState((prevState) => {
+      // If clicking on the same column, cycle through: asc -> desc -> null
+      if (prevState.column === column) {
+        switch (prevState.direction) {
+          case SortDirection.Asc:
+            return { column, direction: SortDirection.Desc };
+          case SortDirection.Desc:
+            return { column: null, direction: null };
+          case null:
+          default:
+            return { column, direction: SortDirection.Asc };
+        }
+      }
+      // If clicking on a different column or no current sort, start with ascending
+      return { column, direction: SortDirection.Asc };
+    });
+  };
+
+  const handleHeaderFilterClick = (e: React.MouseEvent, column: NetworkLogColumn) => {
+    // If filter was clicked, do not trigger sorting by stopping propagation
+    e.stopPropagation();
+    addColumnFilterToInputField(column);
+  };
+
+  const getSortIcon = (column: NetworkLogColumn) => {
+    if (sortState.column !== column) {
+      return "hidden"; // No icon when not sorting by this column
+    }
+    return sortState.direction === SortDirection.Asc
+      ? "codicon-chevron-up"
+      : "codicon-chevron-down";
+  };
+
+  return (
+    <div className="table-container">
+      <div style={{ width: "100%", overflowX: "hidden" }}>
+        <VscodeTable
+          zebra
+          bordered-columns
+          resizable
+          style={{ height: parentHeight }}
+          ref={tableRef}>
+          <VscodeTableHeader slot="header">
+            {LOG_DETAILS_CONFIG.map(({ title }) => (
+              <VscodeTableHeaderCell key={title} onClick={() => handleHeaderClick(title)}>
+                <div className="table-header-cell">
+                  <span className="table-header-title">{capitalize(title)}</span>
+                  <IconButton onClick={(e) => handleHeaderFilterClick(e, title)}>
+                    <span className={`codicon codicon-filter-filled`}></span>
+                  </IconButton>
+                  <span className={`codicon ${getSortIcon(title)}`}></span>
+                </div>
+              </VscodeTableHeaderCell>
+            ))}
+          </VscodeTableHeader>
+          <TableBody
+            networkLogs={sortedNetworkLogs}
+            selectedNetworkLog={selectedNetworkLog}
+            handleSelectedRequest={handleSelectedRequest}
+            tableRef={tableRef}
+            parentHeight={parentHeight}
+          />
+        </VscodeTable>
+      </div>
+    </div>
+  );
+};
+
+interface TableBodyProps {
+  networkLogs: NetworkLog[];
+  selectedNetworkLog: NetworkLog | null;
+  handleSelectedRequest: (requestId: string | null) => void;
+  tableRef: React.RefObject<VscodeTableElement | null>;
+  parentHeight: number | undefined;
+}
+
+const CELL_DEFAULT_HEIGHT = 24;
+const ROW_OVERSCAN = 15;
+
+function TableBody({
+  networkLogs,
+  selectedNetworkLog,
+  handleSelectedRequest,
+  tableRef,
+  parentHeight,
+}: TableBodyProps) {
+  const [selectedRequestIndex, setSelectedRequestIndex] = useState<number>(0);
+  const [cellWidths, setCellWidths] = useState<number[]>([]);
 
   /**
    * Updates the cell widths based on the current sash (column bars) positions from the table component.
@@ -110,30 +235,55 @@ const NetworkRequestLog = ({
 
   useLayoutEffect(() => {
     updateCellWidths();
-  }, [sortedNetworkLogs]);
+  }, [networkLogs]);
 
-  // Scroll to the selected element when user clicks on it
-  useLayoutEffect(() => {
+  /**
+   * Creates a virtual row renderer for the network request log table.
+   * Uses TanStack Virtual to handle large lists by only rendering visible rows.
+   * https://tanstack.com/virtual/latest/docs/api/virtualizer
+   */
+  const rowVirtualizer = useVirtualizer<HTMLDivElement, VscodeTableRowElement>({
+    count: networkLogs.length,
+    overscan: ROW_OVERSCAN,
+    estimateSize: () => CELL_DEFAULT_HEIGHT,
+    getScrollElement: () =>
+      tableRef.current && (getScrollableTableContainer(tableRef.current) ?? null),
+  });
+
+  // If table's height changes and something is selected, scroll to the selected element
+  useEffect(() => {
     const table = tableRef.current;
     if (!table) {
       return;
     }
 
-    const scrollableContainer = getScrollableTableContainer(table);
-    const selectedElement = table.querySelector<HTMLDivElement>(".selected");
+    const timeout = setTimeout(() => {
+      const selectedElement = table.querySelector<HTMLDivElement>(".selected");
+      const selectedElementRect = selectedElement?.getBoundingClientRect();
+      const tableRect = table.getBoundingClientRect();
+      const selectedElementTop = selectedElementRect?.top ?? 0;
+      const tableTotalHeight = tableRect.top + tableRect.height;
+      const tableTop = tableRect.top;
 
-    if (!selectedElement || !scrollableContainer) {
-      return;
-    }
+      if (
+        !selectedElement ||
+        selectedElementTop > tableTotalHeight ||
+        selectedElementTop < tableTop
+      ) {
+        rowVirtualizer.scrollToIndex(selectedRequestIndex, {
+          behavior: "smooth",
+          align: "center",
+        });
+      }
+    }, SCROLL_TO_TOP_TIMEOUT);
 
-    scrollableContainer.scrollTo({
-      top: selectedElement.offsetTop - table.clientHeight / 2,
-      behavior: "smooth",
-    });
-  }, [selectedNetworkLog?.requestId, sortedNetworkLogs]);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [parentHeight]);
 
-  // If table's height changes and something is selected, scroll to the selected element
-  useLayoutEffect(() => {
+  // Scroll to the selected element when user clicks on it
+  useEffect(() => {
     const table = tableRef.current;
     if (!table) {
       return;
@@ -144,139 +294,58 @@ const NetworkRequestLog = ({
       return;
     }
 
-    const timeout = setTimeout(() => {
-      const scrollableContainer = getScrollableTableContainer(table);
-
-      if (selectedElement.offsetTop > table.clientHeight) {
-        scrollableContainer?.scrollTo({
-          top: selectedElement.offsetTop - table.clientHeight / 2,
-          behavior: "smooth",
-        });
-      }
-    }, SCROLL_TO_TOP_TIMEOUT);
-
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, [parentHeight]);
-
-  const getStatusClass = (status: number | string | undefined) => {
-    if (!status) {
-      return "";
-    }
-
-    const statusNum = Number(status);
-    if (statusNum >= 200 && statusNum < 400) {
-      return "status-success";
-    }
-
-    if (statusNum >= 400) {
-      return "status-error";
-    }
-
-    return "";
-  };
-
-  const logDetailsConfig = useMemo(
-    () => [
-      { title: NetworkLogColumn.Name },
-      {
-        title: NetworkLogColumn.Status,
-        getClass: (log: NetworkLog) => getStatusClass(log.response?.status) + " status",
-      },
-      { title: NetworkLogColumn.Method },
-      { title: NetworkLogColumn.Type },
-      { title: NetworkLogColumn.Size },
-      { title: NetworkLogColumn.Time },
-    ],
-    []
-  );
-
-  const handleHeaderClick = (column: NetworkLogColumn) => {
-    setSortState((prevState) => {
-      // If clicking on the same column, cycle through: asc -> desc -> null
-      if (prevState.column === column) {
-        switch (prevState.direction) {
-          case SortDirection.Asc:
-            return { column, direction: SortDirection.Desc };
-          case SortDirection.Desc:
-            return { column: null, direction: null };
-          case null:
-          default:
-            return { column, direction: SortDirection.Asc };
-        }
-      }
-      // If clicking on a different column or no current sort, start with ascending
-      return { column, direction: SortDirection.Asc };
+    rowVirtualizer.scrollToIndex(selectedRequestIndex, {
+      behavior: "smooth",
+      align: "center",
     });
-  };
+  }, [selectedNetworkLog?.requestId]);
 
-  const handleHeaderFilterClick = (e: React.MouseEvent, column: NetworkLogColumn) => {
-    // If filter was clicked, do not trigger sorting by stopping propagation
-    e.stopPropagation();
-    addColumnFilterToInputField(column);
-  };
-
-  const getSortIcon = (column: NetworkLogColumn) => {
-    if (sortState.column !== column) {
-      return "hidden"; // No icon when not sorting by this column
-    }
-    return sortState.direction === SortDirection.Asc
-      ? "codicon-chevron-up"
-      : "codicon-chevron-down";
+  const innerHandleSelectedRequest = (id: string | null, offset: number) => {
+    setSelectedRequestIndex(offset);
+    handleSelectedRequest(id);
   };
 
   return (
-    <div className="table-container">
-      <div style={{ width: "100%", overflowX: "hidden" }}>
-        <VscodeTable
-          zebra
-          bordered-columns
-          resizable
-          responsive
-          style={{ height: parentHeight }}
-          ref={tableRef}>
-          <VscodeTableHeader slot="header">
-            {logDetailsConfig.map(({ title }) => (
-              <VscodeTableHeaderCell key={title} onClick={() => handleHeaderClick(title)}>
-                <div className="table-header-cell">
-                  <span className="table-header-title">{capitalize(title)}</span>
-                  <IconButton onClick={(e) => handleHeaderFilterClick(e, title)}>
-                    <span className={`codicon codicon-filter-filled`}></span>
-                  </IconButton>
-                  <span className={`codicon ${getSortIcon(title)}`}></span>
-                </div>
-              </VscodeTableHeaderCell>
+    <VscodeTableBody style={{ height: `${rowVirtualizer.getTotalSize()}px` }} slot="body">
+      {rowVirtualizer.getVirtualItems().map((virtualRow, index) => {
+        const log = networkLogs[virtualRow.index];
+        return (
+          <VscodeTableRow
+            data-index={virtualRow.index}
+            key={log.requestId}
+            // Style needs to be overwritten using virtualizer values
+            style={{
+              height: `${virtualRow.size}px`,
+              transform: `translateY(${virtualRow.start - index * virtualRow.size}px)`,
+            }}
+            className={classNames(
+              "table-row",
+              selectedNetworkLog?.requestId === log.requestId && "selected"
+            )}
+            onClick={() =>
+              innerHandleSelectedRequest(
+                selectedNetworkLog?.requestId === log.requestId ? null : log.requestId,
+                virtualRow.index
+              )
+            }>
+            {LOG_DETAILS_CONFIG.map(({ title, getClass }, i) => (
+              <VscodeTableCell
+                key={`${log.requestId}-${title}`}
+                className={getClass?.(log) ?? ""}
+                style={{ width: cellWidths[i] || "auto" }}>
+                {getNetworkLogValue(log, title)}
+              </VscodeTableCell>
             ))}
-          </VscodeTableHeader>
-          <VscodeTableBody slot="body">
-            {sortedNetworkLogs.map((log) => (
-              <VscodeTableRow
-                key={log.requestId}
-                className={classNames(
-                  "table-row",
-                  selectedNetworkLog?.requestId === log.requestId && "selected"
-                )}
-                onClick={() =>
-                  handleSelectedRequest(
-                    selectedNetworkLog?.requestId === log.requestId ? null : log.requestId
-                  )
-                }>
-                {logDetailsConfig.map(({ title, getClass }, i) => (
-                  <VscodeTableCell
-                    key={`${log.requestId}-${title}`}
-                    className={getClass?.(log) ?? ""}
-                    style={{ width: cellWidths[i] || "auto" }}>
-                    <div>{getNetworkLogValue(log, title)}</div>
-                  </VscodeTableCell>
-                ))}
-              </VscodeTableRow>
-            ))}
-          </VscodeTableBody>
-        </VscodeTable>
-      </div>
-    </div>
+          </VscodeTableRow>
+        );
+      })}
+      {/* Below row, renedered unconditionally, is needed, because the VscodeTableBody
+      is styled with display:table, which causes rows to stretch to fit the container, despite
+      set size. As we will always have total low height lesser than the table-body height (because
+      virtualization) an additional row is needed to fill the remaining space */}
+      <VscodeTableRow className="hack-table-row"></VscodeTableRow>
+    </VscodeTableBody>
   );
-};
+}
 
 export default NetworkRequestLog;
