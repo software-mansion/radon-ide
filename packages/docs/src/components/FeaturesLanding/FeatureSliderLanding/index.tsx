cea0636b80c038cd60a13314c41a20534ae0f69c
spaleka <agata_spalek@interia.eu>
2025-08-27 14:28:57 +0200

    Feature image paths update to use useBaseUrl

--- packages/docs/src/components/FeaturesLanding/FeatureSliderLanding/index.tsx
+++ packages/docs/src/components/FeaturesLanding/FeatureSliderLanding/index.tsx
@@ -2,42 +2,43 @@ import React, { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 import FeatureCardLanding, { ActiveItem } from "./FeatureCardLanding";
 import { motion, AnimatePresence } from "motion/react";
-
-const features = [
-  {
-    badge: "Built-In Previews",
-    title: "Integrated Experience",
-    content:
-      "See the preview of your application right where you need it the most – close to your codebase. Radon IDE runs iOS Simulator and Android emulator directly in your Visual Studio Code and Cursor project.",
-    imageSrc: "../img/screenshot_hero.png",
-  },
-  {
-    badge: "Element Inspector",
-    title: "Click to Inspect",
-    content:
-      "Jump directly from preview to a file where your React Native component is defined. It can't really get simpler than that.",
-    imageSrc: "../img/hero.webp",
-  },
-  {
-    badge: "Network Inspector",
-    title: "Inspect Network Requests",
-    content:
-      "Use the built-in network panel to inspect your application network activity right in the editor.",
-    imageSrc: "../img/screenshot_hero.png",
-  },
-  {
-    badge: "React Scan Integration",
-    title: "Outline Renders",
-    content:
-      "Radon IDE can highlight components that re-render too frequently. See for yourself what parts of your application need optimization.",
-    imageSrc: "../img/hero.webp",
-  },
-];
+import useBaseUrl from "@docusaurus/useBaseUrl";
 
 export default function FeatureSliderLanding() {
   const [activeItem, setActiveItem] = useState<ActiveItem>({
     index: 0,
   });
+
+  const features = [
+    {
+      badge: "Built-In Previews",
+      title: "Integrated Experience",
+      content:
+        "See the preview of your application right where you need it the most – close to your codebase. Radon IDE runs iOS Simulator and Android emulator directly in your Visual Studio Code and Cursor project.",
+      imageSrc: useBaseUrl("/img/screenshot_hero.png"),
+    },
+    {
+      badge: "Element Inspector",
+      title: "Click to Inspect",
+      content:
+        "Jump directly from preview to a file where your React Native component is defined. It can't really get simpler than that.",
+      imageSrc: useBaseUrl("/img/hero.webp"),
+    },
+    {
+      badge: "Network Inspector",
+      title: "Inspect Network Requests",
+      content:
+        "Use the built-in network panel to inspect your application network activity right in the editor.",
+      imageSrc: useBaseUrl("/img/screenshot_hero.png"),
+    },
+    {
+      badge: "React Scan Integration",
+      title: "Outline Renders",
+      content:
+        "Radon IDE can highlight components that re-render too frequently. See for yourself what parts of your application need optimization.",
+      imageSrc: useBaseUrl("/img/hero.webp"),
+    },
+  ];
 
   useEffect(() => {
     const timeout = setTimeout(() => {
--- packages/docs/src/components/FeaturesLanding/FeaturesGrid/index.tsx
+++ packages/docs/src/components/FeaturesLanding/FeaturesGrid/index.tsx
@@ -1,53 +1,53 @@
 import React from "react";
 import FeaturesGridCard from "./FeaturesGridCard";
 import styles from "./styles.module.css";
-
-const featuresList = [
-  {
-    label: "Debugger",
-    title: "Use breakpoints right in VSCode",
-    content:
-      "Our VSCode extension integrates tightly with your deep-linked application, allowing you to effortlessly jump around the navigation structure. It supports both React Navigation and Expo Router projects.",
-    imageSrc: "./img/features/feature_debugger_dark.svg",
-  },
-  {
-    label: "Router Integration",
-    title: "Navigation made easier",
-    content:
-      "Our VSCode extension integrates tightly with your deep-linked application, allowing you to effortlessly jump around the navigation structure. It supports both React Navigation and Expo Router projects.",
-    imageSrc: "./img/features/feature_router_dark.svg",
-  },
-  {
-    label: "Logs",
-    title: "Search through the logs easily",
-    content:
-      "Radon IDE uses the built-in VSCode console allowing you to filter through the logs. The links displayed in the console automatically link back to your source code.",
-    imageSrc: "./img/features/feature_logs_dark.svg",
-  },
-  {
-    label: "Previews",
-    title: "Develop components in isolation",
-    content:
-      "Radon IDE comes with a package allowing to preview components in full isolation. Develop your components individually without distractions.",
-    imageSrc: "./img/features/feature_previews_dark.svg",
-  },
-  {
-    label: "Device Settings",
-    title: "Adjust device settings on the fly",
-    content:
-      "Adjust text size, light/dark mode and more with just a few clicks. With our IDE for React Native, you can focus fully on your app without switching between windows.",
-    imageSrc: "./img/features/feature_device_dark.svg",
-  },
-  {
-    label: "Screen Recording",
-    title: "Instant Replays",
-    content:
-      "Missed a bug? You can rewatch what happened on the device anytime. No need to manually start the recording ever again.",
-    imageSrc: "./img/features/feature_recording_dark.svg",
-  },
-];
+import useBaseUrl from "@docusaurus/useBaseUrl";
 
 export default function FeaturesGrid() {
+  const featuresList = [
+    {
+      label: "Debugger",
+      title: "Use breakpoints right in VSCode",
+      content:
+        "Our VSCode extension integrates tightly with your deep-linked application, allowing you to effortlessly jump around the navigation structure. It supports both React Navigation and Expo Router projects.",
+      imageSrc: useBaseUrl("/img/features/feature_debugger_dark.svg"),
+    },
+    {
+      label: "Router Integration",
+      title: "Navigation made easier",
+      content:
+        "Our VSCode extension integrates tightly with your deep-linked application, allowing you to effortlessly jump around the navigation structure. It supports both React Navigation and Expo Router projects.",
+      imageSrc: useBaseUrl("/img/features/feature_router_dark.svg"),
+    },
+    {
+      label: "Logs",
+      title: "Search through the logs easily",
+      content:
+        "Radon IDE uses the built-in VSCode console allowing you to filter through the logs. The links displayed in the console automatically link back to your source code.",
+      imageSrc: useBaseUrl("/img/features/feature_logs_dark.svg"),
+    },
+    {
+      label: "Previews",
+      title: "Develop components in isolation",
+      content:
+        "Radon IDE comes with a package allowing to preview components in full isolation. Develop your components individually without distractions.",
+      imageSrc: useBaseUrl("./img/features/feature_previews_dark.svg"),
+    },
+    {
+      label: "Device Settings",
+      title: "Adjust device settings on the fly",
+      content:
+        "Adjust text size, light/dark mode and more with just a few clicks. With our IDE for React Native, you can focus fully on your app without switching between windows.",
+      imageSrc: useBaseUrl("/img/features/feature_device_dark.svg"),
+    },
+    {
+      label: "Screen Recording",
+      title: "Instant Replays",
+      content:
+        "Missed a bug? You can rewatch what happened on the device anytime. No need to manually start the recording ever again.",
+      imageSrc: useBaseUrl("/img/features/feature_recording_dark.svg"),
+    },
+  ];
   return (
     <div className={styles.container}>
       {featuresList.map((feature, index) => (
