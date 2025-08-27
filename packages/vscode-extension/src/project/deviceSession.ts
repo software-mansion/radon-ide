3b404a35935ea13dfbe984dac23072d89861741f
Filip131311 <Filip.kaminski@swmansion.com>
2025-08-27 13:47:55 +0200

    init

--- packages/vscode-extension/src/project/deviceSession.ts
+++ packages/vscode-extension/src/project/deviceSession.ts
@@ -25,6 +25,7 @@ import {
   DeviceSessionStatus,
   FatalErrorDescriptor,
   InspectData,
+  InstallationError,
 } from "../common/Project";
 import { throttle, throttleAsync } from "../utilities/throttle";
 import { getTelemetryReporter } from "../utilities/telemetry";
@@ -42,6 +43,7 @@ import { FrameReporter } from "./FrameReporter";
 import { ScreenCapture } from "./ScreenCapture";
 import { disposeAll } from "../utilities/disposables";
 import { FileTransfer } from "./FileTransfer";
+import { I } from "react-scan/dist/index-KfBHGMR2";
 
 const MAX_URL_HISTORY_SIZE = 20;
 const CACHE_STALE_THROTTLE_MS = 10 * 1000; // 10 seconds
@@ -311,6 +313,16 @@ export class DeviceSession implements Disposable {
           message: e.message,
           buildType: e.buildType,
           platform: this.platform,
+        };
+        this.emitStateChange();
+        return;
+      } else if (e instanceof InstallationError) {
+        this.status = "fatalError";
+        this.fatalError = {
+          kind: "installation",
+          message: e.message,
+          platform: this.platform,
+          reason: e.reason,
         };
         this.emitStateChange();
         return;
@@ -677,6 +689,14 @@ export class DeviceSession implements Disposable {
           message: e.message,
           buildType: e.buildType,
           platform: this.platform,
+        };
+      } else if (e instanceof InstallationError) {
+        this.status = "fatalError";
+        this.fatalError = {
+          kind: "installation",
+          message: e.message,
+          platform: this.platform,
+          reason: e.reason,
         };
       } else {
         this.status = "fatalError";
