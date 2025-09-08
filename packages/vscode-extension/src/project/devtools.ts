babe6515dd257a61b368736d3ba5bbe97b23a869
Juliusz Wajgelt <juliusz.wajgelt@swmansion.com>
2025-09-08 10:48:35 +0200

    fix initialization not always working

--- packages/vscode-extension/src/project/devtools.ts
+++ packages/vscode-extension/src/project/devtools.ts
@@ -206,6 +206,10 @@ export class CDPDevtoolsServer extends DevtoolsServer implements Disposable {
             listener(ev.payload);
           }
         });
+        debugSession.evaluateExpression({
+          expression:
+            'void __FUSEBOX_REACT_DEVTOOLS_DISPATCHER__.initializeDomain("react-devtools")',
+        });
         return () => subscription.dispose();
       },
       send(event, payload, transferable) {
