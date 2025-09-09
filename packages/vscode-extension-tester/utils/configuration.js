b91de3bf5720fb25ad3a38db709c4fcab70cf316
Krzysztof Jablonski <krzysztof.jablonski@swmansion.com>
2025-09-09 10:10:57 +0200

    remove android devices from github actions

--- packages/vscode-extension-tester/utils/configuration.js
+++ packages/vscode-extension-tester/utils/configuration.js
@@ -1,12 +1,12 @@
 export default {
   isRecording: false,
 
-  // Android devices does not work on github actions macos runners nor local macos vms
-  // it's possibly caused by lack of support for hardware acceleration in virtualized environments (HVF error: HV_UNSUPPORTED)
-  // I didn't found any way to turn this acceleration off
+  // Android devices do not work on github actions macOS runners nor local macOS VMs
+  // This is possibly caused by the lack of support for hardware acceleration in virtualized environments (HVF error: HV_UNSUPPORTED)
+  // I didn't find any way to turn this acceleration off
   // GitHub issues related to this topic:
   // https://github.com/ReactiveCircus/android-emulator-runner/issues/350
   // https://github.com/utmapp/UTM/issues/6821
-  // android tests can be run on local machine with android studio installed
+  // android tests can be run on a local machine with android studio installed
   isAndroid: false,
 };
