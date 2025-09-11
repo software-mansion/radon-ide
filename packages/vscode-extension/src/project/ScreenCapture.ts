software-mansion/radon-ide

commit ce953bbcfe1a47d6f4fb9a7f830a87b3610516a8

refactor: move to legend state: Multimedia, zoom controls and initialized  (#1474)
This PR continues the move in the direction of the new state management
solution, by moving:
- multimedia state - we create a new `Multimedia` class that is
responsible for capturing screenshots screen recordings and replays.
This new class instance leaves in the `deviceSession` which allows as to
record multiple devices at once
- initialized - a simple state informing frontend about the end of
project initialization
- zoom controls 

### How Has This Been Tested: 

- run `reactNative81` bare application and test the multimedia features
- test zoom controls

### How Has This Change Been Documented:

internal change altho because of multidevice recording the users might
feel the difference.

packages/vscode-extension/src/project/ScreenCapture.ts
@@ -1,0 +1,100 @@
+import { Disposable } from "vscode";
+import { MultimediaData, ScreenCaptureState } from "../common/State";
+import { ApplicationContext } from "./ApplicationContext";
+import { StateManager } from "./StateManager";
+import { DeviceBase } from "../devices/DeviceBase";
+import { disposeAll } from "../utilities/disposables";
+import { saveMultimedia } from "../utilities/saveMultimedia";
+
+const MAX_RECORDING_TIME_SEC = 10 * 60; // 10 minutes
+
+export class ScreenCapture implements Disposable {
+  private disposables: Disposable[] = [];
+  private recordingTimeout: NodeJS.Timeout | undefined = undefined;
+  private recordingTimer: NodeJS.Timeout | undefined = undefined;
+
+  constructor(
+    private stateManager: StateManager<ScreenCaptureState>,
+    private device: DeviceBase,
+    private applicationContext: ApplicationContext
+  ) {
+    this.disposables.push(
+      new Disposable(() => {
+        this.stopRecording();
+      })
+    );
+
+    this.disposables.push(this.stateManager);
+  }
+
+  public startRecording(): void {
+    this.stateManager.setState({ isRecording: true, recordingTime: 0 });
+
+    this.device.startRecording();
+
+    this.recordingTimer = setInterval(() => {
+      const recordingTime = this.stateManager.getState().recordingTime;
+      this.stateManager.setState({
+        recordingTime: recordingTime + 1,
+      });
+    }, 1000);
+
+    this.recordingTimeout = setTimeout(() => {
+      this.stopRecording();
+    }, MAX_RECORDING_TIME_SEC * 1000);
+  }
+
+  public async captureAndStopRecording() {
+    const recording = await this.stopRecording();
+    await this.saveMultimedia(recording);
+  }
+
+  public async toggleRecording() {
+    if (this.recordingTimeout) {
+      this.captureAndStopRecording();
+    } else {
+      this.startRecording();
+    }
+  }
+
+  public async captureReplay() {
+    const replayData = await this.device.captureReplay(
+      this.applicationContext.workspaceConfiguration.deviceRotation
+    );
+    this.stateManager.setState({ replayData });
+  }
+
+  public async captureScreenshot() {
+    const screenshot = await this.device.captureScreenshot(
+      this.applicationContext.workspaceConfiguration.deviceRotation
+    );
+    await this.saveMultimedia(screenshot);
+  }
+
+  public async getScreenshot() {
+    return this.device.captureScreenshot(
+      this.applicationContext.workspaceConfiguration.deviceRotation
+    );
+  }
+
+  private async saveMultimedia(multimediaData: MultimediaData) {
+    const defaultPath =
+      this.applicationContext.workspaceConfiguration.defaultMultimediaSavingLocation;
+    return saveMultimedia(multimediaData, defaultPath ?? undefined);
+  }
+
+  private async stopRecording() {
+    clearTimeout(this.recordingTimeout);
+    clearInterval(this.recordingTimer);
+
+    this.stateManager.setState({ isRecording: false, recordingTime: 0 });
+
+    return this.device.captureAndStopRecording(
+      this.applicationContext.workspaceConfiguration.deviceRotation
+    );
+  }
+
+  public dispose() {
+    disposeAll(this.disposables);
+  }
+}

packages/vscode-extension/src/ai/mcp/toolExecutors.ts
@@ -15,7 +15,7 @@ export async function screenshotToolExec(): Promise<ToolResponse> {
     );
   }
 
-  const screenshot = await project.deviceSession.captureScreenshot(project.getDeviceRotation());
+  const screenshot = await project.getScreenshot();
 
   const contents = readFileSync(screenshot.tempFileLocation, { encoding: "base64" });
 

packages/vscode-extension/src/common/Project.ts
@@ -9,6 +9,7 @@ import {
   DevicePlatform,
   IOSDeviceTypeInfo,
   IOSRuntimeInfo,
+  MultimediaData,
 } from "./State";
 
 export type Locale = string;
@@ -86,7 +87,6 @@ type DeviceSessionStateCommon = {
   navigationHistory: NavigationHistoryItem[];
   navigationRouteList: NavigationRoute[];
   isUsingStaleBuild: boolean;
-  isRecordingScreen: boolean;
 };
 
 export enum InspectorBridgeStatus {
@@ -147,15 +147,11 @@ export type ConnectState = {
 };
 
 export type ProjectState = {
-  initialized: boolean;
   appRootPath: string | undefined;
-  previewZoom: ZoomLevelType | undefined; // Preview specific. Consider extracting to different location if we store more preview state
   selectedLaunchConfiguration: LaunchConfiguration;
   customLaunchConfigurations: LaunchConfiguration[];
   connectState: ConnectState;
 } & DeviceSessionsManagerState;
-
-export type ZoomLevelType = number | "Fit";
 
 export type AppPermissionType = "all" | "location" | "photos" | "contacts" | "calendar";
 
@@ -254,24 +250,16 @@ export interface ProjectEventMap {
   projectStateChanged: ProjectState;
   deviceSettingsChanged: DeviceSettings;
   licenseActivationChanged: boolean;
-  replayDataCreated: MultimediaData;
 }
 
 export interface ProjectEventListener<T> {
   (event: T): void;
 }
 
-export type MultimediaData = {
-  url: string;
-  tempFileLocation: string;
-  fileName: string;
-};
-
 export type IDEPanelMoveTarget = "new-window" | "editor-tab" | "side-panel";
 
 export interface ProjectInterface {
   getProjectState(): Promise<ProjectState>;
-  updatePreviewZoomLevel(zoom: ZoomLevelType): Promise<void>;
 
   /**
    * Creates a new launch configuration or updates an existing one.
@@ -325,8 +313,7 @@ export interface ProjectInterface {
   openSendFileDialog(): Promise<void>;
   sendFileToDevice(fileDescription: { fileName: string; data: ArrayBuffer }): Promise<void>;
 
-  startRecording(): void;
-  captureAndStopRecording(): void;
+  toggleRecording(): void;
   captureReplay(): void;
   captureScreenshot(): void;
   saveMultimedia(multimediaData: MultimediaData): Promise<boolean>;

packages/vscode-extension/src/common/State.ts
@@ -74,10 +74,27 @@ export type FrameReportingState = {
 
 // #endregion Frame Reporting State
 
+// #region Multimedia
+
+export type MultimediaData = {
+  url: string;
+  tempFileLocation: string;
+  fileName: string;
+};
+
+export type ScreenCaptureState = {
+  isRecording: boolean;
+  recordingTime: number; // in seconds
+  replayData: MultimediaData | null;
+};
+
+// #endregion Multimedia
+
 // #region Device Session
 
 export type DeviceSessionStore = {
   frameReporting: FrameReportingState;
+  screenCapture: ScreenCaptureState;
 };
 
 // #endregion Device Session
@@ -86,9 +103,13 @@ export type DeviceSessionStore = {
 
 export type DeviceSessions = Record<DeviceId, DeviceSessionStore>;
 
+export type ZoomLevelType = number | "Fit";
+
 export type ProjectStore = {
   applicationContext: ApplicationContextState;
   deviceSessions: DeviceSessions;
+  initialized: boolean;
+  previewZoom: ZoomLevelType;
   selectedDeviceSessionId: DeviceId | null;
 };
 
@@ -194,6 +215,11 @@ export const initialDeviceSessionStore: DeviceSessionStore = {
     enabled: false,
     frameReport: null,
   },
+  screenCapture: {
+    isRecording: false,
+    recordingTime: 0,
+    replayData: null,
+  },
 };
 
 export const initialState: State = {
@@ -209,6 +235,8 @@ export const initialState: State = {
       applicationDependencies: {},
     },
     deviceSessions: {},
+    initialized: false,
+    previewZoom: "Fit",
     selectedDeviceSessionId: null,
   },
   telemetry: {

packages/vscode-extension/src/devices/preview.ts
@@ -2,11 +2,11 @@ import path from "path";
 import { Disposable, workspace } from "vscode";
 import { exec, ChildProcess, lineReader } from "../utilities/subprocess";
 import { Logger } from "../Logger";
-import { MultimediaData, TouchPoint, DeviceButtonType, DeviceRotation } from "../common/Project";
+import { TouchPoint, DeviceButtonType, DeviceRotation } from "../common/Project";
 import { simulatorServerBinary } from "../utilities/simulatorServerBinary";
 import { watchLicenseTokenChange } from "../utilities/license";
 import { disposeAll } from "../utilities/disposables";
-import { FrameRateReport } from "../common/State";
+import { FrameRateReport, MultimediaData } from "../common/State";
 
 interface MultimediaPromiseHandlers {
   resolve: (value: MultimediaData) => void;

packages/vscode-extension/src/project/deviceSession.ts
@@ -43,6 +43,7 @@ import { DevicePlatform, DeviceSessionStore } from "../common/State";
 import { ReloadAction } from "./DeviceSessionsManager";
 import { StateManager } from "./StateManager";
 import { FrameReporter } from "./FrameReporter";
+import { ScreenCapture } from "./ScreenCapture";
 import { disposeAll } from "../utilities/disposables";
 
 const MAX_URL_HISTORY_SIZE = 20;
@@ -76,6 +77,7 @@ export class DeviceSession implements Disposable {
   private cancelToken: CancelToken = new CancelToken();
   private watchProjectSubscription: Disposable;
   private frameReporter: FrameReporter;
+  private screenCapture: ScreenCapture;
 
   private status: DeviceSessionStatus = "starting";
   private startupMessage: StartupMessage = StartupMessage.InitializingDevice;
@@ -120,6 +122,13 @@ export class DeviceSession implements Disposable {
       this.device
     );
     this.disposables.push(this.frameReporter);
+
+    this.screenCapture = new ScreenCapture(
+      this.stateManager.getDerived("screenCapture"),
+      this.device,
+      this.applicationContext
+    );
+    this.disposables.push(this.screenCapture);
 
     this.devtools = this.makeDevtools();
     this.metro = new MetroLauncher(this.devtools);
@@ -720,25 +729,33 @@ export class DeviceSession implements Disposable {
     this.frameReporter.stopReportingFrameRate();
   }
 
+  // #region Recording
+
+  public async toggleRecording() {
+    this.screenCapture.toggleRecording();
+  }
+
   public startRecording() {
-    this.isRecordingScreen = true;
-    this.emitStateChange();
-    return this.device.startRecording();
+    this.screenCapture.startRecording();
   }
 
-  public async captureAndStopRecording(rotation: DeviceRotation) {
-    this.isRecordingScreen = false;
-    this.emitStateChange();
-    return this.device.captureAndStopRecording(rotation);
+  public async captureAndStopRecording() {
+    this.screenCapture.captureAndStopRecording();
   }
 
-  public async captureReplay(rotation: DeviceRotation) {
-    return this.device.captureReplay(rotation);
+  public async captureReplay() {
+    this.screenCapture.captureReplay();
   }
 
-  public async captureScreenshot(rotation: DeviceRotation) {
-    return this.device.captureScreenshot(rotation);
+  public async captureScreenshot() {
+    this.screenCapture.captureScreenshot();
   }
+
+  public async getScreenshot() {
+    return this.screenCapture.getScreenshot();
+  }
+
+  // #endregion Recording
 
   public sendTouches(
     touches: Array<TouchPoint>,
@@ -835,7 +852,8 @@ export class DeviceSession implements Disposable {
     this.inspectorBridge.sendShowStorybookStoryRequest(componentTitle, storyName);
   }
 
-  //#region Methods delegated to Application Session
+  //#region Application Session
+
   public async updateToolEnabledState(toolName: ToolKey, enabled: boolean) {
     this.applicationSession?.updateToolEnabledState(toolName, enabled);
   }

packages/vscode-extension/src/project/project.ts
@@ -1,9 +1,8 @@
-import fs from "fs";
 import { EventEmitter } from "stream";
-import os, { homedir } from "os";
+import os from "os";
 import path from "path";
 import assert from "assert";
-import { env, Disposable, commands, workspace, window, Uri } from "vscode";
+import { env, Disposable, commands, workspace, window } from "vscode";
 import _ from "lodash";
 import { TelemetryEventProperties } from "@vscode/extension-telemetry";
 import {
@@ -16,14 +15,12 @@ import {
   DeviceSettings,
   IDEPanelMoveTarget,
   isOfEnumDeviceRotation,
-  MultimediaData,
   ProjectEventListener,
   ProjectEventMap,
   ProjectInterface,
   ProjectState,
   ToolsState,
   TouchPoint,
-  ZoomLevelType,
 } from "../common/Project";
 import { AppRootConfigController } from "../panels/AppRootConfigController";
 import { Logger } from "../Logger";
@@ -59,21 +56,18 @@ import {
   DevicesState,
   IOSDeviceTypeInfo,
   IOSRuntimeInfo,
+  MultimediaData,
   ProjectStore,
   WorkspaceConfiguration,
 } from "../common/State";
 import { EnvironmentDependencyManager } from "../dependency/EnvironmentDependencyManager";
-import { getTimestamp } from "../utilities/getTimestamp";
-import { Platform } from "../utilities/platform";
 import { Telemetry } from "./telemetry";
 import { EditorBindings } from "./EditorBindings";
+import { saveMultimedia } from "../utilities/saveMultimedia";
 
-const PREVIEW_ZOOM_KEY = "preview_zoom";
 const DEEP_LINKS_HISTORY_KEY = "deep_links_history";
 
 const DEEP_LINKS_HISTORY_LIMIT = 50;
-
-const MAX_RECORDING_TIME_SEC = 10 * 60; // 10 minutes
 
 export class Project implements Disposable, ProjectInterface, DeviceSessionsManagerDelegate {
   // #region Properties
@@ -171,9 +165,7 @@ export class Project implements Disposable, ProjectInterface, DeviceSessionsMana
     this.projectState = {
       selectedSessionId: null,
       deviceSessions: {},
-      initialized: false,
       appRootPath: this.relativeAppRootPath,
-      previewZoom: undefined,
       selectedLaunchConfiguration: this.selectedLaunchConfiguration,
       customLaunchConfigurations: this.launchConfigsManager.launchConfigurations,
       connectState: {
@@ -232,7 +224,7 @@ export class Project implements Disposable, ProjectInterface, DeviceSessionsMana
   // #region Device Session
 
   public onInitialized(): void {
-    this.updateProjectState({ initialized: true });
+    this.stateManager.setState({ initialized: true });
   }
 
   public onDeviceSessionsManagerStateChange(state: DeviceSessionsManagerState): void {
@@ -525,33 +517,14 @@ export class Project implements Disposable, ProjectInterface, DeviceSessionsMana
 
   // #region Recording
 
-  private recordingTimeout: NodeJS.Timeout | undefined = undefined;
-
-  public startRecording(): void {
-    getTelemetryReporter().sendTelemetryEvent("recording:start-recording", {
+  public async toggleRecording() {
+    getTelemetryReporter().sendTelemetryEvent("recording:toggle-recording", {
       platform: this.selectedDeviceSessionState?.deviceInfo.platform,
     });
     if (!this.deviceSession) {
       throw new Error("No device session available");
     }
-    this.deviceSession.startRecording();
-
-    this.recordingTimeout = setTimeout(() => {
-      this.stopRecording();
-    }, MAX_RECORDING_TIME_SEC * 1000);
-  }
-
-  public async captureAndStopRecording() {
-    const recording = await this.stopRecording();
-    await this.saveMultimedia(recording);
-  }
-
-  public async toggleRecording() {
-    if (this.recordingTimeout) {
-      this.captureAndStopRecording();
-    } else {
-      this.startRecording();
-    }
+    this.deviceSession.toggleRecording();
   }
 
   public async captureReplay() {
@@ -561,8 +534,7 @@ export class Project implements Disposable, ProjectInterface, DeviceSessionsMana
     if (!this.deviceSession) {
       throw new Error("No device session available");
     }
-    const replay = await this.deviceSession.captureReplay(this.getDeviceRotation());
-    this.eventEmitter.emit("replayDataCreated", replay);
+    this.deviceSession.captureReplay();
   }
 
   public async captureScreenshot() {
@@ -572,59 +544,21 @@ export class Project implements Disposable, ProjectInterface, DeviceSessionsMana
     if (!this.deviceSession) {
       throw new Error("No device session available");
     }
-
-    const screenshot = await this.deviceSession.captureScreenshot(this.getDeviceRotation());
-    await this.saveMultimedia(screenshot);
+    this.deviceSession.captureScreenshot();
   }
 
   public async saveMultimedia(multimediaData: MultimediaData) {
-    const extension = path.extname(multimediaData.tempFileLocation);
-    const timestamp = getTimestamp();
-    const baseFileName = multimediaData.fileName.substring(
-      0,
-      multimediaData.fileName.length - extension.length
-    );
-    const newFileName = `${baseFileName} ${timestamp}${extension}`;
-
-    const defaultMultimediaSavingLocation =
-      this.workspaceStateManager.getState().defaultMultimediaSavingLocation;
-
-    const defaultFolder =
-      defaultMultimediaSavingLocation && fs.existsSync(defaultMultimediaSavingLocation)
-        ? defaultMultimediaSavingLocation
-        : Platform.select({
-            macos: path.join(homedir(), "Desktop"),
-            windows: homedir(),
-            linux: homedir(),
-          });
-    const defaultUri = Uri.file(path.join(defaultFolder, newFileName));
-
-    // save dialog open the location dialog, it also warns the user if the file already exists
-    let saveUri = await window.showSaveDialog({
-      defaultUri: defaultUri,
-      filters: {
-        "Video Files": [extension],
-      },
-    });
-
-    if (!saveUri) {
-      return false;
-    }
-
-    await fs.promises.copyFile(multimediaData.tempFileLocation, saveUri.fsPath);
-    return true;
+    const defaultSavingPath = this.workspaceStateManager.getState().defaultMultimediaSavingLocation;
+    return saveMultimedia(multimediaData, defaultSavingPath ?? undefined);
   }
 
-  private async stopRecording() {
-    clearTimeout(this.recordingTimeout);
-
-    getTelemetryReporter().sendTelemetryEvent("recording:stop-recording", {
-      platform: this.selectedDeviceSessionState?.deviceInfo.platform,
-    });
+  // note: this method is used by the radon AI functionality to capture screenshots of the application
+  // thats why it is not part of the ProjectInterface
+  public async getScreenshot() {
     if (!this.deviceSession) {
       throw new Error("No device session available");
     }
-    return this.deviceSession.captureAndStopRecording(this.getDeviceRotation());
+    return this.deviceSession.getScreenshot();
   }
 
   // #endregion Recording
@@ -900,12 +834,6 @@ export class Project implements Disposable, ProjectInterface, DeviceSessionsMana
   // #endregion Dispose
 
   // #region To Be Removed
-
-  // TODO: this should be moved to new state management
-  public async updatePreviewZoomLevel(zoom: ZoomLevelType): Promise<void> {
-    this.updateProjectState({ previewZoom: zoom });
-    extensionContext.workspaceState.update(PREVIEW_ZOOM_KEY, zoom);
-  }
 
   // TODO: this should be removed from our public API
   // to control it's surface

packages/vscode-extension/src/utilities/saveMultimedia.ts
@@ -1,0 +1,45 @@
+import fs from "fs";
+import { homedir } from "os";
+import path from "path";
+import { Uri, window } from "vscode";
+import { MultimediaData } from "../common/State";
+import { getTimestamp } from "./getTimestamp";
+import { Platform } from "./platform";
+
+export async function saveMultimedia(
+  multimediaData: MultimediaData,
+  defaultSavingLocation?: string
+) {
+  const extension = path.extname(multimediaData.tempFileLocation);
+  const timestamp = getTimestamp();
+  const baseFileName = multimediaData.fileName.substring(
+    0,
+    multimediaData.fileName.length - extension.length
+  );
+  const newFileName = `${baseFileName} ${timestamp}${extension}`;
+
+  const defaultFolder =
+    defaultSavingLocation && fs.existsSync(defaultSavingLocation)
+      ? defaultSavingLocation
+      : Platform.select({
+          macos: path.join(homedir(), "Desktop"),
+          windows: homedir(),
+          linux: homedir(),
+        });
+  const defaultUri = Uri.file(path.join(defaultFolder, newFileName));
+
+  // save dialog open the location dialog, it also warns the user if the file already exists
+  let saveUri = await window.showSaveDialog({
+    defaultUri: defaultUri,
+    filters: {
+      "Video Files": [extension],
+    },
+  });
+
+  if (!saveUri) {
+    return false;
+  }
+
+  await fs.promises.copyFile(multimediaData.tempFileLocation, saveUri.fsPath);
+  return true;
+}

commit 3c02a0842a3e71bed839cd41ddff3f4ab5d81f04

fix: screen recording works only once (#1518)
Fixes a regression from #1474 

### How Has This Been Tested: 
- open an app in Radon
- start and stop recording screen
- start and stop recording screen

### How Has This Change Been Documented:
bugfix

packages/vscode-extension/src/project/ScreenCapture.ts
@@ -87,6 +87,9 @@ export class ScreenCapture implements Disposable {
     clearTimeout(this.recordingTimeout);
     clearInterval(this.recordingTimer);
 
+    this.recordingTimeout = undefined;
+    this.recordingTimer = undefined;
+
     this.stateManager.setState({ isRecording: false, recordingTime: 0 });
 
     return this.device.captureAndStopRecording(