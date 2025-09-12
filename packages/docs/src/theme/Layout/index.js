432a4f8fd86328422e5d3cfba1e126feadbbf907
spaleka <agata_spalek@interia.eu>
2025-09-12 12:04:21 +0200

    Mobile sidebar background change

--- packages/docs/src/theme/Layout/index.js
+++ packages/docs/src/theme/Layout/index.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 import ErrorBoundary from "@docusaurus/ErrorBoundary";
 import { PageMetadata } from "@docusaurus/theme-common";
 import { useKeyboardNavigation } from "@docusaurus/theme-common/internal";
