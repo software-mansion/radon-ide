2051ea3e0453c5121537e53f649aee722c4f4f6a
spaleka <agata_spalek@interia.eu>
2025-09-15 16:33:09 +0200

    light mode implementation and global colors names changes

--- packages/docs/src/components/Features/FeaturesGrid/index.tsx
+++ packages/docs/src/components/Features/FeaturesGrid/index.tsx
@@ -5,50 +5,53 @@ import useBaseUrl from "@docusaurus/useBaseUrl";
 import usePageType from "@site/src/hooks/usePageType";
 import ArrowRightSmallIcon from "../../ArrowRightSmallIcon";
 import { motion } from "motion/react";
+import { useColorMode } from "@docusaurus/theme-common";
 
 export default function FeaturesGrid() {
+  const { colorMode } = useColorMode();
+
   const featuresList = [
     {
       label: "Debugger",
       title: "Use breakpoints right in VSCode",
       content:
         "Our VSCode extension integrates tightly with your deep-linked application, allowing you to effortlessly jump around the navigation structure. It supports both React Navigation and Expo Router projects.",
-      imageSrc: useBaseUrl("/img/features/feature_debugger_dark.svg"),
+      imageSrc: useBaseUrl(`/img/features/feature_debugger_${colorMode}.svg`),
     },
     {
       label: "Router Integration",
       title: "Navigation made easier",
       content:
         "Our VSCode extension integrates tightly with your deep-linked application, allowing you to effortlessly jump around the navigation structure. It supports both React Navigation and Expo Router projects.",
-      imageSrc: useBaseUrl("/img/features/feature_router_dark.svg"),
+      imageSrc: useBaseUrl(`/img/features/feature_router_${colorMode}.svg`),
     },
     {
       label: "Logs",
       title: "Search through the logs easily",
       content:
         "Radon IDE uses the built-in VSCode console allowing you to filter through the logs. The links displayed in the console automatically link back to your source code.",
-      imageSrc: useBaseUrl("/img/features/feature_logs_dark.svg"),
+      imageSrc: useBaseUrl(`/img/features/feature_logs_${colorMode}.svg`),
     },
     {
       label: "Previews",
       title: "Develop components in isolation",
       content:
         "Radon IDE comes with a package allowing to preview components in full isolation. Develop your components individually without distractions.",
-      imageSrc: useBaseUrl("/img/features/feature_previews_dark.svg"),
+      imageSrc: useBaseUrl(`/img/features/feature_previews_${colorMode}.svg`),
     },
     {
       label: "Device Settings",
       title: "Adjust device settings on the fly",
       content:
         "Adjust text size, light/dark mode and more with just a few clicks. With our IDE for React Native, you can focus fully on your app without switching between windows.",
-      imageSrc: useBaseUrl("/img/features/feature_device_dark.svg"),
+      imageSrc: useBaseUrl(`/img/features/feature_device_${colorMode}.svg`),
     },
     {
       label: "Screen Recording",
       title: "Instant Replays",
       content:
         "Missed a bug? You can rewatch what happened on the device anytime. No need to manually start the recording ever again.",
-      imageSrc: useBaseUrl("/img/features/feature_recording_dark.svg"),
+      imageSrc: useBaseUrl(`/img/features/feature_recording_${colorMode}.svg`),
     },
   ];
 
