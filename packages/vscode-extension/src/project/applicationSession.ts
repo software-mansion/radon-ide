software-mansion/radon-ide

commit b5ed36fc4f842ea743833feee8a79be3a315d2d5

refactor: split Devtools into Server and Connection (#1497)
Splits the current `Devtools` class into `DevtoolsServer` (a server the
RN application connects to) and `DevtoolsConnection` (an active
connection between a running app and Radon, created when the app
connects to the `DevtoolsServer`).

This is motivated by two things:
- we may wish to reuse the same `DevtoolsServer` between different
`Application`/`DeviceSession`s. We're planning on reusing the same Metro
server for all currently running applications, but because we use Metro
to provide the port the application should connect to React DevTools
with, we need to run a single Server for all apps using that Metro
instance.
- we may want to move the React DevTools communication from the
WebSocket implementation we currently use to CDP, which is the default
for the new RN DevTools (RN >0.75). This abstracts the communication
channel away from how Radon uses the RDT to talk with the app.

### How Has This Been Tested: 
- open expo-52-prebuild-with-plugins test app in Radon
- check that the devtools (Network, Redux, React Query) still work
- check that element inspection still works
- open an app running Expo Go on Android
- check that debugger works correctly after JS Reloads

### How Has This Change Been Documented:
Internal change

packages/vscode-extension/src/project/applicationSession.ts
@@ -15,13 +15,12 @@ import { ApplicationContext } from "./ApplicationContext";
 import { MetroLauncher } from "./metro";
 import { ReconnectingDebugSession } from "../debugging/ReconnectingDebugSession";
 import { DeviceBase } from "../devices/DeviceBase";
-import { Devtools } from "./devtools";
 import { Logger } from "../Logger";
 import { AppOrientation, InspectData, StartupMessage } from "../common/Project";
 import { disposeAll } from "../utilities/disposables";
 import { ToolKey, ToolPlugin, ToolsManager } from "./tools";
 import { focusSource } from "../utilities/focusSource";
-import { CancelToken } from "../utilities/cancelToken";
+import { CancelError, CancelToken } from "../utilities/cancelToken";
 import { BuildResult } from "../builders/BuildManager";
 import {
   ApplicationSessionState,
@@ -33,26 +32,52 @@ import {
 } from "../common/State";
 import { isAppSourceFile } from "../utilities/isAppSourceFile";
 import { StateManager } from "./StateManager";
+import { DevtoolsConnection, DevtoolsInspectorBridge, DevtoolsServer } from "./devtools";
+import { RadonInspectorBridge } from "./bridge";
 
 interface LaunchApplicationSessionDeps {
   applicationContext: ApplicationContext;
   device: DeviceBase;
   buildResult: BuildResult;
   metro: MetroLauncher;
-  devtools: Devtools;
+  devtoolsServer: DevtoolsServer;
+  devtoolsPort: number;
+}
+
+function waitForAppReady(inspectorBridge: RadonInspectorBridge, cancelToken?: CancelToken) {
+  // set up `appReady` promise
+  const { promise, resolve, reject } = Promise.withResolvers<void>();
+  cancelToken?.onCancel(() => {
+    reject(new CancelError("Cancelled while waiting for the app to be ready."));
+  });
+  const appReadyListener = inspectorBridge.onEvent("appReady", resolve);
+  promise.finally(() => {
+    appReadyListener.dispose();
+  });
+  return promise;
 }
 
 export class ApplicationSession implements Disposable {
   private disposables: Disposable[] = [];
   private debugSession?: DebugSession & Disposable;
   private debugSessionEventSubscription?: Disposable;
-  private toolsManager: ToolsManager;
   private isActive = false;
   private inspectCallID = 7621;
+  private devtools: DevtoolsConnection | undefined;
+  private toolsManager: ToolsManager;
+
+  public readonly inspectorBridge: RadonInspectorBridge;
 
   public static async launch(
     stateManager: StateManager<ApplicationSessionState>,
-    { applicationContext, device, buildResult, metro, devtools }: LaunchApplicationSessionDeps,
+    {
+      applicationContext,
+      device,
+      buildResult,
+      metro,
+      devtoolsServer,
+      devtoolsPort,
+    }: LaunchApplicationSessionDeps,
     getIsActive: () => boolean,
     onLaunchStage: (stage: StartupMessage) => void,
     cancelToken: CancelToken
@@ -66,7 +91,7 @@ export class ApplicationSession implements Disposable {
       applicationContext,
       device,
       metro,
-      devtools,
+      devtoolsServer,
       packageNameOrBundleId,
       supportedOrientations
     );
@@ -94,11 +119,13 @@ export class ApplicationSession implements Disposable {
     try {
       onLaunchStage(StartupMessage.Launching);
       await cancelToken.adapt(
-        device.launchApp(buildResult, metro.port, devtools.port, launchArguments)
+        device.launchApp(buildResult, metro.port, devtoolsPort, launchArguments)
       );
 
+      const appReadyPromise = waitForAppReady(session.inspectorBridge, cancelToken);
+
       onLaunchStage(StartupMessage.WaitingForAppToLoad);
-      await cancelToken.adapt(Promise.race([devtools.appReady(), bundleErrorPromise]));
+      await cancelToken.adapt(Promise.race([appReadyPromise, bundleErrorPromise]));
 
       if (getIsActive()) {
         const activatePromise = session.activate();
@@ -125,14 +152,44 @@ export class ApplicationSession implements Disposable {
     private readonly applicationContext: ApplicationContext,
     private readonly device: DeviceBase,
     private readonly metro: MetroLauncher,
-    private readonly devtools: Devtools,
+    private readonly devtoolsServer: DevtoolsServer,
     private readonly packageNameOrBundleId: string,
     private readonly supportedOrientations: DeviceRotation[]
   ) {
-    this.registerDevtoolsListeners();
+    this.disposables.push(
+      this.devtoolsServer.onConnection((devtools) => {
+        this.devtools?.dispose();
+        this.devtools = devtools;
+        this.stateManager.setState({ inspectorBridgeStatus: InspectorBridgeStatus.Connected });
+        const disconnectedSubscription = devtools.onDisconnected(() => {
+          disconnectedSubscription.dispose();
+          if (devtools !== this.devtools) {
+            return;
+          }
+          if (
+            this.stateManager.getState().inspectorBridgeStatus === InspectorBridgeStatus.Connected
+          ) {
+            this.stateManager.setState({
+              inspectorBridgeStatus: InspectorBridgeStatus.Disconnected,
+            });
+          }
+          this.devtools = undefined;
+        });
+      })
+    );
     this.registerMetroListeners();
-    this.toolsManager = new ToolsManager(this.stateManager.getDerived("toolsState"), this.devtools);
 
+    const devtoolsInspectorBridge = new DevtoolsInspectorBridge(this.devtoolsServer);
+    this.inspectorBridge = devtoolsInspectorBridge;
+    const inspectorBridgeSubscriptions =
+      this.registerInspectorBridgeEventListeners(devtoolsInspectorBridge);
+    this.disposables.push(devtoolsInspectorBridge, ...inspectorBridgeSubscriptions);
+
+    this.toolsManager = new ToolsManager(
+      this.stateManager.getDerived("toolsState"),
+      devtoolsInspectorBridge
+    );
+    this.disposables.push(this.toolsManager);
     this.disposables.push(this.stateManager);
   }
 
@@ -148,7 +205,7 @@ export class ApplicationSession implements Disposable {
         useParentDebugSession: true,
       }),
       this.metro,
-      this.devtools
+      this.devtoolsServer
     );
 
     await session.startParentDebugSession();
@@ -159,11 +216,11 @@ export class ApplicationSession implements Disposable {
   // #region Tools
 
   public async updateToolEnabledState(toolName: ToolKey, enabled: boolean) {
-    this.toolsManager.updateToolEnabledState(toolName, enabled);
+    return this.toolsManager.updateToolEnabledState(toolName, enabled);
   }
 
   public openTool(toolName: ToolKey): void {
-    this.toolsManager.openTool(toolName);
+    return this.toolsManager.openTool(toolName);
   }
 
   public getPlugin(toolName: ToolKey): ToolPlugin | undefined {
@@ -344,70 +401,58 @@ export class ApplicationSession implements Disposable {
     });
   }
 
-  private registerDevtoolsListeners() {
-    this.disposables.push(
-      this.devtools.onEvent("appReady", () => {
+  private registerInspectorBridgeEventListeners(inspectorBridge: RadonInspectorBridge) {
+    const subscriptions = [
+      inspectorBridge.onEvent("appReady", () => {
         // NOTE: since this is triggered by the JS bundle,
         // we can assume that if it fires, the bundle loaded successfully.
         // This is necessary to reset the bundle error state when the app reload
         // is triggered from the app itself (e.g. by in-app dev menu or redbox).
         this.stateManager.setState({ bundleError: null });
       }),
-      this.devtools.onEvent("fastRefreshStarted", () => {
+      inspectorBridge.onEvent("fastRefreshStarted", () => {
         this.stateManager.setState({ bundleError: null, isRefreshing: true });
       }),
-      this.devtools.onEvent("fastRefreshComplete", () => {
+      inspectorBridge.onEvent("fastRefreshComplete", () => {
         this.stateManager.setState({ isRefreshing: false });
       }),
-      this.devtools.onEvent("isProfilingReact", (isProfiling) => {
+      inspectorBridge.onEvent("isProfilingReact", (isProfiling) => {
         if (this.stateManager.getState().profilingReactState !== "saving") {
           this.stateManager.setState({
             profilingReactState: isProfiling ? "profiling" : "stopped",
           });
         }
       }),
-      this.devtools.onEvent("appOrientationChanged", (orientation: AppOrientation) => {
+      inspectorBridge.onEvent("appOrientationChanged", (orientation: AppOrientation) => {
         this.stateManager.setState({ appOrientation: this.determineAppOrientation(orientation) });
       }),
-      this.devtools.onEvent(
+      inspectorBridge.onEvent(
         "inspectorAvailabilityChanged",
         (inspectorAvailability: InspectorAvailabilityStatus) => {
           this.stateManager.setState({ elementInspectorAvailability: inspectorAvailability });
         }
       ),
-      this.devtools.onEvent("disconnected", () => {
-        if (
-          this.stateManager.getState().inspectorBridgeStatus === InspectorBridgeStatus.Connected
-        ) {
-          this.stateManager.setState({ inspectorBridgeStatus: InspectorBridgeStatus.Disconnected });
-        }
-      }),
-      this.devtools.onEvent("connected", () => {
-        if (
-          this.stateManager.getState().inspectorBridgeStatus !== InspectorBridgeStatus.Connected
-        ) {
-          this.stateManager.setState({ inspectorBridgeStatus: InspectorBridgeStatus.Connected });
-        }
-      })
-    );
+    ];
+    return subscriptions;
   }
   //#endregion
 
-  public async reloadJS() {
-    if (!this.devtools.hasConnectedClient) {
+  public async reloadJS(cancelToken: CancelToken) {
+    if (!this.devtools?.connected) {
       Logger.debug(
         "`reloadJS()` was called on an application session while the devtools are not connected. " +
           "This should never happen, since an application session should represent a running and connected application."
       );
       throw new Error("Tried to reload JS on an application which disconnected from Radon");
     }
-    const { promise: bundleErrorPromise, reject } = Promise.withResolvers();
+    const { promise: bundleErrorPromise, reject: rejectBundleError } = Promise.withResolvers();
     const bundleErrorSubscription = this.metro.onBundleError(() => {
-      reject(new Error("Bundle error occurred during reload"));
+      rejectBundleError(new Error("Bundle error occurred during reload"));
     });
     try {
+      const appReadyPromise = waitForAppReady(this.inspectorBridge, cancelToken);
       await this.metro.reload();
-      await Promise.race([this.devtools.appReady(), bundleErrorPromise]);
+      await Promise.race([appReadyPromise, bundleErrorPromise]);
     } finally {
       bundleErrorSubscription.dispose();
     }
@@ -467,12 +512,12 @@ export class ApplicationSession implements Disposable {
 
   //#region React Profiling
   public async startProfilingReact() {
-    return await this.devtools.startProfilingReact();
+    return await this.devtools?.startProfilingReact();
   }
 
   public async stopProfilingReact() {
     try {
-      return await this.devtools.stopProfilingReact();
+      return await this.devtools?.stopProfilingReact();
     } finally {
       this.stateManager.setState({ profilingReactState: "stopped" });
     }
@@ -487,16 +532,16 @@ export class ApplicationSession implements Disposable {
   ): Promise<InspectData> {
     const id = this.inspectCallID++;
     const { promise, resolve, reject } = Promise.withResolvers<InspectData>();
-    const listener = this.devtools.onEvent("inspectData", (payload) => {
+    const listener = this.inspectorBridge.onEvent("inspectData", (payload) => {
       if (payload.id === id) {
-        listener.dispose();
+        listener?.dispose();
         resolve(payload as unknown as InspectData);
       } else if (payload.id >= id) {
-        listener.dispose();
+        listener?.dispose();
         reject("Inspect request was invalidated by a later request");
       }
     });
-    this.devtools.sendInspectRequest(xRatio, yRatio, id, requestStack);
+    this.inspectorBridge.sendInspectRequest(xRatio, yRatio, id, requestStack);
 
     const inspectData = await promise;
     let stack = undefined;

packages/vscode-extension/src/project/bridge.ts
@@ -5,8 +5,6 @@ import { InspectorAvailabilityStatus } from "../common/State";
 
 export interface RadonInspectorBridgeEvents {
   appReady: [];
-  connected: [];
-  disconnected: [];
   navigationChanged: [{ displayName: string; id: string }];
   navigationRouteListUpdated: [NavigationRoute[]];
   fastRefreshStarted: [];

packages/vscode-extension/src/project/devtools.ts
@@ -2,8 +2,9 @@ import http from "http";
 import path from "path";
 import fs from "fs";
 import os from "os";
-import { Disposable, Uri } from "vscode";
-import { WebSocketServer, WebSocket } from "ws";
+import assert from "assert";
+import { Disposable, EventEmitter, Uri } from "vscode";
+import { WebSocketServer } from "ws";
 import { Logger } from "../Logger";
 import {
   createBridge,
@@ -11,8 +12,10 @@ import {
   prepareProfilingDataExport,
   Store,
   Wall,
+  FrontendBridge,
 } from "../../third-party/react-devtools/headless";
-import { BaseInspectorBridge } from "./bridge";
+import { BaseInspectorBridge, RadonInspectorBridgeEvents } from "./bridge";
+import { disposeAll } from "../utilities/disposables";
 
 function filePathForProfile() {
   const fileName = `profile-${Date.now()}.reactprofile`;
@@ -20,108 +23,79 @@ function filePathForProfile() {
   return filePath;
 }
 
-export class Devtools extends BaseInspectorBridge implements Disposable {
-  private _port = 0;
-  private server: any;
-  private socket?: WebSocket;
-  private startPromise: Promise<void> | undefined;
-  private store: Store | undefined;
+type IdeMessageListener = <K extends keyof RadonInspectorBridgeEvents>(event: {
+  type: K;
+  data: RadonInspectorBridgeEvents[K];
+}) => void;
+type IdeMessage = Parameters<IdeMessageListener>[0];
 
-  public get port() {
-    return this._port;
-  }
+/**
+ * InspectorBridge implementation that uses the React DevTools frontend to receive messages from the application.
+ */
+export class DevtoolsInspectorBridge extends BaseInspectorBridge implements Disposable {
+  private devtoolsConnection: DevtoolsConnection | undefined;
+  private devtoolsServerListener: Disposable;
+  private devtoolsConnectionListeners: Disposable[] = [];
 
-  public get hasConnectedClient() {
-    return this.socket !== undefined;
-  }
-
-  public async ready() {
-    if (!this.startPromise) {
-      throw new Error("Devtools not started");
+  constructor(devtoolsServer: DevtoolsServer) {
+    super();
+    if (devtoolsServer.connection) {
+      this.setupBridge(devtoolsServer.connection);
     }
-    await this.startPromise;
+    this.devtoolsServerListener = devtoolsServer.onConnection(this.setupBridge);
   }
 
-  public async appReady() {
-    const { resolve, promise } = Promise.withResolvers<void>();
-    const listener = this.onEvent("appReady", () => {
-      resolve();
-      listener.dispose();
-    });
-    return promise;
-  }
-
-  public async start() {
-    if (this.startPromise) {
-      throw new Error("Devtools already started");
-    }
-    this.startPromise = this.startInternal();
-    return this.startPromise;
-  }
-
-  private async startInternal() {
-    this.server = http.createServer(() => {});
-    const wss = new WebSocketServer({ server: this.server });
-
-    wss.on("connection", (ws) => {
-      if (this.socket !== undefined) {
-        Logger.error("Devtools client already connected");
-        this.socket.close();
-      }
-      Logger.debug("Devtools client connected");
-      this.socket = ws;
-
-      const wall: Wall = {
-        listen(fn) {
-          function listener(message: string) {
-            const parsedMessage = JSON.parse(message);
-            return fn(parsedMessage);
-          }
-          ws.on("message", listener);
-          return () => {
-            ws.off("message", listener);
-          };
-        },
-        send(event, payload) {
-          ws.send(JSON.stringify({ event, payload }));
-        },
-      };
-
-      const bridge = createBridge(wall);
-      const store = createStore(bridge);
-      this.store = store;
-
-      ws.on("close", () => {
-        if (this.socket === ws) {
-          this.socket = undefined;
-          this.emitEvent("disconnected", []);
+  private setupBridge = (connection: DevtoolsConnection) => {
+    this.devtoolsConnection = connection;
+    disposeAll(this.devtoolsConnectionListeners);
+    this.devtoolsConnectionListeners = [
+      connection.onIdeMessage((message) => {
+        this.emitEvent(message.type, message.data);
+      }),
+      connection.onDisconnected(() => {
+        if (connection === this.devtoolsConnection) {
+          this.devtoolsConnectionListeners = [];
+          disposeAll(this.devtoolsConnectionListeners);
         }
-        bridge.shutdown();
-        if (this.store === store) {
-          this.store = undefined;
-        }
-      });
+      }),
+    ];
+  };
 
-      bridge.addListener("RNIDE_message", (payload: any) => {
-        const { type, data } = payload;
-        this.emitEvent(type, data);
-      });
+  dispose() {
+    disposeAll(this.devtoolsConnectionListeners);
+    this.devtoolsServerListener.dispose();
+  }
 
-      // Register for isProfiling event on the profiler store
-      store.profilerStore.addListener("isProfiling", () => {
-        // @ts-ignore - isProfilingBasedOnUserInput exists but types are outdated
-        this.emitEvent("isProfilingReact", store.profilerStore.isProfilingBasedOnUserInput);
-      });
+  protected send(message: unknown): void {
+    this.devtoolsConnection?.send("RNIDE_message", message);
+  }
+}
 
-      this.emitEvent("connected", []);
+export class DevtoolsConnection implements Disposable {
+  private bridge: FrontendBridge;
+  private store: Store;
+
+  public connected: boolean = true;
+
+  private readonly ideMessageEventEmitter: EventEmitter<IdeMessage> = new EventEmitter();
+  private readonly disconnectedEventEmitter: EventEmitter<void> = new EventEmitter();
+
+  public readonly onIdeMessage = this.ideMessageEventEmitter.event;
+  public readonly onDisconnected = this.disconnectedEventEmitter.event;
+
+  constructor(private readonly wall: Wall) {
+    // create the DevTools frontend for the connection
+    this.bridge = createBridge(wall);
+    this.store = createStore(this.bridge);
+
+    this.bridge.addListener("RNIDE_message", (payload: unknown) => {
+      this.ideMessageEventEmitter.fire(payload as IdeMessage);
     });
 
-    return new Promise<void>((resolve) => {
-      this.server.listen(0, () => {
-        this._port = this.server.address().port;
-        Logger.info(`Devtools started on port ${this._port}`);
-        resolve();
-      });
+    // Register for isProfiling event on the profiler store
+    this.store.profilerStore.addListener("isProfiling", () => {
+      // @ts-ignore - isProfilingBasedOnUserInput exists but types are outdated
+      this.emitEvent("isProfilingReact", this.store.profilerStore.isProfilingBasedOnUserInput);
     });
   }
 
@@ -152,11 +126,126 @@ export class Devtools extends BaseInspectorBridge implements Disposable {
     return promise;
   }
 
-  public dispose() {
-    this.server?.close();
+  public send(event: string, payload: unknown) {
+    this.wall.send(event, payload);
   }
 
-  protected send(message: any) {
-    this.socket?.send(JSON.stringify({ event: "RNIDE_message", payload: message }));
+  public disconnect() {
+    if (this.connected) {
+      this.disconnectedEventEmitter.fire();
+    }
+    this.dispose();
   }
+
+  public dispose() {
+    if (!this.connected) {
+      return;
+    }
+    this.connected = false;
+    this.bridge.shutdown();
+    this.disconnectedEventEmitter.dispose();
+    this.ideMessageEventEmitter.dispose();
+  }
+}
+
+export abstract class DevtoolsServer implements Disposable {
+  private readonly connectionEventEmitter: EventEmitter<DevtoolsConnection> = new EventEmitter();
+  private _connection: DevtoolsConnection | undefined;
+
+  protected setConnection(connection: DevtoolsConnection | undefined) {
+    this._connection?.disconnect();
+    this._connection = connection;
+    if (connection) {
+      this.connectionEventEmitter.fire(connection);
+    }
+  }
+
+  public get connection() {
+    return this._connection;
+  }
+
+  public readonly onConnection = this.connectionEventEmitter.event;
+
+  public dispose(): void {
+    this.connectionEventEmitter.dispose();
+  }
+}
+
+class WebSocketDevtoolsServer extends DevtoolsServer implements Disposable {
+  private wss: WebSocketServer;
+
+  public get port() {
+    const address = this.server.address();
+    assert(
+      address !== null && typeof address !== "string",
+      "The address is an instance of `AddressInfo`"
+    );
+    return address.port;
+  }
+
+  constructor(private server: http.Server) {
+    super();
+    this.wss = new WebSocketServer({ server });
+
+    this.wss.on("connection", (ws) => {
+      const wall: Wall = {
+        listen(fn) {
+          function listener(message: string) {
+            const parsedMessage = JSON.parse(message);
+            return fn(parsedMessage);
+          }
+          ws.on("message", listener);
+          return () => {
+            ws.off("message", listener);
+          };
+        },
+        send(event, payload) {
+          ws.send(JSON.stringify({ event, payload }));
+        },
+      };
+
+      const connection = new DevtoolsConnection(wall);
+      ws.on("close", () => {
+        connection.disconnect();
+      });
+      ws.on("error", () => {
+        connection.disconnect();
+      });
+
+      super.setConnection(connection);
+    });
+  }
+
+  public dispose() {
+    super.dispose();
+    this.wss.close();
+    this.server.close();
+  }
+}
+
+export async function createWebSocketDevtoolsServer(): Promise<DevtoolsServer & { port: number }> {
+  const server = http.createServer(() => {});
+  const { promise: listenPromise, resolve, reject } = Promise.withResolvers<void>();
+
+  server.listen(0, () => {
+    const address = server.address();
+    assert(
+      address !== null && typeof address !== "string",
+      "The address is an instance of `AddressInfo`"
+    );
+    const serverPort = address.port;
+    Logger.info(`Devtools started on port ${serverPort}`);
+    resolve();
+  });
+
+  function onErrorCallback(error: Error) {
+    Logger.error("Devtools server error:", error);
+    reject(new Error(`Could not start the React Devtools server`));
+  }
+  server.on("error", onErrorCallback);
+
+  await listenPromise;
+
+  const devtoolsServer = new WebSocketDevtoolsServer(server);
+  return devtoolsServer;
 }

packages/vscode-extension/src/debugging/ReconnectingDebugSession.ts
@@ -4,8 +4,8 @@ import { Metro } from "../project/metro";
 import { CancelToken } from "../utilities/cancelToken";
 import { sleep } from "../utilities/retry";
 import { DebugSession, DebugSource, JSDebugConfiguration } from "./DebugSession";
-import { Devtools } from "../project/devtools";
 import { disposeAll } from "../utilities/disposables";
+import { DevtoolsServer } from "../project/devtools";
 
 const PING_TIMEOUT = 1000;
 export class ReconnectingDebugSession implements DebugSession, Disposable {
@@ -17,10 +17,12 @@ export class ReconnectingDebugSession implements DebugSession, Disposable {
   constructor(
     private readonly debugSession: DebugSession & Partial<Disposable>,
     private readonly metro: Metro,
-    devtools: Devtools
+    devtoolsServer: DevtoolsServer
   ) {
     this.disposables.push(debugSession.onDebugSessionTerminated(this.maybeReconnect));
-    this.disposables.push(devtools.onEvent("appReady", this.maybeReconnect));
+    // NOTE: with Expo Go on Android, the debugger can become unresponsive after a JS reload.
+    // Since a JS reload causes the devtools to reconnect, we can use that as a hint to reconnect the debugger.
+    this.disposables.push(devtoolsServer.onConnection(this.maybeReconnect));
   }
 
   public async startJSDebugSession(configuration: JSDebugConfiguration) {

commit 892e7e21a0e36db51ab91928a82359292024e620

fix: react profiler cannot start (#1532)
Fixes a regression in #1497 where the React Profiler starting does not
set the application state correctly, making it unusable.

### How Has This Been Tested: 
- open an app in Radon
- press "start react profiler" in the tools menu
- cause some app renders
- press "stop react profiler" in the tools menu
- the profile should open

packages/vscode-extension/src/project/applicationSession.ts
@@ -161,8 +161,7 @@ export class ApplicationSession implements Disposable {
         this.devtools?.dispose();
         this.devtools = devtools;
         this.stateManager.setState({ inspectorBridgeStatus: InspectorBridgeStatus.Connected });
-        const disconnectedSubscription = devtools.onDisconnected(() => {
-          disconnectedSubscription.dispose();
+        devtools.onDisconnected(() => {
           if (devtools !== this.devtools) {
             return;
           }
@@ -174,6 +173,13 @@ export class ApplicationSession implements Disposable {
             });
           }
           this.devtools = undefined;
+        });
+        devtools.onProfilingChange((isProfiling) => {
+          if (this.stateManager.getState().profilingReactState !== "saving") {
+            this.stateManager.setState({
+              profilingReactState: isProfiling ? "profiling" : "stopped",
+            });
+          }
         });
       })
     );
@@ -415,13 +421,6 @@ export class ApplicationSession implements Disposable {
       }),
       inspectorBridge.onEvent("fastRefreshComplete", () => {
         this.stateManager.setState({ isRefreshing: false });
-      }),
-      inspectorBridge.onEvent("isProfilingReact", (isProfiling) => {
-        if (this.stateManager.getState().profilingReactState !== "saving") {
-          this.stateManager.setState({
-            profilingReactState: isProfiling ? "profiling" : "stopped",
-          });
-        }
       }),
       inspectorBridge.onEvent("appOrientationChanged", (orientation: AppOrientation) => {
         this.stateManager.setState({ appOrientation: this.determineAppOrientation(orientation) });

packages/vscode-extension/src/project/bridge.ts
@@ -13,7 +13,6 @@ export interface RadonInspectorBridgeEvents {
   inspectData: [{ id: number }];
   devtoolPluginsChanged: [{ plugins: string[] }];
   pluginMessage: [{ pluginId: string; type: string; data: any }];
-  isProfilingReact: [boolean];
   appOrientationChanged: [AppOrientation];
   inspectorAvailabilityChanged: [InspectorAvailabilityStatus];
 }

packages/vscode-extension/src/project/devtools.ts
@@ -79,9 +79,11 @@ export class DevtoolsConnection implements Disposable {
 
   private readonly ideMessageEventEmitter: EventEmitter<IdeMessage> = new EventEmitter();
   private readonly disconnectedEventEmitter: EventEmitter<void> = new EventEmitter();
+  private readonly profilingEventEmitter: EventEmitter<boolean> = new EventEmitter();
 
   public readonly onIdeMessage = this.ideMessageEventEmitter.event;
   public readonly onDisconnected = this.disconnectedEventEmitter.event;
+  public readonly onProfilingChange = this.profilingEventEmitter.event;
 
   constructor(private readonly wall: Wall) {
     // create the DevTools frontend for the connection
@@ -95,7 +97,8 @@ export class DevtoolsConnection implements Disposable {
     // Register for isProfiling event on the profiler store
     this.store.profilerStore.addListener("isProfiling", () => {
       // @ts-ignore - isProfilingBasedOnUserInput exists but types are outdated
-      this.emitEvent("isProfilingReact", this.store.profilerStore.isProfilingBasedOnUserInput);
+      const isProfiling = this.store.profilerStore.isProfilingBasedOnUserInput;
+      this.profilingEventEmitter.fire(isProfiling);
     });
   }
 
@@ -145,6 +148,7 @@ export class DevtoolsConnection implements Disposable {
     this.bridge.shutdown();
     this.disconnectedEventEmitter.dispose();
     this.ideMessageEventEmitter.dispose();
+    this.profilingEventEmitter.dispose();
   }
 }
 