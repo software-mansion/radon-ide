software-mansion/radon-ide

commit d8ec504c9062ca0d9b13430491015ae840b4f4db

refactor: device session state - final  (#1540)
This is another PR that moves as to the state management solution, this
time we move over the rest of the state under `DeviceSessionsManager`.
To achieve that we:
1) add a new functionality that allows us for removing filed from
objects
2) we move the state 

additionally we:
- remove unused `merge.ts` file that got tangled with other PRs.
- change the `setState` method off the stateManager to `updateState` as
it reflects better on the functionality


### How Has This Been Tested: 

- run `radon-ide-test-apps` repo in radon and play around with it, 
- change devices and remove devices 

### How Has This Change Been Documented:

internal

packages/vscode-extension/src/project/StateManager.ts
@@ -9,7 +9,7 @@ export abstract class StateManager<T extends object> implements Disposable {
     return new RootStateManager(initialState);
   }
 
-  abstract setState(partialState: RecursivePartial<T>): void;
+  abstract updateState(partialState: RecursivePartial<T>): void;
   abstract getState(): T;
 
   protected onSetStateEmitter = new EventEmitter<RecursivePartial<T>>();
@@ -60,7 +60,7 @@ class RootStateManager<T extends object> extends StateManager<T> {
     this.state = initialState;
   }
 
-  setState(partialState: RecursivePartial<T>): void {
+  updateState(partialState: RecursivePartial<T>): void {
     const [newState, changes] = mergeAndCalculateChanges(this.state, partialState);
     if (this.state !== newState) {
       this.state = newState;
@@ -90,8 +90,8 @@ class DerivedStateManager<T extends object, K extends object> extends StateManag
     );
   }
 
-  setState(partialValue: RecursivePartial<T>) {
-    this.parent.setState({ [this.keyInParent]: partialValue } as RecursivePartial<K>);
+  updateState(partialValue: RecursivePartial<T>) {
+    this.parent.updateState({ [this.keyInParent]: partialValue } as RecursivePartial<K>);
   }
 
   getState() {

packages/vscode-extension/src/common/Merge.ts
@@ -1,4 +1,4 @@
-import { RecursivePartial } from "../common/State";
+import { RecursivePartial, REMOVE } from "../common/State";
 
 /**
  * Merges an existing state object with updates from a new partial state and calculates changes.
@@ -20,6 +20,11 @@ export function mergeAndCalculateChanges<T extends { [P in keyof T]: T[P] }>(
   const allKeys = [...Object.keys(oldNode), ...Object.keys(newNode)] as (keyof T)[];
 
   for (const key of allKeys) {
+    if (newNode[key] === REMOVE) {
+      changes[key] = REMOVE as (typeof changes)[typeof key];
+      wasChanged = true;
+      continue;
+    }
     if (newNode[key] === null) {
       result[key] = newNode[key];
       changes[key] = newNode[key];

packages/vscode-extension/src/common/Project.ts
@@ -1,12 +1,10 @@
 import { TelemetryEventProperties } from "@vscode/extension-telemetry";
 import { ReloadAction, SelectDeviceOptions } from "../project/DeviceSessionsManager";
-import { BuildType } from "./BuildConfig";
 import { LaunchConfiguration } from "./LaunchConfig";
 import { Output } from "./OutputChannel";
 import {
   AndroidSystemImageInfo,
   DeviceInfo,
-  DevicePlatform,
   DeviceRotation,
   IOSDeviceTypeInfo,
   IOSRuntimeInfo,
@@ -40,73 +38,7 @@ export type DeviceSettings = {
   camera?: CameraSettings;
 };
 
-export enum InstallationErrorReason {
-  NotEnoughStorage = "not_enough_storage",
-  InvalidPlatform = "invalid_platform",
-  Unknown = "unknown",
-}
-
-export class InstallationError extends Error {
-  constructor(
-    message: string,
-    public readonly reason: InstallationErrorReason
-  ) {
-    super(message);
-  }
-}
-
-export type BuildErrorDescriptor = {
-  kind: "build";
-  message: string;
-  platform: DevicePlatform;
-  buildType: BuildType | null;
-};
-
-export type DeviceErrorDescriptor = {
-  kind: "device";
-  message: string;
-};
-
-export type InstallationErrorDescriptor = {
-  kind: "installation";
-  message: string;
-  platform: DevicePlatform;
-  reason: InstallationErrorReason;
-};
-
-export type FatalErrorDescriptor =
-  | BuildErrorDescriptor
-  | DeviceErrorDescriptor
-  | InstallationErrorDescriptor;
-
-export type DeviceSessionStatus = "starting" | "running" | "fatalError";
-
-export type DeviceSessionStateStarting = {
-  status: "starting";
-  startupMessage: StartupMessage | undefined;
-  stageProgress: number | undefined;
-};
-
-export type DeviceSessionStateRunning = {
-  status: "running";
-};
-
-export type DeviceSessionStateFatalError = {
-  status: "fatalError";
-  error: FatalErrorDescriptor;
-};
-
-export type DeviceSessionState =
-  | DeviceSessionStateStarting
-  | DeviceSessionStateRunning
-  | DeviceSessionStateFatalError;
-
 export type DeviceId = DeviceInfo["id"];
-
-export interface DeviceSessionsManagerState {
-  selectedSessionId: DeviceId | null;
-  deviceSessions: Record<DeviceId, DeviceSessionState>;
-}
 
 export type ConnectState = {
   enabled: boolean;
@@ -118,7 +50,7 @@ export type ProjectState = {
   selectedLaunchConfiguration: LaunchConfiguration;
   customLaunchConfigurations: LaunchConfiguration[];
   connectState: ConnectState;
-} & DeviceSessionsManagerState;
+};
 
 export type AppPermissionType = "all" | "location" | "photos" | "contacts" | "calendar";
 
@@ -134,30 +66,6 @@ export type AppOrientation = DeviceRotation | "Landscape";
 export function isOfEnumDeviceRotation(value: any): value is DeviceRotation {
   return Object.values(DeviceRotation).includes(value);
 }
-
-// important: order of values in this enum matters
-export enum StartupMessage {
-  InitializingDevice = "Initializing device",
-  StartingPackager = "Starting packager",
-  BootingDevice = "Booting device",
-  Building = "Building",
-  Installing = "Installing",
-  Launching = "Launching",
-  WaitingForAppToLoad = "Waiting for app to load",
-  AttachingDebugger = "Attaching debugger",
-  Restarting = "Restarting",
-}
-
-export const StartupStageWeight = [
-  { StartupMessage: StartupMessage.InitializingDevice, weight: 1 },
-  { StartupMessage: StartupMessage.StartingPackager, weight: 1 },
-  { StartupMessage: StartupMessage.BootingDevice, weight: 2 },
-  { StartupMessage: StartupMessage.Building, weight: 7 },
-  { StartupMessage: StartupMessage.Installing, weight: 1 },
-  { StartupMessage: StartupMessage.Launching, weight: 1 },
-  { StartupMessage: StartupMessage.WaitingForAppToLoad, weight: 6 },
-  { StartupMessage: StartupMessage.AttachingDebugger, weight: 1 },
-];
 
 export type Frame = {
   x: number;
@@ -316,7 +224,6 @@ export interface ProjectInterface {
   movePanelTo(location: IDEPanelMoveTarget): Promise<void>;
   openExternalUrl(uriString: string): Promise<void>;
   openFileAt(filePath: string, line0Based: number, column0Based: number): Promise<void>;
-  openContentInEditor(codeId: string, content: string, language: string): Promise<void>;
   showDismissableError(errorMessage: string): Promise<void>;
   showToast(message: string, timeout: number): Promise<void>;
 

packages/vscode-extension/src/common/State.ts
@@ -1,12 +1,39 @@
 import { ApplicationRoot } from "./AppRootConfig";
+import { BuildType } from "./BuildConfig";
 import { merge } from "./Merge";
 import { DeviceId } from "./Project";
 
+export const REMOVE = Symbol("remove");
+
 export type RecursivePartial<T> = {
   [P in keyof T]?: NonNullable<T[P]> extends Array<infer U>
-    ? Array<U> | undefined
-    : RecursivePartial<T[P]>;
+    ? Array<U> | undefined | typeof REMOVE
+    : RecursivePartial<T[P]> | typeof REMOVE;
 };
+
+// #region State Serializer
+
+export class StateSerializer {
+  static serialize<T>(state: RecursivePartial<T>): string {
+    return JSON.stringify(state, (_, value) => {
+      if (typeof value === "symbol") {
+        return { __symbol__: value.toString() };
+      }
+      return value;
+    });
+  }
+
+  static deserialize<T>(json: string): RecursivePartial<T> {
+    return JSON.parse(json, (_, value) => {
+      if (value && value.__symbol__ === "Symbol(remove)") {
+        return REMOVE;
+      }
+      return value;
+    });
+  }
+}
+
+// #endregion State Serializer
 
 // #region Workspace Configuration
 
@@ -118,6 +145,66 @@ export type ApplicationSessionState = {
 
 // #endregion Application Session
 
+// #region Device Session Status
+
+export enum InstallationErrorReason {
+  NotEnoughStorage = "not_enough_storage",
+  InvalidPlatform = "invalid_platform",
+  Unknown = "unknown",
+}
+
+export class InstallationError extends Error {
+  constructor(
+    message: string,
+    public readonly reason: InstallationErrorReason
+  ) {
+    super(message);
+  }
+}
+
+export type BuildErrorDescriptor = {
+  kind: "build";
+  message: string;
+  platform: DevicePlatform;
+  buildType: BuildType | null;
+};
+
+export type DeviceErrorDescriptor = {
+  kind: "device";
+  message: string;
+};
+
+export type InstallationErrorDescriptor = {
+  kind: "installation";
+  message: string;
+  platform: DevicePlatform;
+  reason: InstallationErrorReason;
+};
+
+export type FatalErrorDescriptor =
+  | BuildErrorDescriptor
+  | DeviceErrorDescriptor
+  | InstallationErrorDescriptor;
+
+export type DeviceSessionStatus = "starting" | "running" | "fatalError";
+
+export type DeviceSessionStateStarting = {
+  status: "starting";
+  startupMessage: StartupMessage;
+  stageProgress: number;
+};
+
+export type DeviceSessionStateRunning = {
+  status: "running";
+};
+
+export type DeviceSessionStateFatalError = {
+  status: "fatalError";
+  error: FatalErrorDescriptor;
+};
+
+// #endregion Device Session status
+
 // #region Frame Reporting State
 
 export type FrameRateReport = {
@@ -143,6 +230,7 @@ export type FileTransferState = {
 };
 
 // #endregion File Transfer State
+
 // #region Multimedia
 
 export type MultimediaData = {
@@ -176,6 +264,34 @@ export type NavigationRoute = {
 
 // #endregion Navigation
 
+// #region Startup Messages
+
+// important: order of values in this enum matters
+export enum StartupMessage {
+  InitializingDevice = "Initializing device",
+  StartingPackager = "Starting packager",
+  BootingDevice = "Booting device",
+  Building = "Building",
+  Installing = "Installing",
+  Launching = "Launching",
+  WaitingForAppToLoad = "Waiting for app to load",
+  AttachingDebugger = "Attaching debugger",
+  Restarting = "Restarting",
+}
+
+export const StartupStageWeight = [
+  { StartupMessage: StartupMessage.InitializingDevice, weight: 1 },
+  { StartupMessage: StartupMessage.StartingPackager, weight: 1 },
+  { StartupMessage: StartupMessage.BootingDevice, weight: 2 },
+  { StartupMessage: StartupMessage.Building, weight: 7 },
+  { StartupMessage: StartupMessage.Installing, weight: 1 },
+  { StartupMessage: StartupMessage.Launching, weight: 1 },
+  { StartupMessage: StartupMessage.WaitingForAppToLoad, weight: 6 },
+  { StartupMessage: StartupMessage.AttachingDebugger, weight: 1 },
+];
+
+// #endregion Startup Messages
+
 // #region Device Session
 
 export type DeviceSessionStore = {
@@ -188,7 +304,7 @@ export type DeviceSessionStore = {
   previewURL: string | undefined;
   fileTransfer: FileTransferState;
   screenCapture: ScreenCaptureState;
-};
+} & (DeviceSessionStateStarting | DeviceSessionStateRunning | DeviceSessionStateFatalError);
 
 // #endregion Device Session
 
@@ -343,6 +459,9 @@ const initialDeviceSessionStore: DeviceSessionStore = {
     sentFiles: [],
     erroredFiles: [],
   },
+  stageProgress: 0,
+  startupMessage: StartupMessage.InitializingDevice,
+  status: "starting",
 };
 
 export const initialState: State = {

packages/vscode-extension/src/dependency/ApplicationDependencyManager.ts
@@ -89,7 +89,7 @@ export class ApplicationDependencyManager implements Disposable {
   public async checkSupportedReactNativeInstalled() {
     const appRoot = this.launchConfiguration.absoluteAppRoot;
 
-    this.stateManager.setState({
+    this.stateManager.updateState({
       reactNative: {
         status: npmPackageVersionCheck("react-native", appRoot, MinSupportedVersion.reactNative),
         isOptional: false,
@@ -100,7 +100,7 @@ export class ApplicationDependencyManager implements Disposable {
   public async checkSupportedExpoInstalled() {
     const appRoot = this.launchConfiguration.absoluteAppRoot;
 
-    this.stateManager.setState({
+    this.stateManager.updateState({
       expo: {
         status: npmPackageVersionCheck("expo", appRoot, MinSupportedVersion.expo),
         isOptional: !shouldUseExpoCLI(this.launchConfiguration),
@@ -178,7 +178,7 @@ export class ApplicationDependencyManager implements Disposable {
     try {
       const { stdout: nodeVersion } = await exec("node", ["-v"]);
       const isMinimumNodeVersion = semver.satisfies(nodeVersion, minimumNodeVersion);
-      this.stateManager.setState({
+      this.stateManager.updateState({
         nodeVersion: {
           status: isMinimumNodeVersion ? "installed" : "notInstalled",
           isOptional: false,
@@ -190,7 +190,7 @@ export class ApplicationDependencyManager implements Disposable {
         minimumVersion: minimumNodeVersion,
       };
     } catch {
-      this.stateManager.setState({
+      this.stateManager.updateState({
         nodeVersion: {
           status: "notInstalled",
           isOptional: false,
@@ -213,7 +213,7 @@ export class ApplicationDependencyManager implements Disposable {
     ));
 
     const exists = await checkNativeDirectoryExists(appRoot, DevicePlatform.Android);
-    this.stateManager.setState({
+    this.stateManager.updateState({
       android: { status: exists ? "installed" : "notInstalled", isOptional },
     });
     return exists || isOptional;
@@ -228,7 +228,7 @@ export class ApplicationDependencyManager implements Disposable {
     ));
 
     const exists = await checkNativeDirectoryExists(appRoot, DevicePlatform.IOS);
-    this.stateManager.setState({
+    this.stateManager.updateState({
       ios: { status: exists ? "installed" : "notInstalled", isOptional },
     });
     return exists || isOptional;
@@ -239,7 +239,7 @@ export class ApplicationDependencyManager implements Disposable {
     const dependsOnExpoRouter = appDependsOnExpoRouter(appRoot);
     const hasExpoRouterInstalled = npmPackageVersionCheck("expo-router", appRoot);
 
-    this.stateManager.setState({
+    this.stateManager.updateState({
       expoRouter: {
         status: hasExpoRouterInstalled,
         isOptional: !dependsOnExpoRouter,
@@ -257,7 +257,7 @@ export class ApplicationDependencyManager implements Disposable {
       appRoot,
       MinSupportedVersion.storybook
     );
-    this.stateManager.setState({
+    this.stateManager.updateState({
       storybook: {
         status: hasStotybookInstalled,
         isOptional: true,
@@ -270,13 +270,13 @@ export class ApplicationDependencyManager implements Disposable {
     outputChannel: OutputChannel,
     cancelToken: CancelToken
   ): Promise<void> {
-    this.stateManager.setState({ nodeModules: { status: "installing", isOptional: false } });
+    this.stateManager.updateState({ nodeModules: { status: "installing", isOptional: false } });
 
     try {
       await this.packageManager.installNodeModules(outputChannel, cancelToken);
-      this.stateManager.setState({ nodeModules: { status: "installed", isOptional: false } });
+      this.stateManager.updateState({ nodeModules: { status: "installed", isOptional: false } });
     } catch (e) {
-      this.stateManager.setState({ nodeModules: { status: "notInstalled", isOptional: false } });
+      this.stateManager.updateState({ nodeModules: { status: "notInstalled", isOptional: false } });
       throw new Error("Failed to install node modules. Check the logs for details.");
     }
   }
@@ -293,11 +293,11 @@ export class ApplicationDependencyManager implements Disposable {
       getTelemetryReporter().sendTelemetryEvent("build:pod-install-failed", {
         platform: DevicePlatform.IOS,
       });
-      this.stateManager.setState({ pods: { status: "notInstalled", isOptional: false } });
+      this.stateManager.updateState({ pods: { status: "notInstalled", isOptional: false } });
       return;
     }
 
-    this.stateManager.setState({ pods: { status: "installed", isOptional: false } });
+    this.stateManager.updateState({ pods: { status: "installed", isOptional: false } });
     Logger.debug("Project pods installed");
   }
 
@@ -325,7 +325,7 @@ export class ApplicationDependencyManager implements Disposable {
 
   private async checkPodsCommandStatus() {
     const installed = await this.pods.isPodsCommandInstalled();
-    this.stateManager.setState({
+    this.stateManager.updateState({
       cocoaPods: {
         status: installed ? "installed" : "notInstalled",
         isOptional: !(await projectRequiresNativeBuild(
@@ -340,7 +340,7 @@ export class ApplicationDependencyManager implements Disposable {
     // the resolvePackageManager function in getPackageManager checks
     // if a package manager is installed and otherwise returns undefined
     const installed = await this.packageManager.isPackageManagerInstalled();
-    this.stateManager.setState({
+    this.stateManager.updateState({
       packageManager: {
         status: installed ? "installed" : "notInstalled",
         isOptional: false,
@@ -352,7 +352,7 @@ export class ApplicationDependencyManager implements Disposable {
 
   public async checkNodeModulesInstallationStatus() {
     const installed = await this.packageManager.areNodeModulesInstalled();
-    this.stateManager.setState({
+    this.stateManager.updateState({
       nodeModules: { status: installed ? "installed" : "notInstalled", isOptional: false },
     });
     return installed;
@@ -361,7 +361,7 @@ export class ApplicationDependencyManager implements Disposable {
   public async checkPodsInstallationStatus() {
     const installed = await this.pods.arePodsInstalled();
 
-    this.stateManager.setState({
+    this.stateManager.updateState({
       pods: {
         status: installed ? "installed" : "notInstalled",
         isOptional: false,
@@ -373,7 +373,7 @@ export class ApplicationDependencyManager implements Disposable {
   public async checkEasCliInstallationStatus() {
     const appRoot = this.launchConfiguration.absoluteAppRoot;
     const installed = await isEasCliInstalled(appRoot);
-    this.stateManager.setState({
+    this.stateManager.updateState({
       easCli: {
         status: installed ? "installed" : "notInstalled",
         isOptional: true,

packages/vscode-extension/src/dependency/EnvironmentDependencyManager.ts
@@ -28,9 +28,11 @@ export class EnvironmentDependencyManager implements Disposable {
   private async checkAndroidEmulatorBinaryStatus() {
     try {
       await fs.promises.access(EMULATOR_BINARY, fs.constants.X_OK);
-      this.stateManager.setState({ androidEmulator: { status: "installed", isOptional: false } });
+      this.stateManager.updateState({
+        androidEmulator: { status: "installed", isOptional: false },
+      });
     } catch (e) {
-      this.stateManager.setState({
+      this.stateManager.updateState({
         androidEmulator: { status: "notInstalled", isOptional: false },
       });
     }
@@ -42,14 +44,14 @@ export class EnvironmentDependencyManager implements Disposable {
     const isSimctlInstalled = await testCommand("xcrun simctl help");
 
     const isInstalled = isXcodebuildInstalled && isXcrunInstalled && isSimctlInstalled;
-    this.stateManager.setState({
+    this.stateManager.updateState({
       xcode: { status: isInstalled ? "installed" : "notInstalled", isOptional: false },
     });
   }
 
   private async checkNodeCommandStatus() {
     const installed = await testCommand("node -v");
-    this.stateManager.setState({
+    this.stateManager.updateState({
       nodejs: { status: installed ? "installed" : "notInstalled", isOptional: false },
     });
   }

packages/vscode-extension/src/devices/AndroidEmulatorDevice.ts
@@ -17,14 +17,7 @@ import { ANDROID_HOME } from "../utilities/android";
 import { ChildProcess, exec, lineReader } from "../utilities/subprocess";
 import { BuildResult } from "../builders/BuildManager";
 import { Logger } from "../Logger";
-import {
-  AppPermissionType,
-  CameraSettings,
-  DeviceSettings,
-  InstallationError,
-  InstallationErrorReason,
-  Locale,
-} from "../common/Project";
+import { AppPermissionType, CameraSettings, DeviceSettings, Locale } from "../common/Project";
 import { getAndroidSystemImages } from "../utilities/sdkmanager";
 import { EXPO_GO_PACKAGE_NAME, fetchExpoLaunchDeeplink } from "../builders/expoGo";
 import { Platform } from "../utilities/platform";
@@ -33,7 +26,14 @@ import { CancelError, CancelToken } from "../utilities/cancelToken";
 import { extensionContext } from "../utilities/extensionContext";
 import { OutputChannelRegistry } from "../project/OutputChannelRegistry";
 import { Output } from "../common/OutputChannel";
-import { AndroidSystemImageInfo, DeviceInfo, DevicePlatform, DeviceType } from "../common/State";
+import {
+  AndroidSystemImageInfo,
+  DeviceInfo,
+  DevicePlatform,
+  DeviceType,
+  InstallationError,
+  InstallationErrorReason,
+} from "../common/State";
 
 export const EMULATOR_BINARY = path.join(
   ANDROID_HOME,

packages/vscode-extension/src/devices/DeviceManager.ts
@@ -46,12 +46,12 @@ export class DeviceManager implements Disposable {
   ) {
     this.loadDevicesIntoState();
     this.listInstalledIOSRuntimes().then((runtimes) => {
-      this.stateManager.setState({
+      this.stateManager.updateState({
         iOSRuntimes: runtimes,
       });
     });
     this.listInstalledAndroidImages().then((images) => {
-      this.stateManager.setState({
+      this.stateManager.updateState({
         androidImages: images,
       });
     });
@@ -120,7 +120,7 @@ export class DeviceManager implements Disposable {
     }
     const devices = await this.loadDevicesPromise;
     if (!_.isEqual(previousDevices, devices)) {
-      this.stateManager.setState({ devices });
+      this.stateManager.updateState({ devices });
     }
     return devices;
   }
@@ -164,7 +164,7 @@ export class DeviceManager implements Disposable {
     if (devices) {
       // we still want to perform load here in case anything changes, just won't wait for it
       this.loadDevices();
-      this.stateManager.setState({ devices });
+      this.stateManager.updateState({ devices });
     } else {
       return await this.loadDevices();
     }
@@ -224,7 +224,7 @@ export class DeviceManager implements Disposable {
     // This is an optimization to update before costly operations
     const previousDevices = this.stateManager.getState().devices ?? [];
     const devices = previousDevices.filter((d) => d.id !== device.id);
-    this.stateManager.setState({ devices });
+    this.stateManager.updateState({ devices });
 
     if (device.platform === DevicePlatform.IOS) {
       await removeIosSimulator(device.UDID, SimulatorDeviceSet.RN_IDE);

commit ebe7578ee4d8a1da7f42acad723644bfdac7d894

fix: prevent setting state with DerivedStateManager after the item is removed (#1559)
Fixes stopping a selected device failing after #1540 

### How Has This Been Tested: 
- open radon and wait for the app to load
- stop the selected device

packages/vscode-extension/src/project/StateManager.ts
@@ -1,7 +1,7 @@
 import _ from "lodash";
 import { Disposable, EventEmitter } from "vscode";
 import { disposeAll } from "../utilities/disposables";
-import { RecursivePartial } from "../common/State";
+import { RecursivePartial, REMOVE } from "../common/State";
 import { mergeAndCalculateChanges } from "../common/Merge";
 
 export abstract class StateManager<T extends object> implements Disposable {
@@ -74,15 +74,29 @@ class RootStateManager<T extends object> extends StateManager<T> {
 }
 
 class DerivedStateManager<T extends object, K extends object> extends StateManager<T> {
+  // NOTE: after the derived state is removed from the parent, we want to ignore any further updates
+  // made through this derived state manager to prevent accidental re-adding of the removed state or updating
+  // an entry that is added after this one is removed.
+  private removed: boolean;
+
   constructor(
     private parent: StateManager<K>,
     private keyInParent: keyof K
   ) {
     super();
+    this.removed = this.parent.getState()[this.keyInParent] === undefined;
     this.disposables.push(
       parent.onSetState((partialParentState: RecursivePartial<K>) => {
-        const partialState = partialParentState[this.keyInParent] as T | undefined;
+        if (this.removed) {
+          return;
+        }
+
+        const partialState = partialParentState[this.keyInParent];
         if (partialState === undefined) {
+          return;
+        }
+        if (partialState === REMOVE) {
+          this.removed = true;
           return;
         }
         this.onSetStateEmitter.fire(partialState);
@@ -91,10 +105,18 @@ class DerivedStateManager<T extends object, K extends object> extends StateManag
   }
 
   updateState(partialValue: RecursivePartial<T>) {
+    if (this.removed) {
+      return;
+    }
     this.parent.updateState({ [this.keyInParent]: partialValue } as RecursivePartial<K>);
   }
 
   getState() {
     return this.parent.getState()[this.keyInParent] as T;
+  }
+
+  dispose() {
+    super.dispose();
+    this.removed = true;
   }
 }