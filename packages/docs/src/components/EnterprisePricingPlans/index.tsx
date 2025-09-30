764d8ba1d1788f9017fa67ec3ec806a6f9716f9b
spaleka <agata_spalek@interia.eu>
2025-09-30 12:52:14 +0200

    create usePricingLogic hook

--- packages/docs/src/components/EnterprisePricingPlans/index.tsx
+++ packages/docs/src/components/EnterprisePricingPlans/index.tsx
@@ -1,13 +1,21 @@
 import React, { forwardRef } from "react";
 import styles from "./styles.module.css";
 import PricingPlansList from "../Pricing/PricingPlansList";
+import { usePricingLogic } from "@site/src/hooks/usePricingLogic";
 
 const EnterprisePricingPlans = forwardRef<HTMLDivElement, {}>((props, ref) => {
+  const { isMonthly, setIsMonthly, openBusinessCheckout, openIndividualCheckout } =
+    usePricingLogic();
   return (
     <div ref={ref} className={styles.container}>
       <p className={styles.heading}>Enterprise plans tailored to your business</p>
       <p className={styles.subheading}>Choose the subscription plan tailored to your needs</p>
-      <PricingPlansList />
+      <PricingPlansList
+        handleBusiness={openBusinessCheckout}
+        handleIndividual={openIndividualCheckout}
+        isMonthly={isMonthly}
+        setIsMonthly={setIsMonthly}
+      />
 
       <div className={styles.pricingLink}>
         <p>Need to see all plan options? </p>
--- packages/docs/src/components/Pricing/index.tsx
+++ packages/docs/src/components/Pricing/index.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./styles.module.css";
 import PricingPlansList from "./PricingPlansList";
 import FAQ from "../Sections/FAQ";
 import clsx from "clsx";
 import ComparePricingPlans from "./ComparePricingPlans";
-import usePaddle from "@site/src/hooks/usePaddle";
+import { usePricingLogic } from "@site/src/hooks/usePricingLogic";
 
 export interface PricingProps {
   handleIndividual?: () => void;
@@ -13,42 +13,9 @@ export interface PricingProps {
   setIsMonthly?: (value: boolean) => void;
 }
 
-const isProduction = process.env.NODE_ENV === "production";
-
-const INDIVIDUAL_MONTHLY_PRICE_ID = isProduction
-  ? "pri_01hx944ht3wnpvgktatj6v5k4b"
-  : "pri_01j0tjqzqhv6vezhf14pwtxfm0";
-const INDIVIDUAL_YEARLY_PRICE_ID = isProduction
-  ? "pri_01hzf02s579nwrwb756enh8r7g"
-  : "pri_01jb1ajv7btj3cbnshrdq4ncjf";
-const BUSINESS_MONTHLY_PRICE_ID = isProduction
-  ? "pri_01jdyc0j8wkfqx3a7nbf6tsaxy"
-  : "pri_01jdyap7jcydxvewmek2r0e35q";
-const BUSINESS_YEARLY_PRICE_ID = isProduction
-  ? "pri_01jdyc1z1nh3pgp01ya4h8g075"
-  : "pri_01jdyaqnwf3w4pm6hsgwehm1by";
-
-const Pricing = ({ handleBusiness, handleIndividual }: PricingProps) => {
-  const paddle = usePaddle();
-
-  const [isMonthly, setIsMonthly] = useState(true);
-  const openIndividualCheckout = () => {
-    paddle?.Checkout.open({
-      items: [
-        {
-          priceId: isMonthly ? INDIVIDUAL_MONTHLY_PRICE_ID : INDIVIDUAL_YEARLY_PRICE_ID,
-          quantity: 1,
-        },
-      ],
-    });
-  };
-  const openBusinessCheckout = () => {
-    paddle?.Checkout.open({
-      items: [
-        { priceId: isMonthly ? BUSINESS_MONTHLY_PRICE_ID : BUSINESS_YEARLY_PRICE_ID, quantity: 1 },
-      ],
-    });
-  };
+const Pricing = () => {
+  const { isMonthly, setIsMonthly, openBusinessCheckout, openIndividualCheckout } =
+    usePricingLogic();
   return (
     <div className={clsx(styles.container, "border-layout")}>
       <div className={styles.titleContainer}>
