cf629362d3453f5f1fb69aa987c41bc7ae93ea3e
Ignacy Łątka <latkaignacy@gmail.com>
2025-10-01 12:55:34 +0200

    add tool registration entry for reload tool

--- packages/vscode-extension/src/ai/mcp/toolRegistration.ts
+++ packages/vscode-extension/src/ai/mcp/toolRegistration.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp";
 import { getToolSchema, invokeToolCall } from "../shared/api";
 import { ToolSchema } from "./models";
-import { readLogsToolExec, screenshotToolExec } from "./toolExecutors";
+import { readLogsToolExec, restartDeviceExec, screenshotToolExec } from "./toolExecutors";
 import { ConnectionListener } from "../shared/ConnectionListener";
 
 const PLACEHOLDER_ID = "3241"; // This placeholder is needed by the API, but the value doesn't matter
@@ -23,6 +23,21 @@ export async function registerMcpTools(server: McpServer, connectionListener: Co
       inputSchema: {},
     },
     screenshotToolExec
+  );
+
+  server.registerTool(
+    "reload_application",
+    {
+      description: "",
+      inputSchema: {
+        reloadMethod: z.union([
+          z.literal("reloadJs"),
+          z.literal("rebuild"),
+          z.literal("restartProcess"),
+        ]),
+      },
+    },
+    restartDeviceExec
   );
 
   server.registerTool(
