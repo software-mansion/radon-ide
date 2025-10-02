fe2ce34029a96b012042ea7212809c02032a852c
pFornagiel <pawel.fornagiel2@gmail.com>
2025-10-02 13:05:53 +0200

    refactor: move Shiki highlighter logic to a separate utility file for better organization

--- packages/vscode-extension/src/network/components/Tabs/HighlightedCodeBlock.tsx
+++ packages/vscode-extension/src/network/components/Tabs/HighlightedCodeBlock.tsx
@@ -1,39 +1,6 @@
-// Language definitions imports
-import langJson from "@shikijs/langs/json";
-import langJavascript from "@shikijs/langs/javascript";
-import langJsx from "@shikijs/langs/jsx";
-import langTypescript from "@shikijs/langs/typescript";
-import langTsx from "@shikijs/langs/tsx";
-import langHtml from "@shikijs/langs/html";
-import langXml from "@shikijs/langs/xml";
-import langCss from "@shikijs/langs/css";
-import langLess from "@shikijs/langs/less";
-import langSass from "@shikijs/langs/sass";
-import langScss from "@shikijs/langs/scss";
-import langPython from "@shikijs/langs/python";
-import langJava from "@shikijs/langs/java";
-import langKotlin from "@shikijs/langs/kotlin";
-import langCpp from "@shikijs/langs/cpp";
-import langGo from "@shikijs/langs/go";
-import langPhp from "@shikijs/langs/php";
-import langShellscript from "@shikijs/langs/shellscript";
-import langCoffee from "@shikijs/langs/coffee";
-import langClojure from "@shikijs/langs/clojure";
-import langDart from "@shikijs/langs/dart";
-import langScala from "@shikijs/langs/scala";
-import langAngularHtml from "@shikijs/langs/angular-html";
-import langSvelte from "@shikijs/langs/svelte";
-import langVue from "@shikijs/langs/vue";
-import langMarkdown from "@shikijs/langs/markdown";
-import langWasm from "@shikijs/langs/wasm";
-import langYaml from "@shikijs/langs/yaml";
-
 import { useEffect, useState } from "react";
-import {
-  ShikiHighlighter,
-  createHighlighterCore,
-  createJavaScriptRegexEngine,
-} from "react-shiki/core";
+import { ShikiHighlighter } from "react-shiki/core";
+import { getHighlighter } from "../../utils/highlighter";
 
 import { ThemeData } from "../../../common/theme";
 import "./PayloadAndResponseTab.css";
@@ -46,80 +13,11 @@ interface HighlightedCodeBlockProps {
   className?: string;
 }
 
-const JS_REGEX_ENGINE = createJavaScriptRegexEngine();
 /**
  * Maximum content length (in characters) to apply syntax highlighting
  * For larger content, plain text will be displayed to avoid performance issues
  */
 const MAX_HIGHLIGHT_LENGTH = 50_000;
-
-/**
- * Array of language definitions for Shiki library.
- * Supported languages are provided explicitly and used in createHighlighterCore,
- * to avoid loading all languages and bloating the bundle. See bundle options section:
- * https://github.com/avgvstvs96/react-shiki
- */
-const LANG_ARRAY = [
-  langJson,
-  langJavascript,
-  langJsx,
-  langTypescript,
-  langTsx,
-  langHtml,
-  langXml,
-  langCss,
-  langLess,
-  langSass,
-  langScss,
-  langPython,
-  langJava,
-  langKotlin,
-  langCpp,
-  langGo,
-  langPhp,
-  langShellscript,
-  langCoffee,
-  langClojure,
-  langDart,
-  langScala,
-  langAngularHtml,
-  langSvelte,
-  langVue,
-  langMarkdown,
-  langWasm,
-  langYaml,
-];
-
-/**
- * Memoized highlighter instance to ensure we only create one highlighter
- * across all component instances.
- */
-let highlighterInstance: unknown | null = null;
-let highlighterPromise: Promise<unknown> | null = null;
-
-/**
- * Gets or creates a Shiki highlighter instance with memoization.
- * The highlighter is created only once and reused across all calls.
- * @returns Promise that resolves to the highlighter instance
- */
-const getHighlighter = async () => {
-  if (highlighterInstance) {
-    return highlighterInstance;
-  }
-  if (highlighterPromise) {
-    return highlighterPromise;
-  }
-
-  highlighterPromise = createHighlighterCore({
-    langs: LANG_ARRAY,
-    engine: JS_REGEX_ENGINE,
-  }).then((instance) => {
-    highlighterInstance = instance;
-    return instance;
-  });
-
-  return highlighterPromise;
-};
 
 const HighlightedCodeBlock = ({
   content,
