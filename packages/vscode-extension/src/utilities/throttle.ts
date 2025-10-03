7791b35ce55b80d30f17e1fdcf93e023195e3731
Juliusz Wajgelt <juliusz.wajgelt@swmansion.com>
2025-10-03 09:32:55 +0200

    make sure throttled function is not called concurrently

--- packages/vscode-extension/src/utilities/throttle.ts
+++ packages/vscode-extension/src/utilities/throttle.ts
@@ -1,3 +1,5 @@
+import assert from "assert";
+
 type AsyncFn = (...args: any[]) => Promise<any>;
 type ThrottledAsyncFn<T extends AsyncFn> = T & { cancel: () => void; flush: () => void };
 
@@ -15,21 +17,29 @@ export function throttleAsync<T extends AsyncFn>(
 ): ThrottledAsyncFn<(...args: Parameters<T>) => Promise<void>> {
   let timeoutId: NodeJS.Timeout | null = null;
   let recentArgs: Parameters<T> | null;
-  let callCount = 0;
+  let scheduled = false;
+  let running = false;
 
   function execute() {
     if (recentArgs === null) {
       timeoutId = null;
+      scheduled = false;
       return;
     }
-    const currentCallCount = callCount;
+    assert(!running, "Throttled function is never called concurrently");
     const args = recentArgs;
+
     recentArgs = null;
+    timeoutId = null;
+
+    running = true;
+    scheduled = false;
     const result = func(...args);
     result
       .catch(() => {})
       .then(() => {
-        if (currentCallCount === callCount) {
+        running = false;
+        if (!scheduled) {
           timeoutId = null;
           recentArgs = null;
         } else {
@@ -46,24 +56,27 @@ export function throttleAsync<T extends AsyncFn>(
   }
 
   const throttledFunction = async function (...args: Parameters<T>) {
-    if (timeoutId === null) {
+    if (!running && !scheduled) {
       timeoutId = setTimeout(execute, limitMs);
     }
+    scheduled = true;
     recentArgs = args;
-    callCount++;
   };
 
   throttledFunction.cancel = () => {
     if (timeoutId !== null) {
       clearTimeout(timeoutId);
       timeoutId = null;
+      scheduled = false;
       recentArgs = null;
     }
   };
 
   throttledFunction.flush = () => {
     throttledFunction.cancel();
-    execute();
+    if (!running) {
+      execute();
+    }
   };
 
   return throttledFunction;
