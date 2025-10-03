8b88068f98be5d48d1d6b215e504aa0478e4f9ef
spaleka <agata_spalek@interia.eu>
2025-10-03 12:45:25 +0200

    PricingPlansList refactor

--- packages/docs/src/components/Pricing/PricingPlansList/index.tsx
+++ packages/docs/src/components/Pricing/PricingPlansList/index.tsx
@@ -1,15 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useMemo, useState } from "react";
 import styles from "./styles.module.css";
 import { PricingProps } from "..";
-import PricingCardLabel from "../PricingCard/PricingCardLabel";
 import PricingPeriodButton from "./PricingPeriodButton";
 import usePageType from "@site/src/hooks/usePageType";
 import { pricingIndividualData } from "../pricingIndividualData";
 import { pricingBusinessData } from "../pricingBusinessData";
-import PricingFeaturesList from "./PricingFeaturesList";
-import PricingButton from "../ComparePricingPlans/PricingButton";
 import { useModal } from "../../ModalProvider";
 import clsx from "clsx";
+import PricingCard from "../PricingCard";
 
 export interface FeatureProps {
   label: string;
@@ -38,16 +36,13 @@ const PricingPlansList = ({
   isMonthly,
   setIsMonthly,
 }: PricingProps) => {
-  const pricingCardsIndividual = pricingIndividualData;
-  const pricingCardsBusiness = pricingBusinessData;
   const { isEnterprise } = usePageType();
   const [isIndividual, setIsIndividual] = useState(true);
-  const [plan, setPlan] = useState<PricingPlanCardProps[]>(pricingCardsIndividual);
   const { onOpen } = useModal();
 
-  useEffect(() => {
-    isIndividual ? setPlan(pricingCardsIndividual) : setPlan(pricingCardsBusiness);
-    isEnterprise && setPlan(pricingCardsBusiness);
+  const currentPlans = useMemo(() => {
+    if (isEnterprise) return pricingBusinessData;
+    return isIndividual ? pricingIndividualData : pricingBusinessData;
   }, [isIndividual, isEnterprise]);
 
   const handlePlanButtonClick = (id: string) => {
@@ -65,20 +60,7 @@ const PricingPlansList = ({
         handleCustom();
         break;
       default:
-        return null;
-    }
-  };
-
-  const handleHeader = (planId: string) => {
-    switch (planId) {
-      case "PRO":
-        return <p>All the Free features, plus:</p>;
-      case "TEAM":
-        return <p>Development & Testing - All Pro features</p>;
-      case "ENTERPRISE":
-        return <p>All the Team features, plus:</p>;
-      default:
-        return null;
+        return;
     }
   };
 
@@ -104,38 +86,13 @@ const PricingPlansList = ({
         <PricingPeriodButton isMonthly={isMonthly} setIsMonthly={setIsMonthly} />
       </div>
       <div className={styles.list}>
-        {plan.map((el, idx) => (
-          <div
-            className={el.plan === "PRO" ? styles.proCardContainer : styles.pricingCardContainer}
-            key={idx}>
-            <PricingCardLabel key={`${el.plan}_${idx}`} plan={el} isMonthly={isMonthly}>
-              <PricingButton
-                stylingFilled={el.stylingFilled}
-                onClick={() => handlePlanButtonClick(el.plan)}>
-                {el.buttonLabel}
-              </PricingButton>
-            </PricingCardLabel>
-            <div className={clsx(styles.cardMain, el.plan === "PRO" ? styles.proCardBorder : "")}>
-              <div className={styles.cardFeatures}>
-                {handleHeader(el.plan)}
-                <PricingFeaturesList featuresList={el.featuresAll} plan={el.plan} />
-              </div>
-              {el.featuresTeamManagement && (
-                <div className={styles.cardFeatures}>
-                  {" "}
-                  <p>Team Management</p>
-                  <PricingFeaturesList featuresList={el.featuresTeamManagement} />
-                </div>
-              )}
-              {el.featuresSupport && (
-                <div className={styles.cardFeatures}>
-                  {" "}
-                  <p>Support & Updates</p>
-                  <PricingFeaturesList featuresList={el.featuresSupport} />
-                </div>
-              )}
-            </div>
-          </div>
+        {currentPlans.map((planData: PricingPlanCardProps) => (
+          <PricingCard
+            key={planData.plan}
+            planData={planData}
+            isMonthly={isMonthly}
+            onButtonClick={handlePlanButtonClick}
+          />
         ))}
       </div>
     </div>
