software-mansion/radon-ide

commit 31ce33bb148d8cfb59549ebb680d1c1400d17fc2

feat: allow running Radon with a single Metro server on a specific port (#1550)
Adds the ability to configure Radon to use a single Metro server, shared
by all devices, running on a specified port.
- adds a `metroPort` launch configuration entry which allows specifying
the port
- when the `metroPort` config is unspecified, the current behavior
should be preserved -- each running device gets its own Metro server,
running on an ephemeral port
- when the `metroPort` config is specified, switches from the current
behavior to instead running a single metro server on the specified port,
shared by all running devices. This imposes some technical limits on
concurrent devices:
- when the CDP devtools transport is used, any number of iOS devices
_with different display names_ can run at the same time, but only a
single Android emulator can run concurrently -- this is due to the fact,
that we use the `deviceName` inspector page property to differentiate
debugger targets, and for all Android emulators, that field is the same.
- when the "old" websocket devtools transport is used, only a single
device can run at the same time. This is due to the devtools server port
being shared by all running devices, making it hard to tell them apart.

### How Has This Been Tested: 
- try the different combinations of React Native (>= 0.76, <= 0.75) x
`useOldDevtools` (true, false) x `metroPort` (8081, unspecified)
- verify the app launches and the debugger and devtools connect
- verify switching devices works
- verify appropriate device number limits are enforced:
  - none if `metroPort` is unspecified
  - at most 1 Android if it is
  - at most 1 device if both it and `useOldDevtools` are set

### How Has This Change Been Documented:
WIP

packages/vscode-extension/src/connect/Scanner.ts
@@ -1,14 +1,20 @@
 import { Disposable, workspace } from "vscode";
-import { Metro } from "../project/metro";
 import { sleep } from "../utilities/retry";
 import { RADON_CONNECT_PORT_KEY } from "./Connector";
 import { extensionContext } from "../utilities/extensionContext";
+import { Metro, MetroSession } from "../project/metro";
+import { CancelToken } from "../utilities/cancelToken";
+import { waitForDebuggerTarget } from "../project/DebuggerTarget";
 export const PORT_SCAN_INTERVAL_MS = 4000;
 export const DEFAULT_PORTS = [8081, 8082];
 
 export type ScannerDelegate = {
   onPortStatusUpdated: () => void;
-  onDeviceCandidateFound: (metro: Metro, websocketAddress: string) => Promise<void>;
+  onDeviceCandidateFound: (
+    metro: MetroSession,
+    websocketAddress: string,
+    isUsingNewDebugger: boolean
+  ) => Promise<void>;
 };
 
 function isInWorkspace(absoluteFilePath: string) {
@@ -18,6 +24,7 @@ function isInWorkspace(absoluteFilePath: string) {
   );
 }
 
+const DEBUGGER_LOOKUP_TIMEOUT_MS = 5000;
 export class Scanner implements Disposable {
   public portsStatus: Map<number, string> = new Map();
   private disposed = false;
@@ -50,19 +57,28 @@ export class Scanner implements Disposable {
   }
 
   private async verifyAndConnect(port: number, projectRoot: string) {
-    const metro = new Metro(port, [projectRoot]);
-    const websocketAddress = await metro.getDebuggerURL();
-    if (!websocketAddress) {
+    const metro = new Metro(port, projectRoot);
+
+    const timeoutCancelToken = new CancelToken();
+    setTimeout(() => timeoutCancelToken.cancel(), DEBUGGER_LOOKUP_TIMEOUT_MS);
+
+    const debuggerTarget = await waitForDebuggerTarget({ metro, cancelToken: timeoutCancelToken });
+
+    if (!debuggerTarget) {
       this.portsStatus.set(port, "no connected device listed");
       return false;
     }
-    if (!metro.isUsingNewDebugger) {
+    if (!debuggerTarget.isUsingNewDebugger) {
       this.portsStatus.set(port, "using old debugger");
       return false;
     }
 
     this.portsStatus.set(port, "connecting...");
-    await this.delegate?.onDeviceCandidateFound(metro, websocketAddress);
+    await this.delegate?.onDeviceCandidateFound(
+      metro,
+      debuggerTarget.websocketAddress,
+      debuggerTarget.isUsingNewDebugger
+    );
   }
 
   private async scanPort(port: number) {

packages/vscode-extension/lib/wrapper.js
@@ -10,6 +10,7 @@ const {
   findNodeHandle,
   Platform,
   Dimensions,
+  DevSettings,
 } = require("react-native");
 const { storybookPreview } = require("./storybook_helper");
 require("./react_devtools_agent"); // needs to be loaded before inspector_bridge is used
@@ -30,6 +31,10 @@ export function registerDevtoolPlugin(name) {
   devtoolPlugins.add(name);
   devtoolPluginsChanged?.();
 }
+
+globalThis.__RADON_reloadJS = function () {
+  DevSettings.reload("Radon IDE");
+};
 
 let navigationHistory = new Map();
 

packages/vscode-extension/src/common/LaunchConfig.ts
@@ -31,6 +31,7 @@ export interface LaunchOptions {
   usePrebuild?: boolean;
   useOldDevtools?: boolean;
   useCustomJSDebugger?: boolean;
+  metroPort?: number;
 }
 
 export const LAUNCH_OPTIONS_KEYS = [
@@ -49,6 +50,7 @@ export const LAUNCH_OPTIONS_KEYS = [
   "usePrebuild",
   "useOldDevtools",
   "useCustomJSDebugger",
+  "metroPort",
 ] as const;
 
 type IsSuperTypeOf<Base, T extends Base> = T;

packages/vscode-extension/src/common/Project.ts
@@ -1,5 +1,5 @@
 import { TelemetryEventProperties } from "@vscode/extension-telemetry";
-import { ReloadAction, SelectDeviceOptions } from "../project/DeviceSessionsManager";
+import { ReloadAction } from "../project/DeviceSessionsManager";
 import { LaunchConfiguration } from "./LaunchConfig";
 import { Output } from "./OutputChannel";
 import {
@@ -196,10 +196,7 @@ export interface ProjectInterface {
   dispatchCopy(): Promise<void>;
 
   reloadCurrentSession(type: ReloadAction): Promise<void>;
-  startOrActivateSessionForDevice(
-    deviceInfo: DeviceInfo,
-    selectDeviceOptions?: SelectDeviceOptions
-  ): Promise<void>;
+  startOrActivateSessionForDevice(deviceInfo: DeviceInfo): Promise<void>;
   terminateSession(deviceId: DeviceId): Promise<void>;
 
   inspectElementAt(xRatio: number, yRatio: number, requestStack: boolean): Promise<InspectData>;

packages/vscode-extension/src/connect/ConnectSession.ts
@@ -2,7 +2,7 @@ import path from "path";
 import fs from "fs";
 import { Disposable } from "vscode";
 import { DebugSession, DebugSessionImpl } from "../debugging/DebugSession";
-import { Metro } from "../project/metro";
+import { MetroSession } from "../project/metro";
 import { BaseInspectorBridge } from "../project/bridge";
 import { disposeAll } from "../utilities/disposables";
 import { extensionContext } from "../utilities/extensionContext";
@@ -43,7 +43,7 @@ export default class ConnectSession implements Disposable {
   }
 
   constructor(
-    private readonly metro: Metro,
+    private readonly metro: MetroSession,
     private readonly delegate: ConnectSessionDelegate
   ) {
     this.debugSession = new DebugSessionImpl({
@@ -54,8 +54,7 @@ export default class ConnectSession implements Disposable {
     this.inspectorBridge = new DebugSessionInspectorBridge(this.debugSession);
   }
 
-  public async start(websocketAddress: string) {
-    const isUsingNewDebugger = this.metro.isUsingNewDebugger;
+  public async start(websocketAddress: string, isUsingNewDebugger: boolean) {
     if (!isUsingNewDebugger) {
       throw new Error("Auto-connect is only supported for the new React Native debugger");
     }

packages/vscode-extension/src/connect/Connector.ts
@@ -7,7 +7,7 @@ import {
   StatusBarItem,
   window,
 } from "vscode";
-import { Metro } from "../project/metro";
+import { MetroSession } from "../project/metro";
 import { extensionContext } from "../utilities/extensionContext";
 import { disposeAll } from "../utilities/disposables";
 import { Scanner } from "./Scanner";
@@ -119,7 +119,11 @@ export class Connector implements Disposable {
     this.connectSession = null;
   }
 
-  private async tryConnectJSDebuggerWithMetro(websocketAddress: string, metro: Metro) {
+  private async tryConnectJSDebuggerWithMetro(
+    websocketAddress: string,
+    isUsingNewDebugger: boolean,
+    metro: MetroSession
+  ) {
     const connectSession = new ConnectSession(metro, {
       onSessionTerminated: () => {
         getTelemetryReporter().sendTelemetryEvent("radon-connect:disconnected", {});
@@ -132,7 +136,7 @@ export class Connector implements Disposable {
       },
     });
     try {
-      await connectSession.start(websocketAddress);
+      await connectSession.start(websocketAddress, isUsingNewDebugger);
       getTelemetryReporter().sendTelemetryEvent("radon-connect:connected", {});
       this.connectSession?.dispose();
       this.connectSession = connectSession;
@@ -153,8 +157,8 @@ export class Connector implements Disposable {
 
     this.scanner = new Scanner({
       onPortStatusUpdated: () => this.handleStateChange(),
-      onDeviceCandidateFound: async (metro, websocketAddress) => {
-        await this.tryConnectJSDebuggerWithMetro(websocketAddress, metro);
+      onDeviceCandidateFound: async (metro, websocketAddress, isUsingNewDebugger) => {
+        await this.tryConnectJSDebuggerWithMetro(websocketAddress, isUsingNewDebugger, metro);
       },
     });
     this.scanner.start();

packages/vscode-extension/src/debugging/ReconnectingDebugSession.ts
@@ -1,11 +1,13 @@
 import { EventEmitter, Disposable } from "vscode";
 import { Cdp } from "vscode-cdp-proxy";
-import { Metro } from "../project/metro";
 import { CancelToken } from "../utilities/cancelToken";
 import { sleep } from "../utilities/retry";
 import { DebugSession, DebugSource, JSDebugConfiguration } from "./DebugSession";
 import { disposeAll } from "../utilities/disposables";
 import { DevtoolsServer } from "../project/devtools";
+import { MetroSession } from "../project/metro";
+import { DeviceInfo } from "../common/State";
+import { getDebuggerTargetForDevice } from "../project/DebuggerTarget";
 
 const PING_TIMEOUT = 1000;
 export class ReconnectingDebugSession implements DebugSession, Disposable {
@@ -19,7 +21,8 @@ export class ReconnectingDebugSession implements DebugSession, Disposable {
 
   constructor(
     private readonly debugSession: DebugSession & Partial<Disposable>,
-    private readonly metro: Metro,
+    private readonly metro: MetroSession,
+    private readonly deviceInfo: DeviceInfo,
     devtoolsServer?: DevtoolsServer
   ) {
     this.disposables.push(debugSession.onDebugSessionTerminated(this.maybeReconnect));
@@ -64,14 +67,17 @@ export class ReconnectingDebugSession implements DebugSession, Disposable {
           // if we're connected to a responsive session, we can break
           break;
         }
-        const websocketAddress = await this.metro.getDebuggerURL(undefined, cancelToken);
-        if (!websocketAddress) {
+        const target = await getDebuggerTargetForDevice({
+          metro: this.metro,
+          deviceInfo: this.deviceInfo,
+          cancelToken,
+        });
+        if (!target) {
           throw new Error("No connected device listed");
         }
         await this.debugSession.startJSDebugSession({
-          websocketAddress,
+          ...target,
           displayDebuggerOverlay: false,
-          isUsingNewDebugger: this.metro.isUsingNewDebugger,
           expoPreludeLineCount: this.metro.expoPreludeLineCount,
           sourceMapPathOverrides: this.metro.sourceMapPathOverrides,
         });

commit e21145cba19e8866fa0210d5265a97039f782dcf

fix: radon connect  (#1635)
This PR fixes a regression introduced in #1550 which removed an option
to pass `watchFolders` to the metro class directly. Normally we really
on metro process handing as this information during the set up process,
but when we use radon connect we do not have an option of capturing
metro config like in a normal case, so we need to add the `watchFolders`
information by hand.

### How Has This Been Tested: 

- run expo 54 test app from terminal 
- use radon connect 

### How Has This Change Been Documented:

internal

packages/vscode-extension/src/connect/Scanner.ts
@@ -57,7 +57,7 @@ export class Scanner implements Disposable {
   }
 
   private async verifyAndConnect(port: number, projectRoot: string) {
-    const metro = new Metro(port, projectRoot);
+    const metro = new Metro(port, projectRoot, [projectRoot]);
 
     const timeoutCancelToken = new CancelToken();
     setTimeout(() => timeoutCancelToken.cancel(), DEBUGGER_LOOKUP_TIMEOUT_MS);

packages/vscode-extension/src/project/metro.ts
@@ -258,7 +258,7 @@ async function launchMetro({
 
 export class Metro implements MetroSession, Disposable {
   protected _expoPreludeLineCount = 0;
-  protected _watchFolders: string[] | undefined = undefined;
+  protected _watchFolders: string[] | undefined;
   protected readonly metroOutputChannel;
 
   protected readonly bundleErrorEventEmitter = new EventEmitter<BundleErrorEvent>();
@@ -274,8 +274,11 @@ export class Metro implements MetroSession, Disposable {
 
   constructor(
     public readonly port: number,
-    protected readonly appRoot: string
+    protected readonly appRoot: string,
+    watchFolders: string[] | undefined = undefined
   ) {
+    this._watchFolders = watchFolders;
+
     const metroOutputChannel =
       IDE.getInstanceIfExists()?.outputChannelRegistry.getOrCreateOutputChannel(
         Output.MetroBundler