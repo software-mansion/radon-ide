fe1aae692918dade22f7ee2d8bfd9dc16648148d
spaleka <agata_spalek@interia.eu>
2025-10-09 15:02:43 +0200

    unused components remove

--- packages/docs/src/components/Hero/LandingBackground/index.tsx
+++ packages/docs/src/components/Hero/LandingBackground/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
-import HeroElipse from "@site/src/components/Hero/HeroElipse";
 import styles from "./styles.module.css";
 
 const LandingBackground = () => {
--- packages/docs/src/pages/index.tsx
+++ packages/docs/src/pages/index.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import Layout from "@theme/Layout";
 import Hero from "@site/src/components/Hero/StartScreen";
-import Disclaimer from "@site/src/components/Disclaimer";
-import LearnMoreHero from "@site/src/components/LearnMore/LearnMoreHero";
 import LearnMoreFooter from "@site/src/components/LearnMore/LearnMoreFooter";
-import Installation from "@site/src/components/Sections/Installation";
 import LandingBackground from "@site/src/components/Hero/LandingBackground";
 
 import usePaddle from "@site/src/hooks/usePaddle";
@@ -26,9 +23,6 @@ export default function Home(): JSX.Element {
       <div className={styles.preventfulContainer}>
         <div className={clsx(styles.container, "border-layout")}>
           <Hero />
-          {/* <Disclaimer /> */}
-          {/* <LearnMoreHero /> */}
-          {/* <Installation /> */}
           <LandingBanner />
           <FeatureSliderLanding />
           <FeaturesLanding />
