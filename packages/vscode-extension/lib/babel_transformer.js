beeb6caeae3376d9a3a10852431a1c98a0c117f8
Krzysztof Magiera <krzys@swmansion.com>
2025-10-15 01:18:22 +0200

    0.80 works

--- packages/vscode-extension/lib/babel_transformer.js
+++ packages/vscode-extension/lib/babel_transformer.js
@@ -9,12 +9,12 @@ const {
 } = require("./metro_helpers");
 const buildPluginWarnOnDeeImports = require("./babel_plugins/build-plugin-warn-on-deep-imports");
 
-// The access to component stack in versions prior to 0.81 rely on the JSX transform plugin. Apparently
+// The access to component stack in versions prior to 0.80 we rely on the JSX transform plugin. Apparently
 // in many setup the transform is misconfigured and does not produce the desirable output that we can later access.
-// In addition, on versions between 0.74 and 0.81 we patch the React renderer in order to make the source information
+// In addition, on versions between 0.74 and 0.79 we patch the React renderer in order to make the source information
 // being added and passed down to the fiber nodes (more on that below).
-// Radon IDE only supports RN 0.74+ so it is ok to only check for 0.7 prefix and for 0.80 version (hopefully we will never reach 0.700)
-if (RN_VERSION.startsWith("0.7") || RN_VERSION.startsWith("0.80")) {
+// Radon IDE only supports RN 0.74+ so it is ok to only check for 0.7 prefix (hopefully we will never reach 0.700)
+if (RN_VERSION.startsWith("0.7")) {
   // In some configurations, React Native may pull several different version of JSX transform plugins:
   // plugin-transform-react-jsx-self, plugin-transform-react-jsx-source, plugin-transform-react-jsx and
   // plugin-transform-react-jsx-development. For line and column numbers to be added to components, we
@@ -140,9 +140,9 @@ function transformWrapper({ filename, src, ...rest }) {
     // This breaks module resolution inside the renderer, so we must replace the entire
     // renderer source code in-place instead of using module re-exports.
     //
-    // Thankfully, in RN 0.81 the new owner stack-based approach made it possible to
+    // Thankfully, in RN 0.80 the new owner stack-based approach made it possible to
     // retrieve the actual call-site stack for components. Therefore the below logic
-    // only covers versions after 0.74 but before 0.81.
+    // only covers versions on or after 0.74 but before 0.80.
     const { version } = requireFromAppDir("react-native/package.json");
     const rendererFileName = filename.split(path.sep).pop();
     if (
@@ -170,24 +170,11 @@ function transformWrapper({ filename, src, ...rest }) {
       const rendererAsString = fs.readFileSync(rendererFilePath, "utf-8");
       src = rendererAsString;
     }
-    if (version.startsWith("0.80")) {
-      const rendererFilePath = path.join(
-        process.env.RADON_IDE_LIB_PATH,
-        "rn-renderer",
-        "react-native-80-81",
-        rendererFileName
-      );
-      const rendererAsString = fs.readFileSync(rendererFilePath, "utf-8");
-      src = rendererAsString;
-    }
   } else if (isTransforming("node_modules/react/cjs/react-jsx-dev-runtime.development.js")) {
     const { version } = requireFromAppDir("react-native/package.json");
     const jsxRuntimeFileName = filename.split(path.sep).pop();
     if (version.startsWith("0.78") || version.startsWith("0.79")) {
       src = `module.exports = require("__RNIDE_lib__/JSXRuntime/react-native-78-79/${jsxRuntimeFileName}");`;
-    }
-    if (version.startsWith("0.80")) {
-      src = `module.exports = require("__RNIDE_lib__/JSXRuntime/react-native-80-81/${jsxRuntimeFileName}");`;
     }
   } else if (
     isTransforming("node_modules/@tanstack/react-query/src/index.ts") ||
