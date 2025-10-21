eb7545fd8c7a18bacc9d1ab61599e45b27e41909
Filip131311 <Filip.kaminski@swmansion.com>
2025-10-21 14:21:38 +0200

    init

--- packages/vscode-extension/lib/preview.js
+++ packages/vscode-extension/lib/preview.js
@@ -1,5 +1,6 @@
 const { useEffect, useState } = require("react");
 const { AppRegistry, View } = require("react-native");
+const RNInternals = require("./rn-internals/rn-internals");
 
 export const PREVIEW_APP_KEY = "RNIDE_preview";
 
@@ -44,25 +45,66 @@ function getComponentName({ type }) {
   return "(unnamed)";
 }
 
+async function getSourceFromComponent(component) {
+  if (component._source) {
+    return component._source;
+  }
+
+  const debugStack = component._debugStack;
+  const parsedStack = RNInternals.parseErrorStack(debugStack.stack);
+
+  const { file } = parsedStack[1];
+
+  const url = new URL(file);
+  const metroAddress = url.origin;
+
+  const metroSymbolicateResponse = await fetch(
+    `${metroAddress}/symbolicate`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        stack: parsedStack,
+        extraData: {
+        }
+      }),
+    }
+  );
+
+  const metroSymbolicateJson = await metroSymbolicateResponse.json();
+
+  const symbolicatedStack = metroSymbolicateJson.stack;
+
+  return {
+    fileName: symbolicatedStack[1].file,
+    lineNumber: symbolicatedStack[1].lineNumber,
+    columnNumber: symbolicatedStack[1].column,
+  }
+}
+
 export function preview(component) {
+
+  console.log("Frytki preview registered", RNInternals.parseErrorStack(component._debugStack.stack));
   // eslint-disable-next-line eqeqeq
-  if (!component || component._source == null) {
+  if (!component || (component._debugStack == null && component._source == null)) {
     return;
   }
 
-  const key = `preview:/${component._source.fileName}:${component._source.lineNumber}`;
+  getSourceFromComponent(component).then((source) => {
+    const key = `preview:/${source.fileName}:${source.lineNumber}`;
 
-  const lastPreview = global.__RNIDE_previews.get(key);
+    const lastPreview = global.__RNIDE_previews.get(key);
 
-  global.__RNIDE_previews.set(key, {
-    component,
-    name: getComponentName(component),
+    global.__RNIDE_previews.set(key, {
+      component,
+      name: getComponentName(component),
+    });
+
+    // send update request to the last preview instance if it existed
+    if (lastPreview && lastPreview.renderTrigger) {
+      setTimeout(lastPreview.renderTrigger, 0);
+    }
   });
-
-  // send update request to the last preview instance if it existed
-  if (lastPreview && lastPreview.renderTrigger) {
-    setTimeout(lastPreview.renderTrigger, 0);
-  }
 }
 
 AppRegistry.registerComponent(PREVIEW_APP_KEY, () => Preview);
