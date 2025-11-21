0bba8f2085e21fe93be39301297d5a10084d6698
Ignacy Łątka <latkaignacy@gmail.com>
2025-11-21 14:33:40 +0100

    register remote tools statically

--- packages/vscode-extension/src/ai/mcp/toolRegistration.ts
+++ packages/vscode-extension/src/ai/mcp/toolRegistration.ts
@@ -1,5 +1,10 @@
 import vscode, { Disposable } from "vscode";
 import { screenshotToolExec } from "./toolExecutors";
+import { invokeToolCall } from "../shared/api";
+import { textToToolResponse } from "./utils";
+import { AuthorizationError } from "../../common/Errors";
+
+const PLACEHOLDER_ID = "1234";
 
 /*
 
@@ -21,6 +26,60 @@ query_documentation:
 
 */
 
+interface LibraryDescriptionToolArgs {
+  library_npm_name: string;
+}
+
+export class LibraryDescriptionTool
+  implements vscode.LanguageModelTool<LibraryDescriptionToolArgs>
+{
+  async invoke(
+    options: vscode.LanguageModelToolInvocationOptions<LibraryDescriptionToolArgs>,
+    token: vscode.CancellationToken
+  ): Promise<vscode.LanguageModelToolResult> {
+    const toolName = "get_library_description";
+    try {
+      return await invokeToolCall(toolName, options.input, PLACEHOLDER_ID);
+    } catch (error) {
+      if (error instanceof AuthorizationError) {
+        // This error is a fallback, as LLM tools should be disabled when no valid license is present.
+        const msg = `You have to have a valid Radon IDE license to use the ${toolName} tool.`;
+        return textToToolResponse(msg);
+      }
+
+      // TODO: Disable tools for users with no license
+      return textToToolResponse(String(error));
+    }
+  }
+}
+
+interface QueryDocumentationToolArgs {
+  text: string;
+}
+
+export class QueryDocumentationTool
+  implements vscode.LanguageModelTool<QueryDocumentationToolArgs>
+{
+  async invoke(
+    options: vscode.LanguageModelToolInvocationOptions<QueryDocumentationToolArgs>,
+    token: vscode.CancellationToken
+  ): Promise<vscode.LanguageModelToolResult> {
+    const toolName = "query_documentation";
+    try {
+      return await invokeToolCall(toolName, options.input, PLACEHOLDER_ID);
+    } catch (error) {
+      if (error instanceof AuthorizationError) {
+        // This error is a fallback, as LLM tools should be disabled when no valid license is present.
+        const msg = `You have to have a valid Radon IDE license to use the ${toolName} tool.`;
+        return textToToolResponse(msg);
+      }
+
+      // TODO: Disable tools for users with no license
+      return textToToolResponse(String(error));
+    }
+  }
+}
+
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface ViewScreenshotToolArgs {}
 
@@ -33,6 +92,17 @@ export class ViewScreenshotTool implements vscode.LanguageModelTool<ViewScreensh
 }
 
 export function registerRadonAI(): Disposable {
+  const queryDocumentationTool = vscode.lm.registerTool(
+    "query_documentation",
+    new ViewScreenshotTool()
+  );
+
+  const libraryDescriptionTool = vscode.lm.registerTool(
+    "get_library_description",
+    new ViewScreenshotTool()
+  );
+
   const viewScreenshotTool = vscode.lm.registerTool("view_screenshot", new ViewScreenshotTool());
-  return Disposable.from(viewScreenshotTool);
+
+  return Disposable.from(queryDocumentationTool, libraryDescriptionTool, viewScreenshotTool);
 }
