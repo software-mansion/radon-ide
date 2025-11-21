017d91e2326f8269d6c9e2fd16b245b001f340d7
KeyJay <krzys.jablonski2003@gmail.com>
2025-11-21 16:18:42 +0100

    add diagnostics tests

--- packages/vscode-extension-tester/ui-tests/17-diagnostics.test.js
+++ packages/vscode-extension-tester/ui-tests/17-diagnostics.test.js
@@ -1,7 +1,9 @@
+import { execSync } from "child_process";
 import { WebView, EditorView } from "vscode-extension-tester";
 import initServices from "../services/index.js";
 import { safeDescribe } from "../utils/helpers.js";
 import { get } from "./setupTest.js";
+import { assert } from "chai";
 
 safeDescribe("17 - Diagnostics tests", () => {
   let driver, elementHelperService, radonViewsService, managingDevicesService;
@@ -32,5 +34,134 @@ safeDescribe("17 - Diagnostics tests", () => {
     await elementHelperService.findAndWaitForElementByTag("diagnostics-view");
   });
 
-  it("Should open diagnostics window", async function () {});
+  async function testDiagnostic(
+    name,
+    damageFunction,
+    fixFunction,
+    errorMessage
+  ) {
+    await elementHelperService.findAndWaitForElementByTag(
+      `diagnostic-icon-${name}-installed`
+    );
+    await damageFunction();
+    try {
+      await elementHelperService.findAndClickElementByTag(
+        "rerun-diagnostics-button"
+      );
+      console.log(`Testing ${name} diagnostic`);
+      await elementHelperService.findAndWaitForElementByTag(
+        `diagnostic-icon-${name}-notInstalled`
+      );
+      const errorElement = await elementHelperService.findAndClickElementByTag(
+        `diagnostic-error-${name}`
+      );
+      assert.equal(
+        await errorElement.getText(),
+        errorMessage,
+        `Error message for ${name} is not correct`
+      );
+    } finally {
+      await fixFunction();
+      await elementHelperService.findAndClickElementByTag(
+        "rerun-diagnostics-button"
+      );
+      await elementHelperService.findAndWaitForElementByTag(
+        `diagnostic-icon-${name}-installed`
+      );
+    }
+  }
+
+  it("should correct node diagnostic", async function () {
+    await testDiagnostic(
+      "nodejs",
+      async () => {
+        execSync("brew unlink node");
+      },
+      async () => {
+        execSync("brew link node");
+      },
+      `Node.js was not found, or the version in the PATH does not satisfy minimum version requirements. You can find more information in our documentation.`
+    );
+  });
+
+  it("should correct npm diagnostic", async function () {
+    await testDiagnostic(
+      "packageManager",
+      async () => {
+        execSync("rm /opt/homebrew/bin/npm");
+      },
+      async () => {
+        execSync("brew unlink node && brew link node");
+      },
+      `Package manager not found or uninstalled. Make sure to install the package manager used in the project.`
+    );
+  });
+
+  it("should correct node_modules diagnostic", async function () {
+    await testDiagnostic(
+      "nodeModules",
+      async () => {
+        execSync(
+          "mv ./data/react-native-app/node_modules ./data/react-native-app/not_node_modules"
+        );
+      },
+      async () => {
+        execSync(
+          "mv ./data/react-native-app/not_node_modules ./data/react-native-app/node_modules"
+        );
+      },
+      `Node modules are not installed.`
+    );
+  });
+
+  it("should correct pods diagnostic", async function () {
+    await testDiagnostic(
+      "pods",
+      async () => {
+        execSync(
+          "mv ./data/react-native-app/ios/Pods ./data/react-native-app/ios/not_Pods"
+        );
+      },
+      async () => {
+        execSync(
+          "mv ./data/react-native-app/ios/not_Pods ./data/react-native-app/ios/Pods"
+        );
+      },
+      `Pods are not installed.`
+    );
+  });
+
+  it("should correct ios diagnostic", async function () {
+    await testDiagnostic(
+      "ios",
+      async () => {
+        execSync(
+          "mv ./data/react-native-app/ios ./data/react-native-app/not_ios"
+        );
+      },
+      async () => {
+        execSync(
+          "mv ./data/react-native-app/not_ios ./data/react-native-app/ios"
+        );
+      },
+      `"ios" directory does not exist in the main application directory`
+    );
+  });
+
+  it("should correct android diagnostic", async function () {
+    await testDiagnostic(
+      "android",
+      async () => {
+        execSync(
+          "mv ./data/react-native-app/android ./data/react-native-app/not_android"
+        );
+      },
+      async () => {
+        execSync(
+          "mv ./data/react-native-app/not_android ./data/react-native-app/android"
+        );
+      },
+      `"android" directory does not exist in the main application directory`
+    );
+  });
 });
