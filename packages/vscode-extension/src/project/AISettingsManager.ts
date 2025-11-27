6ca3a0694402519439eb0bf6af8fee18a7356654
Ignacy Łątka <latkaignacy@gmail.com>
2025-11-27 14:28:44 +0100

    further reduce AISettingsManager

--- packages/vscode-extension/src/project/AISettingsManager.ts
+++ packages/vscode-extension/src/project/AISettingsManager.ts
@@ -1,27 +1,23 @@
 import { Disposable, workspace } from "vscode";
 import { StateManager } from "../project/StateManager";
-import { RadonAISettings, RecursivePartial, State } from "../common/State";
+import { State } from "../common/State";
 
 export class AISettingsManager implements Disposable {
-  private state: StateManager<RadonAISettings>;
   private configurationListenerDisposable: Disposable;
 
   private static getIsAIEnabled() {
     return workspace.getConfiguration("RadonIDE").get<boolean>("radonAI.enabledBoolean") ?? true;
   }
 
-  private updateState(partialState: RecursivePartial<RadonAISettings>) {
-    this.state.updateState(partialState);
-  }
-
   constructor(stateManager: StateManager<State>) {
-    this.state = stateManager.getDerived("workspaceConfiguration").getDerived("radonAI");
-
     this.configurationListenerDisposable = workspace.onDidChangeConfiguration((event) => {
       if (event.affectsConfiguration("RadonIDE.radonAI.enabledBoolean")) {
-        const updatedRadonAiEnabled = AISettingsManager.getIsAIEnabled();
-        this.updateState({
-          enableRadonAI: updatedRadonAiEnabled,
+        stateManager.updateState({
+          workspaceConfiguration: {
+            radonAI: {
+              enableRadonAI: AISettingsManager.getIsAIEnabled(),
+            },
+          },
         });
       }
     });
