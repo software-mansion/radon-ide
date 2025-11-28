c58a75cbb8f6edbd42dbac43a1f558751fb9d4a7
Filip131311 <Filip.kaminski@swmansion.com>
2025-11-28 11:02:09 +0100

    init

---
+++ packages/vscode-extension/lib/rn-internals/rn-internals-0.83.js
@@ -0,0 +1,26 @@
+module.exports = {
+  parseErrorStack: require("__REACT_NATIVE_INTERNALS__/Libraries/Core/Devtools/parseErrorStack")
+    .default,
+  symbolicateStackTrace: require("__REACT_NATIVE_INTERNALS__/Libraries/Core/Devtools/symbolicateStackTrace")
+    .default,
+  get LogBoxData() {
+    return require("__REACT_NATIVE_INTERNALS__/Libraries/LogBox/Data/LogBoxData");
+  },
+  get SceneTracker() {
+    return require("__REACT_NATIVE_INTERNALS__/Libraries/Utilities/SceneTracker").default;
+  },
+  get getInspectorDataForViewAtPoint() {
+    return require("__REACT_NATIVE_INTERNALS__/src/private/devsupport/devmenu/elementinspector/getInspectorDataForViewAtPoint")
+      .default;
+  },
+  get LoadingView() {
+    return require("__REACT_NATIVE_INTERNALS__/Libraries/Utilities/DevLoadingView").default;
+  },
+  get XHRInterceptor() {
+    return require("__REACT_NATIVE_INTERNALS__/src/private/devsupport/devmenu/elementinspector/XHRInterceptor")
+      .default;
+  },
+  get FabricUIManager() {
+    return require("__REACT_NATIVE_INTERNALS__/Libraries/ReactNative/FabricUIManager");
+  },
+};
