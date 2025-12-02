d67cc4a8939bf4ff16ae96da0a3db9d7512955da
KeyJay <krzys.jablonski2003@gmail.com>
2025-12-02 11:50:23 +0100

    debug

--- packages/vscode-extension-tester/services/appManipulationService.js
+++ packages/vscode-extension-tester/services/appManipulationService.js
@@ -168,6 +168,8 @@ export default class AppManipulationService {
       console.warn("No appWebsocket provided to sendMessageAndWaitForResponse");
     }
 
+    console.log(`Sending message to app: ${message}`);
+
     const id = `${Date.now()}-${Math.floor(Math.random() * 1e6)}`;
     const messagePromise = waitForMessage(id);
 
