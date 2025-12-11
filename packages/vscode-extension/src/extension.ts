138cc60fe240650591b8bf364dbca4c19e58459d
Ignacy Łątka <latkaignacy@gmail.com>
2025-12-11 08:54:49 +0100

    fix: Fix extension crashing on startup (#1810)
    Extension crashes on startup. This PR fixes that.
    
    This was happening because `workspace.getConfiguration();` (located in
    `getEditorType();`) was being called before
    `setExtensionContext(context);`.
    
    <img width="914" height="152" alt="image"
    src="https://github.com/user-attachments/assets/adc99ef2-7518-4b2e-9e79-925b70ca3fa6"
    />
    
    ### How Has This Been Tested: 
    
    - Start the extension
    - The panel works, and no errors are thrown

--- packages/vscode-extension/src/extension.ts
+++ packages/vscode-extension/src/extension.ts
@@ -102,8 +102,6 @@ export async function activate(context: ExtensionContext) {
   // after improper deactivation of the extension.
   commands.executeCommand("setContext", "RNIDE.panelIsOpen", false);
 
-  getTelemetryReporter().sendTelemetryEvent(`extension:activated:${getEditorType()}`);
-
   context.subscriptions.push(
     window.registerWebviewPanelSerializer(TabPanel.viewType, new TabPanelSerializer())
   );
@@ -122,6 +120,7 @@ export async function activate(context: ExtensionContext) {
   }
 
   migrateOldConfiguration();
+  getTelemetryReporter().sendTelemetryEvent(`extension:activated:${getEditorType()}`);
 
   commands.executeCommand("setContext", "RNIDE.sidePanelIsClosed", false);
 
