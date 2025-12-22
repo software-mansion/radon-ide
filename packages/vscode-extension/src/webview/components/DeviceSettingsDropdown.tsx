b8de38217d5896ea6bc714add6fe65265376e2bf
pFornagiel <pawel.fornagiel2@gmail.com>
2025-12-22 22:54:17 +0100

    fix: update onSelect handler in DeviceSettingsDropdown to use arrow function

--- packages/vscode-extension/src/webview/components/DeviceSettingsDropdown.tsx
+++ packages/vscode-extension/src/webview/components/DeviceSettingsDropdown.tsx
@@ -321,7 +321,7 @@ function DeviceSettingsDropdown({ children, disabled }: DeviceSettingsDropdownPr
               className="dropdown-menu-item"
               data-testid="device-settings-send-file"
               disabled={isSendFilesAdminDisabled}
-              onSelect={project.openSendFileDialog}>
+              onSelect={() => project.openSendFileDialog()}>
               <span className="codicon codicon-share" />
               Send File
             </PaywallDropdownMenu.Item>
