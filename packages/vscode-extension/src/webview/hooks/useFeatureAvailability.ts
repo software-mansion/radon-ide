eb41edaadff99920949a61fa3291b694bb3cd982
Paweł Fornagiel <85541429+pFornagiel@users.noreply.github.com>
2026-01-08 15:12:09 +0100

    feat: improve paywalled features signalling (#1837)
    ### Description
    
    The PR introduces changes to how paywalled and admin-blocked features
    are signaled to the user.
    
    Changes mainly apply to the `Device Settings` menu and `Icon Buttons` in
    the top bar of the extension. Instead of silently disabling items,
    paywalled are now greyed-out, show a **Pro** badge on hover and allow
    interaction to open the paywall modal.
    
    
    https://github.com/user-attachments/assets/71394b4f-c2a0-4cae-a594-af59d9085cfa
    
    When features are disabled due to administrative restrictions
    (`ADMIN_DISABLED` state), the options are also graded out but trigger
    appropriate modal and do not showcase the **Pro** badge.
    
    The `DeviceSettingsDropdown` has been refactored, made a little more
    readable and edited so that the paywalled functionality checks are
    hidden within the `PaywolledDropdownMenu` components, where proper
    checks take place and showcase the paywall accordingly. The name of the
    feature (via `feature` prop accepting `Feature` enum value) can be
    provided to the component to make proper checks.
    
    Note: The Appearance menu does not currently use the above API. If there
    is a potential need to disable or gate it in the future, the necessary
    changes can be discussed and implemented.
    
    Similar changes have been made to the `IconButton` which now also
    accepts `feature` prop and makes necessary checks when needed.
    
    The paywall modal now also accepts a `feature` prop, which helps to
    display the information which of the features caused it to be activated.
    
    ### How Has This Been Tested: 
    
    Performed tests with: lack of license, free license pro license -
    verified that lack of license and free license cause proper behaviour
    and visual look.
    
    Made features disabled via setting `ADMIN_DISABLED` in
    `DefaultFeaturesAvailability` and commented the line, which would
    override it via token - checked whether the behaviour is correct with
    pro license.
    
    ### How Has This Change Been Documented:
    
    Not applicable.

--- packages/vscode-extension/src/webview/hooks/useFeatureAvailability.ts
+++ packages/vscode-extension/src/webview/hooks/useFeatureAvailability.ts
@@ -2,9 +2,8 @@ import { use$ } from "@legendapp/state/react";
 import { Feature, FeatureAvailabilityStatus } from "../../common/License";
 import { useStore } from "../providers/storeProvider";
 
-export function useIsFeatureAdminDisabled(feature: Feature) {
+export function useFeatureAvailability(feature: Feature): FeatureAvailabilityStatus {
   const store$ = useStore();
   const featuresAvailability = use$(store$.license.featuresAvailability);
-
-  return featuresAvailability[feature] === FeatureAvailabilityStatus.ADMIN_DISABLED;
+  return featuresAvailability[feature];
 }
