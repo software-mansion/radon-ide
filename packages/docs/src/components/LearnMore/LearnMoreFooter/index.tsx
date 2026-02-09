f346ca0532490e7716fa3946b1ea6ac02df4f4d6
pFornagiel <pawel.fornagiel2@gmail.com>
2026-02-09 12:38:45 +0100

    feat: add dynamically generated submenu for docs in MobileSidebar

--- packages/docs/src/components/LearnMore/LearnMoreFooter/index.tsx
+++ packages/docs/src/components/LearnMore/LearnMoreFooter/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./styles.module.css";
 import DownloadButtons from "../../DownloadButtons";
 import RadonIconGreen from "../../RadonIconGreen";
@@ -6,12 +6,50 @@ import usePageType from "@site/src/hooks/usePageType";
 import clsx from "clsx";
 import useInstallCount from "@site/src/hooks/useInstallCount";
 
+const RADON_IDE_MARKETPLACE_URL =
+  "https://marketplace.visualstudio.com/items?itemName=swmansion.react-native-ide";
+const RADON_IDE_OPEN_VSX_API = "https://open-vsx.org/api/swmansion/react-native-ide";
+const DEFAULT_INSTALLS_NO = "34,000+";
+
 const LearnMoreFooter = () => {
   const { data: installCountData, isLoading } = useInstallCount();
   const installCount =
     installCountData !== null ? installCountData.toLocaleString("en-US") : "45,000+";
   const { isLanding } = usePageType();
   const trackFrom = isLanding ? "Landing bottom" : "Features bottom";
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const marketplaceResponse = await fetch(RADON_IDE_MARKETPLACE_URL);
+        let installsCount = 0;
+        if (marketplaceResponse.ok) {
+          const htmlString = await marketplaceResponse.text();
+          const regex = /<span class="installs-text"[^>]*>\s*([\d,]+)\s*installs\s*<\/span>/i;
+          const match = htmlString.match(regex);
+          if (match) installsCount = parseInt(match[1].replace(/,/g, ""));
+        }
+
+        const openvsxResponse = await fetch(RADON_IDE_OPEN_VSX_API);
+        let downloadsCount = 0;
+        if (openvsxResponse.ok) {
+          const data = await openvsxResponse.json();
+          downloadsCount = parseInt(data.downloadCount ?? 0);
+        }
+
+        const sum = installsCount + downloadsCount;
+        if (marketplaceResponse.ok && openvsxResponse.ok) {
+          setSummaryInstalls(sum.toLocaleString("en-US"));
+        }
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setIsLoaded(true);
+      }
+    }
+
+    fetchData();
+  }, []);
 
   return (
     <div
